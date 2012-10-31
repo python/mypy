6f0826a9c169c4f05bb8938347ac0416f8154d91
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-10-31 14:20:23 +0000

    Add converted lexer and utils (not working!)

---
+++ lex.py
@@ -0,0 +1,533 @@
+import re
+from util import short_type
+from re import MatchResult
+from encodings import Encoding, ascii, utf8, decode, STRICT, DecodeError
+
+
+# Base class for all tokens
+class Token:
+    str pre = '' # Space, comments etc. before token
+    str string # Token string
+    int line # Token line number
+    
+    void __init__(self, str string, str pre=''):
+        self.string = string
+        self.pre = pre
+    
+    str __str__(self):
+        t = short_type(self)
+        return t + '(' + self.fix(self.pre) + self.fix(self.string) + ')'
+    
+    @property
+    str rep():
+        return self.pre + self.string
+    
+    str fix(self, str s):
+        return s.replace('\n', '\\n').replace('\t', '\\t').replace('\r', '\\r')
+
+
+# Token classes
+
+# Statement break (line break or semicolon)
+class Break(Token): pass
+
+# Increase block indent level.
+class Indent(Token): pass
+
+# Decrease block indent level.
+class Dedent(Token): pass
+
+# End of file
+class Eof(Token): pass
+
+# Reserved word (other than keyword operators; they use Op)
+class Keyword(Token): pass
+
+# An alphanumeric identifier
+class Name(Token): pass
+
+# Integer literal
+class IntLit(Token): pass
+
+str_prefix_re = Pattern('[rRbB]*')
+escape_re = Pattern("\\\\([abfnrtv'\"]|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|[0-7]{1,3})")
+
+escape_map = {'a': '\u0007', 'b': '\u0008', 'f': '\u000c', 'n': '\u000a', 'r': '\u000d', 't': '\u0009', 'v': '\u000b', '"': '"', "'": "'"}
+
+# String literal
+class StrLit(Token):
+    # Return the parsed contents of the literal.
+    str parsed(self):
+        prefix = re.match(str_prefix_re, self.string).group(0).lower()
+        s = self.string[len(prefix):]
+        if s.startswith("'''") or s.startswith('"""'):
+            return s[3:-3]
+        elif 'r' in prefix:
+            return s[1:-1].replace('\\' + s[0], s[0])
+        else:
+            return self.replace_escapes(s[1:-1], prefix)
+    
+    str replace_escapes(self, str s, str prefix):
+        r = xxx_str (MatchResult m):
+            seq = m.group(1)
+            if len(seq) == 1 and escape_map.has_key(seq):
+                return escape_map[seq]
+            elif seq.startswith('x'):
+                return chr(int(seq[1:], 16))
+            elif seq.startswith('u'):
+                if 'b' not in prefix:
+                    return chr(int(seq[1:], 16))
+                else:
+                    return '\\' + seq
+            else:
+                # Octal sequence.
+                ord = int(seq, 8)
+                if 'b' in prefix:
+                    # Make sure code is no larger than 255 for bytes literals.
+                    ord = ord % 256
+                return chr(ord)
+        
+        return re.sub(s, escape_re, r)
+
+# Float literal
+class FloatLit(Token): pass
+
+# Punctuator (e.g. comma or paranthesis)
+class Punct(Token): pass
+
+class Colon(Token): pass
+
+# Operator (e.g. '+' or 'and')
+class Op(Token): pass
+
+# Byte order mark (at the start of a file)
+class Bom(Token): pass
+
+# Lexer error token
+class LexError(Token):
+    Constant type # One of the error types below
+    
+    void __init__(self, str string, Constant type):
+        super().__init__(string)
+        self.type = type
+
+
+# Lexer error types
+any NUMERIC_LITERAL_ERROR, any UNTERMINATED_STRING_LITERAL, any INVALID_CHARACTER, any NON_ASCII_CHARACTER_IN_COMMENT, any NON_ASCII_CHARACTER_IN_STRING, any INVALID_UTF8_SEQUENCE, any INVALID_BACKSLASH, any INVALID_DEDENT
+
+# Encoding contexts
+any STR_CONTEXT, any COMMENT_CONTEXT
+
+
+# Analyze s and return an array of token objects, the last of which is always
+# Eof.
+list<Token> lex(str s):
+    l = Lexer()
+    l.lex(s)
+    return l.tok
+
+
+# Reserved words (not including operators)
+set<str> keywords = set(['any', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'from', 'for', 'global', 'if', 'import', 'interface', 'lambda', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'])
+
+# Alphabetical operators (reserved words)
+set<str> alpha_operators = set(['in', 'is', 'not', 'and', 'or'])
+
+# String literal prefixes
+set<str> str_prefixes = set(['r', 'b', 'br'])  
+
+# List of regular expressions that match non-alphabetical operators
+list<Pattern> operators = [Pattern('[-+*/<>.%&|^~]'), Pattern('==|!=|<=|>=|\\*\\*|//|<<|>>')]
+
+# List of regular expressions that match punctuator tokens
+list<Pattern> punctuators = [Pattern('[=,()@]'), Pattern('\\['), Pattern(']'), Pattern('([-+*/%&|^]|\\*\\*|//|<<|>>)=')]
+
+
+# Source file encodings
+any DEFAULT_ENCODING, any ASCII_ENCODING, any LATIN1_ENCODING, any UTF8_ENCODING
+
+
+# Lexical analyzer
+class Lexer:
+    int i
+    str s
+    int line
+    str pre = ''
+    Constant enc = DEFAULT_ENCODING
+    
+    list<Token> tok = []
+    list<func<void>> map = [self.unknown_character] * 256
+    
+    list<int> indents = [0]
+    # Open ('s, ['s and {'s without matching closing bracket.
+    list<str> open_brackets = []
+    
+    void __init__(self):
+        for seq, method in [('ABCDEFGHIJKLMNOPQRSTUVWXYZ', self.lex_name), ('abcdefghijklmnopqrstuvwxyz_', self.lex_name), ('0123456789', self.lex_number), ('.', self.lex_number_or_dot), (' ' + '\t' + '\u000c', self.lex_space), ('"', self.lex_str_double), ("'", self.lex_str_single), ('\r' + '\n', self.lex_break), (';', self.lex_semicolon), (':', self.lex_colon), ('#', self.lex_comment), ('\\', self.lex_backslash), ('([{', self.lex_open_bracket), (')]}', self.lex_close_bracket), ('-+*/<>%&|^~=!,@', self.lex_misc)]:
+            for c in seq:
+                self.map[ord(c)] = method
+    
+    # Lexically analyze a string, storing the tokens at the tok array.
+    void lex(self, str s):
+        self.s = s
+        self.i = 0
+        self.line = 1    
+        
+        if s.startswith('\u00ef\u00bb\u00bf'):
+            self.add_token(Bom(s[0:3]))
+        self.lex_indent()
+        
+        map = self.map
+        while self.i < len(s):
+            c = ord(s[self.i])
+            map[c]()
+        
+        # Append a break if there is no statement/block terminator at the end of
+        # input.
+        if len(self.tok) > 0 and (not isinstance(self.tok[-1], Break) and not isinstance(self.tok[-1], Dedent)):
+            self.add_token(Break(''))
+        self.lex_indent()
+        self.add_token(Eof(''))
+    
+    # Analyse a token starting with a dot (either the member access operator or
+    # a Float literal).
+    void lex_number_or_dot(self):
+        if self.is_at_number():
+            self.lex_number()
+        else:
+            self.lex_misc()
+    
+    Pattern number_exp = Pattern('[0-9]|\\.[0-9]')  # Used by isAtNumber
+    
+    # Is the current location at a numeric literal?
+    bool is_at_number(self):
+        return self.match(self.number_exp) != ''
+    
+    # Regexps used by lexNumber
+    Pattern number_exp1 = Pattern('0[xXoO][0-9a-fA-F]+|[0-9]+')
+    Pattern number_exp2 = Pattern('[0-9]*\\.[0-9]*([eE][-+]?[0-9]+)?|[0-9]+[eE][-+]?[0-9]+')
+    Pattern name_char_exp = Pattern('[a-zA-Z0-9_]')
+    
+    # Analyse an Int or Float literal. Assume that the current location points
+    # to one of them.
+    void lex_number(self):
+        s1 = self.match(self.number_exp1)
+        s2 = self.match(self.number_exp2)
+        
+        max = max(len(s1), len(s2))
+        if re.match(self.name_char_exp, self.s[self.i + max:self.i + max + 1]) is not None:
+            s3 = self.match('[0-9][0-9a-zA-Z_]*')
+            max = max(max, len(s3))
+            self.add_token(LexError(' ' * max, NUMERIC_LITERAL_ERROR))
+        elif len(s1) > len(s2):
+            self.add_token(IntLit(s1))
+        else:
+            self.add_token(FloatLit(s2))
+    
+    Pattern name_exp = Pattern('[a-zA-Z_][a-zA-Z0-9_]*') # Used by lexName
+    
+    # Analyse a name (an identifier, a keyword or an alphabetical operator).
+    # This also deals with prefixed string literals such as r'...'.
+    void lex_name(self):
+        s = self.match(self.name_exp)
+        if s in keywords:
+            self.add_token(Keyword(s))
+        elif s in alpha_operators:
+            self.add_token(Op(s))
+        elif s in str_prefixes and self.match('[a-z]+[\'"]') != '':
+            self.lex_prefixed_str(s)
+        else:
+            self.add_token(Name(s))
+    
+    # Regexps representing components of string literals
+    
+    Pattern str_exp_single = Pattern("[a-z]*'([^'\\\\\\r\\n]|\\\\[^\\r\\n])*('|\\\\(\\n|\\r\\n?))")
+    Pattern str_exp_single_multi = Pattern("([^'\\\\\\r\\n]|\\\\[^\\r\\n])*('|\\\\(\\n|\\r\\n?))")
+    Pattern str_exp_raw_single = Pattern("[a-z]*'([^'\\r\\n\\\\]|\\\\'|\\\\[^\\n\\r])*('|\\\\(\\n|\\r\\n?))")
+    Pattern str_exp_raw_single_multi = Pattern("([^'\\r\\n]|'')*('|\\\\(\\n|\\r\\n?))")
+    
+    Pattern str_exp_single3 = Pattern("[a-z]*'''")
+    Pattern str_exp_single3end = Pattern("[^\\n\\r]*?'''")
+    
+    Pattern str_exp_double = Pattern('[a-z]*"([^"\\\\\\r\\n]|\\\\[^\\r\\n])*("|\\\\(\\n|\\r\\n?))')
+    Pattern str_exp_double_multi = Pattern('([^"\\\\\\r\\n]|\\\\[^\\r\\n])*("|\\\\(\\n|\\r\\n?))')  
+    Pattern str_exp_raw_double = Pattern('[a-z]*"([^"\\r\\n\\\\]|\\\\"|\\\\[^\\n\\r])*("|\\\\(\\n|\\r\\n?))')
+    Pattern str_exp_raw_double_multi = Pattern('([^"\\r\\n]|"")*("|\\\\(\\n|\\r\\n?))')
+    
+    Pattern str_exp_double3 = Pattern('[a-z]*"""')
+    Pattern str_exp_double3end = Pattern('[^\\n\\r]*?"""')
+    
+    # Analyse single-quoted string literal
+    void lex_str_single(self):
+        self.lex_str(self.str_exp_single, self.str_exp_single_multi, self.str_exp_single3, self.str_exp_single3end)
+    
+    # Analyse double-quoted string literal
+    void lex_str_double(self):
+        self.lex_str(self.str_exp_double, self.str_exp_double_multi, self.str_exp_double3, self.str_exp_double3end)
+    
+    # Analyse a string literal with a prefix, such as r'...'.
+    void lex_prefixed_str(self, str prefix):
+        s = self.match('[a-z]+[\'"]')
+        if s.endswith("'"):
+            re = self.str_exp_single
+            re2 = self.str_exp_single_multi
+            if 'r' in prefix:
+                re = self.str_exp_raw_single
+                re2 = self.str_exp_raw_single_multi
+            self.lex_str(re, re2, self.str_exp_single3, self.str_exp_single3end, prefix)
+        else:
+            re = self.str_exp_double
+            re2 = self.str_exp_double_multi
+            if 'r' in prefix:
+                re = self.str_exp_raw_double
+                re2 = self.str_exp_raw_double_multi
+            self.lex_str(re, re2, self.str_exp_double3, self.str_exp_double3end, prefix)
+    
+    # Analyse a string literal described by regexps. Assume that the current
+    # location is at the beginning of the literal. The arguments re3 and re3end
+    # describe the corresponding triple-quoted literals.
+    void lex_str(self, Pattern re, Pattern re2, Pattern re3, Pattern re3end, str prefix=''):
+        s3 = self.match(re3)
+        if s3 != '':
+            self.lex_triple_quoted_str(re3end, prefix)
+        else:
+            # Single or double quoted string literal.
+            s = self.match(re)
+            if s != '':
+                if s.endswith('\n') or s.endswith('\r'):
+                    self.lex_multiline_literal(re2, s)
+                else:
+                    self.verify_encoding(s, STR_CONTEXT)
+                    self.add_token(StrLit(s))
+            else:
+                # Unterminated string literal.
+                s = self.match('[^\\n\\r]*')
+                self.add_token(LexError(s, UNTERMINATED_STRING_LITERAL))
+    
+    def lex_triple_quoted_str(self, re3end, prefix):
+        line = self.line
+        ss = self.s[self.i:self.i + len(prefix) + 3]
+        self.i += len(prefix) + 3
+        any m
+        while True:
+            m = re.match(re3end, self.s[self.i:])
+            if m is not None:
+                break
+            m = re.match('[^\\n\\r]*(\\n|\\r\\n?)', self.s[self.i:])
+            if m is None:
+                self.add_special_token(LexError(ss, UNTERMINATED_STRING_LITERAL), line, 0)
+                return 
+            s = m.group(0)
+            ss += s
+            self.line += 1
+            self.i += len(s)
+        self.add_special_token(StrLit(ss + m.group(0)), line, len(m.group(0)))
+    
+    def lex_multiline_literal(self, re_end, prefix):
+        line = self.line
+        self.i += len(prefix)
+        ss = prefix
+        any m
+        while True:
+            m = self.match(re_end)
+            if m == '':
+                self.add_special_token(LexError(ss, UNTERMINATED_STRING_LITERAL), line, 0)
+                return 
+            ss += m
+            self.line += 1
+            self.i += len(m)        
+            if not m.endswith('\n') and not m.endswith('\r'): break
+        self.add_special_token(StrLit(ss), line, 0)
+    
+    Pattern comment_exp = Pattern('#[^\\n\\r]*')
+    
+    # Analyse a comment.
+    void lex_comment(self):
+        s = self.match(self.comment_exp)
+        self.verify_encoding(s, COMMENT_CONTEXT)
+        self.add_pre(s)
+    
+    Pattern backslash_exp = Pattern('\\\\(\\n|\\r\\n?)')
+    
+    void lex_backslash(self):
+        s = self.match(self.backslash_exp)
+        if s != '':
+            self.add_pre(s)
+            self.line += 1
+        else:
+            self.add_token(LexError('\\', INVALID_BACKSLASH))
+    
+    Pattern space_exp = Pattern('[ \\t\u000c]*')
+    Pattern indent_exp = Pattern('[ \\t]*[#\\n\\r]?')
+    
+    # Analyse a run of whitespace characters.
+    void lex_space(self):
+        s = self.match(self.space_exp)
+        self.add_pre(s)
+    
+    comment_or_newline = '#' + '\n' + '\r'
+    
+    void lex_indent(self):
+        s = self.match(self.indent_exp)
+        if s != '' and s[-1] in self.comment_or_newline:
+            self.add_pre(s[:-1])
+            if s[-1] == '#':
+                self.lex_comment()
+            else:
+                self.lex_break()
+            self.lex_indent()
+            return 
+        indent = self.calc_indent(s)
+        if indent == self.indents[-1]:
+            self.add_pre(s)
+        elif indent > self.indents[-1]:
+            self.indents.append(indent)
+            self.add_token(Indent(s))
+        else:
+            while indent < self.indents[-1]:
+                self.add_token(Dedent(s))
+                self.indents.remove_at(-1)
+                s = ''
+            if indent != self.indents[-1]:
+                self.add_token(LexError('', INVALID_DEDENT))
+    
+    int calc_indent(self, str s):
+        indent = 0
+        for ch in s:
+            if ch == ' ':
+                indent += 1
+            else:
+                indent += 8 - indent % 8
+        return indent
+    
+    Pattern break_exp = Pattern('\\r\\n|\\r|\\n|;')
+    
+    # Analyse a line break.
+    void lex_break(self):
+        s = self.match(self.break_exp)
+        if self.ignore_break():
+            self.add_pre(s)
+            self.line += 1
+        else:
+            self.add_token(Break(s))
+            self.line += 1
+            self.lex_indent()
+    
+    void lex_semicolon(self):
+        self.add_token(Break(';'))
+    
+    void lex_colon(self):
+        self.add_token(Colon(':'))
+    
+    Pattern open_bracket_exp = Pattern('[[({]')
+    
+    void lex_open_bracket(self):
+        s = self.match(self.open_bracket_exp)
+        self.open_brackets.append(s)
+        self.add_token(Punct(s))
+    
+    Pattern close_bracket_exp = Pattern('[])}]')
+    
+    dict<str, str> open_bracket = {')': '(', ']': '[', '}': '{'}
+    
+    void lex_close_bracket(self):
+        s = self.match(self.close_bracket_exp)
+        if self.open_brackets != [] and self.open_bracket[s] == self.open_brackets[-1]:
+            self.open_brackets.remove_at(-1)
+        self.add_token(Punct(s))
+    
+    # Analyse a non-alphabetical operator or a punctuator.
+    void lex_misc(self):
+        s = ''
+        any t = None
+        for re_list, type in [(operators, Op), (punctuators, Punct)]:
+            for re in re_list:
+                s2 = self.match(re)
+                if len(s2) > len(s):
+                    t = type
+                    s = s2
+        if s == '':
+            self.add_token(LexError(self.s[self.i], INVALID_CHARACTER))
+        else:
+            self.add_token(t(s))
+    
+    # Report an unknown character as a lexical analysis error.
+    void unknown_character(self):
+        self.add_token(LexError(self.s[self.i], INVALID_CHARACTER))
+    
+    
+    # Utility methods
+    #----------------
+    
+    
+    # If the argument regexp is matched at the current location, return the
+    # matched string; otherwise return the empty string.
+    str match(self, Pattern re):
+        m = re.match(re, self.s[self.i:])
+        if m is not None:
+            return m.group(0)
+        else:
+            return ''
+    
+    str match(self, str re):
+        m = re.match(re, self.s[self.i:])
+        if m is not None:
+            return m.group(0)
+        else:
+            return ''
+    
+    # Record string representing whitespace or comment after the previous.
+    # The accumulated whitespace/comments will be associated with the next
+    # token and then it will be cleared.
+    void add_pre(self, str s):
+        self.pre += s
+        self.i += len(s)
+    
+    # Store a token. Update its line number and record preceding whitespace
+    # characters and comments.
+    void add_token(self, Token tok):
+        if tok.string == '' and not isinstance(tok, Eof) and not isinstance(tok, Break) and not isinstance(tok, LexError) and not isinstance(tok, Dedent):
+            raise ValueError('Empty token')
+        tok.pre = self.pre
+        tok.line = self.line
+        self.tok.append(tok)
+        self.i += len(tok.string)
+        self.pre = ''
+    
+    void add_special_token(self, Token tok, int line, int skip):
+        if tok.string == '' and not isinstance(tok, Eof) and not isinstance(tok, Break) and not isinstance(tok, LexError) and not isinstance(tok, Dedent):
+            raise ValueError('Empty token')
+        tok.pre = self.pre
+        tok.line = line
+        self.tok.append(tok)
+        self.i += skip
+        self.pre = ''
+    
+    # If the next token is a break, can we ignore it?
+    bool ignore_break(self):
+        if len(self.open_brackets) > 0 or len(self.tok) == 0:
+            return True
+        else:
+            t = self.tok[-1]
+            return isinstance(t, Break) or isinstance(t, Dedent)
+    
+    # Verify that a token (represented by a string) is encoded correctly
+    # according to the file encoding.
+    void verify_encoding(self, str string, Constant context):
+        Encoding codec = None
+        _x = self.enc
+        if _x == ASCII_ENCODING:
+            codec = ascii
+        elif _x in [UTF8_ENCODING, DEFAULT_ENCODING]:
+            codec = utf8
+        if codec is not None:
+            try:
+                decode(string, codec, STRICT)
+            except DecodeError:
+                type = INVALID_UTF8_SEQUENCE
+                if self.enc == ASCII_ENCODING:
+                    if context == STR_CONTEXT:
+                        type = NON_ASCII_CHARACTER_IN_STRING
+                    else:
+                        type = NON_ASCII_CHARACTER_IN_COMMENT
+                self.add_token(LexError('', type))
---
+++ util.py
@@ -0,0 +1,50 @@
+
+
+# Return the last component of the type name of an object. If obj is nil,
+# return 'nil'. For example, if obj is 1, return 'Int'.
+str short_type(object obj):
+    if obj is None:
+        return 'nil'
+    t = str(type(obj))
+    return t.split('::')[1]
+
+
+# Indent all the lines in s (separated by Newlines) by n spaces.
+str indent(str s, int n):
+    s = ' ' * n + s
+    s = s.replace('\n', '\n' + ' ' * n)
+    return s
+
+
+# Return the items of an array converted to strings using Repr.
+list<str> array_repr<T>(list<T> a):
+    list<str> aa = []
+    for x in a:
+        aa.append(repr(x))
+    return aa
+
+
+# Convert an array into a pretty-printed multiline string representation.
+# The format is
+#   tag(
+#     item1..
+#     itemN)
+# Individual items are formatted like this:
+#  - arrays are flattened
+#  - pairs (str : array) are converted recursively, so that str is the tag
+#  - other items are converted to strings and indented
+str dump_tagged(list<any> nodes, str tag):
+    list<str> a = []
+    if tag is not None:
+        a.append(tag + '(')
+    for n in nodes:
+        if isinstance(n, list):
+            a.append(dump_tagged(n, None))
+        elif isinstance(n, Pair):
+            s = dump_tagged(n.right, n.left)
+            a.append(indent(s, 2))
+        elif n is not None:
+            a.append(indent(str(n), 2))
+    if tag is not None:
+        a[-1] += ')'
+    return '\n'.join(a)
