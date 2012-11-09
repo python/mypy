37be24f688640935120ae231a045457974988c5a
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-09 15:05:43 +0000

    Add translated parser test cases

---
+++ test-parser.py
@@ -0,0 +1,23 @@
+import os.path
+from unittest import Suite
+from test.helpers import parse_test_cases, assert_string_arrays_equal
+from parser import parse
+from errors import CompileError
+
+
+class ParserSuite(Suite):
+    def cases(self):
+        # The test case descriptions are stored in an external file.
+        return parse_test_cases(os.path.join(test_data_prefix, 'parse.test'), test_parser)
+
+
+# Perform a single parser test case. The argument contains the description
+# of the test case.
+def test_parser(testcase):
+    any a
+    try:
+        n = parse('\n'.join(testcase.input))
+        a = str(n).split('\n')
+    except CompileError as e:
+        a = e.messages
+    assert_string_arrays_equal(testcase.output, a, 'Invalid parser output ({}, line {})'.format(testcase.file, testcase.line))
