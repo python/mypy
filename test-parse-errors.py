cdb35a61c5f4aa77317baf7bca84145931731ff2
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-12 15:37:48 +0000

    Add parse error tests

---
+++ test-parse-errors.py
@@ -0,0 +1,27 @@
+import os.path
+from unittest import Suite, AssertionFailure
+from test.helpers import parse_test_cases, assert_string_arrays_equal
+from parser import parse
+from errors import CompileError
+
+
+# The file name shown in test case output. This is displayed in error
+# messages, and must match the file name in the test case descriptions.
+INPUT_FILE_NAME = 'file.alo'
+
+
+class ParseErrorSuite(Suite):
+    def cases(self):
+        # Test case descriptions are in an external file.
+        return parse_test_cases(os.path.join(test_data_prefix, 'parse-errors.test'), test_parse_error)
+
+
+def test_parse_error(testcase):
+    try:
+        # Compile temporary file.
+        parse('\n'.join(testcase.input), INPUT_FILE_NAME)
+        raise AssertionFailure('No errors reported')
+    except CompileError as e:
+        # Verify that there was a compile error and that the error messages
+        # are equivalent.
+        assert_string_arrays_equal(testcase.output, e.messages, 'Invalid compiler output ({}, line {})'.format(testcase.file, testcase.line))
