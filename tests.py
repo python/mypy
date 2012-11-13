da2e18b5655d3f5aa1624acb9f9cabbbded5eb6f
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-13 17:15:01 +0000

    Minor refactoring

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 from unittest import Suite, run_test
 import testlex
 import testparse
-import testparseerr
 import testsemanal
 import sys
 
@@ -10,7 +9,7 @@ class AllSuite(Suite):
     def __init__(self):
         self.test_lex = testlex.LexerSuite()
         self.test_parse = testparse.ParserSuite()
-        self.test_parse_errors = testparseerr.ParseErrorSuite()
+        self.test_parse_errors = testparse.ParseErrorSuite()
         self.test_semanal = testsemanal.SemAnalSuite()
         self.test_semanal_errors = testsemanal.SemAnalErrorSuite()
         self.test_semanal_symtable = testsemanal.SemAnalSymtableSuite()
