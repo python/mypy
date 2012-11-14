c08b63cffff065954d87713d49b9230d950f112a
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-14 10:55:37 +0000

    Add translated Python-gen code and test cases

---
+++ pythongen.py
@@ -0,0 +1,271 @@
+from output import OutputVisitor
+from parser import none
+from types import Any, Instance, Void, TypeVar, TupleType, Callable, UnboundType
+from nodes import IfStmt, ForStmt, WhileStmt, WithStmt, TryStmt
+from checker import function_type
+
+
+# Names present in mypy but not in Python. Imports of these names are removed
+# during translation. These are generally type names, so references to these
+# generally appear in declarations, which are erased during translation.
+#
+# TODO for many of these a corresponding alternative exists in Python; if
+#      these names are used in a non-erased context (e.g. isinstance test or
+#      overloaded method signature), we should change the reference to the
+#      Python alternative
+removed_names = {'re': ['Pattern', 'Match']}
+
+
+# Python backend. Translate semantically analyzed parse trees to Python.
+# Reuse most of the generation logic from the mypy pretty printer implemented
+# in OutputVisitor.
+class PythonGenerator(OutputVisitor):
+    def visit_import_from(self, o):
+        if removed_names.has_key(o.id):
+            r = o.repr
+            
+            # Filter out any names not defined in Python from an from ... import
+            # statement.
+            
+            toks = []
+            comma = none
+            for i in range(len(o.names)):
+                if o.names[i][0] not in removed_names[o.id]:
+                    toks.append(comma)
+                    toks.extend(r.names[i][0])
+                    comma = r.names[i][1]
+            
+            # If everything was filtered out, omit the statement.
+            if toks != []:
+                # Output the filtered statement.
+                self.token(r.from_tok)
+                self.tokens(r.components)
+                self.token(r.import_tok)
+                self.token(r.lparen)
+                self.tokens(toks)
+                self.token(r.rparen)
+                self.token(r.br)
+        else:
+            super().visit_import_from(o)
+    
+    def visit_func_def(self, o, name_override=None):
+        r = o.repr
+        
+        if r.def_tok is not None and r.def_tok.string != '':
+            self.token(r.def_tok)
+        else:
+            self.string(self.get_pre_whitespace(o.typ.typ.ret_type) + 'def')
+        
+        if name_override is None:
+            self.token(r.name)
+        else:
+            self.string(' ' + name_override)
+        self.function_header(o, r.args, None, True)
+        if o.body.body == []:
+            self.string(': pass' + '\n')
+        else:
+            self.node(o.body)
+    
+    # Return whitespace before the first token of a type.
+    def get_pre_whitespace(self, t):
+        if isinstance(t, Any):
+            return t.repr.any_tok.pre
+        elif isinstance(t, Instance):
+            return t.repr.components[0].pre
+        elif isinstance(t, Void):
+            return t.repr.void.pre
+        elif isinstance(t, TypeVar):
+            return t.repr.name.pre
+        elif isinstance(t, TupleType) or isinstance(t, Callable):
+            return t.repr.components[0].pre
+        else:
+            raise RuntimeError('Unsupported type {}'.format(t))
+    
+    def visit_var_def(self, o):
+        r = o.repr
+        self.string(self.get_pre_whitespace(o.items[0][1]))
+        self.omit_next_space = True
+        for v, t in o.items:
+            self.node(v)
+        if o.init is not None:
+            self.token(r.assign)
+            self.node(o.init)
+        else:
+            self.string(' = {}'.format(', '.join(['None'] * len(o.items))))
+        self.token(r.br)
+    
+    def visit_cast_expr(self, o):
+        self.string(o.repr.lparen.pre)
+        self.node(o.expr)
+    
+    def visit_for_stmt(self, o):
+        r = o.repr
+        self.token(r.for_tok)
+        for i in range(len(o.index)):
+            self.token(o.index[i].repr.name)
+            self.token(o.index[i].repr.comma)
+        self.token(r.in_tok)
+        self.node(o.expr)
+        
+        self.node(o.body)
+        if o.else_body is not None:
+            self.token(r.else_tok)
+            self.node(o.else_body)
+    
+    def visit_type_def(self, o):
+        r = o.repr
+        self.string(r.class_tok.pre)
+        self.string('class')
+        self.token(r.name)
+        self.token(r.lparen)
+        for i in range(len(o.base_types)):
+            self.string(self.erased_type(o.base_types[i]))
+            if i < len(r.commas):
+                self.token(r.commas[i])
+        self.token(r.rparen)
+        self.node(o.defs)
+    
+    def erased_type(self, t):
+        if isinstance(t, Instance) or isinstance(t, UnboundType):
+            a = []
+            if t.repr is not None:
+                for tok in t.repr.components:
+                    a.append(tok.rep)
+            return ''.join(a)
+        elif isinstance(t, TupleType):
+            return 'tuple' # FIX: aliasing?
+        else:
+            raise RuntimeError('Cannot translate type {}'.format(t))
+    
+    def visit_func_expr(self, o):
+        r = o.repr
+        self.token(r.lambda_tok)
+        self.function_header(o, r.args, None, True, False)
+        self.token(r.colon)
+        self.node(o.body.body[0].expr)
+    
+    def visit_overloaded_func_def(self, o):
+        indent = self.indent * ' '
+        first = o.items[0]
+        r = first.repr
+        if r.def_tok is not None and r.def_tok.string != '':
+            self.token(r.def_tok)
+        else:
+            # TODO omit (some) comments; now comments may be duplicated
+            self.string(self.get_pre_whitespace(first.typ.typ.ret_type) + 'def')
+        self.string(' {}('.format(first.name))
+        self.extra_indent += 4
+        fixed_args, is_more = self.get_overload_args(o)
+        self.string(', '.join(fixed_args))
+        rest_args = None
+        if is_more:
+            rest_args = self.make_unique('args', fixed_args)
+            self.string(', *{}'.format(rest_args))
+        self.string('):' + '\n' + indent)
+        n = 1
+        for f in o.items:
+            self.visit_func_def(f, '{}{}'.format(f.name, n))
+            n += 1
+        self.string('\n')
+        
+        n = 1
+        for f in o.items:
+            c = self.make_overload_check(f, fixed_args, rest_args)
+            self.string(indent)
+            if n == 1:
+                self.string('if ')
+            else:
+                self.string('elif ')
+            self.string(c)
+            self.string(':' + '\n' + indent)
+            self.string('    return {}'.format(self.make_overload_call(f, n, fixed_args, rest_args)) + '\n')
+            n += 1
+        self.string(indent + 'else:' + '\n')
+        self.string(indent + '    raise TypeError("Invalid argument types")')
+        self.extra_indent -= 4
+        last_stmt = o.items[-1].body.body[-1]
+        self.token(self.find_break_after_statement(last_stmt))
+    
+    def find_break_after_statement(self, s):
+        any blocks
+        if isinstance(s, IfStmt):
+            blocks = s.body + [s.else_body]
+        elif isinstance(s, ForStmt) or isinstance(s, WhileStmt):
+            blocks = [s.body, s.else_body]
+        elif isinstance(s, WithStmt):
+            blocks = [s.body]
+        elif isinstance(s, TryStmt):
+            blocks = s.handlers + [s.else_body, s.finally_body]
+        else:
+            return s.repr.br
+        for b in reversed(blocks):
+            if b is not None:
+                return self.find_break_after_statement(b.body[-1])
+        raise RuntimeError('Could not find break after statement')
+    
+    def make_unique(self, n, others):
+        if n in others:
+            return self.make_unique('_' + n, others)
+        else:
+            return n
+    
+    def get_overload_args(self, o):
+        fixed = []
+        min_fixed = 100000
+        max_fixed = 0
+        for f in o.items:
+            if len(f.args) > len(fixed):
+                for v in f.args[len(fixed):]:
+                    fixed.append(v.name)
+            min_fixed = min(min_fixed, f.min_args)
+            max_fixed = max(max_fixed, len(f.args))
+        return fixed[:min_fixed], max_fixed > min_fixed
+    
+    def make_overload_check(self, f, fixed_args, rest_args):
+        a = []
+        i = 0
+        if rest_args is not None:
+            a.append(self.make_argument_count_check(f, len(fixed_args), rest_args))
+        for t in function_type(f).arg_types:
+            if not isinstance(t, Any) and (t.repr is not None or isinstance(t, Callable)):
+                a.append(self.make_argument_check(f.args[i].name, t))
+            i += 1
+        return ' and '.join(a)
+    
+    def make_argument_count_check(self, f, num_fixed, rest_args):
+        return 'len({}) == {}'.format(rest_args, f.min_args - num_fixed)
+    
+    def make_argument_check(self, name, typ):
+        if isinstance(typ, Callable):
+            return 'callable({})'.format(name)
+        else:
+            cond = 'isinstance({}, {})'.format(name, self.erased_type(typ))
+            return cond.replace('  ', ' ')
+    
+    def make_overload_call(self, f, n, fixed_args, rest_args):
+        a = []
+        for i in range(len(f.args)):
+            if i < len(fixed_args):
+                a.append(fixed_args[i])
+            else:
+                a.append('{}[{}]'.format(rest_args, i - len(fixed_args)))
+        return '{}{}({})'.format(f.name, n, ', '.join(a))
+    
+    def visit_list_expr(self, o):
+        r = o.repr
+        self.token(r.lbracket)
+        self.comma_list(o.items, r.commas)
+        self.token(r.rbracket)
+    
+    def visit_dict_expr(self, o):
+        r = o.repr
+        self.token(r.lbrace)
+        i = 0
+        for k, v in o.items:
+            self.node(k)
+            self.token(r.colons[i])
+            self.node(v)
+            if i < len(r.commas):
+                self.token(r.commas[i])
+            i += 1
+        self.token(r.rbrace)
---
+++ test-pythongen.py
@@ -0,0 +1,53 @@
+import os.path
+from unittest import Suite
+from test.helpers import parse_test_cases, assert_string_arrays_equal
+from build import build
+from os import separator
+from pythongen import PythonGenerator
+from errors import CompileError
+
+
+# Files which contain test case descriptions.
+python_generation_files = ['pythongen.test']
+
+
+class PythonGenerationSuite(Suite):
+    def cases(self):
+        c = []
+        for f in python_generation_files:
+            c += parse_test_cases(os.path.join(test_data_prefix, f), test_python_generation, test_temp_dir, True)
+        return c
+
+
+# Perform a mypy-to-Python source code transformation test case.
+def test_python_generation(testcase):
+    any a
+    expected = testcase.output
+    # By default, assume an identity translation. This is useful for dynamically
+    # typed code.
+    if expected == []:
+        expected = testcase.input
+    try:
+        src = '\n'.join(testcase.input)
+        # Parse and semantically analyze the source program.
+        trees, symtable, infos, types = build(src, 'main', True, test_temp_dir)
+        a = []
+        first = True
+        # Produce an output containing the pretty-printed forms (with original
+        # formatting) of all the relevant source files.
+        for t in trees:
+            # Omit the builtins module and files marked for omission.
+            if not t.path.endswith(separator + 'builtins.py') and '-skip.' not in t.path:
+                # Add file name + colon for files other than the first.
+                if not first:
+                    a.append('{}:'.format(fix_path(remove_prefix(t.path, test_temp_dir))))
+                
+                v = PythonGenerator()
+                t.accept(v)
+                s = v.output()
+                if s != '':
+                    a += s.split('\n')
+            first = False
+    except CompileError as e:
+        a = e.messages
+    assert_string_arrays_equal(expected, a, 'Invalid source code output ({}, line {})'.format(testcase.file, testcase.line))
