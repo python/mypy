1b27e42095b5d12ab349670c957e1925d128e416
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-16 17:09:18 +0000

    Translate comments to doc strings

--- typerepr.py
+++ typerepr.py
@@ -1,54 +1,52 @@
+"""Representation classes for Typ subclasses and TypeVars.
+
+These are used for source-source transformation that preserves original
+formatting and comments.
+"""
+
 from lex import Token
 
-# Representation classes for Typ subclasses and TypeVars. These are used for
-# source-source transformation that preserves original formatting and
-# comments.
 
-
-# Representation of UnboundType, Instance and Callable.
 class CommonTypeRepr:
-    # Note: langle and rangle may be empty.
+    """Representation of UnboundType, Instance and Callable."""
     void __init__(self, list<Token> components,  langle, list<Token> commas,
                   any rangle):
+        # Note: langle and rangle may be empty.
         self.components = components
         self.langle = langle
         self.commas = commas
         self.rangle = rangle
 
 
-# Representation of Any.
 class AnyRepr:
+    """Representation of Any."""
     void __init__(self, any any_tok):
         self.any_tok = any_tok
 
 
-# Representation of Void.
 class VoidRepr:
+    """Representation of Void."""
     void __init__(self, any void):
         self.void = void
 
 
-# Representation of NoneTyp.
-class NoneTypRepr: pass
-
-
-# Representation of TypeVar.
 class TypeVarRepr:
+    """Representation of TypeVar."""
     void __init__(self, any name):
         self.name = name
 
 
-# Representation of TypeVars.
 class TypeVarsRepr:
+    """Representation of TypeVars."""
     void __init__(self, any langle, list<Token> commas, any rangle):
         self.langle = langle
         self.commas = commas
         self.rangle = rangle
 
 
-# Representation of TypeVarDef.
 class TypeVarDefRepr:
-    # TODO remove is_tok
+    """Representation of TypeVarDef."""
     void __init__(self, any name, any is_tok):
+        # TODO remove is_tok
         self.name = name
         self.is_tok = is_tok
