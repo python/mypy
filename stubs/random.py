19c383c63b6e6207878d34ce01aa3d87d422f08a
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-21 18:55:27 +0000

    Add trivial stubs for random

---
+++ stubs/random.py
@@ -0,0 +1,7 @@
+# Stubs for random
+
+# TODO this is a partial implementation
+
+int randrange(int stop): pass
+int randrange(int start, int stop, int step=1): pass
+void shuffle(list<any> x): pass # TODO better
