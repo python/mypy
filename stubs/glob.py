8bb7245e7548d1edde0867596b08bda860735963
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-26 14:35:26 +0000

    Add stubs for glob

---
+++ stubs/glob.py
@@ -0,0 +1,9 @@
+# Stubs for glob
+
+# Based on http://docs.python.org/3.2/library/glob.html
+
+list<str> glob(str pathname): pass
+list<bytes> glob(bytes pathname): pass
+Iterator<str> iglob(str pathname): pass
+Iterator<bytes> iglob(bytes pathname): pass
+
