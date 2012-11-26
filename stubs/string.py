2288e0e8fcd5885b07bef9a6b534730e2f8b0136
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-26 16:37:18 +0000

    Add basic stubs for "string"

--- stubs/string.py
+++ stubs/string.py
@@ -1,3 +1,16 @@
 # Stubs for string
 
-# TODO add stubs
+str ascii_letters
+str ascii_lowercase
+str ascii_uppercase
+str digits
+str hexdigits
+str octdigits
+str punctuation
+str printable
+str whitespace
+
+str capwords(str s, str sep=None): pass
+
+# TODO Formatter
+# TODO Template
