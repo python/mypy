8e92c6e1f4b2c1a6b60d67dcf4aca7b2a80b499e
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-27 11:08:49 +0000

    Add stubs for signal

---
+++ stubs/signal.py
@@ -0,0 +1,44 @@
+# Stubs for signal
+
+# Based on http://docs.python.org/3.2/library/signal.html
+
+int SIG_DFL
+int SIG_IGN
+
+# TODO more SIG* constants (these should be platform specific?)
+int SIGHUP
+int SIGINT
+int SIGQUIT
+int SIGABRT
+int SIGKILL
+int SIGALRM
+int SIGTERM
+
+int SIGUSR1
+int SIGUSR2
+int SIGCONT
+int SIGSTOP
+
+int SIGPOLL
+int SIGVTALRM
+
+# CTRL_C_EVENT
+# CTRL_BREAK_EVENT
+
+int NSIG
+int ITIMER_REAL
+int ITIMER_VIRTUAL
+int ITIMER_PROF
+
+class ItimerError(IOError): pass
+
+#int alarm(int time): pass # Unix
+any getsignal(int signalnum): pass
+#void pause(): pass # Unix
+#tuple<float, float> setitimer(int which, float seconds,
+#                              float interval=None): pass # Unix
+#def getitimer(int which): pass # Unix
+void set_wakeup_fd(int fd): pass
+void siginterrupt(int signalnum, bool flag): pass
+any signal(int signalnum,
+           func<int, any, void> handler): pass # TODO frame object
