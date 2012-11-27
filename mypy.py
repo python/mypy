44eb4b51bc18524d6d1e8048ba4ce7e281c9b4a3
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-27 16:10:07 +0000

    Major update to mypy driver program

--- mypy.py
+++ mypy.py
@@ -13,24 +13,14 @@ from pythongen import PythonGenerator
 from errors import CompileError
 
 
+# Options
+verbose = False
+pyversion = 3
+interpreter = 'python'
+
+
 void main():
-    verbose = False
-    pyversion = 3
-    args = sys.argv[1:]
-    while args and args[0].startswith('-'):
-        if args[0] == '--verbose':
-            verbose = True
-            args = args[1:]
-        elif args[0] == '--py2':
-            pyversion = 2
-            args = args[1:]
-        else:
-            fail('Invalid option {}'.format(args[0]))
-    
-    if not args:
-        fail()
-    
-    path = args[0]
+    path, args = process_options()
     
     mainfile = open(path)
     text = mainfile.read()
@@ -51,8 +41,7 @@ void main():
         for t in trees:
             if not is_stub(t.path):
                 out_path = os.path.join(output_dir, os.path.basename(t.path))
-                if verbose:
-                    print('LOG: translate {} to {}'.format(t.path, out_path))
+                log('translate {} to {}'.format(t.path, out_path))
                 v = PythonGenerator(pyversion)
                 t.accept(v)
                 outfile = open(out_path, 'w')
@@ -60,19 +49,14 @@ void main():
                 outfile.close()
         
         # Run the translated program.
-        # TODO determine path to Python interpreter reliably
         
         a = <str> []
         for arg in args[1:]:
             # TODO escape arguments etc.
             a.append('"{}"'.format(arg))
 
-        cmd = 'python3'
-        if pyversion == 2:
-            cmd = 'python'
-        
         os.system('{} "{}/{}" {}'.format(
-                             cmd, output_dir, os.path.basename(path),
+                             interpreter, output_dir, os.path.basename(path),
                              ' '.join(a)))
     except CompileError as e:
         for m in e.messages:
@@ -80,16 +64,46 @@ void main():
         sys.exit(2)
 
 
-def fail(msg=None):
+tuple<str, str[]> process_options():
+    if sys.executable:
+        global interpreter
+        interpreter = sys.executable
+    args = sys.argv[1:]
+    while args and args[0].startswith('-'):
+        if args[0] == '--verbose':
+            global verbose
+            verbose = True
+            args = args[1:]
+        elif args[0] == '--py2' and args[1:]:
+            # Generate Python 2 (but this is very buggy).
+            global pyversion
+            pyversion = 2
+            interpreter = args[1]
+            args = args[2:]
+        else:
+            fail('Invalid option {}'.format(args[0]))
+    
+    if not args:
+        fail()
+    
+    return args[0], args[1:]    
+
+
+void fail(str msg=None):
     if msg:
         sys.stderr.write('%s\n' % msg)
     sys.stderr.write('Usage: mypy.py [--verbose] PROGRAM\n')
     sys.exit(1)
 
 
-def is_stub(path):
+bool is_stub(str path):
     # TODO make the check more precise
     return path.startswith('stubs/') or '/stubs/' in path
+
+
+void log(str message):
+    if verbose:
+        print('LOG: {}'.format(message))
 
 
 if __name__ == '__main__':
