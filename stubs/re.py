585a9e28fe9fa09e0f98b39a8005409f0c95ee9c
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-12-05 09:40:08 +0000

    Fixes to re module stubs

--- stubs/re.py
+++ stubs/re.py
@@ -18,13 +18,12 @@ int DOTALL
 int X
 int VERBOSE
 
-???exception_type error
+class error(Exception): pass
 
 # TODO functions and classes that work on bytes objects
-# TODO more functionality
 
 # ----- re classes -----
-class regex:
+class Pattern:
     int flags, groupindex, groups
     str pattern
 
@@ -32,35 +31,20 @@ class regex:
     Match match(self, str string, int pos=0, int endpos=-1): pass
     list<str> split(self, str string, int maxsplit=0): pass
     list<str> findall(self, str string, int pos=0, int endpos=-1): pass
-    ???iterator_yielding_match_objects finditer(self, str string, int pos=0, int endpos=-1): pass
+    Iterator<Match> finditer(self, str string, int pos=0, int endpos=-1): pass
     str sub(self, str repl, str string, int count=0): pass
     str sub(self, func<Match, str> repl, str string, int count=0): pass
     tuple<str, int> subn(self, str repl, str string, int count=0): pass
-    tuple<str, int> subn(self, func<Match, str> repl, str string, int count=0): pass
-
-class Pattern:
-    Match match(self, str string, int pos=0, int endpos=-1): pass
-    Match search(self, str string, int pos=0, int endpos=-1): pass
-    str sub(self, str repl, str string, int count=0, int flags=0): pass
-    str sub(self, func<Match, str> repl, str string, int count=0,
-            int flags=0): pass
+    tuple<str, int> subn(self, func<Match, str> repl, str string,
+                         int count=0): pass
 
 class Match:
-    str expand(self, str template): pass
-    str group(self): pass
-    str group(self, int group1): pass
-    # TODO group(...) with multiple groups
-    int start(self, int group=0): pass
-    int end(self, int group=0): pass
-    tuple<int, int> span(self, int group=0): pass
-
-class match:
     int pos, endpos, lastindex
     str lastgroup, string
     
     # The regular expression object whose match() or search() method produced
     # this match instance.
-    ???regular_expression_object re
+    Pattern re
     
     str expand(self, str template): pass
     str group(self): pass
@@ -87,7 +71,7 @@ list<str> findall(str pattern, str string, int flags=0): pass
 # for the RE pattern in string. The string is scanned left-to-right, and 
 # matches are returned in the order found. Empty matches are included in the 
 # result unless they touch the beginning of another match.
-???iterator_yielding_match_objects finditer(str pattern, str string, int flags=0): pass
+Iterator<Match> finditer(str pattern, str string, int flags=0): pass
 
 str sub(str pattern, str repl, str string, int count=0, int flags=0): pass
 str sub(str pattern, func<Match, str> repl, str string, int count=0,
@@ -98,4 +82,3 @@ tuple<str, int> subn(str pattern, func<Match, str> repl, str string,
                      int count=0, int flags=0): pass
 str escape(str string): pass
 void purge(): pass
-
