4552b9574b8653705465995c710e40a9cf87a61e
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-12-06 15:04:13 +0000

    Add sample program

---
+++ samples/dict.py
@@ -0,0 +1,7 @@
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print('I owe the grocer $%.2f' % grocery_bill)
