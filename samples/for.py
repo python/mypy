97253807bd88cdf94c0bd9bd550cd385c40fa1d6
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-12-06 15:00:34 +0000

    Update sample program

--- samples/for.py
+++ samples/for.py
@@ -1,3 +1,3 @@
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print("iteration {} is {}".format(i, name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
