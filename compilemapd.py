ad61e827dc2fd64126528065a80429dc998040a3
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-12-17 14:27:05 +0000

    Add code translated from Alore

---
+++ compilemapd.py
@@ -0,0 +1,125 @@
+from nodes import TypeInfo
+from types import Instance, Typ, TypeVar, Any
+
+
+interface MapPremise: pass
+
+interface MapExpr: pass
+
+
+class AssertClass(MapPremise):
+    MapExpr i
+    TypeInfo c
+    
+    def __str__(self):
+        return str(self.i) + ' = ' + self.c.name + '<...>'
+    
+    void __init__(self, MapExpr i, TypeInfo c):
+        self.i = i
+        self.c = c
+
+class AssertDyn(MapPremise):
+    MapExpr i
+    
+    def __str__(self):
+        return str(self.i) + ' = dyn'
+    
+    void __init__(self, MapExpr i):
+        self.i = i
+
+class AssertEq(MapPremise):
+    MapExpr i1
+    MapExpr i2
+    
+    def __str__(self):
+        return str(self.i1) + ' = ' + str(self.i2)
+    
+    void __init__(self, MapExpr i1, MapExpr i2):
+        self.i1 = i1
+        self.i2 = i2
+
+
+class TypeVarRef(MapExpr):
+    int n
+    
+    def __str__(self):
+        return str(self.n)
+    
+    void __init__(self, int n):
+        self.n = n
+
+class TypeArgRef(MapExpr):
+    MapExpr base
+    int n
+    
+    def __str__(self):
+        return str(self.base) + '.' + str(self.n)
+    
+    void __init__(self, MapExpr base, int n):
+        self.base = base
+        self.n = n
+
+class DefaultArg(MapExpr):
+    def __str__(self):
+        return 'd'
+
+
+# Compile mapping from superclass to subclass type variables.
+#
+#   numSubtypeTypeVars: number of type variables in subclass
+#   superType:          definition of supertype; this may contain type
+#                       variable references
+tuple<list<MapPremise>, list<MapExpr>> compile_subclass_mapping(int num_subtype_type_vars, Instance super_type):
+    premises = find_eq_premises(super_type, None)
+    list<MapExpr> exprs = []
+    
+    for i in range(1, num_subtype_type_vars + 1):
+        paths = find_all_paths(i, super_type, None)
+        if len(paths) == 0:
+            exprs.append(DefaultArg())
+        else:
+            exprs.append(paths[0])
+            if len(paths) > 1:
+                # Multiple paths; make sure they are all the same.
+                for j in range(1, len(paths)):
+                    premises.append(AssertEq(paths[0], paths[j]))
+    
+    return premises, exprs  
+
+
+list<MapExpr> find_all_paths(int tv, Typ typ, MapExpr expr):
+    if isinstance(typ, TypeVar) and ((TypeVar)typ).id == tv:
+        return [expr]
+    elif isinstance(typ, Instance) and ((Instance)typ).args != []:
+        inst = (Instance)typ
+        list<MapExpr> res = []
+        for i in range(len(inst.args)):
+            MapExpr e
+            if expr is None:
+                e = TypeVarRef(i + 1)
+            else:
+                e = TypeArgRef(expr, i + 1)
+            res += find_all_paths(tv, inst.args[i], e)
+        return res
+    else:
+        return []
+
+
+list<MapPremise> find_eq_premises(Typ typ, MapExpr expr):
+    if isinstance(typ, Instance):
+        inst = (Instance)typ
+        list<MapPremise> res = []
+        if expr is not None:
+            res.append(AssertClass(expr, inst.typ))
+        for i in range(len(inst.args)):
+            MapExpr e
+            if expr is None:
+                e = TypeVarRef(i + 1)
+            else:
+                e = TypeArgRef(expr, i + 1)
+            res += find_eq_premises(inst.args[i], e)
+        return res
+    elif isinstance(typ, Any):
+        return [AssertDyn(expr)]
+    else:
+        return []
