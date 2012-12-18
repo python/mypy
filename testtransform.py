f50213e2b7a4662c704763ef6d2b1fcbf1acef09
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-12-18 14:46:58 +0000

    Fix translated code

--- testtransform.py
+++ testtransform.py
@@ -1,9 +1,10 @@
 import os
 import os.path
 import shutil
+import sys
 
 from build import build
-from myunit import Suite
+from myunit import Suite, run_test
 from testhelpers import assert_string_arrays_equal_wildcards
 from testdata import parse_test_cases
 from testconfig import test_data_prefix, test_temp_dir
@@ -19,12 +20,19 @@ TRANSFORM_STD_MODULE = 'fixtures/transform.py'
 
 
 class DyncheckTransformSuite(Suite):
-    test_case_files = ['dyncheck-trans-basic.test', 'dyncheck-trans-generics.test', 'dyncheck-trans-generic-inheritance.test']
+    test_case_files = ['dyncheck-trans-basic.test',
+                       'dyncheck-trans-generics.test',
+                       'dyncheck-trans-generic-inheritance.test']
     
     def cases(self):
         c = []
         for f in self.test_case_files:
-            c += parse_test_cases(os.path.join(test_data_prefix, f), std_wrapper(test_transform, os.path.join(test_data_prefix, TRANSFORM_STD_MODULE)), test_temp_dir, True)
+            c += parse_test_cases(
+                os.path.join(test_data_prefix, f),
+                std_wrapper(test_transform,
+                            os.path.join(test_data_prefix,
+                                         TRANSFORM_STD_MODULE)),
+                test_temp_dir, True)
         return c
 
 
@@ -36,16 +44,21 @@ def test_transform(testcase):
         # Construct input as a single single.
         src = '\n'.join(testcase.input)
         # Parse and type check the input program.
-        trees, symtable, infos, types = build(src, 'main', False, test_temp_dir, True)
+        trees, symtable, infos, types = build(program_text=src,
+                                              program_file_name='main',
+                                              use_test_builtins=False,
+                                              alt_lib_path=test_temp_dir,
+                                              do_type_check=True)
         a = []
         first = True
         # Transform each file separately.
         for t in trees:
             # Skip the std module and files with '-skip.' in the path.
-            if not t.path.endswith('/std.alo') and '-skip.' not in t.path:
+            if not t.path.endswith('/builtins.py') and '_skip.' not in t.path:
                 if not first:
                     # Display path for files other than the first.
-                    a.append('{}:'.format(remove_prefix(t.path, test_temp_dir)))
+                    a.append('{}:'.format(remove_prefix(t.path,
+                                                        test_temp_dir)))
                 
                 # Transform parse tree and produce pretty-printed output.
                 v = DyncheckTransformVisitor(types, symtable, True)
@@ -59,12 +72,16 @@ def test_transform(testcase):
             first = False
     except CompileError as e:
         a = e.messages
-    assert_string_arrays_equal_wildcards(expected, a, 'Invalid source code output ({}, line {})'.format(testcase.file, testcase.line))
+    assert_string_arrays_equal_wildcards(
+        expected, a,
+        'Invalid source code output ({}, line {})'.format(testcase.file,
+                                                          testcase.line))
 
 
 def remove_comment_lines(a):
-    """Return a copy of array with comment lines starting with '--' (but not with
-    '---') removed.
+    """Return a copy of array with comments removed.
+
+    Lines starting with '--' (but not with '---') are removed.
     """
     r = []
     for s in a:
@@ -77,24 +94,17 @@ def remove_comment_lines(a):
 
 def std_wrapper(func, path):
     """Decorate a function that implements a data-driven test case to copy an
-    alternative std module implementation in place before performing the test
-    case. Clean up after executing the test case.
+    alternative builtins module implementation in place before performing the
+    test case. Clean up after executing the test case.
     """
     return lambda testcase: perform_test(func, path, testcase)
 
 
-def perform_test(func, path, testcase):        
-    dir = os.path.join(test_temp_dir, 'std')
-    new_dir = not os.path.isdir(dir)
-    if new_dir:
-        os.makedirs(dir)
-    try:
-        if new_dir:
-            shutil.copyfile(path, os.path.join(dir, 'std.alo'))
-        func(testcase)
-    finally:
-        # Note that if the test case used a custom std module, the std
-        # directory might be handled by the test case (setUp and tearDown).
-        # Therefore only remove the directory if we created it ourselves.
-        if new_dir:
-            shutil.rmtree(dir)
+def perform_test(func, path, testcase):
+    builtins = os.path.join(test_temp_dir, 'builtins.py')
+    shutil.copyfile(path, builtins)
+    func(testcase)
+
+
+if __name__ == '__main__':
+    run_test(DyncheckTransformSuite(), sys.argv[1:])
