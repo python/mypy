9cf11fae7849afa6d60b23b59cb45d1505397a15
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-01-16 14:36:52 +0000

    Add icode generation test cases
    These can't be run yet.

---
+++ test/data/fixtures/icodegen.py
@@ -0,0 +1,24 @@
+# These builtins stubs are used implicitly in parse-tree to icode generation
+# test cases (testicodegen.py and test/data/icode-basic.test).
+
+class object:
+    void __init__(self): pass
+
+class type: pass
+class str: pass
+
+# Primitive types are special in generated code.
+
+class int:
+    int __add__(self, int n): pass
+    int __sub__(self, int n): pass
+    int __mul__(self, int n): pass
+    bool __eq__(self, int n): pass
+    bool __ne__(self, int n): pass
+    bool __lt__(self, int n): pass
+    bool __gt__(self, int n): pass
+    bool __le__(self, int n): pass
+    bool __ge__(self, int n): pass
+
+class float: pass
+class bool: pass
