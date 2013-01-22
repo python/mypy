18d23b8b33ef137267697f8a99f1591b7522b1c4
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-01-22 17:49:06 +0000

    Add C back end test cases

---
+++ testcgen.py
@@ -0,0 +1,53 @@
+"""Test cases for running mypy programs by compiling to C.
+
+Each test cases translates a mypy program to C and then compiles and runs it.
+The output (stdout) of the program is compared to expected output. The
+translation uses the C builtins (lib/builtins.py).
+
+Note: These test cases are not included in the main test suite yet.
+"""
+
+import os.path
+import subprocess
+import sys
+
+import build
+import errors
+from myunit import Suite, run_test
+from testconfig import test_data_prefix, test_temp_dir
+from testdata import parse_test_cases
+from testhelpers import assert_string_arrays_equal
+
+
+# Files which contain test case descriptions.
+cgen_files = ['cgen-basic.test']
+
+
+class CGenSuite(Suite):
+    def cases(self):
+        c = []
+        for f in cgen_files:
+            c += parse_test_cases(os.path.join(test_data_prefix, f),
+                                  test_cgen, test_temp_dir, True)
+        return c
+
+
+def test_cgen(testcase):
+    # Build the program.
+    text = '\n'.join(testcase.input)
+    program = '_program.py'
+    build.build(text, program, target=build.C, alt_lib_path='lib')
+    # Run the program.
+    outfile = './a.out'
+    outb = subprocess.check_output([outfile])
+    # Split output into lines.
+    out = [s.rstrip('\n\r') for s in str(outb, 'utf8').splitlines()]
+    # Remove temp file.
+    os.remove(outfile)
+    assert_string_arrays_equal(testcase.output, out,
+                               'Invalid output ({}, line {})'.format(
+                                   testcase.file, testcase.line))
+
+
+if __name__ == '__main__':
+    run_test(CGenSuite(), sys.argv[1:])
