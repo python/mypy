99ec051679eaae94589278d2e6ad201e00afccb2
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-01-23 16:51:13 +0000

    mymy.py: Enable C back end with the '-c' option
    This still needs more work.

--- mypy.py
+++ mypy.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-"""Mypy type checker and Python translator
+"""Mypy compiler and runner.
 
-Type check and program, translate it to Python and run it. Note that you must
-use a translated mypy translator to run this program.
+Type check and program, compile it to Python or C and run it. Note that you
+must use a translated mypy translator to run this program.
+
+Note that the C back end is early in development and has a very limited feature
+set.
 """
 
 import os
@@ -19,6 +22,7 @@ from pythongen import PythonGenerator
 
 # Fallback options
 verbose = False
+target = build.PYTHON
 pyversion = 3
 interpreter = 'python'
 
@@ -28,44 +32,63 @@ void main():
 
     try:
         mainfile = open(path)
-        text = mainfile.read()
+        program_text = mainfile.read()
         mainfile.close()
     except IOError as ioerr:
         fail("mypy: can't read file '{}': {}".format(path,
                                                      ioerr.strerror))
     
     try:
-        outputdir = os.path.join(os.path.dirname(path), '__mycache__')
-        tempdir = False
-        if not os.path.isdir(outputdir):
-            try:
-                os.mkdir(outputdir)
-            except OSError:
-                # Could not create a directory under program directory; must
-                # fall back to a temp directory. It will be removed later.
-                outputdir = tempfile.mkdtemp()
-                tempdir = True
-
-        try:
-            # Type check the program and dependencies and translate to Python.
-            build.build(text, path,
-                        target=build.PYTHON,
-                        output_dir=outputdir,
-                        python_version=pyversion)
-            
-            # Run the translated program.
-            status = subprocess.call(
-                [interpreter,
-                 '{}/{}'.format(outputdir,os.path.basename(path))] +
-                args)
-            sys.exit(status)
-        finally:
-            if tempdir:
-                shutil.rmtree(outputdir)
+        if target == build.PYTHON:
+            compile_to_python(program_text, path, args)
+        elif target == build.C:
+            compile_to_c(program_text, path)
+        else:
+            raise RuntimeError('unsupported target %d' % target)
     except CompileError as e:
         for m in e.messages:
             print(m)
         sys.exit(1)
+
+
+void compile_to_python(str program_text, str path, str[] args):
+    outputdir = os.path.join(os.path.dirname(path), '__mycache__')
+    tempdir = False
+    if not os.path.isdir(outputdir):
+        try:
+            os.mkdir(outputdir)
+        except OSError:
+            # Could not create a directory under program directory; must
+            # fall back to a temp directory. It will be removed later.
+            outputdir = tempfile.mkdtemp()
+            tempdir = True
+
+    try:
+        # Type check the program and dependencies and translate to Python.
+        build.build(program_text, path,
+                    target=build.PYTHON,
+                    output_dir=outputdir,
+                    python_version=pyversion)
+
+        # Run the translated program.
+        status = subprocess.call(
+            [interpreter,
+             '{}/{}'.format(outputdir,os.path.basename(path))] +
+            args)
+        sys.exit(status)
+    finally:
+        if tempdir:
+            shutil.rmtree(outputdir)
+
+
+void compile_to_c(str program_text, str path):
+    # Compile the progam to C.
+    build.build(program_text, path, target=build.C, alt_lib_path='lib')
+
+    # Run the translated program.
+    # TODO command line arguments
+    status = subprocess.call(['./a.out'])
+    sys.exit(status)
 
 
 tuple<str, str[]> process_options():
@@ -84,6 +107,10 @@ tuple<str, str[]> process_options():
             pyversion = 2
             interpreter = args[1]
             args = args[2:]
+        elif args[0] == '-c':
+            global target
+            target = build.C
+            args = args[1:]
         else:
             usage('Invalid option {}'.format(args[0]))
     
@@ -96,7 +123,7 @@ tuple<str, str[]> process_options():
 void usage(str msg=None):
     if msg:
         sys.stderr.write('%s\n' % msg)
-    sys.stderr.write('Usage: mypy.py [--verbose] PROGRAM\n')
+    sys.stderr.write('Usage: mypy.py [--verbose] [-c] PROGRAM\n')
     sys.exit(2)
 
 
