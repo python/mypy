85f38cb76e298aea6acea686f7ce61ee7757c7b8
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-01-28 16:32:58 +0000

    Add C code generation test case runner
    These perform checks on the generated C code.
    
    Also add a single trivial test case.
    
    Closes #68.

--- testcgen.py
+++ testcgen.py
@@ -1,9 +1,7 @@
-"""Test cases for running mypy programs by compiling to C.
+"""Test cases for compiling mypy programs to C and running them.
 
-Each test cases translates a mypy program to C and then compiles and runs it.
-The output (stdout) of the program is compared to expected output. The
-translation uses the C builtins (lib/builtins.py).
-
+The compilation to C uses the full C builtins (lib/builtins.py).
+    
 Note: These test cases are not included in the main test suite yet.
 """
 
@@ -17,19 +15,57 @@ import errors
 from myunit import Suite, run_test
 from testconfig import test_data_prefix, test_temp_dir
 from testdata import parse_test_cases
-from testhelpers import assert_string_arrays_equal
+from testhelpers import (
+    assert_string_arrays_equal, assert_string_arrays_equal_wildcards
+)
 
 
-# Files which contain test case descriptions.
-cgen_files = ['cgen-basic.test',
-              'cgen-intops.test',
-              'cgen-classes.test']
+class CGenCompileSuite(Suite):
+    """Test cases that compile to C and perform checks on the C code."""
 
+    files = ['cgen-codeoutput.test']
 
-class CGenSuite(Suite):
     def cases(self):
         c = []
-        for f in cgen_files:
+        for f in self.files:
+            c += parse_test_cases(os.path.join(test_data_prefix, f),
+                                  test_cgen_compile, test_temp_dir, True)
+        return c
+
+
+def test_cgen_compile(testcase):
+    # Build the program.
+    text = '\n'.join(testcase.input)
+    try:
+        build.build(text, '_program.py', target=build.C, alt_lib_path='lib',
+                    flags=[build.COMPILE_ONLY])
+        outfile = '_program.c'
+        f = open(outfile)
+        out = [s.rstrip('\n\r') for s in f.readlines()]
+        f.close()
+        os.remove(outfile)
+    except errors.CompileError as e:
+        out = e.messages
+    # Verify output.
+    assert_string_arrays_equal_wildcards(testcase.output, out,
+                               'Invalid output ({}, line {})'.format(
+                                   testcase.file, testcase.line))
+
+
+class CGenRunSuite(Suite):
+    """Test cases that compile a program to C and run it.
+
+    The output (stdout) of the program is compared to expected output.
+    """
+
+    # Test case descriptions
+    files = ['cgen-basic.test',
+             'cgen-intops.test',
+             'cgen-classes.test']
+
+    def cases(self):
+        c = []
+        for f in self.files:
             c += parse_test_cases(os.path.join(test_data_prefix, f),
                                   test_cgen, test_temp_dir, True)
         return c
@@ -60,6 +96,13 @@ def test_cgen(testcase):
     assert_string_arrays_equal(testcase.output, out,
                                'Invalid output ({}, line {})'.format(
                                    testcase.file, testcase.line))
+
+
+class CGenSuite(Suite):
+    def __init__(self):
+        self.test_compile = CGenCompileSuite()
+        self.test_run = CGenRunSuite()
+        super().__init__()
 
 
 if __name__ == '__main__':
