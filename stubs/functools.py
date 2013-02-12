c0db8fb522cf620f2fc41c53d1361d9c72a89373
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-12 12:53:58 +0000

    Add minimal functools stubs

---
+++ stubs/functools.py
@@ -0,0 +1,10 @@
+# Stubs for functools
+
+# NOTE: These are incomplete!
+
+# TODO implement as class; more precise typing
+# TODO cache_info and __wrapped__ attributes
+# TODO None valid as value for maxsize
+func<any(any)> lru_cache(int maxsize=100): pass
+
+
