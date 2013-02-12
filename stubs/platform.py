b48b0774f9d23074f8c7816312715eb697231fd7
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-12 13:25:27 +0000

    Add mininal platform module stubs

---
+++ stubs/platform.py
@@ -0,0 +1,8 @@
+# Stubs for platform
+
+# NOTE: These are incomplete!
+
+tuple<str, tuple<str, str, str>, str> \
+           mac_ver(str release='',
+                   tuple<str, str, str> version_info=('', '', ''),
+                   str machine=''): pass
