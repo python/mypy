37ad0be8a879eddf025b9dfbbdb5f200b9d276e4
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-12 14:00:17 +0000

    Add minimal doctest stubs

---
+++ stubs/doctest.py
@@ -0,0 +1,8 @@
+# Stubs for doctest
+
+# NOTE: These are incomplete!
+
+# TODO arguments missing
+# TODO bool with None default
+tuple<int, int> testmod(any module=None, *, str name=None, any globs=None,
+                        bool verbose=None): pass
