1ac04a699535b610d7ae13f4a51c66bf547e4913
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-18 17:00:11 +0000

    Add minimal stubs for _random

---
+++ stubs/_random.py
@@ -0,0 +1,10 @@
+# Stubs for _random
+
+# NOTE: These are incomplete!
+
+class Random:
+    void seed(self, int x=None): pass
+    tuple getstate(self): pass
+    void setstate(self, tuple state): pass
+    float random(self): pass
+    int getrandbits(self, int k): pass
