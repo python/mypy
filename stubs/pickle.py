94b1c7b2bfabee8f87ce4d38c4d7371dd92b3975
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-18 13:03:30 +0000

    Add minimal pickle stubs

---
+++ stubs/pickle.py
@@ -0,0 +1,7 @@
+# Stubs for pickle
+
+# NOTE: These are incomplete!
+
+bytes dumps(any obj, int protocol=None, *, bool fix_imports=True): pass
+any loads(bytes p, *, bool fix_imports=True, str encoding='ASCII',
+          str errors='strict'): pass
