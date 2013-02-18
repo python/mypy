65f9bd295fd77532965d4e9b8f2a51e2b38c3045
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-18 13:03:12 +0000

    Add minimal weakref stub

---
+++ stubs/weakref.py
@@ -0,0 +1,13 @@
+# Stubs for weakref
+
+# NOTE: These are incomplete!
+
+class ReferenceType<t>:
+    # TODO members
+    pass
+
+# TODO callback
+ReferenceType<t> ref<t>(t o): pass
+
+# TODO callback
+t proxy<t>(t object): pass
