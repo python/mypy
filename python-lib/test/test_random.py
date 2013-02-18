d40637b26aa901821978e98e73053faa6c9c2eb1
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-18 17:15:29 +0000

    test_random: Refactor dict call with kw argument

--- python-lib/test/test_random.py
+++ python-lib/test/test_random.py
@@ -37,7 +37,7 @@ class TestBasicOps(unittest.TestCase):
         for arg in [None, 0, 0, 1, 1, -1, -1, 10**20, -(10**20),
                     3.14, complex(1., 2.), 'a', tuple('abc')]:
             self.gen.seed(arg)
-        for arg in [list(range(3)), dict(one=1)]:
+        for arg in [list(range(3)), {'one': 1}]:
             self.assertRaises(TypeError, self.gen.seed, arg)
         self.assertRaises(TypeError, self.gen.seed, 1, 2, 3, 4)
         self.assertRaises(TypeError, type(self.gen), [])
