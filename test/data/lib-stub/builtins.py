064a38ec357eef3f3626808c9db9ed8a17434355
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-20 09:32:53 +0000

    Add int and str to default test stubs

--- test/data/lib-stub/builtins.py
+++ test/data/lib-stub/builtins.py
@@ -4,5 +4,9 @@ class object:
 class type:
     def __init__(self, x): pass
 
+# These aren't really required, but provided here for convenience.
+class int: pass
+class str: pass
+
 # Definition of None is implicit
 
