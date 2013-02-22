cb7591314f19ea6d6e7724f8ca1e16daf22b4485
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-22 12:16:38 +0000

    Refactor glob stubs

--- stubs/glob.py
+++ stubs/glob.py
@@ -2,8 +2,8 @@
 
 # Based on http://docs.python.org/3.2/library/glob.html
 
-list<str> glob(str pathname): pass
-list<bytes> glob(bytes pathname): pass
+str[] glob(str pathname): pass
+bytes[] glob(bytes pathname): pass
 Iterator<str> iglob(str pathname): pass
 Iterator<bytes> iglob(bytes pathname): pass
 
