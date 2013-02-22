e27d106318f3e3e2778b4b5a2d94e09eb3ed1916
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-22 12:26:54 +0000

    Refactor getopt stubs

--- stubs/getopt.py
+++ stubs/getopt.py
@@ -2,11 +2,11 @@
 
 # Based on http://docs.python.org/3.2/library/getopt.html
 
-tuple<list<tuple<str, str>>, str[]> \
+tuple<tuple<str, str>[], str[]> \
                       getopt(str[] args, str shortopts,
                              str[] longopts): pass
 
-tuple<list<tuple<str, str>>, str[]> \
+tuple<tuple<str, str>[], str[]> \
                       gnu_getopt(str[] args, str shortopts,
                                  str[] longopts): pass
 
