1e203a79273baa6e7a95612bd88dee4c33faece4
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-22 09:31:36 +0000

    Add minimal distutils stubs

---
+++ stubs/distutils/errors.py
@@ -0,0 +1,2 @@
+class DistutilsError(Exception): pass
+class DistutilsExecError(DistutilsError): pass
---
+++ stubs/distutils/spawn.py
@@ -0,0 +1,3 @@
+# In Python, arguments have integer default values
+void spawn(str[] cmd, bool search_path=True, bool verbose=False,
+           bool dry_run=False): pass
