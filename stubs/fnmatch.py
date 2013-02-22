af2e5f38c849a2f15067e94e876fcfd414d0da18
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-22 12:02:32 +0000

    Update argument names in fnmatch stubs
    Make the argument names consistent with the module implementation
    instead of the Python library reference.

--- stubs/fnmatch.py
+++ stubs/fnmatch.py
@@ -1,11 +1,12 @@
 # Stubs for fnmatch
 
-# Based on http://docs.python.org/3.2/library/fnmatch.html
+# Based on http://docs.python.org/3.2/library/fnmatch.html and
+# python-lib/fnmatch.py
 
-bool fnmatch(str filename, str pattern): pass
-bool fnmatch(bytes filename, bytes pattern): pass
-bool fnmatchcase(str filename, str pattern): pass
-bool fnmatchcase(bytes filename, bytes pattern): pass
-str[] filter(Iterable<str> names, str pattern): pass
-bytes[] filter(Iterable<bytes> names, bytes pattern): pass
-str translate(str pattern): pass
+bool fnmatch(str name, str pat): pass
+bool fnmatch(bytes name, bytes pat): pass
+bool fnmatchcase(str name, str pat): pass
+bool fnmatchcase(bytes name, bytes pat): pass
+str[] filter(Iterable<str> names, str pat): pass
+bytes[] filter(Iterable<bytes> names, bytes pat): pass
+str translate(str pat): pass
