00239802a32ba244516b1623cb6ce3e37fc033f4
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-22 11:56:05 +0000

    Supports 'bytes' in fnmatch stubs

--- stubs/fnmatch.py
+++ stubs/fnmatch.py
@@ -3,6 +3,9 @@
 # Based on http://docs.python.org/3.2/library/fnmatch.html
 
 bool fnmatch(str filename, str pattern): pass
+bool fnmatch(bytes filename, bytes pattern): pass
 bool fnmatchcase(str filename, str pattern): pass
+bool fnmatchcase(bytes filename, bytes pattern): pass
 str[] filter(Iterable<str> names, str pattern): pass
+bytes[] filter(Iterable<bytes> names, bytes pattern): pass
 str translate(str pattern): pass
