8deb47fc43cc6b4fffc59ef339c30ec342bd3171
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-22 10:14:33 +0000

    Update distutils.spawn stubs

--- stubs/distutils/spawn.py
+++ stubs/distutils/spawn.py
@@ -1,3 +1,4 @@
 # In Python, arguments have integer default values
 void spawn(str[] cmd, bool search_path=True, bool verbose=False,
            bool dry_run=False): pass
+str find_executable(str executable, str path=None): pass
