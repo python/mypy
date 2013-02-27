8e8149022a0f603b1dcc7522ecf0c1c7cd6ea517
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-27 17:32:04 +0000

    Update _posixsubprocess stubs

--- stubs/_posixsubprocess.py
+++ stubs/_posixsubprocess.py
@@ -3,3 +3,8 @@
 # NOTE: These are incomplete!
 
 tuple<int, int> cloexec_pipe(): pass
+int fork_exec(Sequence<str> args,
+              executable_list, close_fds, fds_to_keep, str cwd, env_list,
+              int p2cread, int p2cwrite, int c2pred, int c2pwrite, int errread,
+              int errwrite, int errpipe_read, int errpipe_write,
+              restore_signals, start_new_session, preexec_fn): pass
