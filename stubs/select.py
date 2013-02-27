10f86bebc003c1b206aff9ade84fdd398617893a
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-02-27 14:23:23 +0000

    Improve select module stubs

--- stubs/select.py
+++ stubs/select.py
@@ -1,3 +1,21 @@
 # Stubs for select
 
 # NOTE: These are incomplete!
+
+class error(Exception): pass
+
+int POLLIN
+int POLLPRI
+int POLLOUT
+int POLLERR
+int POLLHUP
+int POLLNVAL
+
+class poll:
+    void __init__(self): pass
+    void register(self, any fd, int eventmask=POLLIN|POLLPRI|POLLOUT): pass
+    void modify(self, any fd, int eventmask): pass
+    tuple<int, int>[] poll(self, int timeout=None): pass
+
+tuple<int[], int[], int[]> select(Sequence rlist, Sequence wlist,
+                                  Sequence xlist, float timeout=None): pass
