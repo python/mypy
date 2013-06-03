9db95c7ca592bcb9d066e60e0d6a456e5b1170b3
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-06-03 13:20:51 +0100

    Add skeleton calendar stub

---
+++ stubs/calendar.py
@@ -0,0 +1,9 @@
+# Stubs for calendar
+
+# NOTE: These are incomplete!
+
+# TODO actually, any number of items larger than 5 is fine
+int timegm(tuple<int, int, int, int, int, int> t): pass
+int timegm(tuple<int, int, int, int, int, int, int> t): pass
+int timegm(tuple<int, int, int, int, int, int, int, int> t): pass
+int timegm(tuple<int, int, int, int, int, int, int, int, int> t): pass
