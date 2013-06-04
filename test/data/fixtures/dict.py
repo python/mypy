799e1d8dee678d6945b31b1e43d62541ccf9912d
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-06-04 13:40:03 +0100

    Add type alias test case

--- test/data/fixtures/dict.py
+++ test/data/fixtures/dict.py
@@ -1,10 +1,16 @@
 # Builtins stub used in dictionary-related test cases.
 
+from typing import typevar, Generic
+
+T = typevar('T')
+S = typevar('S')
+
 class object:
-    def __init__(self): pass
+    def __init__(self) -> None: pass
 
 class type: pass
 
-class dict<kt, vt>: pass
+class dict(Generic[T, S]): pass
+class int: pass # for convenience
 class str: pass # for keyword argument key type
-class list<t>: pass # needed by some test cases
+class list(Generic[T]): pass # needed by some test cases
