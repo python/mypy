7169b0993e14c8ef3aae7235e06dd6a805043044
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-06-07 12:26:39 +0100

    Disable unsupported test cases

--- tests.py
+++ tests.py
@@ -33,11 +33,11 @@ class AllSuite(Suite):
         self.test_semanal_symtable = testsemanal.SemAnalSymtableSuite()
         self.test_semanal_typeinfos = testsemanal.SemAnalTypeInfoSuite()
         self.test_check = testcheck.TypeCheckSuite()
-        self.test_typegen = testtypegen.TypeExportSuite()
-        self.test_transform = testtransform.DyncheckTransformSuite()
-        self.test_icodegen = testicodegen.IcodeGenerationSuite()
-        self.test_output = testoutput.OutputSuite()
-        self.test_pythongen = testpythongen.PythonGenerationSuite()
+        #self.test_typegen = testtypegen.TypeExportSuite()
+        #self.test_transform = testtransform.DyncheckTransformSuite()
+        #self.test_icodegen = testicodegen.IcodeGenerationSuite()
+        #self.test_output = testoutput.OutputSuite()
+        #self.test_pythongen = testpythongen.PythonGenerationSuite()
         super().__init__()
 
 
