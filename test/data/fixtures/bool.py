d4d85793020198079072ed0556650a48f0445b59
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-06-12 16:15:55 +0100

    Update bool operation test cases

--- test/data/fixtures/bool.py
+++ test/data/fixtures/bool.py
@@ -1,7 +1,10 @@
 # builtins stub used in boolean-related test cases.
 
-class object: pass
+class object:
+    def __init__(self) -> None: pass
 
 class type: pass
 
 class bool: pass
+
+class int: pass
