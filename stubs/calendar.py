154e64f8a21a5f08846271e36c66c641ed76c664
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-06-19 14:31:56 +0100

    Translate calendar stubs to new syntax

--- stubs/calendar.py
+++ stubs/calendar.py
@@ -2,8 +2,14 @@
 
 # NOTE: These are incomplete!
 
+from typing import overload, Tuple
+
 # TODO actually, any number of items larger than 5 is fine
-int timegm(tuple<int, int, int, int, int, int> t): pass
-int timegm(tuple<int, int, int, int, int, int, int> t): pass
-int timegm(tuple<int, int, int, int, int, int, int, int> t): pass
-int timegm(tuple<int, int, int, int, int, int, int, int, int> t): pass
+@overload
+def timegm(t: Tuple[int, int, int, int, int, int]) -> int: pass
+@overload
+def timegm(t: Tuple[int, int, int, int, int, int, int]) -> int: pass
+@overload
+def timegm(t: Tuple[int, int, int, int, int, int, int, int]) -> int: pass
+@overload
+def timegm(t: Tuple[int, int, int, int, int, int, int, int, int]) -> int: pass
