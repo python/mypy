8b6a846f6912b50bb6c8126f220ca90937d813dc
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-06-19 16:34:02 +0100

    Translate weakref stubs to new syntax

--- stubs/weakref.py
+++ stubs/weakref.py
@@ -2,44 +2,69 @@
 
 # NOTE: These are incomplete!
 
-class ReferenceType<t>:
+from typing import (
+    typevar, Generic, Any, Function, overload, Mapping, Iterator, Dict, Tuple,
+    Iterable
+)
+
+T = typevar('T')
+KT = typevar('KT')
+VT = typevar('VT')
+
+class ReferenceType(Generic[T]):
     # TODO members
     pass
 
-ReferenceType<t> ref<t>(t o,
-                        func<any(ReferenceType<t>)> callback=None): pass
+def ref(o: T, callback: Function[[ReferenceType[T]],
+                                 Any] = None) -> ReferenceType[T]: pass
 
 # TODO callback
-t proxy<t>(t object): pass
+def proxy(object: T) -> T: pass
 
-class WeakValueDictionary<kt, vt>:
+class WeakValueDictionary(Generic[KT, VT]):
     # TODO tuple iterable argument?
-    void __init__(self): pass
-    void __init__(self, Mapping<kt, vt> map): pass
+    @overload
+    def __init__(self) -> None: pass
+    @overload
+    def __init__(self, map: Mapping[KT, VT]) -> None: pass
     
-    int __len__(self): pass    
-    vt __getitem__(self, kt k): pass
-    void __setitem__(self, kt k, vt v): pass
-    void __delitem__(self, kt v): pass
-    bool __contains__(self, object o): pass
-    Iterator<kt> __iter__(self): pass
-    str __str__(self): pass
+    def __len__(self) -> int: pass    
+    def __getitem__(self, k: KT) -> VT: pass
+    def __setitem__(self, k: KT, v: VT) -> None: pass
+    def __delitem__(self, v: KT) -> None: pass
+    def __contains__(self, o: object) -> bool: pass
+    def __iter__(self) -> Iterator[KT]: pass
+    def __str__(self) -> str: pass
     
-    void clear(self): pass
-    dict<kt, vt> copy(self): pass
-    vt get(self, kt k): pass
-    vt get(self, kt k, vt default): pass
-    vt pop(self, kt k): pass
-    vt pop(self, kt k, vt default): pass
-    tuple<kt, vt> popitem(self): pass
-    vt setdefault(self, kt k): pass
-    vt setdefault(self, kt k, vt default): pass    
-    void update(self, Mapping<kt, vt> m): pass
-    void update(self, Iterable<tuple<kt, vt>> m): pass
+    def clear(self) -> None: pass
+    def copy(self) -> Dict[KT, VT]: pass
+    
+    @overload
+    def get(self, k: KT) -> VT: pass
+    @overload
+    def get(self, k: KT, default: VT) -> VT: pass
+    
+    @overload
+    def pop(self, k: KT) -> VT: pass
+    @overload
+    def pop(self, k: KT, default: VT) -> VT: pass
+    
+    def popitem(self) -> Tuple[KT, VT]: pass
+    
+    @overload
+    def setdefault(self, k: KT) -> VT: pass
+    @overload
+    def setdefault(self, k: KT, default: VT) -> VT: pass
+    
+    @overload
+    def update(self, m: Mapping[KT, VT]) -> None: pass
+    @overload
+    def update(self, m: Iterable[Tuple[KT, VT]]) -> None: pass
+    
     # NOTE: incompatible with Mapping
-    Iterator<kt> keys(self): pass
-    Iterator<vt> values(self): pass
-    Iterator<tuple<kt, vt>> items(self): pass
+    def keys(self) -> Iterator[KT]: pass
+    def values(self) -> Iterator[VT]: pass
+    def items(self) -> Iterator[Tuple[KT, VT]]: pass
 
     # TODO return type
-    Iterable<any> valuerefs(self): pass
+    def valuerefs(self) -> Iterable[Any]: pass
