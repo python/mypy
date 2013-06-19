c9a175c70dd1890d780f8517fbc0be506f1fcd96
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-06-19 16:09:53 +0100

    Translate string stubs to new syntax

--- stubs/string.py
+++ stubs/string.py
@@ -2,23 +2,26 @@
 
 # Based on http://docs.python.org/3.2/library/string.html
 
-str ascii_letters
-str ascii_lowercase
-str ascii_uppercase
-str digits
-str hexdigits
-str octdigits
-str punctuation
-str printable
-str whitespace
+from typing import Mapping
 
-str capwords(str s, str sep=None): pass
+ascii_letters = ''
+ascii_lowercase = ''
+ascii_uppercase = ''
+digits = ''
+hexdigits = ''
+octdigits = ''
+punctuation = ''
+printable = ''
+whitespace = ''
+
+def capwords(s: str, sep: str = None) -> str: pass
 
 class Template:
-    str template
+    template = ''
     
-    void __init__(self, str template): pass
-    str substitute(self, Mapping<str, str> mapping, str **kwds): pass
-    str safe_substitute(self, Mapping<str, str> mapping, str **kwds): pass
+    def __init__(self, template: str) -> None: pass
+    def substitute(self, mapping: Mapping[str, str], **kwds: str) -> str: pass
+    def safe_substitute(self, mapping: Mapping[str, str],
+                        **kwds: str) -> str: pass
 
 # TODO Formatter
