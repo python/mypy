5e1b3ce5fcf4c93c23f3368aeaf575f0b9bdf98f
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-07-12 17:36:28 +0100

    Add staticmethod test case

--- mypy/test/data/fixtures/staticmethod.py
+++ mypy/test/data/fixtures/staticmethod.py
@@ -1,10 +1,16 @@
+import typing
+
 class object:
     def __init__(self) -> None: pass
 
 class type:
     def __init__(self, x) -> None: pass
 
-class int: pass
-class str: pass
-
 staticmethod = object() # Dummy definition.
+
+class int:
+    @staticmethod
+    def from_bytes(bytes: bytes, byteorder: str) -> int: pass
+
+class str: pass
+class bytes: pass
