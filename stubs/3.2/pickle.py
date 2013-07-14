aa4825caf4f9811725739168a64b85917a717868
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-07-14 12:51:57 +0100

    Add pickle.load

--- stubs/3.2/pickle.py
+++ stubs/3.2/pickle.py
@@ -2,9 +2,11 @@
 
 # NOTE: These are incomplete!
 
-from typing import Any
+from typing import Any, IO
 
 def dumps(obj: Any, protocol: int = None, *,
           fix_imports: bool = True) -> bytes: pass
 def loads(p: bytes, *, fix_imports: bool = True,
           encoding: str = 'ASCII', errors: str = 'strict') -> Any: pass
+def load(file: IO, *, fix_imports: bool = True, encoding: str = 'ASCII',
+         errors: str = 'strict') -> Any: pass
