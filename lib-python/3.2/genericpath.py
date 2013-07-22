49acaf2b7cf6af8c58aa03cf0483a734807cd659
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-07-22 15:05:08 +0100

    genericpath: Use AnyStr

--- lib-python/3.2/genericpath.py
+++ lib-python/3.2/genericpath.py
@@ -6,7 +6,7 @@ functions from this module themselves.
 import os
 import stat
 
-from typing import overload as overload_, Any, List
+from typing import overload as overload_, Any, List, AnyStr, Tuple as Tuple_
 
 __all__ = ['commonprefix', 'exists', 'getatime', 'getctime', 'getmtime',
            'getsize', 'isdir', 'isfile']
@@ -14,26 +14,18 @@ __all__ = ['commonprefix', 'exists', 'getatime', 'getctime', 'getmtime',
 
 # Does a path exist?
 # This is false for dangling symbolic links on systems that support them.
-def _exists(path):
+def exists(path: AnyStr) -> bool:
+    """Test whether a path exists.  Returns False for broken symbolic links"""
     try:
         os.stat(path)
     except os.error:
         return False
     return True
 
-@overload_
-def exists(path: str) -> bool:
-    """Test whether a path exists.  Returns False for broken symbolic links"""
-    return _exists(path)
-
-@overload_
-def exists(path: bytes) -> bool:
-    return _exists(path)
-
 
 # This follows symbolic links, so both islink() and isdir() can be true
 # for the same path ono systems that support symlinks
-def _isfile(path):
+def isfile(path: AnyStr) -> bool:
     """Test whether a path is a regular file"""
     try:
         st = os.stat(path)
@@ -41,78 +33,42 @@ def _isfile(path):
         return False
     return stat.S_ISREG(st.st_mode)
 
-@overload_
-def isfile(path: str) -> bool:
-    """Test whether a path is a regular file"""
-    return _isfile(path)
-
-@overload_
-def isfile(path: bytes) -> bool:
-    return _isfile(path)
-
 
 # Is a path a directory?
 # This follows symbolic links, so both islink() and isdir()
 # can be true for the same path on systems that support symlinks
-def _isdir(s):
+def isdir(s: AnyStr) -> bool:
+    """Return true if the pathname refers to an existing directory."""
     try:
         st = os.stat(s)
     except os.error:
         return False
     return stat.S_ISDIR(st.st_mode)
 
-@overload_
-def isdir(s: str) -> bool:
-    """Return true if the pathname refers to an existing directory."""
-    return _isdir(s)
 
-@overload_
-def isdir(s: bytes) -> bool:
-    return _isdir(s)
-
-
-@overload_
-def getsize(filename: str) -> int:
+def getsize(filename: AnyStr) -> int:
     """Return the size of a file, reported by os.stat()."""
     return os.stat(filename).st_size
 
-@overload_
-def getsize(filename: bytes) -> int:
-    return os.stat(filename).st_size
 
-
-@overload_
-def getmtime(filename: str) -> Any:
+def getmtime(filename: AnyStr) -> float:
     """Return the last modification time of a file, reported by os.stat()."""
     return os.stat(filename).st_mtime
 
-@overload_
-def getmtime(filename: bytes) -> Any:
-    return os.stat(filename).st_mtime
 
-
-@overload_
-def getatime(filename: str) -> Any:
+def getatime(filename: AnyStr) -> float:
     """Return the last access time of a file, reported by os.stat()."""
     return os.stat(filename).st_atime
 
-@overload_
-def getatime(filename: bytes) -> Any:
-    return os.stat(filename).st_atime
 
-
-@overload_
-def getctime(filename: str) -> Any:
+def getctime(filename: AnyStr) -> float:
     """Return the metadata change time of a file, reported by os.stat()."""
-    return os.stat(filename).st_ctime
-
-@overload_
-def getctime(filename: bytes) -> Any:
     return os.stat(filename).st_ctime
 
 
 # Return the longest prefix of all list elements.
-def _commonprefix(m):
+def commonprefix(m: List[str]) -> str:
+    "Given a list of pathnames, returns the longest common leading component"
     if not m: return ''
     s1 = min(m)
     s2 = max(m)
@@ -120,15 +76,6 @@ def _commonprefix(m):
         if c != s2[i]:
             return s1[:i]
     return s1
-
-@overload_
-def commonprefix(m: List[str]) -> str:
-    "Given a list of pathnames, returns the longest common leading component"
-    return _commonprefix(m)
-
-@overload_
-def commonprefix(m: List[bytes]) -> bytes:
-    return _commonprefix(m)
 
 
 # Split a path in root and extension.
@@ -138,7 +85,8 @@ def commonprefix(m: List[bytes]) -> bytes:
 
 # Generic implementation of splitext, to be parametrized with
 # the separators
-def _splitext(p, sep, altsep, extsep):
+def _splitext(p: AnyStr, sep: AnyStr, altsep: AnyStr,
+              extsep: AnyStr) -> Tuple_[AnyStr, AnyStr]:
     """Split the extension from a pathname.
 
     Extension is everything from the last dot to the end, ignoring
