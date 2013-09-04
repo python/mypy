e57a7b4c9d6cfa3497657d1dc7e674a9566a8319
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-09-04 18:59:26 +0100

    Improve pwd stubs

--- stubs/3.2/pwd.py
+++ stubs/3.2/pwd.py
@@ -4,5 +4,15 @@
 
 from typing import Any
 
-def getpwuid(uid: int) -> Any: pass
-def getpwnam(name: str) -> Any: pass
+class struct_passwd:
+    # TODO use namedtuple
+    pw_name = ''
+    pw_passwd = ''
+    pw_uid = 0
+    pw_gid = 0
+    pw_gecos = ''
+    pw_dir = ''
+    pw_shell = ''
+
+def getpwuid(uid: int) -> struct_passwd: pass
+def getpwnam(name: str) -> struct_passwd: pass
