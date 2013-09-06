093d5292c012f375d7b73a1462cf0932f637fd53
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-09-06 12:48:40 +0100

    Add proper subtype test cases

--- mypy/test/testtypes.py
+++ mypy/test/testtypes.py
@@ -210,6 +210,8 @@ class TypeOpsSuite(Suite):
         
         assert_true(is_proper_subtype(fx.ga, fx.ga))
         assert_true(is_proper_subtype(fx.gdyn, fx.gdyn))
+        assert_true(is_proper_subtype(fx.gsab, fx.gb))
+        assert_false(is_proper_subtype(fx.gsab, fx.ga))
         assert_false(is_proper_subtype(fx.gb, fx.ga))
         assert_false(is_proper_subtype(fx.ga, fx.gdyn))
         assert_false(is_proper_subtype(fx.gdyn, fx.ga))
