97a97090804882c13a52f510b87cd8673818bcf3
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2013-09-06 14:27:50 +0100

    Minor refactoring

--- mypy/checker.py
+++ mypy/checker.py
@@ -503,34 +503,39 @@ class TypeChecker(NodeVisitor[Type]):
         if base:
             if defn.name() != '__init__':
                 # Check method override (__init__ is special).
-                base_attr = base.names.get(defn.name())
-                if base_attr:
-                    # The name of the method is defined in the base class.
-                    
-                    # Construct the type of the overriding method.
-                    typ = method_type(defn)
-                    # Map the overridden method type to subtype context so that
-                    # it can be checked for compatibility.
-                    original_type = base_attr.type
-                    if original_type is None and isinstance(base_attr.node,
-                                                            FuncDef):
-                        original_type = function_type(cast(FuncDef,
-                                                           base_attr.node))
-                    if isinstance(original_type, FunctionLike):
-                        original = map_type_from_supertype(
-                            method_type(original_type),
-                            defn.info, base)
-                        # Check that the types are compatible.
-                        # TODO overloaded signatures
-                        self.check_override(cast(FunctionLike, typ),
-                                            cast(FunctionLike, original),
-                                            defn.name(),
-                                            base.name(),
-                                            defn)
-                    else:
-                        assert original_type is not None
-                        self.msg.signature_incompatible_with_supertype(
-                            defn.name(), base.name(), defn)
+                self.check_method_override_for_base_with_name(
+                    defn, defn.name(), base)
+
+    def check_method_override_for_base_with_name(
+            self, defn: FuncBase, name: str, base: TypeInfo) -> None:
+        base_attr = base.names.get(name)
+        if base_attr:
+            # The name of the method is defined in the base class.
+
+            # Construct the type of the overriding method.
+            typ = method_type(defn)
+            # Map the overridden method type to subtype context so that
+            # it can be checked for compatibility.
+            original_type = base_attr.type
+            if original_type is None and isinstance(base_attr.node,
+                                                    FuncDef):
+                original_type = function_type(cast(FuncDef,
+                                                   base_attr.node))
+            if isinstance(original_type, FunctionLike):
+                original = map_type_from_supertype(
+                    method_type(original_type),
+                    defn.info, base)
+                # Check that the types are compatible.
+                # TODO overloaded signatures
+                self.check_override(cast(FunctionLike, typ),
+                                    cast(FunctionLike, original),
+                                    defn.name(),
+                                    base.name(),
+                                    defn)
+            else:
+                assert original_type is not None
+                self.msg.signature_incompatible_with_supertype(
+                    defn.name(), base.name(), defn)
     
     def check_override(self, override: FunctionLike, original: FunctionLike,
                        name: str, supertype: str, node: Context) -> None:
