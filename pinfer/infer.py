26783faa0a444ddeca55d4e5c8748ac4b6e80b34
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2014-07-12 16:13:43 -0700

    Add initial code

---
+++ pinfer/infer.py
@@ -0,0 +1,404 @@
+"""Tools for runtime type inference"""
+
+import inspect
+
+
+MAX_INFERRED_TUPLE_LENGTH = 10
+PREFERRED_LINE_LENGTH = 79
+
+
+var_db = {} # (location, variable) -> type
+func_argid_db = {} # funcname -> set of (argindex, name)
+func_arg_db = {} # (funcname, argindex/name) -> type
+func_return_db = {} # funcname -> type
+
+
+def reset():
+    global var_db, func_argid_db, func_arg_db, func_return_db
+    var_db = {}
+    func_argid_db = {}
+    func_arg_db = {}
+    func_return_db = {}
+
+
+def format_state(pretty=False):
+    lines = []
+    for loc, var in sorted(var_db.keys()):
+        lines.append('%s: %s' % (var, var_db[(loc, var)]))
+    funcnames = sorted(set(name for name, arg in func_arg_db))
+    prevclass = ''
+    indent = ''
+    for name in funcnames:
+        if '.' in name:
+            curclass, shortname = name.split('.', 1)
+        else:
+            curclass, shortname = '', name
+        if curclass != prevclass:
+            if curclass:
+                lines.append('class %s(...):' % curclass)
+                indent = ' ' * 4
+            else:
+                indent = ''
+            prevclass = curclass
+        args = []
+        kwargs = []
+        # Sort argid set by index.
+        argids = sorted(func_argid_db[name], key=lambda x: x[0])
+        for i, arg in argids:
+            if i == 0 and arg == 'self':
+                # Omit type of self argument.
+                t = ''
+            else:
+                t = ': %s' % func_arg_db[(name, i)]
+            argstr = '%s%s' % (arg, t)
+            if i >= 0:
+                args.append(argstr)
+            else:
+                kwargs.append(argstr)
+        ret = str(func_return_db.get(name, Unknown()))
+        sig = 'def %s(%s) -> %s' % (shortname, ', '.join(args + kwargs), ret)
+        if not pretty or len(sig) <= PREFERRED_LINE_LENGTH or not args:
+            lines.append(indent + sig)
+        else:
+            # Format into multiple lines to conserve horizontal space.
+            first = 'def %s(' % shortname
+            if args[0] == 'self':
+                first += 'self,'
+                args = args[1:]
+            lines.append(indent + first)
+            for arg in args:
+                lines.append(indent + ' ' * 8 + '%s,' % arg)
+            if len(lines[-1]) + 4 + len(ret) <= PREFERRED_LINE_LENGTH:
+                lines[-1] = lines[-1][:-1] + ') -> %s' % ret
+            else:
+                lines.append(indent + ' ' * 8 + ') -> %s' % ret)
+    return '\n'.join(lines)
+
+
+def dump():
+    s = format_state(pretty=True)
+    if s:
+        print()
+        print('INFERRED TYPES:')
+        print(s)
+    reset()
+
+
+def dump_at_exit():
+    import atexit
+    atexit.register(dump)
+
+
+def infer_var(name, value):
+    key = (None, name)
+    update_var_db(key, value)
+
+
+def infer_attrs(x):
+    cls = type(x).__name__
+    typedict = type(x).__dict__
+    for dict in x.__dict__, typedict:
+        for attr, value in dict.items():
+            if attr in ('__dict__', '__doc__', '__module__', '__weakref__'):
+                continue
+            if type(value) is type(infer_attrs) and dict is typedict:
+                # Skip methods.
+                continue
+            key = (None, '%s.%s' % (cls, attr))
+            update_var_db(key, value)
+
+
+def infer_signature(func):
+    """Decorator that infers the signature of a function."""
+    return infer_method_signature('')(func)
+
+
+def infer_method_signature(class_name):
+    """Construct a function decorator that infer the signature of a function.
+    """
+    def outer_wrapper(func):
+        name = func.__name__
+        if class_name:
+            name = '%s.%s' % (class_name, name)
+        try:
+            argspec = inspect.getfullargspec(func)
+        except TypeError:
+            # Not supported.
+            return func
+        argnames = argspec.args
+        varargs = argspec.varargs
+        varkw = argspec.varkw
+        defaults = argspec.defaults
+        kwonlyargs = argspec.kwonlyargs
+        min_arg_count = len(argnames)
+        if defaults:
+            min_arg_count -= len(defaults)
+
+        def wrapper(*args, **kwargs):
+            argids = func_argid_db.setdefault(name, set())
+            used_kwargs = set()
+            for i, arg in enumerate(argnames):
+                if i < len(args):
+                    argvalue = args[i]
+                elif i >= min_arg_count and not arg in kwargs:
+                    argvalue = defaults[i - min_arg_count]
+                else:
+                    argvalue = kwargs[arg]
+                    used_kwargs.add(arg)
+                key = (name, i)
+                update_db(func_arg_db, key, argvalue)
+                argids.add((i, arg))
+            if varargs:
+                for i in range(len(argnames), len(args)):
+                    key = (name, len(argnames))
+                    update_db(func_arg_db, key, args[i])
+                    argids.add((len(argnames), varargs))
+            if kwonlyargs:
+                used_kwonlyargs = set()
+                for arg, value in kwargs.items():
+                    if arg in kwonlyargs:
+                        index = len(argnames) + kwonlyargs.index(arg)
+                        key = (name, index)
+                        update_db(func_arg_db, key, value)
+                        argids.add((index, arg))
+                        used_kwargs.add(arg)
+                        used_kwonlyargs.add(arg)
+                for i, arg in enumerate(kwonlyargs):
+                    if arg not in used_kwonlyargs:
+                        index = len(argnames) + i
+                        key = (name, index)
+                        update_db(func_arg_db, key,
+                                  argspec.kwonlydefaults[arg])
+                        argids.add((index, arg))
+            if kwargs:
+                for arg, value in kwargs.items():
+                    if arg not in used_kwargs:
+                        key = (name, -1)
+                        update_db(func_arg_db, key, value)
+                        argids.add((-1, varkw))
+            ret = func(*args, **kwargs)
+            update_db(func_return_db, name, ret)
+            return ret
+
+        if hasattr(func, '__name__'):
+            wrapper.__name__ = func.__name__
+        return wrapper
+    return outer_wrapper
+
+
+def infer_class(cls):
+    """Class decorator for inferring signatures of all methods of the class."""
+    for attr, value in cls.__dict__.items():
+        if type(value) is type(infer_class):
+            setattr(cls, attr, infer_method_signature(cls.__name__)(value))
+    return cls
+
+
+def infer_module(namespace):
+    if hasattr(namespace, '__dict__'):
+        namespace = namespace.__dict__
+    for name, value in list(namespace.items()):
+        if inspect.isfunction(value):
+            namespace[name] = infer_signature(value)
+        elif inspect.isclass(value):
+            namespace[name] = infer_class(value)
+
+
+def update_var_db(key, value):
+    update_db(var_db, key, value)
+
+
+def update_db(db, key, value):
+    type = infer_value_type(value)
+    if key not in db:
+        db[key] = type
+    else:
+        db[key] = combine_types(db[key], type)
+
+
+def infer_value_type(value):
+    if value is None:
+        return None
+    elif isinstance(value, list):
+        return Generic('List', [infer_value_types(value)])
+    elif isinstance(value, dict):
+        keytype = infer_value_types(value.keys())
+        valuetype = infer_value_types(value.values())
+        return Generic('Dict', (keytype, valuetype))
+    elif isinstance(value, tuple):
+        if len(value) <= MAX_INFERRED_TUPLE_LENGTH:
+            return Tuple(infer_value_type(item)
+                         for item in value)
+        else:
+            return Generic('TupleSequence', [infer_value_types(value)])
+    elif isinstance(value, set):
+        return Generic('Set', [infer_value_types(value)])
+    else:
+        return Instance(type(value))
+
+
+def infer_value_types(values):
+    """Infer a single type for an iterable of values.
+
+    >>> infer_value_types((1, 'x'))
+    Either(int, str)
+    >>> infer_value_types([])
+    Unknown
+    """
+    inferred = Unknown()
+    for value in sample(values):
+        type = infer_value_type(value)
+        inferred = combine_types(inferred, type)
+    return inferred
+
+
+def sample(values):
+    # TODO only return a sample of values
+    return list(values)
+
+
+def combine_types(x, y):
+    """Perform a union of two types.
+
+    >>> combine_types(Instance(int), None)
+    Optional(int)
+    """
+    if isinstance(x, Unknown):
+        return y
+    if isinstance(y, Unknown):
+        return x
+    if isinstance(x, Either):
+        return combine_either(x, y)
+    if isinstance(y, Either):
+        return combine_either(y, x)
+    if x == y:
+        return x
+    return simplify_either([x], [y])
+
+
+def combine_either(either, x):
+    if isinstance(x, Either):
+        xtypes = x.types
+    else:
+        xtypes = [x]
+    return simplify_either(either.types, xtypes)
+
+
+def simplify_either(x, y):
+    # TODO this is O(n**2); use an O(n) algorithm instead
+    result = list(x)
+    for type in y:
+        if isinstance(type, Generic):
+            for i, rt in enumerate(result):
+                if isinstance(rt, Generic) and type.typename == rt.typename:
+                    result[i] = Generic(rt.typename,
+                                        (combine_types(t, s)
+                                         for t, s in zip(type.args, rt.args)))
+                    break
+            else:
+                if type not in result:
+                    result.append(type)
+        elif isinstance(type, Tuple):
+            for i, rt in enumerate(result):
+                if isinstance(rt, Tuple) and len(type) == len(rt):
+                    result[i] = Tuple(combine_types(t, s)
+                                      for t, s in zip(type.itemtypes,
+                                                      rt.itemtypes))
+                    break
+            else:
+                if type not in result:
+                    result.append(type)
+        elif type not in result:
+            result.append(type)
+    if len(result) > 1:
+        return Either(result)
+    else:
+        return result[0]
+
+
+class TypeBase:
+    """Abstract base class of all type objects.
+
+    Type objects use isinstance tests librarally -- they don't support duck
+    typing well.
+    """
+    
+    def __eq__(self, other):
+        if type(other) is not type(self):
+            return False
+        for attr in self.__dict__:
+            if getattr(other, attr) != getattr(self, attr):
+                return False
+        return True
+    
+    def __ne__(self, other):
+        return not self == other
+
+    def __repr__(self):
+        return str(self)
+
+
+class Instance(TypeBase):
+    def __init__(self, typeobj):
+        self.typeobj = typeobj
+
+    def __str__(self):
+        return self.typeobj.__name__
+
+    def __repr__(self):
+        return 'Instance(%s)' % self
+
+
+class Generic(TypeBase):
+    def __init__(self, typename, args):
+        self.typename = typename
+        self.args = tuple(args)
+
+    def __str__(self):
+        return '%s[%s]' % (self.typename, ', '.join(str(t)
+                                                    for t in self.args))
+
+
+class Tuple(TypeBase):
+    def __init__(self, itemtypes):
+        self.itemtypes = tuple(itemtypes)
+
+    def __len__(self):
+        return len(self.itemtypes)
+
+    def __str__(self):
+        return 'Tuple[%s]' % (', '.join(str(t) for t in self.itemtypes))
+
+
+class Either(TypeBase):
+    def __init__(self, types):
+        assert len(types) > 1
+        self.types = tuple(types)
+
+    def __eq__(self, other):
+        if type(other) is not Either:
+            return False
+        # TODO this is O(n**2); use an O(n) algorithm instead
+        for t in self.types:
+            if t not in other.types:
+                return False
+        for t in other.types:
+            if t not in self.types:
+                return False
+        return True
+
+    def __str__(self):
+        types = self.types
+        if len(types) == 2 and None in types:
+            type = [t for t in types if t is not None][0]
+            return 'Optional(%s)' % type
+        else:
+            return 'Either(%s)' % (', '.join(str(t) for t in types))
+
+
+class Unknown(TypeBase):
+    def __str__(self):
+        return 'Unknown'
+
+    def __repr__(self):
+        return 'Unknown()'
---
+++ pinfer/test_infer.py
@@ -0,0 +1,298 @@
+"""Test cases for the infer module"""
+
+import unittest
+
+from infer import Instance, Generic, Tuple, Either, Unknown
+import infer
+
+
+class TestInfer(unittest.TestCase):
+    def setUp(self):
+        self.int = Instance(int)
+        self.float = Instance(float)
+
+    def tearDown(self):
+        infer.reset()
+
+    def test_instance(self):
+        i = self.int
+        self.assertEqual(i.typeobj, int)
+        self.assertEqual(str(i), 'int')
+        self.assertEqual(repr(i), 'Instance(int)')
+        
+        self.assertTrue(i == Instance(int))
+        self.assertFalse(i != Instance(int))
+        self.assertTrue(i != self.float)
+        self.assertFalse(i == self.float)
+        self.assertNotEqual(i, None)
+    
+    def test_generic_with_one_arg(self):
+        g = Generic('List', [self.int])
+        self.assertEqual(g.typename, 'List')
+        self.assertEqual(str(g.args), '(Instance(int),)')
+        self.assertEqual(str(g), 'List[int]')
+        self.assertEqual(repr(g), 'List[int]')
+
+        self.assertEqual(g, Generic('List', [self.int]))
+        self.assertNotEqual(g, Generic('Set', [self.int]))
+        self.assertNotEqual(g, Generic('List', [self.float]))
+        self.assertNotEqual(g, self.int)
+    
+    def test_generic_with_two_args(self):
+        g = Generic('Dict', (self.int, self.float))
+        self.assertEqual(g.typename, 'Dict')
+        self.assertEqual(str(g), 'Dict[int, float]')
+
+    def test_tuple(self):
+        t0 = Tuple(())
+        t1 = Tuple([self.int])
+        t2 = Tuple((self.float, self.int))
+        self.assertEqual(t0.itemtypes, ())
+        self.assertEqual(str(t1.itemtypes[0]), 'int')
+        self.assertEqual(str(t2.itemtypes[0]), 'float')
+        self.assertEqual(str(t2.itemtypes[1]), 'int')
+        self.assertEqual(str(t0), 'Tuple[]')
+        self.assertEqual(str(t1), 'Tuple[int]')
+        self.assertEqual(str(t2), 'Tuple[float, int]')
+
+        self.assertEqual(t1, Tuple([self.int]))
+        self.assertNotEqual(t1, Tuple([self.float]))
+        self.assertNotEqual(t1, Tuple([self.int, self.int]))
+        self.assertNotEqual(t1, self.int)
+
+    def test_either(self):
+        i = self.int
+        f = self.float
+        s = Instance(str)
+        
+        e2 = Either((i, f))
+        self.assertEqual(len(e2.types), 2)
+        self.assertEqual(str(e2), 'Either(int, float)')
+
+        self.assertEqual(e2, Either((i, f)))
+        self.assertEqual(e2, Either((f, i)))
+        self.assertNotEqual(e2, Either((i, s)))
+        self.assertNotEqual(e2, Either((i, f, s)))
+        self.assertNotEqual(Either((i, f, s)), e2)
+        self.assertNotEqual(e2, i)
+
+    def test_either_as_optional(self):
+        optint = Either((self.int, None))
+        self.assertEqual(str(optint), 'Optional(int)')
+        optfloat = Either((None, self.float))
+        self.assertEqual(str(optfloat), 'Optional(float)')
+        eithernone = Either((self.int, self.float, None))
+        self.assertEqual(str(eithernone), 'Either(int, float, None)')
+
+    def test_unknown(self):
+        unknown = Unknown()
+        self.assertEqual(str(unknown), 'Unknown')
+        self.assertEqual(repr(unknown), 'Unknown()')
+        
+        self.assertEqual(unknown, Unknown())
+        self.assertNotEqual(unknown, self.int)
+
+    def test_combine_types(self):
+        i = self.int
+        f = self.float
+        s = Instance(str)
+        # Simple types
+        self.assert_combine(i, i, i)
+        self.assert_combine(f, f, f)
+        self.assert_combine(i, f, Either((i, f)))
+        self.assert_combine(i, None, Either((i, None)))
+        # Unknowns
+        self.assert_combine(i, Unknown(), i)
+        self.assert_combine(Unknown(), Unknown(), Unknown())
+        # Either types
+        self.assert_combine(i, Either((f, s)), Either((i, f, s)))
+        self.assert_combine(i, Either((i, f)), Either((i, f)))
+        self.assert_combine(Either((i, f)), Either((i, s)), Either((i, f, s)))
+        # Tuple types
+        self.assert_combine(Tuple([i, i]), Tuple([i, i]), Tuple([i, i]))
+        self.assert_combine(Tuple([i, i]), Tuple([f, s]),
+                            Tuple([Either([f, i]), Either([s, i])]))
+
+    def test_combine_special_cases(self):
+        i = self.int
+        f = self.float
+        u = Unknown()
+        def list_(x):
+            return Generic('List', [x])
+        # Simplify generic types.
+        self.assert_combine(list_(i), list_(u), list_(i))
+
+    def assert_combine(self, t, s, combined):
+        self.assertEqual(infer.combine_types(t, s), combined)
+        self.assertEqual(infer.combine_types(s, t), combined)
+
+    def test_sample(self):
+        sample = infer.sample
+        self.assertEqual(sample(()), [])
+        self.assertEqual(sample((1, 2)), [1, 2])
+        self.assertEqual(sample([]), [])
+        self.assertEqual(sample([1]), [1])
+        self.assertEqual(sample([1, 2]), [1, 2])
+        # TODO larger collections
+    
+    def test_infer_simple_value_type(self):
+        self.assert_infer_type(1, 'int')
+        self.assert_infer_type('', 'str')
+        self.assert_infer_type(None, 'None')
+
+    def test_infer_collection_type(self):
+        # List
+        self.assert_infer_type([], 'List[Unknown]')
+        self.assert_infer_type([1], 'List[int]')
+        self.assert_infer_type([1, None], 'List[Optional(int)]')
+        # Dict
+        self.assert_infer_type({1: 'x', 2: None},
+                               'Dict[int, Optional(str)]')
+        # Set
+        self.assert_infer_type({1, None}, 'Set[Optional(int)]')
+        # Tuple
+        self.assert_infer_type((1, 'x'), 'Tuple[int, str]')
+        self.assert_infer_type((1, None) * 100, 'TupleSequence[Optional(int)]')
+
+    def assert_infer_type(self, value, type):
+        self.assertEqual(str(infer.infer_value_type(value)), type)
+
+    def test_infer_variables(self):
+        infer.infer_var('x', 1)
+        self.assert_infer_state('x: int')
+        infer.infer_var('x', 1)
+        infer.infer_var('x', None)
+        infer.infer_var('y', 1.1)
+        self.assert_infer_state('x: Optional(int)\n'
+                                'y: float')
+
+    def test_infer_instance_var(self):
+        class A: pass
+        a = A()
+        a.x = 1
+        a.y = 'x'
+        infer.infer_attrs(a)
+        self.assert_infer_state('A.x: int\n'
+                                'A.y: str')
+
+    def test_infer_class_var(self):
+        class A:
+            x = 1.1
+        infer.infer_attrs(A())
+        self.assert_infer_state('A.x: float')
+
+    def test_infer_function_attr(self):        
+        class A:
+            def f(self): pass
+        a = A()
+        a.g = lambda x: 1
+        infer.infer_attrs(a)
+        self.assert_infer_state('A.g: function')
+
+    def test_infer_simple_function_signature(self):
+        @infer.infer_signature
+        def f(a):
+            return 'x'
+        f(1)
+        f(None)
+        self.assertEqual(f.__name__, 'f')
+        self.assert_infer_state('def f(a: Optional(int)) -> str')
+
+    def test_infer_function_with_two_args(self):
+        @infer.infer_signature
+        def f(x, y):
+            return x * y
+        f(1, 2)
+        f(1, 'x')
+        self.assert_infer_state(
+            'def f(x: int, y: Either(int, str)) -> Either(int, str)')
+
+    def test_infer_method(self):
+        class A:
+            @infer.infer_signature
+            def f(self, x): pass
+        A().f('x')
+        self.assert_infer_state('def f(self, x: str) -> None')
+
+    def test_infer_default_arg_values(self):
+        @infer.infer_signature
+        def f(x=1, y=None): pass
+        f()
+        self.assert_infer_state('def f(x: int, y: None) -> None')
+        f('x')
+        f('x', 1.1)
+        f()
+        self.assert_infer_state(
+            'def f(x: Either(str, int), y: Optional(float)) -> None')
+
+    def test_infer_varargs(self):
+        @infer.infer_signature
+        def f(x, *y): pass
+        f(1)
+        f(1, 'x', None)
+        self.assert_infer_state('def f(x: int, y: Optional(str)) -> None')
+        f(1)
+        self.assert_infer_state('def f(x: int) -> None')
+
+    def test_infer_keyword_args(self):
+        @infer.infer_signature
+        def f(x): pass
+        f(x=1)
+        self.assert_infer_state('def f(x: int) -> None')
+        
+        @infer.infer_signature
+        def f(x='x'): pass
+        f(x=1)
+        self.assert_infer_state('def f(x: int) -> None')
+
+    def test_infer_keyword_varargs(self):
+        @infer.infer_signature
+        def f(a, **kwargs): pass
+        f(None, x=1, y='x')
+        self.assert_infer_state(
+            'def f(a: None, kwargs: Either(str, int)) -> None')
+
+    def test_infer_keyword_only_args(self):
+        @infer.infer_signature
+        def f(x, *, y=0): pass
+        f(1, y='x')
+        self.assert_infer_state(
+            'def f(x: int, y: str) -> None')
+        
+        @infer.infer_signature
+        def f(*, x=None, y=None): pass
+        f(y='x')
+        self.assert_infer_state(
+            'def f(x: None, y: str) -> None')
+
+    def test_infer_class(self):
+        @infer.infer_class
+        class A:
+            def f(self, x): return 0
+        A().f('x')
+        self.assert_infer_state('class A(...):\n'
+                                '    def f(self, x: str) -> int')
+
+        @infer.infer_class
+        class A:
+            def f(self, x): return 0
+        @infer.infer_class
+        class B:
+            def f(self): pass
+            def g(self): pass
+        A().f('')
+        B().f()
+        B().g()
+        self.assert_infer_state('class A(...):\n'
+                                '    def f(self, x: str) -> int\n'
+                                'class B(...):\n'
+                                '    def f(self) -> None\n'
+                                '    def g(self) -> None')
+
+    def assert_infer_state(self, expected):
+        self.assertEqual(infer.format_state(), expected)
+        infer.reset()
+
+
+if __name__ == '__main__':
+    unittest.main()
