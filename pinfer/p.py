d7b33f95f4aeb181cb009dfd0eb08bb1aab9c461
Jared Pochtar <jared@dropbox.com>
2014-07-15 14:14:22 -0700

    runs testfile as __main__, so you don't need to give p.py a mainexpr

--- pinfer/p.py
+++ pinfer/p.py
@@ -3,44 +3,39 @@
 
 Usage is currently awkward (better UI is TBD):
 
-  p.py modname testname mainexpr [testargs]
+  p.py modname testfile [testargs]
 
 Where:
 
   modname:  the full target module (e.g. textwrap)
-  testname: the full test module name (e.g. test.test_textwrap)
-  mainargs: an expression to run in the testname module
-            (e.g. 'unittest.main()')
+  testfile: the full test module file (e.g. test/test_textwrap.py)
 
 Example invocation:
 
-  python3 p.py textwrap test.test_textwrap 'unittest.main()'
+  python3 p.py textwrap test/test_textwrap.py
 """
 
 
 import sys
-
+import imp
 import pinfer
 
 
 def main():
     try:
-        modname, testname, mainexpr = sys.argv[1:4]
+        modname, testname = sys.argv[1:3]
     except Exception:
-        print('Usage: %s modname testname mainexpr [testargs]\n' % sys.argv[0],
+        print('Usage: %s modname testfile [testargs]\n' % sys.argv[0],
               file=sys.stderr)
         sys.exit(2)
+        
     __import__(modname)
     mod = sys.modules[modname]
     pinfer.infer_module(mod)
     pinfer.dump_at_exit()
-    del sys.argv[1:4]
-    __import__(testname)
-    testmod = sys.modules[testname]
-    sys.modules['__main__'] = testmod
-    testmod.__name__ = '__main__'
-    eval(mainexpr, testmod.__dict__)
 
+    del sys.argv[1:3]
+    imp.load_source('__main__', testname)
 
 if __name__ == '__main__':
     main()
