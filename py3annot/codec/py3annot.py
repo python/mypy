a159d5a9f058c94ee93fefec463b67b9101d022d
Schuyler Smith <schuyler@dropbox.com>
2014-07-16 16:58:47 -0700

    Made py3annot.py much more python3 compliant.

--- py3annot/codec/py3annot.py
+++ py3annot/codec/py3annot.py
@@ -1,24 +1,24 @@
 from __future__ import absolute_import
 
 import codecs
-import cStringIO
 import traceback
 from encodings import utf_8
+from io import BytesIO
 
 from .tokenizer import py3annot_tokenize, py3annot_untokenize
 
 def py3annot_transform(stream):
     try:
         output = py3annot_untokenize(py3annot_tokenize(stream.readline))
-    except Exception, ex:
-        print ex
+    except Exception as ex:
+        print(ex)
         traceback.print_exc()
         raise
 
     return output
 
 def py3annot_transform_string(text):
-    stream = cStringIO.StringIO(text)
+    stream = BytesIO(text)
     return py3annot_transform(stream)
 
 def py3annot_decode(input, errors='strict'):
@@ -36,4 +36,4 @@ class Py3annotIncrementalDecoder(utf_8.IncrementalDecoder):
 class Py3annotStreamReader(utf_8.StreamReader):
     def __init__(self, *args, **kwargs):
         codecs.StreamReader.__init__(self, *args, **kwargs)
-        self.stream = cStringIO.StringIO(py3annot_transform(self.stream))
+        self.stream = BytesIO(py3annot_transform(self.stream))
