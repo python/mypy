acd1aab6e451e2f8035ab1b46baf59200f4fefbe
Guido van Rossum <guido@dropbox.com>
2014-07-15 21:16:33 -0700

    Add __name__ and __file__ to ModuleType.

--- stubs/3.2/types.py
+++ stubs/3.2/types.py
@@ -2,9 +2,11 @@
 
 # TODO this is work in progress
 
-from typing import Any
+from typing import Any, Undefined
 
 class ModuleType:
+    __name__ = Undefined(str)
+    __file__ = Undefined(str)
     def __init__(self, name: str, doc: Any) -> None: pass
 
 class MethodType: pass
