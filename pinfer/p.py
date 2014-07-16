2599892701827c5d93097aec43f817ab714e3115
Jared Pochtar <jared@dropbox.com>
2014-07-16 16:22:40 -0700

    added support for specifying a module to restrict inference to

--- pinfer/p.py
+++ pinfer/p.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """Stub to run pinfer on a stdlib module.
 
-Usage is currently awkward (better UI is TBD):
+Usage:
 
-  p.py testfile [testargs]
+  p.py modname testfile [testargs]
 
 Where:
 
+  modname:  the full target module (e.g. textwrap).  If modname is "-",
+            infer the types of all imported modules.
   testfile: the full test module file (e.g. test/test_textwrap.py)
 
 Example invocation:
@@ -31,19 +33,25 @@ def inferring_import(*args, **kwargs):
 
 def main():
     try:
-        testfile = sys.argv[1]
-        del sys.argv[1]
+        modname, testfile = sys.argv[1], sys.argv[2]
+        del sys.argv[1:3]
     except Exception:
-        print('Usage: %s testfile [testargs]\n' % sys.argv[0], file=sys.stderr)
+        print('Usage: %s outputfile modname testfile [testargs]\n' % sys.argv[0], file=sys.stderr)
         sys.exit(2)
-        
+
     pinfer.dump_at_exit()
 
-    __builtins__.__import__ = inferring_import
-
-    # run testfile as main
+    # help us with local imports
     filemodule = os.path.dirname(os.path.abspath(testfile))
     sys.path.append(filemodule)
+
+    if modname == '-':
+      __builtins__.__import__ = inferring_import
+    else:
+      module = __import__(modname)
+      pinfer.infer_module(module)
+
+    # run testfile as main
     del sys.modules['__main__']
     imp.load_source('__main__', testfile)
 
