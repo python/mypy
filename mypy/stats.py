8f0a02fa8af1c3000c1e16738f020b46113d8cb7
Jukka Lehtosalo <jukka@dropbox.com>
2014-07-16 17:27:16 -0700

    Adapt stats generation to static typing

--- mypy/stats.py
+++ mypy/stats.py
@@ -1,37 +1,20 @@
 """Utilities for calculating and reporting statistics about types."""
 
 from os.path import basename
+
+from typing import Any, Dict, cast
     
 from mypy.traverser import TraverserVisitor
 from mypy.types import (
-    AnyType, Instance, FunctionLike, TupleType, Void, TypeVar,
-    TypeQuery, ANY_TYPE_STRATEGY
+    Type, AnyType, Instance, FunctionLike, TupleType, Void, TypeVar,
+    TypeQuery, ANY_TYPE_STRATEGY, Callable
 )
 from mypy import nodes
-
-
-def analyze_types(tree, path, inferred=False, typemap=None):
-    if basename(path) in ('abc.py', 'typing.py', 'builtins.py'):
-        return
-    print(path)
-    v = StatisticsVisitor(inferred, typemap)
-    tree.accept(v)
-    print('  ** precision **')
-    print('  precise  ', v.num_precise)
-    print('  imprecise', v.num_imprecise)
-    print('  any      ', v.num_any)
-    print('  ** kinds **')
-    print('  simple   ', v.num_simple)
-    print('  generic  ', v.num_generic)
-    print('  function ', v.num_function)
-    print('  tuple    ', v.num_tuple)
-    print('  typevar  ', v.num_typevar)
-    print('  complex  ', v.num_complex)
-    print('  any      ', v.num_any)
+from mypy.nodes import Node, FuncDef, TypeApplication, AssignmentStmt
 
 
 class StatisticsVisitor(TraverserVisitor):
-    def __init__(self, inferred, typemap=None):
+    def __init__(self, inferred: bool, typemap: Dict[Node, Type] = None) -> None:
         self.inferred = inferred
         self.typemap = typemap
         
@@ -47,17 +30,19 @@ class StatisticsVisitor(TraverserVisitor):
         self.num_complex = 0
 
         self.line = -1
+
+        self.line_map = Dict[int, int]()
         
         TraverserVisitor.__init__(self)
     
-    def visit_func_def(self, o):
+    def visit_func_def(self, o: FuncDef) -> None:
         self.line = o.line
         if len(o.expanded) > 1:
             for defn in o.expanded:
-                self.visit_func_def(defn)
+                self.visit_func_def(cast(FuncDef, defn))
         else:
             if o.type:
-                sig = o.type
+                sig = cast(Callable, o.type)
                 arg_types = sig.arg_types
                 if (sig.arg_names and sig.arg_names[0] == 'self' and
                     not self.inferred):
@@ -67,30 +52,34 @@ class StatisticsVisitor(TraverserVisitor):
                 self.type(sig.ret_type)
             super().visit_func_def(o)
 
-    def visit_type_application(self, o):
+    def visit_type_application(self, o: TypeApplication) -> None:
         self.line = o.line
         for t in o.types:
             self.type(t)
         super().visit_type_application(o)
 
-    def visit_assignment_stmt(self, o):
+    def visit_assignment_stmt(self, o: AssignmentStmt) -> None:
         self.line = o.line
         if (isinstance(o.rvalue, nodes.CallExpr) and
-            isinstance(o.rvalue.analyzed, nodes.TypeVarExpr)):
+            isinstance(cast(nodes.CallExpr, o.rvalue).analyzed,
+                       nodes.TypeVarExpr)):
             # Type variable definition -- not a real assignment.
             return
         if o.type:
             self.type(o.type)
         elif self.inferred:
             for lvalue in o.lvalues:
-                if isinstance(lvalue, nodes.ParenExpr):
-                    lvalue = lvalue.expr
-                if isinstance(lvalue, (nodes.TupleExpr, nodes.ListExpr)):
+                lvalue_ref = lvalue
+                if isinstance(lvalue_ref, nodes.ParenExpr):
+                    lvalue = lvalue_ref.expr
+                if isinstance(lvalue, nodes.TupleExpr):
+                    items = lvalue.items
+                elif isinstance(lvalue, nodes.ListExpr):
                     items = lvalue.items
                 else:
                     items = [lvalue]
                 for item in items:
-                    if hasattr(item, 'is_def') and item.is_def:
+                    if hasattr(item, 'is_def') and Any(item).is_def:
                         t = self.typemap.get(item)
                         if t:
                             self.type(t)
@@ -98,7 +87,7 @@ class StatisticsVisitor(TraverserVisitor):
                             print('  !! No inferred type on line', self.line)
         super().visit_assignment_stmt(o)
 
-    def type(self, t):
+    def type(self, t: Type) -> None:
         if isinstance(t, AnyType):
             print('  !! Any type around line', self.line)
             self.num_any += 1
@@ -129,28 +118,49 @@ class StatisticsVisitor(TraverserVisitor):
             self.num_typevar += 1
 
 
-def is_imprecise(t):
+def analyze_types(tree: Node, path: str, inferred: bool = False,
+                  typemap: Dict[Node, Type] = None) -> None:
+    if basename(path) in ('abc.py', 'typing.py', 'builtins.py'):
+        return
+    print(path)
+    v = StatisticsVisitor(inferred, typemap)
+    tree.accept(v)
+    print('  ** precision **')
+    print('  precise  ', v.num_precise)
+    print('  imprecise', v.num_imprecise)
+    print('  any      ', v.num_any)
+    print('  ** kinds **')
+    print('  simple   ', v.num_simple)
+    print('  generic  ', v.num_generic)
+    print('  function ', v.num_function)
+    print('  tuple    ', v.num_tuple)
+    print('  typevar  ', v.num_typevar)
+    print('  complex  ', v.num_complex)
+    print('  any      ', v.num_any)
+
+
+def is_imprecise(t: Type) -> bool:
     return t.accept(HasAnyQuery())
 
 
 class HasAnyQuery(TypeQuery):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(False, ANY_TYPE_STRATEGY)
 
-    def visit_any(self, t):
+    def visit_any(self, t: AnyType) -> bool:
         return True
 
-    def visit_instance(self, t):
+    def visit_instance(self, t: Instance) -> bool:
         if t.type.fullname() == 'builtins.tuple':
             return True
         else:
             return super().visit_instance(t)
 
 
-def is_generic(t):
-    return isinstance(t, Instance) and t.args
+def is_generic(t: Type) -> bool:
+    return isinstance(t, Instance) and bool(cast(Instance, t).args)
 
 
-def is_complex(t):
+def is_complex(t: Type) -> bool:
     return is_generic(t) or isinstance(t, (FunctionLike, TupleType,
                                            TypeVar))
