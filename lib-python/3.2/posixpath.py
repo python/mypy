3289b929bb7a0fe9e7b5a18059c4a3be27b7b4aa
Eric Price <ecprice@mit.edu>
2014-07-22 22:38:19 -0700

    commonprefix in stdlib actually applies to -- and is used for -- general ordered sequences

--- lib-python/3.2/posixpath.py
+++ lib-python/3.2/posixpath.py
@@ -455,7 +455,7 @@ def relpath(path: AnyStr, start: AnyStr = None) -> AnyStr:
     path_list = [x for x in abspath(path).split(sep) if x]
 
     # Work out how much of the filepath is shared by start and path.
-    i = len(commonprefix(Any([start_list, path_list])))
+    i = len(commonprefix([start_list, path_list]))
 
     rel_list = [pardir] * (len(start_list)-i) + path_list[i:]
     if not rel_list:
