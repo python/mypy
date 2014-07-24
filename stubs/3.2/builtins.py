c47b386d18b44d6b0e5816deb5b918302cd99d0f
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2014-07-23 23:31:35 -0700

    Fix stubs problem due to merge

--- stubs/3.2/builtins.py
+++ stubs/3.2/builtins.py
@@ -113,7 +113,6 @@ class int(SupportsInt, SupportsFloat):
     def __rlshift__(self, n: int) -> int: pass
     def __rrshift__(self, n: int) -> int: pass
 
-    def __pos__(self) -> int: pass
     def __neg__(self) -> int: pass
     def __pos__(self) -> int: pass
     def __invert__(self) -> int: pass
@@ -180,7 +179,6 @@ class float(SupportsFloat, SupportsInt):
     def __le__(self, x: float) -> bool: pass
     def __gt__(self, x: float) -> bool: pass
     def __ge__(self, x: float) -> bool: pass
-    def __pos__(self) -> float: pass
     def __neg__(self) -> float: pass
     def __pos__(self) -> float: pass
 
