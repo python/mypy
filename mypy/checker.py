49ed2118ee19423a3e2ec474ab9087e1b5e3f4f8
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2014-07-25 11:21:09 -0700

    Fix type check error

--- mypy/checker.py
+++ mypy/checker.py
@@ -953,7 +953,7 @@ class TypeChecker(NodeVisitor[Type]):
             self.fail(messages.INVALID_RETURN_TYPE_FOR_YIELD, s)
             return None
         if s.expr is None:
-            actual_item_type = Void()
+            actual_item_type = Void() # type: Type
         else:
             actual_item_type = self.accept(s.expr, expected_item_type)
         self.check_subtype(actual_item_type, expected_item_type, s,
