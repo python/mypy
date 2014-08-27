fcc0bf31c54f46a0e2009f3f5d713b03e59f2012
Igor Vuk <parcijala@gmail.com>
2014-08-27 22:46:53 +0200

    PEP 8 fixes for test_typing.py

--- lib-typing/2.7/test_typing.py
+++ lib-typing/2.7/test_typing.py
@@ -14,15 +14,15 @@ class TestTyping(unittest.TestCase):
     def test_List(self):
         self.assertIs(List[int], list)
         self.assertIs(List[unicode], list)
-        
+
     def test_Dict(self):
         self.assertIs(Dict[int, unicode], dict)
         self.assertIs(Dict[unicode, int], dict)
-        
+
     def test_Set(self):
         self.assertIs(Set[int], set)
         self.assertIs(Set[unicode], set)
-        
+
     def test_Tuple(self):
         self.assertIs(Tuple[int], tuple)
         self.assertIs(Tuple[unicode, int], tuple)
@@ -41,7 +41,7 @@ class TestTyping(unittest.TestCase):
         self.assertIs(type(re.match('', '')), BytesMatch)
         # Note that actually Match is the same as BytesMatch, which is
         # a bit awkward.
-        
+
     def test_Any(self):
         o = object()
         self.assertIs(Any(o), o)
@@ -70,15 +70,15 @@ class TestTyping(unittest.TestCase):
         def f(x): return x + u'string'
         @overload
         def f(x): return x + 1
-        
+
         self.assertEqual(f(u'x'), u'xstring')
         self.assertEqual(f(1), 2)
-        
+
         @overload
         def g(x): return u'integer'
         @overload
         def g(x): return u'string'
-        
+
         self.assertEqual(g(u'x'), u'string')
         self.assertEqual(g(1), u'integer')
 
@@ -90,7 +90,7 @@ class TestTyping(unittest.TestCase):
         def f(x): return u'integer'
         @overload
         def f(x): return u'floating'
-        
+
         self.assertEqual(f(u'x'), u'string')
         self.assertEqual(f(1), u'integer')
         self.assertEqual(f(1.0), u'floating')
@@ -104,7 +104,7 @@ class TestTyping(unittest.TestCase):
 
         self.assertEqual(f(u'x', u'y'), (1, u'x', u'y'))
         self.assertEqual(f(u'z', 3), (2, u'z', 3))
-        
+
         @overload
         def g(x, y): return 1
         @overload
@@ -127,11 +127,11 @@ class TestTyping(unittest.TestCase):
 
         self.assertEqual(f(u'x', object()), 1)
         self.assertEqual(f(object(), 1), 2)
-        
+
     @overload
     def method_overload(self, x):
         return u's'
-    
+
     @overload
     def method_overload(self, x):
         return u'i'
@@ -177,7 +177,7 @@ class TestTyping(unittest.TestCase):
 
         msg1 = ur'f\(\) takes exactly 1 argument \(%d given\)'
         msg2 = ur'f\(\) takes exactly 1 positional argument \(%d given\)'
-        
+
         with self.assertRaisesRegex(TypeError, msg1 % 0):
             f()
         with self.assertRaisesRegex(TypeError, msg2 % 2):
@@ -194,7 +194,7 @@ class TestTyping(unittest.TestCase):
 
         self.assertEqual(f(), None)
         self.assertEqual(f(1), 1)
-        
+
         @overload
         def g(x): return x + 1
         @overload
@@ -202,7 +202,7 @@ class TestTyping(unittest.TestCase):
 
         self.assertEqual(g(), None)
         self.assertEqual(g(1), 2)
-        
+
         msg = ur'g\(\) takes no arguments \(2 given\)'
         with self.assertRaisesRegex(TypeError, msg):
             g(1, 2)
@@ -260,7 +260,7 @@ class TestTyping(unittest.TestCase):
         self.assertEqual(f(), u'x!')
         self.assertEqual(f(u'y'), u'y!')
         self.assertEqual(f(3), 4)
-        
+
         @overload
         def g(a, x=u'x', y=u'z'): return 1
         @overload
@@ -271,7 +271,7 @@ class TestTyping(unittest.TestCase):
         self.assertEqual(g(1, u'XX'), 1)
         self.assertEqual(g(1, 2), 2)
         self.assertEqual(g(1, u'XX', u'YY'), 1)
-        
+
         with self.assertRaises(TypeError):
             g(1, u'x', 2)
 
@@ -303,9 +303,9 @@ class TestTyping(unittest.TestCase):
 
         class A(object):
             def __call__(self): pass
-            
+
         self.assertEqual(f(A()), 1)
-        
+
         self.assertEqual(f(1), 2)
         self.assertEqual(f(object()), 2)
 
@@ -316,12 +316,12 @@ class TestTyping(unittest.TestCase):
     @unittest.skip("overloads not supported in 2.7 yet")
     def test_typevar_in_overload(self):
         t = typevar(u't')
-        
+
         @overload
         def f(x, y): return 1
         @overload
         def f(x, y): return 2
-        
+
         self.assertEqual(f((), u'x'), 1)
         self.assertEqual(f((), 1.1), 2)
 
@@ -359,7 +359,7 @@ class TestTyping(unittest.TestCase):
 
     def test_sequence(self):
         self.assertIs(Sequence[int], Sequence)
-        
+
         self.assertIsInstance([], Sequence)
         self.assertIsInstance((), Sequence)
         self.assertIsInstance(u'', Sequence)
@@ -369,7 +369,7 @@ class TestTyping(unittest.TestCase):
 
     def test_abstract_set(self):
         self.assertIs(AbstractSet[int], AbstractSet)
-        
+
         self.assertIsInstance(set(), AbstractSet)
         self.assertIsInstance(frozenset(), AbstractSet)
 
@@ -422,7 +422,7 @@ class TestTyping(unittest.TestCase):
 
         self.assertIsInstance(A(), P)
         self.assertNotIsInstance(B(), P)
-        
+
         self.assertTrue(issubclass(A, P))
         self.assertFalse(issubclass(B, P))
         self.assertTrue(issubclass(P, Protocol))
@@ -442,7 +442,7 @@ class TestTyping(unittest.TestCase):
             # Conforms to P; attribute values don't need to be similar
             __int__ = 0
             def x(self): pass
-            def f(self): pass # Extra method
+            def f(self): pass  # Extra method
 
         class B(object):
             # Does not conform to P
@@ -486,7 +486,7 @@ class TestTyping(unittest.TestCase):
         self.assertIsInstance([], P)
         self.assertIsInstance(1, P)
         self.assertNotIsInstance({}, P)
-        
+
         self.assertTrue(issubclass(unicode, P))
         self.assertFalse(issubclass(dict, P))
 
@@ -534,18 +534,18 @@ class TestTyping(unittest.TestCase):
     def test_iterator(self):
         self.assertIsInstance(iter(u''), Iterator)
         self.assertIsInstance(iter([]), Iterator)
-        self.assertIsInstance(iter({}), Iterator)        
+        self.assertIsInstance(iter({}), Iterator)
         self.assertNotIsInstance([], Iterator)
-        
+
         class A(object):
             def __iter__(self): pass
             def next(self): pass
         self.assertIsInstance(A(), Iterator)
-        
+
         class B(object):
             def __iter__(self): pass
         self.assertNotIsInstance(B(), Iterator)
-        
+
         class C(object):
             def next(self): pass
         self.assertNotIsInstance(C(), Iterator)
@@ -631,7 +631,7 @@ class TestTyping(unittest.TestCase):
         def f(a): return u's'
         @overload
         def f(a): return u'i'
-        
+
         self.assertEqual(f(u''), u's')
         self.assertEqual(f(2), u'i')
 
@@ -641,7 +641,7 @@ class TestTyping(unittest.TestCase):
         def f(a): return 1
         @overload
         def f(a): return 2
-        
+
         self.assertEqual(f(Dummy()), 1)
         self.assertEqual(f(2), 2)
 
@@ -651,7 +651,7 @@ class TestTyping(unittest.TestCase):
         def f(a): return 1
         @overload
         def f(a): return 2
-        
+
         self.assertEqual(f(Dummy()), 1)
         self.assertEqual(f(2), 2)
 
@@ -661,7 +661,7 @@ class TestTyping(unittest.TestCase):
         def f(a): return 1
         @overload
         def f(a): return 2
-        
+
         class C(object): pass
         self.assertEqual(f(C()), 1)
         self.assertEqual(f(2), 2)
@@ -672,7 +672,7 @@ class TestTyping(unittest.TestCase):
         def f(a): return 1
         @overload
         def f(a): return 2
-        
+
         self.assertEqual(f(object()), 1)
         self.assertEqual(f(None), 1)
 
@@ -682,7 +682,7 @@ class TestTyping(unittest.TestCase):
         def f(a): return 1
         @overload
         def f(a): return 2
-        
+
         self.assertEqual(f([]), 1)
         self.assertEqual(f(()), 2)
 
@@ -692,7 +692,7 @@ class TestTyping(unittest.TestCase):
         def f(a): return 1
         @overload
         def f(a): return 2
-        
+
         self.assertEqual(f(()), 1)
         self.assertEqual(f([]), 2)
 
@@ -702,7 +702,7 @@ class TestTyping(unittest.TestCase):
         def f(a): return 1
         @overload
         def f(a): return 2
-        
+
         self.assertEqual(f(ord), 1)
         self.assertEqual(f([]), 2)
 
@@ -722,7 +722,7 @@ class TestTyping(unittest.TestCase):
 
     def test_construct_class_with_abstract_method(self):
         t = typevar(u't')
-        
+
         class A(AbstractGeneric[t]):
             @abstractmethod
             def f(self): pass
@@ -738,15 +738,15 @@ class TestTyping(unittest.TestCase):
         class A(Protocol):
             @abstractmethod
             def f(self): pass
-            
+
         with self.assertRaises(TypeError):
-            A() # No implementation for abstract method.
+            A()  # No implementation for abstract method.
 
     def test_protocol_inheritance(self):
         class A(Protocol):
             def f(self): return 1
         class B(A): pass
-        
+
         self.assertEqual(B().f(), 1)
 
         class C(A): pass
@@ -759,9 +759,9 @@ class TestTyping(unittest.TestCase):
             def f(self): pass
         class B(A):
             pass
-        
+
         with self.assertRaises(TypeError):
-            B() # No implementation for abstract method.
+            B()  # No implementation for abstract method.
         class C(A):
             def f(self): pass
         C()
@@ -779,13 +779,13 @@ class TestTyping(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             A()
-            
+
         class B():
             __metaclass__ = ABCMeta
             @overload
             @abstractmethod
             def f(self, x): pass
-            
+
             @overload
             @abstractmethod
             def f(self, x): pass
@@ -797,7 +797,7 @@ class TestTyping(unittest.TestCase):
             @overload
             def f(self, x):
                 return 1
-            
+
             @overload
             def f(self, x):
                 return u'x'
@@ -809,6 +809,7 @@ class TestTyping(unittest.TestCase):
 @overload
 def global_overload(x):
     return u's'
+
 
 @overload
 def global_overload(x):
