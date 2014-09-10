0ce53ac475a7b6df7d48abd0d7ae70dcccc38d62
Sander Kersten <spkersten@gmail.com>
2014-09-10 20:37:02 +0200

    Replaced overload with Union type in os/path module

--- stubs/3.2/os/path.py
+++ stubs/3.2/os/path.py
@@ -3,7 +3,7 @@
 
 # based on http://docs.python.org/3.2/library/os.path.html
 
-from typing import overload, List, Any, AnyStr, Tuple, BinaryIO, TextIO
+from typing import overload, List, Any, AnyStr, Tuple, BinaryIO, TextIO, Union
 
 # ----- os.path variables -----
 supports_unicode_filenames = False
@@ -18,91 +18,38 @@ defpath = ''
 devnull = ''
 
 # ----- os.path function stubs -----
-@overload
-def abspath(path: str) -> str: pass
-@overload
-def abspath(path: bytes) -> bytes: pass
-@overload
-def basename(path: str) -> str: pass
-@overload
-def basename(path: bytes) -> bytes: pass
+def abspath(path: Union[str, bytes]) -> str: pass
+def basename(path: Union[str, bytes]) -> str: pass
+
 # NOTE: Empty List[bytes] results in '' (str) => fall back to Any return type.
 def commonprefix(list: List[AnyStr]) -> Any: pass
-@overload
-def dirname(path: str) -> str: pass
-@overload
-def dirname(path: bytes) -> bytes: pass
-@overload
-def exists(path: str) -> bool: pass
-@overload
-def exists(path: bytes) -> bool: pass
-@overload
-def lexists(path: str) -> bool: pass
-@overload
-def lexists(path: bytes) -> bool: pass
-@overload
-def expanduser(path: str) -> str: pass
-@overload
-def expanduser(path: bytes) -> bytes: pass
-@overload
-def expandvars(path: str) -> str: pass
-@overload
-def expandvars(path: bytes) -> bytes: pass
+def dirname(path: Union[str, bytes]) -> str: pass
+def exists(path: Union[str, bytes]) -> bool: pass
+def lexists(path: Union[str, bytes]) -> bool: pass
+def expanduser(path: Union[str, bytes]) -> str: pass
+def expandvars(path: Union[str, bytes]) -> str: pass
+
 
 # These return float if os.stat_float_times() == True
-@overload
-def getatime(path: str) -> Any: pass
-@overload
-def getatime(path: bytes) -> Any: pass
-@overload
-def getmtime(path: str) -> Any: pass
-@overload
-def getmtime(path: bytes) -> Any: pass
-@overload
-def getctime(path: str) -> Any: pass
-@overload
-def getctime(path: bytes) -> Any: pass
+def getatime(path: Union[str, bytes]) -> Any: pass
+def getmtime(path: Union[str, bytes]) -> Any: pass
+def getctime(path: Union[str, bytes]) -> Any: pass
 
-@overload
-def getsize(path: str) -> int: pass
-@overload
-def getsize(path: bytes) -> int: pass
-@overload
-def isabs(path: str) -> bool: pass
-@overload
-def isabs(path: bytes) -> bool: pass
-@overload
-def isfile(path: str) -> bool: pass
-@overload
-def isfile(path: bytes) -> bool: pass
-@overload
-def isdir(path: str) -> bool: pass
-@overload
-def isdir(path: bytes) -> bool: pass
-@overload
-def islink(path: str) -> bool: pass
-@overload
-def islink(path: bytes) -> bool: pass
-@overload
-def ismount(path: str) -> bool: pass
-@overload
-def ismount(path: bytes) -> bool: pass
+def getsize(path: Union[str, bytes]) -> int: pass
+def isabs(path: Union[str, bytes]) -> bool: pass
+def isfile(path: Union[str, bytes]) -> bool: pass
+def isdir(path: Union[str, bytes]) -> bool: pass
+def islink(path: Union[str, bytes]) -> bool: pass
+def ismount(path: Union[str, bytes]) -> bool: pass
+
 @overload
 def join(path: str, *paths: str) -> str: pass
 @overload
 def join(path: bytes, *paths: bytes) -> bytes: pass
-@overload
-def normcase(path: str) -> str: pass
-@overload
-def normcase(path: bytes) -> bytes: pass
-@overload
-def normpath(path: str) -> str: pass
-@overload
-def normpath(path: bytes) -> bytes: pass
-@overload
-def realpath(path: str) -> str: pass
-@overload
-def realpath(path: bytes) -> bytes: pass
+
+def normcase(path: Union[str, bytes]) -> str: pass
+def normpath(path: Union[str, bytes]) -> str: pass
+def realpath(path: Union[str, bytes]) -> str: pass
 @overload
 def relpath(path: str, start: str = None) -> str: pass
 @overload
