fc4b89382ef1dcaaa9a3037945a7ec8d5ab091fa
Sander Kersten <spkersten@gmail.com>
2014-09-11 18:59:26 +0200

    Replaced bytes/str with AnyStr

--- stubs/3.2/os/path.py
+++ stubs/3.2/os/path.py
@@ -3,7 +3,7 @@
 
 # based on http://docs.python.org/3.2/library/os.path.html
 
-from typing import overload, List, Any, AnyStr, Tuple, BinaryIO, TextIO, Union
+from typing import overload, List, Any, AnyStr, Tuple, BinaryIO, TextIO
 
 # ----- os.path variables -----
 supports_unicode_filenames = False
@@ -18,59 +18,44 @@ defpath = ''
 devnull = ''
 
 # ----- os.path function stubs -----
-def abspath(path: Union[str, bytes]) -> str: pass
-def basename(path: Union[str, bytes]) -> str: pass
+def abspath(path: AnyStr) -> AnyStr: pass
+def basename(path: AnyStr) -> AnyStr: pass
 
 # NOTE: Empty List[bytes] results in '' (str) => fall back to Any return type.
 def commonprefix(list: List[AnyStr]) -> Any: pass
-def dirname(path: Union[str, bytes]) -> str: pass
-def exists(path: Union[str, bytes]) -> bool: pass
-def lexists(path: Union[str, bytes]) -> bool: pass
-def expanduser(path: Union[str, bytes]) -> str: pass
-def expandvars(path: Union[str, bytes]) -> str: pass
+def dirname(path: AnyStr) -> AnyStr: pass
+def exists(path: AnyStr) -> bool: pass
+def lexists(path: AnyStr) -> bool: pass
+def expanduser(path: AnyStr) -> AnyStr: pass
+def expandvars(path: AnyStr) -> AnyStr: pass
 
 
 # These return float if os.stat_float_times() == True
-def getatime(path: Union[str, bytes]) -> Any: pass
-def getmtime(path: Union[str, bytes]) -> Any: pass
-def getctime(path: Union[str, bytes]) -> Any: pass
+def getatime(path: AnyStr) -> Any: pass
+def getmtime(path: AnyStr) -> Any: pass
+def getctime(path: AnyStr) -> Any: pass
 
-def getsize(path: Union[str, bytes]) -> int: pass
-def isabs(path: Union[str, bytes]) -> bool: pass
-def isfile(path: Union[str, bytes]) -> bool: pass
-def isdir(path: Union[str, bytes]) -> bool: pass
-def islink(path: Union[str, bytes]) -> bool: pass
-def ismount(path: Union[str, bytes]) -> bool: pass
+def getsize(path: AnyStr) -> int: pass
+def isabs(path: AnyStr) -> bool: pass
+def isfile(path: AnyStr) -> bool: pass
+def isdir(path: AnyStr) -> bool: pass
+def islink(path: AnyStr) -> bool: pass
+def ismount(path: AnyStr) -> bool: pass
 
-@overload
-def join(path: str, *paths: str) -> str: pass
-@overload
-def join(path: bytes, *paths: bytes) -> bytes: pass
+def join(path: AnyStr, *paths: AnyStr) -> AnyStr: pass
 
-def normcase(path: Union[str, bytes]) -> str: pass
-def normpath(path: Union[str, bytes]) -> str: pass
-def realpath(path: Union[str, bytes]) -> str: pass
-@overload
-def relpath(path: str, start: str = None) -> str: pass
-@overload
-def relpath(path: bytes, start: bytes = None) -> bytes: pass
-@overload
-def samefile(path1: str, path2: str) -> bool: pass
-@overload
-def samefile(path1: bytes, path2: bytes) -> bool: pass
+def normcase(path: AnyStr) -> AnyStr: pass
+def normpath(path: AnyStr) -> AnyStr: pass
+def realpath(path: AnyStr) -> AnyStr: pass
+def relpath(path: AnyStr, start: AnyStr = None) -> AnyStr: pass
+
+def samefile(path1: AnyStr, path2: AnyStr) -> bool: pass
 def sameopenfile(fp1: int, fp2: int) -> bool: pass
 #def samestat(stat1: stat_result,
 #             stat2: stat_result) -> bool: pass  # Unix only
-@overload
-def split(path: str) -> Tuple[str, str]: pass
-@overload
-def split(path: bytes) -> Tuple[bytes, bytes]: pass
-@overload
-def splitdrive(path: str) -> Tuple[str, str]: pass
-@overload
-def splitdrive(path: bytes) -> Tuple[bytes, bytes]: pass
-@overload
-def splitext(path: str) -> Tuple[str, str]: pass
-@overload
-def splitext(path: bytes) -> Tuple[bytes, bytes]: pass
+
+def split(path: AnyStr) -> Tuple[AnyStr, AnyStr]: pass
+def splitdrive(path: AnyStr) -> Tuple[AnyStr, AnyStr]: pass
+def splitext(path: AnyStr) -> Tuple[AnyStr, AnyStr]: pass
+
 #def splitunc(path: str) -> Tuple[str, str]: pass  # Windows only, deprecated
