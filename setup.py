a3d7f250ebb7bee137097867c188209dd38542e9
Remigiusz Modrzejewski <lrem@maxnet.org.pl>
2014-10-29 22:07:46 +0000

    Small fixes in setup.py to be able to submit to PyPI.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (3, 2, 0):
     sys.stderr.write("ERROR: You need Python 3.2 or later to use mypy.\n")
     exit(1)
 
-version = '0.0.1.dev1'
+version = '0.0.1'
 description = 'Optional static type checker for Python'
 long_description = '''
 Mypy -- Optional Static Type Checker for Python
@@ -24,15 +24,15 @@ interpreter.
 
 stubs = []
 
-for version in ['3.4', '3.3', '3.2', '2.7']:
-    base = os.path.join('stubs', version)
+for py_version in ['3.4', '3.3', '3.2', '2.7']:
+    base = os.path.join('stubs', py_version)
     if not os.path.exists(base):
         os.mkdir(base)
 
     stub_dirs = [''] + [name for name in os.listdir(base)
                         if os.path.isdir(os.path.join(base, name))]
     for stub_dir in stub_dirs:
-        target = os.path.join('lib', 'mypy', 'stubs', version, stub_dir)
+        target = os.path.join('lib', 'mypy', 'stubs', py_version, stub_dir)
         files = glob.glob(os.path.join(base, stub_dir, '*.py'))
         stubs.append((target, files))
 
@@ -47,7 +47,7 @@ classifiers = [
     'Topic :: Software Development',
 ]
 
-setup(name='mypy',
+setup(name='mypy-lang',
       version=version,
       description=description,
       long_description=long_description,
