f17463c99290db25610f81c2409b4563d53508b8
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2014-11-02 11:04:40 -0800

    Add test cases

--- mypy/test/testsubtypes.py
+++ mypy/test/testsubtypes.py
@@ -112,6 +112,16 @@ class SubtypingSuite(Suite):
             self.fx.callable_var_arg(0, self.fx.a, self.fx.d, self.fx.a),
             self.fx.callable(self.fx.b, self.fx.a))
 
+    def test_var_arg_callable_subtyping_6(self):
+        self.assert_proper_subtype(
+            self.fx.callable_var_arg(0, self.fx.a, self.fx.f, self.fx.d),
+            self.fx.callable_var_arg(0, self.fx.b, self.fx.e, self.fx.d))
+
+    def test_var_arg_callable_subtyping_7(self):
+        self.assert_not_subtype(
+            self.fx.callable_var_arg(0, self.fx.b, self.fx.d),
+            self.fx.callable(self.fx.a, self.fx.d))
+
     def test_type_callable_subtyping(self):
         self.assert_proper_subtype(
             self.fx.callable_type(self.fx.d, self.fx.a), self.fx.type_type)
