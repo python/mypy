98342eb80a916ff3f7b6f7dee4f602b4a9fad67c
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2014-11-30 17:04:39 -0800

    Add namedtuple test case

--- mypy/test/data/fixtures/list.py
+++ mypy/test/data/fixtures/list.py
@@ -13,6 +13,7 @@ class type: pass
 class list(Iterable[T], Generic[T]):
     def __iter__(self) -> Iterator[T]: pass
     def __mul__(self, x: int) -> list[T]: pass
+    def __getitem__(self, x: int) -> T: pass
 
 class tuple: pass
 class function: pass
