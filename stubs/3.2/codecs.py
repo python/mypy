d4cff5de0e64df08951f0c31de5bc27237028472
Marek Sapota <mareksapota@fb.com>
2015-01-29 22:36:13 -0800

    Add more functions to `codecs.py` stub

--- stubs/3.2/codecs.py
+++ stubs/3.2/codecs.py
@@ -1,6 +1,9 @@
-from typing import Callable
+from typing import Any, BinaryIO, Callable
 
 BOM_UTF8 = b''
+
+class Codec(): pass
+class StreamWriter(Codec): pass
 
 class CodecInfo(tuple):
     def __init__(self, *args) -> None: pass
@@ -8,5 +11,10 @@ class CodecInfo(tuple):
 def register(search_function: Callable[[str], CodecInfo]) -> None:
     pass
 
+def register_error(name: str, error_handler: Callable[[UnicodeError], Any]) -> None: pass
+
 def lookup(encoding: str) -> CodecInfo:
     pass
+
+# TODO This Callable is actually a StreamWriter constructor
+def getwriter(encoding: str) -> Callable[[BinaryIO], StreamWriter]: pass
