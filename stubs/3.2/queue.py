c35a1ea84d28b99829b2ff523424b0d1f7c52b66
Marek Sapota <mareksapota@fb.com>
2015-01-29 22:55:11 -0800

    Add `queue.py` stub

---
+++ stubs/3.2/queue.py
@@ -0,0 +1,10 @@
+# Stubs for queue
+
+# NOTE: These are incomplete!
+
+from typing import Any
+
+class Queue():
+    def get(block: bool = True, timeout: float = None) -> Any: pass
+
+class Empty(): pass
