16b501fe0a1f0036853953e6c4f541573d861f59
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2015-03-14 15:22:29 -0700

    Add test cases for hard coded type promotions

---
+++ mypy/test/data/fixtures/complex.py
@@ -0,0 +1,10 @@
+# Builtins stub used for some float/complex test cases.
+
+class object:
+    def __init__(self): pass
+
+class type: pass
+class function: pass
+class int: pass
+class float: pass
+class complex: pass
