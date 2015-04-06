3cc16683ae6076789366866b8816f447ac10654a
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2015-04-05 21:39:23 -0700

    Update version number to 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (3, 2, 0):
     sys.stderr.write("ERROR: You need Python 3.2 or later to use mypy.\n")
     exit(1)
 
-version = '0.1.0'
+version = '0.2.0'
 description = 'Optional static typing for Python'
 long_description = '''
 Mypy -- Optional Static Typing for Python
