d8154d4624a03feacebc327cb9095c2c29546191
David <wizzardx@gmail.com>
2015-06-21 21:42:07 +0200

    Fix a typo in traceback.pyi

--- stubs/3.2/traceback.pyi
+++ stubs/3.2/traceback.pyi
@@ -4,6 +4,6 @@ import typing
 
 # TODO signatures
 def format_tb(traceback): pass
-def print_ecx(limit=None, file=None, chain=True): pass
+def print_exc(limit=None, file=None, chain=True): pass
 
 # TODO add more
