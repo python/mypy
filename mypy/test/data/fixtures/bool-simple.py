deec7066b7d50c0ec36e7f70ab3066f8a7f66fab
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2015-08-09 16:24:58 -0700

    Fix type export test case failures

---
+++ mypy/test/data/fixtures/bool-simple.py
@@ -0,0 +1,14 @@
+# builtins stub used in boolean-related test cases (minimal version).
+
+from typing import builtinclass
+
+@builtinclass
+class object:
+    def __init__(self) -> None: pass
+
+class type: pass
+class tuple: pass
+class function: pass
+class bool: pass
+class int: pass
+class str: pass
