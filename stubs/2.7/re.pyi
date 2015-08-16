35b96edbbe2d7809cc5b9b47dc5d5c805125f64d
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2015-08-16 16:16:57 -0700

    Replace 'pass' with '...' in stubs

--- stubs/2.7/re.pyi
+++ stubs/2.7/re.pyi
@@ -24,40 +24,40 @@ DOTALL = 0
 X = 0
 VERBOSE = 0
 
-class error(Exception): pass
+class error(Exception): ...
 
-def compile(pattern: AnyStr, flags: int = 0) -> Pattern[AnyStr]: pass
+def compile(pattern: AnyStr, flags: int = 0) -> Pattern[AnyStr]: ...
 def search(pattern: AnyStr, string: AnyStr,
-           flags: int = 0) -> Match[AnyStr]: pass
+           flags: int = 0) -> Match[AnyStr]: ...
 def match(pattern: AnyStr, string: AnyStr,
-          flags: int = 0) -> Match[AnyStr]: pass
+          flags: int = 0) -> Match[AnyStr]: ...
 def split(pattern: AnyStr, string: AnyStr, maxsplit: int = 0,
-          flags: int = 0) -> List[AnyStr]: pass
+          flags: int = 0) -> List[AnyStr]: ...
 def findall(pattern: AnyStr, string: AnyStr,
-            flags: int = 0) -> List[AnyStr]: pass
+            flags: int = 0) -> List[AnyStr]: ...
 
 # Return an iterator yielding match objects over all non-overlapping matches
 # for the RE pattern in string. The string is scanned left-to-right, and
 # matches are returned in the order found. Empty matches are included in the
 # result unless they touch the beginning of another match.
 def finditer(pattern: AnyStr, string: AnyStr,
-             flags: int = 0) -> Iterator[Match[AnyStr]]: pass
+             flags: int = 0) -> Iterator[Match[AnyStr]]: ...
 
 @overload
 def sub(pattern: AnyStr, repl: AnyStr, string: AnyStr, count: int = 0,
-        flags: int = 0) -> AnyStr: pass
+        flags: int = 0) -> AnyStr: ...
 @overload
 def sub(pattern: AnyStr, repl: Callable[[Match[AnyStr]], AnyStr],
-        string: AnyStr, count: int = 0, flags: int = 0) -> AnyStr: pass
+        string: AnyStr, count: int = 0, flags: int = 0) -> AnyStr: ...
 
 @overload
 def subn(pattern: AnyStr, repl: AnyStr, string: AnyStr, count: int = 0,
-         flags: int = 0) -> Tuple[AnyStr, int]: pass
+         flags: int = 0) -> Tuple[AnyStr, int]: ...
 @overload
 def subn(pattern: AnyStr, repl: Callable[[Match[AnyStr]], AnyStr],
          string: AnyStr, count: int = 0,
-         flags: int = 0) -> Tuple[AnyStr, int]: pass
+         flags: int = 0) -> Tuple[AnyStr, int]: ...
 
-def escape(string: AnyStr) -> AnyStr: pass
+def escape(string: AnyStr) -> AnyStr: ...
 
-def purge() -> None: pass
+def purge() -> None: ...
--- stubs/2.7/sys.pyi
+++ stubs/2.7/sys.pyi
@@ -108,46 +108,46 @@ class _version_info:
 
 
 # ----- sys function stubs -----
-def call_tracing(fn: Any, args: Any) -> object: pass
-def _clear_type_cache() -> None: pass
-def _current_frames() -> Dict[int, Any]: pass
-def displayhook(value: int) -> None: pass  # value might be None
+def call_tracing(fn: Any, args: Any) -> object: ...
+def _clear_type_cache() -> None: ...
+def _current_frames() -> Dict[int, Any]: ...
+def displayhook(value: int) -> None: ...  # value might be None
 def excepthook(type_: type, value: BaseException, traceback: Any) -> None:
     # TODO traceback type
-    pass
-def exc_info() -> Tuple[type, Any, Any]: pass # see above
-def exit(arg: int = 0) -> None: pass  # arg might be None
-def getcheckinterval() -> int: pass  # deprecated
-def getdefaultencoding() -> str: pass
-#def getdlopenflags() -> int: pass  # Unix only
-def getfilesystemencoding() -> str: pass  # cannot return None
-#def getrefcount(object) -> int: pass  # no ref counts in MyPy!
-def getrecursionlimit() -> int: pass
+    ...
+def exc_info() -> Tuple[type, Any, Any]: ... # see above
+def exit(arg: int = 0) -> None: ...  # arg might be None
+def getcheckinterval() -> int: ...  # deprecated
+def getdefaultencoding() -> str: ...
+#def getdlopenflags() -> int: ...  # Unix only
+def getfilesystemencoding() -> str: ...  # cannot return None
+#def getrefcount(object) -> int: ...  # no ref counts in MyPy!
+def getrecursionlimit() -> int: ...
 
 @overload
-def getsizeof(obj: object) -> int: pass
+def getsizeof(obj: object) -> int: ...
 @overload
-def getsizeof(obj: object, default: int) -> int: pass
+def getsizeof(obj: object, default: int) -> int: ...
 
-def getswitchinterval() -> float: pass
+def getswitchinterval() -> float: ...
 
 @overload
-def _getframe() -> Any: pass
+def _getframe() -> Any: ...
 @overload
-def _getframe(depth: int) -> Any: pass
+def _getframe(depth: int) -> Any: ...
 
-def getprofile() -> Any: pass # TODO return type
-def gettrace() -> Any: pass # TODO return
-def getwindowsversion() -> Any: pass  # Windows only, TODO return type
-def intern(string: str) -> str: pass
-def setcheckinterval(interval: int) -> None: pass  # deprecated
-#def setdlopenflags(n: int) -> None: pass  # Linux only
-def setprofile(profilefunc: Any) -> None: pass # TODO type
-def setrecursionlimit(limit: int) -> None: pass
-def setswitchinterval(interval: float) -> None: pass
-def settrace(tracefunc: Any) -> None: pass # TODO type
+def getprofile() -> Any: ... # TODO return type
+def gettrace() -> Any: ... # TODO return
+def getwindowsversion() -> Any: ...  # Windows only, TODO return type
+def intern(string: str) -> str: ...
+def setcheckinterval(interval: int) -> None: ...  # deprecated
+#def setdlopenflags(n: int) -> None: ...  # Linux only
+def setprofile(profilefunc: Any) -> None: ... # TODO type
+def setrecursionlimit(limit: int) -> None: ...
+def setswitchinterval(interval: float) -> None: ...
+def settrace(tracefunc: Any) -> None: ... # TODO type
 # Trace functions should have three arguments: frame, event, and arg. frame
 # is the current stack frame. event is a string: 'call', 'line', 'return',
 # 'exception', 'c_call', 'c_return', or 'c_exception'. arg depends on the
 # event type.
-def settscdump(on_flag: bool) -> None: pass
+def settscdump(on_flag: bool) -> None: ...
