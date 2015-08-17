a24149f75b3fc096d47ab011fa90401c3f1b4921
Della Anjeh <dellaanjeh@Dellas-MacBook-Air.local>
2015-08-17 14:45:45 -0700

    adds methods to collections.pyi in 2.7

--- stubs/2.7/collections.pyi
+++ stubs/2.7/collections.pyi
@@ -1,24 +1,34 @@
+# Stubs for collections
+
+# Based on http://docs.python.org/3.2/library/collections.html
+
+# TODO UserDict
+# TODO UserList
+# TODO UserString
+# TODO more abstract base classes (interfaces in mypy)
+
 # NOTE: These are incomplete!
 
-from typing import Dict, Generic, TypeVar, Iterable, Tuple, Callable, Mapping, overload, Iterator, Sized, Optional
+from typing import Dict, Generic, TypeVar, Iterable, Tuple, Callable, Mapping, overload, Iterator, Sized, Optional, List, Set, Sequence, Union
 
 # namedtuple is special-cased in the type checker; the initializer is ignored.
 namedtuple = object()
 
-# TODO
-class MutableMapping(object):
-    pass
-
-# TODO
-class OrderedDict(object):
-    pass
+import typing
 
 _T = TypeVar('_T')
 _KT = TypeVar('_KT')
 _VT = TypeVar('_VT')
 
+# namedtuple is special-cased in the type checker; the initializer is ignored.
+namedtuple = object()
+
+MutableMapping = typing.MutableMapping
+
 class deque(Sized, Iterable[_T], Generic[_T]):
-    maxlen = 0 # type: Optional[int] # TODO readonly
+    @property
+    def maxlen(self) -> Optional[int]: ...
+    
     def __init__(self, iterable: Iterable[_T] = None,
                  maxlen: int = None) -> None: ...
     def append(self, x: _T) -> None: ...
@@ -43,6 +53,41 @@ class deque(Sized, Iterable[_T], Generic[_T]):
     def __contains__(self, o: _T) -> bool: ...
 
     # TODO __reversed__
+
+
+class Counter(Dict[_T, int], Generic[_T]):
+    @overload
+    def __init__(self) -> None: ...
+    @overload
+    def __init__(self, Mapping: Mapping[_T, int]) -> None: ...
+    @overload
+    def __init__(self, iterable: Iterable[_T]) -> None: ...
+    # TODO keyword arguments
+
+    def elements(self) -> Iterator[_T]: ...
+
+    @overload
+    def most_common(self) -> List[_T]: ...
+    @overload
+    def most_common(self, n: int) -> List[_T]: ...
+
+    @overload
+    def subtract(self, Mapping: Mapping[_T, int]) -> None: ...
+    @overload
+    def subtract(self, iterable: Iterable[_T]) -> None: ...
+
+    # The Iterable[Tuple[...]] argument type is not actually desirable
+    # (the tuples will be added as keys, breaking type safety) but
+    # it's included so that the signature is compatible with
+    # Dict.update. Not sure if we should use '# type: ignore' instead
+    # and omit the type from the union.
+    def update(self, m: Union[Mapping[_T, int],
+                              Iterable[Tuple[_T, int]],
+                              Iterable[_T]]) -> None: ...
+
+class OrderedDict(Dict[_KT, _VT], Generic[_KT, _VT]):
+    def popitem(self, last: bool = True) -> Tuple[_KT, _VT]: ...
+    def move_to_end(self, key: _KT, last: bool = True) -> None: ...
 
 
 class defaultdict(Dict[_KT, _VT], Generic[_KT, _VT]):
