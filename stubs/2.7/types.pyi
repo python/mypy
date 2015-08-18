45f26a94f14cd446f7834243651e7add6ffcbd4b
Jing Wang <jing@dropbox.com>
2015-08-17 17:52:00 -0700

    Add gflags stubs

---
+++ stubs/2.7/types.pyi
@@ -0,0 +1,8 @@
+# Stubs for types
+
+from typing import Any
+
+class ModuleType:
+    __name__ = ... # type: str
+    __file__ = ... # type: str
+    def __init__(self, name: str, doc: Any) -> None: ...
