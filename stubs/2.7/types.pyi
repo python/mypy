9c4e10a54b39821fc9bb0b4785bd9c4da4226b3c
Li Haoyi <haoyi@dropbox.com>
2015-08-18 12:11:08 -0700

    Added traceback standard library module stubs

--- stubs/2.7/types.pyi
+++ stubs/2.7/types.pyi
@@ -6,3 +6,9 @@ class ModuleType:
     __name__ = ... # type: str
     __file__ = ... # type: str
     def __init__(self, name: str, doc: Any) -> None: ...
+
+class TracebackType:
+    ...
+
+class FrameType:
+    ...
