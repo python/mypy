1272aa054fdd8487a9da503a1da949d507c4361d
Michael Pokorny <prvak@dropbox.com>
2015-08-19 11:14:50 -0700

    minor stuff

--- stubs/2.7/UserDict.pyi
+++ stubs/2.7/UserDict.pyi
@@ -1,4 +1,7 @@
-from typing import Dict, Generic, Mapping
+from typing import Dict, Generic, Mapping, TypeVar
+
+_KT = TypeVar('_KT')
+_VT = TypeVar('_VT')
 
 class UserDict(Dict[_KT, _VT], Generic[_KT, _VT]):
     data = ... # type: Mapping[_KT, _VT]
