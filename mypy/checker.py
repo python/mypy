1b19e04d6cfb4b69ea8e179561018760d14e58f3
Eric Price <ecprice@mit.edu>
2015-08-22 13:05:56 -0700

    Fix bug in binder

--- mypy/checker.py
+++ mypy/checker.py
@@ -180,7 +180,7 @@ class ConditionalTypeBinder:
         """
         result = self.frames.pop()
 
-        options = self.frames_on_escape.get(len(self.frames) - 1, [])
+        options = self.frames_on_escape.pop(len(self.frames) - 1, [])
         if canskip:
             options.append(self.frames[-1])
         if fallthrough:
