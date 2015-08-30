0c3c8416bd1b6b6fcf43552150a63fd9475e8060
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2015-08-30 10:36:39 -0700

    Test variable-length tuple indexing

--- mypy/test/data/fixtures/tuple.py
+++ mypy/test/data/fixtures/tuple.py
@@ -8,12 +8,14 @@ class object:
     def __init__(self): pass
 
 class type: pass
-class tuple(Generic[Tco]): pass
+class tuple(Generic[Tco]):
+    def __getitem__(self, x: int) -> Tco: ...
 class function: pass
 
 # We need int for indexing tuples.
 class int: pass
 class str: pass # For convenience
+class ellipsis: pass
 
 T = TypeVar('T')
 
