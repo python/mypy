5a02f5becd1f53a2f7be43dc349773651a1b9cdc
Ben Longbons <b.r.longbons@gmail.com>
2015-09-24 18:58:38 -0700

    Use multiline repr when tests fail

--- mypy/myunit/__init__.py
+++ mypy/myunit/__init__.py
@@ -39,15 +39,25 @@ def assert_false(b: bool, msg: str = None) -> None:
     if b:
         raise AssertionFailure(msg)
 
+def good_repr(obj: object) -> str:
+    if isinstance(obj, str):
+        if obj.count('\n') > 1:
+            bits = ["'''\\"]
+            for line in obj.split('\n'):
+                # force repr to use ' not ", then cut it off
+                bits.append(repr('"' + line)[2:-1])
+            bits[-1] += "'''"
+            return '\n'.join(bits)
+    return repr(obj)
 
 def assert_equal(a: object, b: object, fmt: str = '{} != {}') -> None:
     if a != b:
-        raise AssertionFailure(fmt.format(repr(a), repr(b)))
+        raise AssertionFailure(fmt.format(good_repr(a), good_repr(b)))
 
 
 def assert_not_equal(a: object, b: object, fmt: str = '{} == {}') -> None:
     if a == b:
-        raise AssertionFailure(fmt.format(repr(a), repr(b)))
+        raise AssertionFailure(fmt.format(good_repr(a), good_repr(b)))
 
 
 def assert_raises(typ: type, *rest: Any) -> None:
