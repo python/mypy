58bc74210458a53b0672f80d903a2c4256bfefa8
Ben Longbons <b.r.longbons@gmail.com>
2015-10-13 04:49:54 -0700

    Ensure that lib-python is treated as stub-only

--- runtests.py
+++ runtests.py
@@ -265,7 +265,7 @@ def add_libpython(driver: Driver) -> None:
     seen = set()  # type: Set[str]
     for version in driver.versions:
         libpython_dir = join('lib-python', version)
-        for f in find_files(libpython_dir, prefix='test_', suffix='.py'):
+        for f in find_files(libpython_dir, prefix='test_', suffix='.pyi'):
             module = file_to_module(f, libpython_dir)
             if module not in seen:
                 seen.add(module)
