bc933740511cedad2376699e10634e66907496f8
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2015-11-27 15:30:02 -0800

    Simplify test builtins
    This will speed up tests somewhat.

--- mypy/test/data/lib-stub/builtins.py
+++ mypy/test/data/lib-stub/builtins.py
@@ -1,24 +1,16 @@
-from typing import builtinclass
-
-@builtinclass
 class object:
     def __init__(self) -> None: pass
 
-@builtinclass
 class type:
     def __init__(self, x) -> None: pass
 
 # These are provided here for convenience.
-@builtinclass
 class int: pass
-@builtinclass
 class str: pass
 
 class tuple: pass
 class function: pass
 
-@builtinclass
-class ellipsis:
-    def __init__(self) -> None: pass
+class ellipsis: pass
 
 # Definition of None is implicit
