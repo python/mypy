1437bb03e335a015854da81e0bef8b8c49f4a610
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2015-11-29 09:53:26 -0800

    Add missing stub
    This was breaking test cases.

---
+++ mypy/test/data/lib-stub/__builtin__.py
@@ -0,0 +1,16 @@
+class object:
+    def __init__(self) -> None: pass
+
+class type:
+    def __init__(self, x) -> None: pass
+
+# These are provided here for convenience.
+class int: pass
+class str: pass
+
+class tuple: pass
+class function: pass
+
+class ellipsis: pass
+
+# Definition of None is implicit
