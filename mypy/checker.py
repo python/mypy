e14ee0b3658a7d0c86c6dcaa923de21233004901
David Fisher <ddfisher@dropbox.com>
2016-04-07 17:33:51 -0700

    Get type information from isinstance assertions
    Fixes #475.

--- mypy/checker.py
+++ mypy/checker.py
@@ -1709,6 +1709,16 @@ class TypeChecker(NodeVisitor[Type]):
     def visit_assert_stmt(self, s: AssertStmt) -> Type:
         self.accept(s.expr)
 
+        # If this is asserting some isinstance check, bind that type in the following code
+        true_map, _ = find_isinstance_check(
+            s.expr, self.type_map,
+            self.typing_mode_weak()
+        )
+
+        if true_map:
+            for var, type in true_map.items():
+                self.binder.push(var, type)
+
     def visit_raise_stmt(self, s: RaiseStmt) -> Type:
         """Type check a raise statement."""
         self.breaking_out = True
