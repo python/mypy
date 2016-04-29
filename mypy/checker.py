5b7f8d9680c18eac5e8e40ca29ba86571ccdb1cd
Guido van Rossum <guido@dropbox.com>
2016-04-28 17:40:52 -0700

    Avoid crash in edge case of multiple assignment. Fix #974, # 1420, #1438.

--- mypy/checker.py
+++ mypy/checker.py
@@ -1143,6 +1143,8 @@ class TypeChecker(NodeVisitor[Type]):
         if len(s.lvalues) > 1:
             # Chained assignment (e.g. x = y = ...).
             # Make sure that rvalue type will not be reinferred.
+            if s.rvalue not in self.type_map:
+                self.accept(s.rvalue)
             rvalue = self.temp_node(self.type_map[s.rvalue], s)
             for lv in s.lvalues[:-1]:
                 self.check_assignment(lv, rvalue, s.type is None)
