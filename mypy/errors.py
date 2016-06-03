703ca811d9a2d64f69c10abfc9d8cff64bdc546f
Margaret Sy <margaretsy1016@gmail.com>
2016-06-02 17:11:55 -0700

    Handle error instead of crash on file with bad encoding (#1619)
    Fixes #1521
    
    * Handle LookupError generated by decoding a file with an invalid encoding
    - Re-raise the LookupError as a slightly more helpful DecodeError
    - Catch the DecodeError and raise as CompileError so the BuildManager will
    handle it instead of crashing.
    
    * Add unit test to check for crashing on files with invalid encoding
    - In cmdline.test so the file parsing happens in build.py. The tests in
    parse-errors.test call the parse() method in parse.py directly.

--- mypy/errors.py
+++ mypy/errors.py
@@ -346,6 +346,13 @@ class CompileError(Exception):
         self.use_stdout = use_stdout
 
 
+class DecodeError(Exception):
+    """Exception raised when a file cannot be decoded due to an unknown encoding type.
+
+    Essentially a wrapper for the LookupError raised by `bytearray.decode`
+    """
+
+
 def remove_path_prefix(path: str, prefix: str) -> str:
     """If path starts with prefix, return copy of path with the prefix removed.
     Otherwise, return path. If path is None, return None.
