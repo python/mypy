564fbb4a25dd02502cc411d498fca482c53e8f17
Guido van Rossum <guido@dropbox.com>
2016-06-29 17:28:28 -0700

    Add tests for all supported literal types.

--- test-data/unit/lib-stub/builtins.py
+++ test-data/unit/lib-stub/builtins.py
@@ -9,8 +9,11 @@ class type:
 # These are provided here for convenience.
 class int:
     def __add__(self, other: 'int') -> 'int': pass
+class float: pass
+
 class str:
     def __add__(self, other: 'str') -> 'str': pass
+class bytes: pass
 
 class tuple: pass
 class function: pass
