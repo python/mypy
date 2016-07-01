bb4b1302bd76f32193309fc2866cd535a3646ee9
Michael Lee <michaellee@dropbox.com>
2016-07-01 12:25:52 -0700

    Make type(obj) return Type[...] instead of type
    This commit is a fix for #1758 -- it special-cases the single
    constructor `type` builtin function within mypy so it returns an
    instance of `typing.Type` rather then just the generic `type` object.

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -250,6 +250,11 @@ class ExpressionChecker:
             self.check_argument_types(arg_types, arg_kinds, callee,
                                       formal_to_actual, context,
                                       messages=arg_messages)
+
+            ret_val_is_type_obj = is_equivalent(callee.ret_type, self.named_type('builtins.type'))
+            if callee.is_type_obj() and (len(arg_types) == 1) and ret_val_is_type_obj:
+                callee.ret_type = TypeType(arg_types[0])
+
             if callable_node:
                 # Store the inferred callable type.
                 self.chk.store_type(callable_node, callee)
