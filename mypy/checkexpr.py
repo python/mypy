1c849da3464c152cd1f903b263cdd0ccccee4a3d
Michael Lee <michael.lee.0x2a@gmail.com>
2016-07-03 10:13:13 -0700

    Adjust long line continuation

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -251,8 +251,8 @@ class ExpressionChecker:
                                       formal_to_actual, context,
                                       messages=arg_messages)
 
-            if callee.is_type_obj() and (len(arg_types) == 1) and \
-                    is_equivalent(callee.ret_type, self.named_type('builtins.type')):
+            if (callee.is_type_obj() and (len(arg_types) == 1) 
+                    and is_equivalent(callee.ret_type, self.named_type('builtins.type'))):
                 callee = callee.copy_modified(ret_type=TypeType(arg_types[0]))
 
             if callable_node:
