89763f56cf4695ba36643f5c3a2d302f2de8e9f5
Michael Lee <michaellee@dropbox.com>
2016-07-06 16:51:27 -0700

    Add missing import

--- mypy/test/data.py
+++ mypy/test/data.py
@@ -6,7 +6,7 @@ import re
 from os import remove, rmdir
 import shutil
 
-from typing import Callable, List, Tuple, Set
+from typing import Callable, List, Tuple, Set, Optional
 
 from mypy.myunit import TestCase, SkipTestCaseException
 
