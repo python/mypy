49f4de20c5dd3d53f703821b593d90d5b102a625
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2016-07-22 16:49:47 +0100

    Don't crash when we can't determine MRO
    Fix #1794.

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -2675,7 +2675,7 @@ def calculate_class_mro(defn: ClassDef, fail: Callable[[str, Context], None]) ->
         defn.info.calculate_mro()
     except MroError:
         fail("Cannot determine consistent method resolution order "
-             '(MRO) for "%s"' % defn.name, defn)
+             '(MRO) for "%s"' % defn.name, defn, blocker=True)
         defn.info.mro = []
     # The property of falling back to Any is inherited.
     defn.info.fallback_to_any = any(baseinfo.fallback_to_any for baseinfo in defn.info.mro)
