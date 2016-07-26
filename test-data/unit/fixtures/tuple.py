868f90cf0b941039d0e85e6cf5ab585ffc3e4203
Michael Lee <michaellee@dropbox.com>
2016-07-26 14:22:50 -0700

    Fix error in tests

--- test-data/unit/fixtures/tuple.py
+++ test-data/unit/fixtures/tuple.py
@@ -32,6 +32,7 @@ class list(Iterable[T], Generic[T]):
     def __init__(self, x: Iterable[T]) -> None: pass
     def __iter__(self) -> Iterator[T]: pass
     def __mul__(self, x: int) -> list[T]: pass
+    def __add__(self, x: list[T]) -> list[T]: pass
     def __getitem__(self, x: int) -> T: pass
     def append(self, x: T) -> None: pass
     def extend(self, x: Iterable[T]) -> None: pass
