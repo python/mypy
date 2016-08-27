d76554b181c24acd4d2b3749a0f932276d6504cf
johnthagen <johnthagen@users.noreply.github.com>
2016-08-27 13:04:32 -0400

    Remove Python 3.2 from trove classifier and README (#2063)
    * Remove Python 3.2 trove classifier and add generic Python :: 3.  Mypy 0.4.3 dropped support for Python 3.2.
    
    * Update README to indicate Python 3.3+ required.

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ classifiers = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: POSIX',
-    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
