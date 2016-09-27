a3493df0c5357b14501178a9db72ff0e96f63fff
Guido van Rossum <guido@dropbox.com>
2016-09-27 11:14:44 -0700

    Remove stray pdb call

--- mypy/messages.py
+++ mypy/messages.py
@@ -494,7 +494,6 @@ class MessageBuilder:
             except IndexError:  # Varargs callees
                 expected_type = callee.arg_types[-1]
             arg_type_str, expected_type_str = self.format_distinctly(arg_type, expected_type)
-            if 'Tuple[]' in expected_type_str: import pdb; pdb.set_trace()
             if arg_kind == ARG_STAR:
                 arg_type_str = '*' + arg_type_str
             elif arg_kind == ARG_STAR2:
