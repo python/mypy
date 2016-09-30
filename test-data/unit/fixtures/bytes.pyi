44f5c951f895c4e272116d13aede7b85cfd886a9
Guido van Rossum <guido@dropbox.com>
2016-09-30 14:49:25 -0700

    Add the first bytes/str/unicode tests

---
+++ test-data/unit/fixtures/bytes.pyi
@@ -0,0 +1,20 @@
+class Any: pass
+
+class object:
+    def __init__(self) -> None: pass
+class type:
+    def __init__(self, x: Any) -> None: pass
+class bytes:
+    pass
+class str:
+    pass
+class unicode:
+    pass
+Text = unicode
+NativeStr = str
+
+class int: pass
+class float: pass
+class tuple: pass
+class function: pass
+class ellipsis: pass
