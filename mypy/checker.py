ef76c37f8780c5f3bb43b19f6e6b6c61ada43e5b
Elazar Gershuni <elazarg@gmail.com>
2016-10-22 21:45:52 +0300

    undo swap

--- mypy/checker.py
+++ mypy/checker.py
@@ -948,12 +948,12 @@ class TypeChecker(NodeVisitor[Type]):
         self.enter_partial_types()
         old_binder = self.binder
         self.binder = ConditionalTypeBinder()
-        if not defn.has_incompatible_baseclass:
-            # Otherwise we've already found errors; more errors are not useful
-            self.check_multiple_inheritance(typ)
         with self.binder.top_frame_context():
             self.accept(defn.defs)
         self.binder = old_binder
+        if not defn.has_incompatible_baseclass:
+            # Otherwise we've already found errors; more errors are not useful
+            self.check_multiple_inheritance(typ)
         self.leave_partial_types()
         self.errors.pop_type()
 
