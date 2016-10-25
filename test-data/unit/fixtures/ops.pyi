257c0768d6426c4a76b5ae2053a474b3286605e6
David Fisher <ddf1991@gmail.com>
2016-10-24 19:35:10 -0700

    Understand that types are not Optional if they're equal to a non-Optional type (#2313)

--- test-data/unit/fixtures/ops.pyi
+++ test-data/unit/fixtures/ops.pyi
@@ -51,8 +51,8 @@ class float: pass
 
 class BaseException: pass
 
-True = None # type: bool
-False = None # type: bool
+True = bool() # type: bool
+False = bool() # type: bool
 
 def __print(a1=None, a2=None, a3=None, a4=None): pass
 
