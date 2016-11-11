56e663cc501407ae0b764043ee6444cbb434445d
JdeH <info@qquick.org>
2016-11-11 14:31:50 +0100

    Output for std_out and std_err returned separately

--- mypy/api.py
+++ mypy/api.py
@@ -1,28 +1,57 @@
-# This module makes it possible to use mypy as part of a Python application.
-#
-# Since mypy still changes, the API was kept utterly simple and non-intrusive.
-# It just mimics command line activation without starting a new interpreter.
-# So the normal docs about the mypy command line apply.
-# Changes in the command line version of mypy will be immediately useable.
-#
-# Just import this module and then call the 'run' function with exactly the
-# string you would have passed to mypy from the command line.
-# Function 'run' returns all reporting info that's normally printed.
-# Any pretty formatting is left to the caller.
+'''
+This module makes it possible to use mypy as part of a Python application.
+
+Since mypy still changes, the API was kept utterly simple and non-intrusive.
+It just mimics command line activation without starting a new interpreter.
+So the normal docs about the mypy command line apply.
+Changes in the command line version of mypy will be immediately useable.
+
+Just import this module and then call the 'run' function with exactly the
+string you would have passed to mypy from the command line.
+Function 'run' returns a tuple of strings: (<normal_report>, <error_report>),
+in which <normal_report> is what mypy normally writes to sys.stdout and
+<error_report> is what mypy normally writes to sys.stderr.
+Any pretty formatting is left to the caller.
+
+Trivial example of code using this module:
 
 import sys
+from mypy import api
+
+result = api.run (' '.join (sys.argv [1:]))
+
+if result [0]:
+    print ('\nType checking report:\n')
+    print (result [0])  # stdout
+
+if result [1]:
+    print ('\nError report:\n')
+    print (result [1])  # stderr
+'''
+
+import sys
+import traceback
 from io import StringIO
 from mypy.main import main
 
 def run (params):
-	sys.argv = [None] + params.split ()
-	string_io = StringIO ()
-	old_stdout = sys.stdout
-	sys.stdout = string_io
-	try:
-		main (None)
-	except Exception as exception:
-		print (exception)
-	sys.stdout = old_stdout
-	return string_io.getvalue ()
-	
+    sys.argv = [None] + params.split ()
+
+    old_stdout = sys.stdout
+    new_stdout = StringIO ()
+    sys.stdout = new_stdout
+
+    old_stderr = sys.stderr
+    new_stderr = StringIO ()
+    sys.stderr = new_stderr
+
+    try:
+        main (None)
+    except:
+        pass
+        
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    
+    return (new_stdout.getvalue (), new_stderr.getvalue ())
+    
