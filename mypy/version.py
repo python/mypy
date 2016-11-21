d5410b1e62ca4deb94351220f00c50edec37b0e4
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2016-11-21 20:50:28 +0000

    Bump version to 0.4.7-dev

--- mypy/version.py
+++ mypy/version.py
@@ -1,7 +1,7 @@
 import os
 from mypy import git
 
-__version__ = '0.4.6'
+__version__ = '0.4.7-dev'
 base_version = __version__
 
 mypy_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
