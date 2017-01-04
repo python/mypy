622c9ee4903b74a0cce17d2b636325cb0fda0fc4
David Fisher <ddfisher@dropbox.com>
2017-01-04 13:11:03 -0800

    fix whitespace errors

--- extensions/mypy_extensions.py
+++ extensions/mypy_extensions.py
@@ -22,8 +22,10 @@ def _check_fails(cls, other):
         pass
     return False
 
+
 def _dict_new(cls, *args, **kwargs):
     return dict(*args, **kwargs)
+
 
 def _typeddict_new(cls, _typename, _fields=None, **kwargs):
     if _fields is None:
@@ -32,6 +34,7 @@ def _typeddict_new(cls, _typename, _fields=None, **kwargs):
         raise TypeError("TypedDict takes either a dict or keyword arguments,"
                         " but not both")
     return _TypedDictMeta(_typename, (), {'__annotations__': dict(_fields)})
+
 
 class _TypedDictMeta(type):
     def __new__(cls, name, bases, ns):
@@ -88,6 +91,7 @@ TypedDict.__doc__ = \
     The latter syntax is only supported in Python 3.6+, while two other
     syntax forms work for Python 2.7 and 3.2+
     """
+
 
 # Return type that indicates a function does not return
 class NoReturn: pass
