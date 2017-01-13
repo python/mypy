fb622e88e71a8cbcabfbaea3685fbd301484baf1
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2017-01-13 17:30:28 +0000

    Bump mypy_extensions version to 0.1.0

--- extensions/setup.py
+++ extensions/setup.py
@@ -4,7 +4,7 @@
 
 from distutils.core import setup
 
-version = '0.1-dev'
+version = '0.1.0'
 description = 'Experimental type system extensions for programs checked with the mypy typechecker.'
 long_description = '''
 Mypy Extensions
