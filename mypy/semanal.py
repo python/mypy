d5fb9e0a65feb78cc0b0b9c385c17238282ff50d
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-01-17 06:04:09 -0800

    Accept unicode docstrings in Python 2
    Closes #2471

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -2828,7 +2828,16 @@ class FirstPass(NodeVisitor):
 
         # Add implicit definitions of module '__name__' etc.
         for name, t in implicit_module_attrs.items():
-            v = Var(name, UnboundType(t))
+            # Issue #2471: unicode docstrings should be accepted in Python 2
+            if name == '__doc__':
+                if self.pyversion < (3, 0):
+                    typ = UnionType([UnboundType('__builtins__.str'),
+                                     UnboundType('__builtins__.unicode')])
+                else:
+                    typ = UnboundType('__builtins__.str')
+            else:
+                typ = UnboundType(t)
+            v = Var(name, typ)
             v._fullname = self.sem.qualified_name(name)
             self.sem.globals[name] = SymbolTableNode(GDEF, v, self.sem.cur_mod_id)
 
