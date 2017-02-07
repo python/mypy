45be745825ce725281dfc46d1408835028ce8bc4
elazar <elazarg@gmail.com>
2017-02-07 17:06:06 +0200

    check for fallback only for type objects

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -2054,7 +2054,7 @@ class ExpressionChecker(ExpressionVisitor[Type]):
         # TODO TupleType => also consider tuple attributes
         if isinstance(typ, Instance):
             return typ.type.has_readable_member(member)
-        if isinstance(typ, CallableType):
+        if isinstance(typ, CallableType) and typ.is_type_obj():
             return typ.fallback.type.has_readable_member(member)
         elif isinstance(typ, AnyType):
             return True
