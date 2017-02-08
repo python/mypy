4d67292033bd3f93ab7dd0c2b1ee4fdf86030b96
elazar <elazarg@gmail.com>
2017-02-08 16:05:35 +0200

    make sure Any becomes AnyType()

--- mypy/types.py
+++ mypy/types.py
@@ -384,6 +384,7 @@ class Instance(Type):
 
     def __init__(self, typ: Optional[mypy.nodes.TypeInfo], args: List[Type],
                  line: int = -1, column: int = -1, erased: bool = False) -> None:
+        assert(typ is None or typ.fullname() not in ["builtins.Any", "typing.Any"])
         self.type = typ
         self.args = args
         self.erased = erased
