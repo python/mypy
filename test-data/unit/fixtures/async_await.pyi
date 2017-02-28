4ac90a75f07616704a7905d32635b7cfbde13dd1
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-02-28 13:53:23 -0800

    Tests for https://github.com/python/mypy/issues/2924

--- test-data/unit/fixtures/async_await.pyi
+++ test-data/unit/fixtures/async_await.pyi
@@ -5,5 +5,12 @@ class type: pass
 class function: pass
 class int: pass
 class str: pass
+class dict: pass
 class list: pass
+class set: pass
 class tuple: pass
+class BaseException: pass
+class StopIteration(BaseException): pass
+class StopAsyncIteration(BaseException): pass
+def iter(obj: typing.Any) -> typing.Any: pass
+def next(obj: typing.Any) -> typing.Any: pass
