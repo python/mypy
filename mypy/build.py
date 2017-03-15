1aeda229a1cbbeeddb8abd85456f9f3f6d03aea4
Guido van Rossum <guido@dropbox.com>
2017-03-15 09:23:18 -0700

    Can't use TYPE_CHECKING because it's not in the Python 3.5.1 stdlib

--- mypy/build.py
+++ mypy/build.py
@@ -21,8 +21,10 @@ import time
 from os.path import dirname, basename
 
 from typing import (AbstractSet, Dict, Iterable, Iterator, List,
-                    NamedTuple, Optional, Set, Tuple, Union, TYPE_CHECKING)
-if TYPE_CHECKING:
+                    NamedTuple, Optional, Set, Tuple, Union)
+# Can't use TYPE_CHECKING because it's not in the Python 3.5.1 stdlib
+MYPY = False
+if MYPY:
     from typing import Deque
 
 from mypy.nodes import (MypyFile, Node, ImportBase, Import, ImportFrom, ImportAll)
