f430c6586e8215dbbf66a7be2362ae4bcec3b135
Ivan Levkivskyi <levkivslyi@gmail.com>
2017-03-27 16:02:42 +0200

    Fix typo

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -660,7 +660,7 @@ class SemanticAnalyzer(NodeVisitor):
     def analyze_class_decorator(self, defn: ClassDef, decorator: Expression) -> bool:
         decorator.accept(self)
         return (isinstance(decorator, RefExpr) and
-                decorator.fullname in ('typing.runtime', 'mypy_extensions.runtime')
+                decorator.fullname in ('typing.runtime', 'mypy_extensions.runtime'))
 
     def calculate_abstract_status(self, typ: TypeInfo) -> None:
         """Calculate abstract status of a class.
