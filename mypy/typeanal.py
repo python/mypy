12def8b72f297b529175e353371b3ede64657948
Naomi Seyfer <naomi@seyfer.org>
2017-04-01 10:45:30 -0700

    Typo fix

--- mypy/typeanal.py
+++ mypy/typeanal.py
@@ -45,7 +45,7 @@ def analyze_type_alias(node: Expression,
     if isinstance(node, RefExpr):
         # Note that this misses the case where someone tried to use a
         # class-referenced type variable as a type alias.  It's easier to catch
-        # that one in checkmemeber.py
+        # that one in checkmember.py
         if node.kind == UNBOUND_TVAR or node.kind == BOUND_TVAR:
             fail_func('Type variable "{}" is invalid as target for type alias'.format(
                 node.fullname), node)
