b3578a516ded0506ddc902fb290b938763e9d1b0
Guido van Rossum <guido@dropbox.com>
2017-04-04 16:00:20 -0700

    Always write the cache (unless cache_dir is /dev/null)

--- mypy/build.py
+++ mypy/build.py
@@ -891,7 +891,12 @@ def write_cache(id: str, path: str, tree: MypyFile,
         manager.trace("Interface for {} has changed".format(id))
 
     # Obtain and set up metadata
-    st = manager.get_stat(path)  # TODO: Handle errors
+    try:
+        st = manager.get_stat(path)
+    except OSError:
+        manager.log("Cannot write {}".format(path))
+        return interface_hash
+
     mtime = st.st_mtime
     size = st.st_size
     options = manager.options.clone_for_module(id)
@@ -1523,26 +1528,26 @@ class State:
         return valid_refs
 
     def write_cache(self) -> None:
-        ok = self.path and self.options.incremental
-        if ok:
-            if self.manager.options.quick_and_dirty:
-                is_errors = self.manager.errors.is_errors_for_file(self.path)
-            else:
-                is_errors = self.manager.errors.is_errors()
-            ok = not is_errors
-        if ok:
-            dep_prios = [self.priorities.get(dep, PRI_HIGH) for dep in self.dependencies]
-            new_interface_hash = write_cache(
-                self.id, self.path, self.tree,
-                list(self.dependencies), list(self.suppressed), list(self.child_modules),
-                dep_prios, self.interface_hash,
-                self.manager)
-            if new_interface_hash == self.interface_hash:
-                self.manager.log("Cached module {} has same interface".format(self.id))
-            else:
-                self.manager.log("Cached module {} has changed interface".format(self.id))
-                self.mark_interface_stale()
-                self.interface_hash = new_interface_hash
+        if not self.path or self.options.cache_dir == os.devnull:
+            return
+        if self.manager.options.quick_and_dirty:
+            is_errors = self.manager.errors.is_errors_for_file(self.path)
+        else:
+            is_errors = self.manager.errors.is_errors()
+        if is_errors:
+            return
+        dep_prios = [self.priorities.get(dep, PRI_HIGH) for dep in self.dependencies]
+        new_interface_hash = write_cache(
+            self.id, self.path, self.tree,
+            list(self.dependencies), list(self.suppressed), list(self.child_modules),
+            dep_prios, self.interface_hash,
+            self.manager)
+        if new_interface_hash == self.interface_hash:
+            self.manager.log("Cached module {} has same interface".format(self.id))
+        else:
+            self.manager.log("Cached module {} has changed interface".format(self.id))
+            self.mark_interface_stale()
+            self.interface_hash = new_interface_hash
 
 
 def dispatch(sources: List[BuildSource], manager: BuildManager) -> None:
