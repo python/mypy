def95ce981a8fd7b9a9fa92c4ac7f70b8e9a96c8
Max Moroz <maxmoroz@gmail.com>
2017-04-12 17:13:01 -0700

    Address CR

--- test-data/unit/lib-stub/types.pyi
+++ test-data/unit/lib-stub/types.pyi
@@ -6,29 +6,4 @@ def coroutine(func: T) -> T:
 
 class bool: ...
 
-class ModuleSpec:
-    def __init__(self, name: str, loader: Optional['Loader'], *,
-                 origin: str = None, loader_state: Any = None,
-                 is_package: bool = None) -> None: ...
-    name = ...  # type: str
-    loader = ...  # type: Optional[Loader]
-    origin = ...  # type: Optional[str]
-    submodule_search_locations = ...  # type: Optional[List[str]]
-    loader_state = ...  # type: Any
-    cached = ...  # type: Optional[str]
-    parent = ...  # type: Optional[str]
-    has_location = ...  # type: bool
-
-class Loader:
-    def load_module(self, fullname: str) -> ModuleType: ...
-    def module_repr(self, module: ModuleType) -> str: ...
-    def create_module(self, spec: ModuleSpec) -> Optional[ModuleType]: ...
-    def exec_module(self, module: ModuleType) -> None: ...
-
-class ModuleType:
-    __name__ = ...  # type: str
-    __file__ = ...  # type: str
-    __loader__ = ...  # type: Optional[Loader]
-    __package__ = ...  # type: Optional[str]
-    __spec__ = ...  # type: Optional[ModuleSpec]
-    def __init__(self, name: str, doc: Optional[str] = ...) -> None: ...
+class ModuleType: ...
