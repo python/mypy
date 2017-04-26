c4efb8938feebc1d2e0e0bc9c4e3c6892a8aafad
Daniel F Moisset <dmoisset@machinalis.com>
2017-04-26 17:11:47 +0100

    Add type.pyi fixture to make test pass

--- mypy/checkmember.py
+++ mypy/checkmember.py
@@ -181,8 +181,8 @@ def analyze_member_access(name: str,
             ignore_messages = msg.copy()
             ignore_messages.disable_errors()
             return analyze_member_access(name, fallback, node, is_lvalue, is_super,
-                                     is_operator, builtin_type, not_ready_callback, ignore_messages,
-                                     original_type=original_type, chk=chk)
+                                     is_operator, builtin_type, not_ready_callback,
+                                     ignore_messages, original_type=original_type, chk=chk)
         elif isinstance(typ.item, TypeVarType):
             if isinstance(typ.item.upper_bound, Instance):
                 item = typ.item.upper_bound
