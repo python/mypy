d7c69a083cef1859d640a7236b6c5747059c6eac
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2017-05-05 14:25:39 +0100

    Fix serialization and deserialization of metaclasses
    This has some overlap with #3304.

--- mypy/fixup.py
+++ mypy/fixup.py
@@ -66,6 +66,10 @@ class NodeFixer(NodeVisitor[None]):
                 info.tuple_type.accept(self.type_fixer)
             if info.typeddict_type:
                 info.typeddict_type.accept(self.type_fixer)
+            if info.declared_metaclass:
+                info.declared_metaclass.accept(self.type_fixer)
+            if info.metaclass_type:
+                info.metaclass_type.accept(self.type_fixer)
         finally:
             self.current_info = save_info
 
