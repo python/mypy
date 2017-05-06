1ce917d165f4cb62810ec5e7636c1c5d61920b03
Naomi Seyfer <naomi@seyfer.org>
2017-05-06 10:04:26 -0700

    Fix for crash on decorated non-overload function redefinition

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -511,6 +511,10 @@ class SemanticAnalyzer(NodeVisitor):
             defn.type = Overloaded(types)
             defn.type.line = defn.line
 
+        if not defn.items:
+            return # It was not any kind of overload def after all. We've
+                   # visited the redfinitions already.
+
         if self.is_class_scope():
             self.type.names[defn.name()] = SymbolTableNode(MDEF, defn,
                                                            typ=defn.type)
