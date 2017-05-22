5b077aeb19f351f2b4e8293d71c1672855928054
Max Moroz <maxmoroz@gmail.com>
2017-05-22 14:39:01 -0700

    CR fix

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -3327,7 +3327,7 @@ class SemanticAnalyzer(NodeVisitor):
     def name_already_defined(self, name: str, ctx: Context,
                              original_ctx: Optional[SymbolTableNode] = None) -> None:
         if original_ctx:
-            extra_msg = ' in line {}'.format(original_ctx.node.get_line())
+            extra_msg = ' on line {}'.format(original_ctx.node.get_line())
         else:
             extra_msg = ''
         self.fail("Name '{}' already defined{}".format(name, extra_msg), ctx)
