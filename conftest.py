39ca9364436f622dae35b907cdac471469a1e92a
Greg Price <gnprice@gmail.com>
2017-05-23 14:22:49 -0700

    conftest.py: Fix lint
    Oops!  My #3426 had passed the Appveyor build, and Travis
    was backed up, so I pushed it.  But apparently we run the
    linter only in Travis and not Appveyor (quite sensibly
    running it only in one place.)  So I missed a lint issue
    that broke the build.
    
    Fix that.

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ pytest_plugins = [
     'mypy.test.data',
 ]
 
+
 def pytest_configure(config):
     mypy_source_root = os.path.dirname(os.path.abspath(__file__))
     if os.getcwd() != mypy_source_root:
