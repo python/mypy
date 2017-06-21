fa909c19ce47794500b713dd624c3cb8bc4d8f67
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2017-06-21 17:09:38 +0100

    Don't pass None as the full name to a plugin
    We would sometimes pass `None` as the full name of a function to a
    plugin, which could cause a crash within a plugin that doesn't expect
    it.

--- test-data/unit/plugins/fnplugin.py
+++ test-data/unit/plugins/fnplugin.py
@@ -4,6 +4,7 @@ class MyPlugin(Plugin):
     def get_function_hook(self, fullname):
         if fullname == '__main__.f':
             return my_hook
+        assert fullname is not None
         return None
 
 def my_hook(ctx):
