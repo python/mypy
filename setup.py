ccb4cba6d263626d0d3da5ba9517356d80d8b2ec
Thomas Grainger <tom@procensus.com>
2017-07-12 11:54:39 +0100

    move typeshed to external package

--- setup.py
+++ setup.py
@@ -99,8 +99,11 @@ package_dir = {'mypy': 'mypy'}
 # E.g. "pip3 install ." or
 # "pip3 install git+git://github.com/python/mypy.git"
 # (as suggested by README.md).
-install_requires = []
-install_requires.append('typed-ast >= 1.0.4, < 1.1.0')
+install_requires = [
+    'typed-ast >= 1.0.4, < 1.1.0',
+    'typeshed >= 0.0.1, < 0.1.0',
+]
+
 if sys.version_info < (3, 5):
     install_requires.append('typing >= 3.5.3')
 
