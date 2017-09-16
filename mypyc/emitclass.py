eb4f587311e62835560109d7a0f89cad7ebebbb6
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2017-09-16 15:11:36 +0100

    Fix reference counting problem in getters

--- mypyc/emitclass.py
+++ mypyc/emitclass.py
@@ -287,6 +287,7 @@ def generate_getter(cl: ClassIR,
                                                                         repr(cl.name)))
     emitter.emit_line('return NULL;')
     emitter.emit_line('}')
+    emitter.emit_inc_ref('self->{}'.format(attr), rtype)
     emitter.emit_box('self->{}'.format(attr), 'retval', rtype, 'abort();', declare_dest=True)
     emitter.emit_line('return retval;')
     emitter.emit_line('}')
