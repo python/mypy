2108a0350b485a441859a28c1cf82ebb51c9553c
elazar <elazarg@gmail.com>
2017-09-24 21:51:50 +0300

    revert uncalled-for fix

--- mypy/types.py
+++ mypy/types.py
@@ -5,7 +5,7 @@ from abc import abstractmethod
 from collections import OrderedDict
 from typing import (
     Any, TypeVar, Dict, List, Tuple, cast, Generic, Set, Optional, Union, Iterable, NamedTuple,
-    Callable,
+    Callable
 )
 
 import mypy.nodes
