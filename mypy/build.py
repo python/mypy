65df983e363faf53972bc354f6b083dc32533eb6
Ethan Smith <ethan@ethanhs.me>
2017-09-25 13:01:15 -0700

    make prefixes a list

--- mypy/build.py
+++ mypy/build.py
@@ -215,7 +215,7 @@ def default_data_dir(bin_dir: Optional[str]) -> str:
     """
     if not bin_dir:
         if os.name == 'nt':
-            prefixes = os.path.join(sys.prefix, 'Lib'), os.path.join(site.getuserbase(), 'lib')
+            prefixes = [os.path.join(sys.prefix, 'Lib'), os.path.join(site.getuserbase(), 'lib')]
             for parent in prefixes:
                     data_dir = os.path.join(parent, 'mypy')
                     if os.path.exists(data_dir):
