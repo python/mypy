98c0f33ab6c5c394a3596b9f70fd01ab22f7956c
Ethan Smith <ethan@ethanhs.me>
2017-09-27 16:03:44 -0700

    Move pythoneval to use mypy.api.run
    This replaces the old subprocess based method. In addition, the test cases
    are no longer run, in order to reduce test time. This has led to a 10-20%
    speedup in pytest. This might help with #3895, as it removes the
    subprocesses.
    
    Fixes #1671

--- mypy/test/testpythoneval.py
+++ mypy/test/testpythoneval.py
@@ -10,12 +10,9 @@ Note: These test cases are *not* included in the main test suite, as including
       this suite would slow down the main suite too much.
 """
 
-from contextlib import contextmanager
-import errno
 import os
 import os.path
 import re
-import subprocess
 import sys
 
 import pytest  # type: ignore  # no pytest in typeshed
@@ -25,6 +22,7 @@ from mypy.test.config import test_data_prefix, test_temp_dir
 from mypy.test.data import DataDrivenTestCase, parse_test_cases, DataSuite
 from mypy.test.helpers import assert_string_arrays_equal
 from mypy.util import try_find_python2_interpreter
+from mypy.api import run
 
 # Files which contain test case descriptions.
 python_eval_files = ['pythoneval.test',
@@ -61,73 +59,38 @@ def test_python_evaluation(testcase: DataDrivenTestCase) -> None:
     version.
     """
     assert testcase.old_cwd is not None, "test was not properly set up"
-    mypy_cmdline = [
-        python3_path,
-        os.path.join(testcase.old_cwd, 'scripts', 'mypy'),
-        '--show-traceback',
-    ]
+    mypy_cmdline = ['--show-traceback']
     py2 = testcase.name.lower().endswith('python2')
     if py2:
         mypy_cmdline.append('--py2')
-        interpreter = try_find_python2_interpreter()
-        if interpreter is None:
-            # Skip, can't find a Python 2 interpreter.
-            pytest.skip()
-            # placate the type checker
-            return
-    else:
-        interpreter = python3_path
-
     # Write the program to a file.
     program = '_' + testcase.name + '.py'
-    mypy_cmdline.append(program)
     program_path = os.path.join(test_temp_dir, program)
+    mypy_cmdline.append(program_path)
     with open(program_path, 'w') as file:
         for s in testcase.input:
             file.write('{}\n'.format(s))
     # Type check the program.
     # This uses the same PYTHONPATH as the current process.
-    returncode, out = run(mypy_cmdline)
-    if returncode == 0:
-        # Execute the program.
-        returncode, interp_out = run([interpreter, program])
-        out += interp_out
+    out, err, returncode = run(mypy_cmdline)
+    output = split_lines(out, err)
     # Remove temp file.
     os.remove(program_path)
-    assert_string_arrays_equal(adapt_output(testcase), out,
+    assert_string_arrays_equal(adapt_output(testcase), output,
                                'Invalid output ({}, line {})'.format(
                                    testcase.file, testcase.line))
 
 
-def split_lines(*streams: bytes) -> List[str]:
+def split_lines(*streams: str) -> List[str]:
     """Returns a single list of string lines from the byte streams in args."""
     return [
         s.rstrip('\n\r')
         for stream in streams
-        for s in str(stream, 'utf8').splitlines()
+        for s in stream.splitlines()
     ]
 
 
 def adapt_output(testcase: DataDrivenTestCase) -> List[str]:
     """Translates the generic _program.py into the actual filename."""
     program = '_' + testcase.name + '.py'
-    return [program_re.sub(program, line) for line in testcase.output]
-
-
-def run(
-    cmdline: List[str], *, env: Optional[Dict[str, str]] = None, timeout: int = 30
-) -> Tuple[int, List[str]]:
-    """A poor man's subprocess.run() for 3.3 and 3.4 compatibility."""
-    process = subprocess.Popen(
-        cmdline,
-        env=env,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        cwd=test_temp_dir,
-    )
-    try:
-        out, err = process.communicate(timeout=timeout)
-    except subprocess.TimeoutExpired:
-        out = err = b''
-        process.kill()
-    return process.returncode, split_lines(out, err)
+    return [test_temp_dir + os.sep + program_re.sub(program, line) for line in testcase.output]
