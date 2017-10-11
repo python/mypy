02e495c1f9648905864e6330362a387d368d7f24
Ivan Levkivskyi <levkivslyi@gmail.com>
2017-10-11 20:22:44 +0200

    Add explanatory assert message

--- mypy/checker.py
+++ mypy/checker.py
@@ -3326,7 +3326,7 @@ class Scope:
         top = self.top_function()
         assert top, "This method must be called from inside a function"
         index = self.stack.index(top)
-        assert index
+        assert index, "Scope stack must always start with a module"
         enclosing = self.stack[index - 1]
         if isinstance(enclosing, TypeInfo):
             return enclosing
