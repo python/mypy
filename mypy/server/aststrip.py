af5fa9fe411c952e3af3a57f2814d15b842cc8e4
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2017-11-16 17:12:33 +0000

    Fix merge problem in AST strip

--- mypy/server/aststrip.py
+++ mypy/server/aststrip.py
@@ -82,15 +82,16 @@ class NodeStripVisitor(TraverserVisitor):
         if node.assignments:
             node.assignments = []
         else:
-            # Reset entries in the symbol table. This is necessary since
-            # otherwise the semantic analyzer will think that the import
-            # assigns to an existing name instead of defining a new one.
-            for name, as_name in node.ids:
-                imported_name = as_name or name
-                initial = imported_name.split('.')[0]
-                symnode = self.names[initial]
-                symnode.kind = UNBOUND_IMPORTED
-                symnode.node = None
+            if self.names:
+                # Reset entries in the symbol table. This is necessary since
+                # otherwise the semantic analyzer will think that the import
+                # assigns to an existing name instead of defining a new one.
+                for name, as_name in node.ids:
+                    imported_name = as_name or name
+                    initial = imported_name.split('.')[0]
+                    symnode = self.names[initial]
+                    symnode.kind = UNBOUND_IMPORTED
+                    symnode.node = None
 
     def visit_name_expr(self, node: NameExpr) -> None:
         # Global assignments are processed in semantic analysis pass 1, and we
