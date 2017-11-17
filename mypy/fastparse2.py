e9fc69d84f65d89a57f19d8eca8979c5d3de7f68
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-11-17 01:53:08 +0100

    Fix crash in relative imports (#4259)
    Fixes #4111
    
    This corrects a previous wrong --strict-optional change (and adds various tests).

--- mypy/fastparse2.py
+++ mypy/fastparse2.py
@@ -647,8 +647,8 @@ class ASTConverter(ast27.NodeTransformer):
     def visit_ImportFrom(self, n: ast27.ImportFrom) -> ImportBase:
         assert n.level is not None
         if len(n.names) == 1 and n.names[0].name == '*':
-            assert n.module is not None
-            i = ImportAll(n.module, n.level)  # type: ImportBase
+            mod = n.module if n.module is not None else ''
+            i = ImportAll(mod, n.level)  # type: ImportBase
         else:
             i = ImportFrom(self.translate_module_id(n.module) if n.module is not None else '',
                            n.level,
