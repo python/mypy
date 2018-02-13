bf49a874224f480f55d2b277fdbcce4299eb523a
David Euresti <david@euresti.com>
2018-02-12 22:46:47 -0800

    Add more incremental tests

--- mypy/plugins/attrs.py
+++ mypy/plugins/attrs.py
@@ -367,6 +367,7 @@ def _add_cmp(ctx: 'mypy.plugin.ClassDefContext', adder: 'MethodAdder') -> None:
 
 def _make_frozen(ctx: 'mypy.plugin.ClassDefContext', attributes: List[Attribute]) -> None:
     """Turn all the attributes into properties to simulate frozen classes."""
+    # TODO: Handle subclasses of frozen classes.
     for attribute in attributes:
         node = ctx.cls.info.names[attribute.name].node
         assert isinstance(node, Var)
