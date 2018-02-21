84fd527722a8479a588d13d4a617eb8e23f7899e
Ethan Smith <ethan@ethanhs.me>
2018-02-20 16:20:10 -0800

    Make test package typesafe to avoid error

--- test-data/packages/typedpkg/typedpkg/sample.py
+++ test-data/packages/typedpkg/typedpkg/sample.py
@@ -3,4 +3,4 @@ from typing import Iterable, Tuple
 
 def ex(a: Iterable[str]) -> Tuple[str, ...]:
     """Example typed package. This intentionally has an error."""
-    return a + ('Hello')
+    return tuple(a)
