d5df3259e1941346a93cc62dcf259958c9e4d400
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2018-02-21 11:53:14 +0000

    Fix callable types with inconsistent argument counts
    This fixes potential issues with callable types where there are a
    different number of argument names compared to argument types. These
    malformed callable types might have caused crashes.
    
    This may address #4599. Even if the issue remains unfixed, future
    tracebacks could be more useful due to an assert that I added.

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -3904,6 +3904,8 @@ class SemanticAnalyzerPass2(NodeVisitor[None], SemanticAnalyzerPluginInterface):
 
 def replace_implicit_first_type(sig: FunctionLike, new: Type) -> FunctionLike:
     if isinstance(sig, CallableType):
+        if len(sig.arg_types) == 0:
+            return sig
         return sig.copy_modified(arg_types=[new] + sig.arg_types[1:])
     elif isinstance(sig, Overloaded):
         return Overloaded([cast(CallableType, replace_implicit_first_type(i, new))
