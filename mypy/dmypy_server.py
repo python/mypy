55dadfc2d56206538dbc657f339b8c30065a1429
Dominik Miedziński <dominik@mdzn.pl>
2018-02-26 20:44:05 +0100

    Make psutil an optional dependency (#4634)
    If it's not installed, when running `dmypy status -v` a message will be displayed instead of the memory stats.

--- mypy/dmypy_server.py
+++ mypy/dmypy_server.py
@@ -349,10 +349,10 @@ class Server:
 MiB = 2**20
 
 
-def get_meminfo() -> Mapping[str, float]:
+def get_meminfo() -> Dict[str, Any]:
     # See https://stackoverflow.com/questions/938733/total-memory-used-by-python-process
     import resource  # Since it doesn't exist on Windows.
-    res = {}
+    res = {}  # type: Dict[str, Any]
     rusage = resource.getrusage(resource.RUSAGE_SELF)
     if sys.platform == 'darwin':
         factor = 1
@@ -363,7 +363,11 @@ def get_meminfo() -> Mapping[str, float]:
     try:
         import psutil  # type: ignore  # It's not in typeshed yet
     except ImportError:
-        pass
+        if sys.platform != 'win32':
+            res['memory_psutil_missing'] = (
+                'psutil not found, run pip install mypy[dmypy] '
+                'to install the needed components for dmypy'
+            )
     else:
         process = psutil.Process(os.getpid())
         meminfo = process.memory_info()
