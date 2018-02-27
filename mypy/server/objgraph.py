ecc1b6bca2139c4265a5821ff833ed3da73978ca
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2018-02-27 16:42:44 +0000

    Fix tests

--- mypy/server/objgraph.py
+++ mypy/server/objgraph.py
@@ -24,12 +24,12 @@ ATTR_BLACKLIST = {
     '__class__',
     '__dict__',
 
-    # mypy specific attr blacklists
+    # Mypy specific attribute blacklists
     'indirection_detector',
     'all_types',
     'type_maps',
-    'semantic_analyzer', # semantic analyzer has stale caches
-    'semantic_analyzer_pass3', # semantic analyzer has stale caches
+    'semantic_analyzer',  # Semantic analyzer has stale caches
+    'semantic_analyzer_pass3',  # Semantic analyzer has stale caches
 }
 
 # Instances of these types can't have references to other objects
