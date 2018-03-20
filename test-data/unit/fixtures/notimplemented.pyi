dafce6ccb4bdbfc91b2cdc919a3923a335dc7b2e
Peter Ludemann <peter.ludemann@gmail.com>
2018-03-20 12:10:01 -0700

    Handle NotImplemented from __ne__; improve test cases Also change sharedparse.py to avoid this kind of bug in future. Fixes #4767.

--- test-data/unit/fixtures/notimplemented.pyi
+++ test-data/unit/fixtures/notimplemented.pyi
@@ -3,11 +3,13 @@ from typing import Any, cast
 
 
 class object:
+    __class__ = None
     def __init__(self) -> None: pass
 
 class type: pass
 class function: pass
-class bool: pass
+class bool:
+    def __init__(self, o: Any) -> None: pass
 class int: pass
 class str: pass
 NotImplemented = cast(Any, None)
