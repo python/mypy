60433e0717175b7c310c2d971be5db3dd912ac12
Ivan Levkivskyi <levkivslyi@gmail.com>
2018-03-26 02:07:56 +0100

    Remove debugging statements

--- mypy/build.py
+++ mypy/build.py
@@ -60,7 +60,7 @@ from mypy.fscache import FileSystemCache, FileSystemMetaCache
 # mode only that is useful during development. This produces only a subset of
 # output compared to --verbose output. We use a global flag to enable this so
 # that it's easy to enable this when running tests.
-DEBUG_FINE_GRAINED = True
+DEBUG_FINE_GRAINED = False
 
 
 PYTHON_EXTENSIONS = ['.pyi', '.py']
@@ -2108,7 +2108,7 @@ def dispatch(sources: List[BuildSource], manager: BuildManager) -> Graph:
             manager.proto_deps = proto_deps
             write_protocol_deps_cache(proto_deps, manager)
 
-    if manager.options.dump_deps or True:
+    if manager.options.dump_deps:
         # This speeds up startup a little when not using the daemon mode.
         from mypy.server.deps import dump_all_dependencies
         dump_all_dependencies(manager.modules, manager.all_types, manager.options.python_version)
