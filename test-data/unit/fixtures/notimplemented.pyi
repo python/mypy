67b979f3fbf3af0e5aec40e1e34a6eefaf83af13
Peter Ludemann <peter.ludemann@gmail.com>
2018-03-28 14:43:42 -0700

    remove extra tests; these will be handled separately

--- test-data/unit/fixtures/notimplemented.pyi
+++ test-data/unit/fixtures/notimplemented.pyi
@@ -3,13 +3,11 @@ from typing import Any, cast
 
 
 class object:
-    __class__ = None
     def __init__(self) -> None: pass
 
 class type: pass
 class function: pass
-class bool:
-    def __init__(self, o: Any) -> None: pass
+class bool: pass
 class int: pass
 class str: pass
 NotImplemented = cast(Any, None)
