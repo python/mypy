7935be3189e920772891cbe5029414ee277c08df
Michael Sullivan <sully@msully.net>
2018-04-02 11:24:52 -0700

    use a FileSystemCache for find_sources

--- mypy/find_sources.py
+++ mypy/find_sources.py
@@ -5,6 +5,7 @@ import os
 from typing import List, Sequence, Set, Tuple, Optional
 
 from mypy.build import BuildSource, PYTHON_EXTENSIONS
+from mypy.fscache import FileSystemMetaCache
 from mypy.options import Options
 
 
@@ -15,19 +16,20 @@ class InvalidSourceList(Exception):
     """Exception indicating a problem in the list of sources given to mypy."""
 
 
-# TODO: use a FileSystemCache for this
-def create_source_list(files: Sequence[str], options: Options) -> List[BuildSource]:
+def create_source_list(files: Sequence[str], options: Options,
+                       fscache: Optional[FileSystemMetaCache]=None) -> List[BuildSource]:
     """From a list of source files/directories, makes a list of BuildSources.
 
     Raises InvalidSourceList on errors.
     """
+    fscache = fscache or FileSystemMetaCache()
     targets = []
     for f in files:
         if f.endswith(PY_EXTENSIONS):
             # Can raise InvalidSourceList if a directory doesn't have a valid module name.
-            targets.append(BuildSource(f, crawl_up(f)[1], None))
-        elif os.path.isdir(f):
-            sub_targets = expand_dir(f)
+            targets.append(BuildSource(f, crawl_up(fscache, f)[1], None))
+        elif fscache.isdir(f):
+            sub_targets = expand_dir(fscache, f)
             if not sub_targets:
                 raise InvalidSourceList("There are no .py[i] files in directory '{}'"
                                         .format(f))
@@ -50,24 +52,25 @@ def keyfunc(name: str) -> Tuple[int, str]:
     return (-1, name)
 
 
-def expand_dir(arg: str, mod_prefix: str = '') -> List[BuildSource]:
+def expand_dir(fscache: FileSystemMetaCache,
+               arg: str, mod_prefix: str = '') -> List[BuildSource]:
     """Convert a directory name to a list of sources to build."""
-    f = get_init_file(arg)
+    f = get_init_file(fscache, arg)
     if mod_prefix and not f:
         return []
     seen = set()  # type: Set[str]
     sources = []
     if f and not mod_prefix:
-        top_dir, top_mod = crawl_up(f)
+        top_dir, top_mod = crawl_up(fscache, f)
         mod_prefix = top_mod + '.'
     if mod_prefix:
         sources.append(BuildSource(f, mod_prefix.rstrip('.'), None))
-    names = os.listdir(arg)
+    names = fscache.listdir(arg)
     names.sort(key=keyfunc)
     for name in names:
         path = os.path.join(arg, name)
-        if os.path.isdir(path):
-            sub_sources = expand_dir(path, mod_prefix + name + '.')
+        if fscache.isdir(path):
+            sub_sources = expand_dir(fscache, path, mod_prefix + name + '.')
             if sub_sources:
                 seen.add(name)
                 sources.extend(sub_sources)
@@ -82,7 +85,7 @@ def expand_dir(arg: str, mod_prefix: str = '') -> List[BuildSource]:
     return sources
 
 
-def crawl_up(arg: str) -> Tuple[str, str]:
+def crawl_up(fscache: FileSystemMetaCache, arg: str) -> Tuple[str, str]:
     """Given a .py[i] filename, return (root directory, module).
 
     We crawl up the path until we find a directory without
@@ -90,7 +93,7 @@ def crawl_up(arg: str) -> Tuple[str, str]:
     """
     dir, mod = os.path.split(arg)
     mod = strip_py(mod) or mod
-    while dir and get_init_file(dir):
+    while dir and get_init_file(fscache, dir):
         dir, base = os.path.split(dir)
         if not base:
             break
@@ -116,7 +119,7 @@ def strip_py(arg: str) -> Optional[str]:
     return None
 
 
-def get_init_file(dir: str) -> Optional[str]:
+def get_init_file(fscache: FileSystemMetaCache, dir: str) -> Optional[str]:
     """Check whether a directory contains a file named __init__.py[i].
 
     If so, return the file's name (with dir prefixed).  If not, return
@@ -126,6 +129,6 @@ def get_init_file(dir: str) -> Optional[str]:
     """
     for ext in PY_EXTENSIONS:
         f = os.path.join(dir, '__init__' + ext)
-        if os.path.isfile(f):
+        if fscache.isfile(f):
             return f
     return None
