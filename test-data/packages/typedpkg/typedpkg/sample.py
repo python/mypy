88279fa3e85cb2b2f84e2d35d72cb9cee9500665
Ethan Smith <ethan@ethanhs.me>
2018-04-06 15:12:51 -0700

    Fix outdated docstring

--- test-data/packages/typedpkg/typedpkg/sample.py
+++ test-data/packages/typedpkg/typedpkg/sample.py
@@ -3,5 +3,5 @@ from typing import Iterable, Tuple
 
 def ex(a):
     # type: (Iterable[str]) -> Tuple[str, ...]
-    """Example typed package. This intentionally has an error."""
+    """Example typed package."""
     return tuple(a)
