9a224b1b8f65a0bf5f38985a4d28249a3595c8bc
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2018-04-12 17:28:38 +0100

    Fix lint

--- mypy/main.py
+++ mypy/main.py
@@ -82,8 +82,9 @@ def main(script_path: Optional[str], args: Optional[List[str]] = None) -> None:
     serious = False
     blockers = False
     try:
-        # Keep a dummy reference (res) for memory profiling below.
-        res = type_check_only(sources, bin_dir, options, flush_errors)
+        # Keep a dummy reference (res) for memory profiling below, as otherwise
+        # the result could be freed.
+        res = type_check_only(sources, bin_dir, options, flush_errors)  # noqa
     except CompileError as e:
         blockers = True
         if not e.use_stdout:
@@ -97,8 +98,8 @@ def main(script_path: Optional[str], args: Optional[List[str]] = None) -> None:
         t1 = time.time()
         util.write_junit_xml(t1 - t0, serious, messages, options.junit_xml)
 
-    #from mypy.memprofile import print_memory_profile
-    #print_memory_profile()
+    # from mypy.memprofile import print_memory_profile
+    # print_memory_profile()
 
     if messages:
         code = 2 if blockers else 1
