8d179dc8eb8e93cc2c5e6ede24ac349f52126869
Michael J. Sullivan <sully@msully.net>
2018-04-13 15:18:07 -0700

    Fix which mtimes I use

--- mypy/build.py
+++ mypy/build.py
@@ -1110,8 +1110,8 @@ def validate_meta(meta: Optional[CacheMeta], id: str, path: Optional[str],
         return None
     deps_mtime = None
     if manager.options.cache_fine_grained:
-        deps_mtime = getmtime(meta.data_json)
-        if data_mtime != meta.deps_mtime:
+        deps_mtime = getmtime(meta.deps_json)
+        if deps_mtime != meta.deps_mtime:
             manager.log('Metadata abandoned for {}: deps cache is modified'.format(id))
             return None
 
