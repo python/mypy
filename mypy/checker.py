1f312523fc6998e8c4c0a8fded61008542f8ae78
Michael Lee <michael.lee.0x2a@gmail.com>
2018-05-24 16:39:00 -0700

    Add rudimentary support for partial overlap checks
    The previous plan was that I would defer checking for partial overlaps
    for a future PR.
    
    However, that led to some regressions -- for example, the tuple ellipsis
    variance test case.
    
    So, I decided to add in very rudimentary partial overlap checks as a
    stop-gap measure to help catch more obviously unsafe signatures. The
    checks only attempt to check for overlaps at the top-level, and handle
    only unions, tuples, and unrestricted TypeVars.

--- mypy/checker.py
+++ mypy/checker.py
@@ -51,7 +51,7 @@ from mypy.visitor import NodeVisitor
 from mypy.join import join_types
 from mypy.treetransform import TransformVisitor
 from mypy.binder import ConditionalTypeBinder, get_declaration
-from mypy.meet import is_overlapping_types
+from mypy.meet import is_overlapping_types, is_partially_overlapping_types
 from mypy.options import Options
 from mypy.plugin import Plugin, CheckerPluginInterface
 from mypy.sharedparse import BINARY_MAGIC_METHODS
@@ -3613,22 +3613,28 @@ def is_unsafe_overlapping_overload_signatures(signature: CallableType,
     # function and use "is_more_precise(...) or is_partially_overlapping(...)" for the is_compat
     # checks.
     #
+    # (We already have a rudimentary implementation of 'is_partially_overlapping', but it only
+    # attempts to handle the obvious cases -- see its docstring for more info.)
+    #
     # Similarly, the signatures "f(x: A, y: A) -> str" and "f(*x: A) -> int" are also unsafe:
     # the parameter *counts* or arity are partially overlapping.
     #
     # To fix this, we need to modify is_callable_compatible so it can optionally detect
     # functions that are *potentially* compatible rather then *definitely* compatible.
 
+    def is_more_precise_or_partially_overlapping(t: Type, s: Type) -> bool:
+        return is_more_precise(t, s) or is_partially_overlapping_types(t, s)
+
     # The reason we repeat this check twice is so we can do a slightly better job of
     # checking for potentially overlapping param counts. Both calls will actually check
     # the param and return types in the same "direction" -- the only thing that differs
     # is how is_callable_compatible checks non-positional arguments.
     return (is_callable_compatible(signature, other,
-                                   is_compat=is_more_precise,
+                                   is_compat=is_more_precise_or_partially_overlapping,
                                    is_compat_return=lambda l, r: not is_subtype(l, r),
                                    check_args_covariantly=True) or
             is_callable_compatible(other, signature,
-                                   is_compat=is_more_precise,
+                                   is_compat=is_more_precise_or_partially_overlapping,
                                    is_compat_return=lambda l, r: not is_subtype(r, l)))
 
 
