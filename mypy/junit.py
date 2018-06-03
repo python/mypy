c6f83fa682a1545b7b41920ea5358ae30fcf2a70
Kenny Do <chinesedewey@gmail.com>
2018-06-02 22:11:07 -0700

    Use \u escapes in regex

--- mypy/junit.py
+++ mypy/junit.py
@@ -228,24 +228,24 @@ def _clean_illegal_xml_chars(string_to_clean: str) -> str:
     Based on: https://stackoverflow.com/q/1707890
     """
     illegal_unichrs = [
-        (0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F),
-        (0x7F, 0x84), (0x86, 0x9F), (0xFDD0, 0xFDDF),
-        (0xFFFE, 0xFFFF),
+        (r"\u0000", r"\u0008"), (r"\u000B", r"\u000C"), (r"\u000E", r"\u001F"),
+        (r"\u007F", r"\u0084"), (r"\u0086", r"\u009F"), (r"\uFDD0", r"\uFDDF"),
+        (r"\uFFFE", r"\uFFFF"),
     ]
     if sys.maxunicode >= 0x10000:
         illegal_unichrs.extend([
-            (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF),
-            (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF),
-            (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
-            (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
-            (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF),
-            (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
-            (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF),
-            (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF),
+            (r"\U0001FFFE", r"\U0001FFFF"), (r"\U0002FFFE", r"\U0002FFFF"),
+            (r"\U0003FFFE", r"\U0003FFFF"), (r"\U0004FFFE", r"\U0004FFFF"),
+            (r"\U0005FFFE", r"\U0005FFFF"), (r"\U0006FFFE", r"\U0006FFFF"),
+            (r"\U0007FFFE", r"\U0007FFFF"), (r"\U0008FFFE", r"\U0008FFFF"),
+            (r"\U0009FFFE", r"\U0009FFFF"), (r"\U000AFFFE", r"\U000AFFFF"),
+            (r"\U000BFFFE", r"\U000BFFFF"), (r"\U000CFFFE", r"\U000CFFFF"),
+            (r"\U000DFFFE", r"\U000DFFFF"), (r"\U000EFFFE", r"\U000EFFFF"),
+            (r"\U000FFFFE", r"\U000FFFFF"), (r"\U0010FFFE", r"\U0010FFFF"),
         ])
 
     illegal_ranges = [
-        "%s-%s" % (chr(low), chr(high))
+        "%s-%s" % (low, high)
         for (low, high) in illegal_unichrs
     ]
 
