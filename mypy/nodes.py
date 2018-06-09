1b7a3ce637c94a7bf9fd7a8c2d360490c930ce59
Ivan Levkivskyi <levkivskyi@gmail.com>
2018-06-09 23:52:10 +0100

    Remove some dead code (#5184)

--- mypy/nodes.py
+++ mypy/nodes.py
@@ -1298,27 +1298,7 @@ class NameExpr(RefExpr):
         return visitor.visit_name_expr(self)
 
     def serialize(self) -> JsonDict:
-        # TODO: Find out where and why NameExpr is being serialized (if at all).
         assert False, "Serializing NameExpr: %s" % (self,)
-        return {'.class': 'NameExpr',
-                'kind': self.kind,
-                'node': None if self.node is None else self.node.serialize(),
-                'fullname': self.fullname,
-                'is_new_def': self.is_new_def,
-                'is_inferred_def': self.is_inferred_def,
-                'name': self.name,
-                }
-
-    @classmethod
-    def deserialize(cls, data: JsonDict) -> 'NameExpr':
-        assert data['.class'] == 'NameExpr'
-        ret = NameExpr(data['name'])
-        ret.kind = data['kind']
-        ret.node = None if data['node'] is None else SymbolNode.deserialize(data['node'])
-        ret.fullname = data['fullname']
-        ret.is_new_def = data['is_new_def']
-        ret.is_inferred_def = data['is_inferred_def']
-        return ret
 
 
 class MemberExpr(RefExpr):
