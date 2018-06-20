14555305cc365a9548632b2ca17e2d5b93a44de5
Michael Lee <michael.lee.0x2a@gmail.com>
2018-06-20 08:28:01 -0700

    Make overload checks more strict when there are multiple 'Any's
    Resolves https://github.com/python/mypy/issues/5250
    
    This makes the "multiple overload matches due to Any" even more strict:
    we now return a non-Any type only if all of the return types are the
    same.

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -1314,12 +1314,9 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             return None
         elif any_causes_overload_ambiguity(matches, return_types, arg_types, arg_kinds, arg_names):
             # An argument of type or containing the type 'Any' caused ambiguity.
-            if all(is_subtype(ret_type, return_types[-1]) and
-                   is_subtype(return_types[-1], ret_type)
-                   for ret_type in return_types[:-1]):
-                # The last match is mutually compatible with all previous ones, so it's safe
-                # to return that inferred type.
-                return return_types[-1], inferred_types[-1]
+            if all_same_types(return_types):
+                # The return types are all the same, so it's safe to return that instead of 'Any'
+                return return_types[0], inferred_types[0]
             else:
                 # We give up and return 'Any'.
                 return self.check_call(callee=AnyType(TypeOfAny.special_form),
