70702768059bc1a4993b288ecabff6c979e42406
Sebastian Rittau <srittau@rittau.biz>
2018-08-05 20:17:45 +0200

    Remove superfluous overload

--- mypy/messages.py
+++ mypy/messages.py
@@ -997,7 +997,6 @@ class MessageBuilder:
     def operator_method_signatures_overlap(
             self, reverse_class: TypeInfo, reverse_method: str, forward_class: Type,
             forward_method: str, context: Context) -> None:
-        assert reverse_class is not None
         self.fail('Signatures of "{}" of "{}" and "{}" of {} '
                   'are unsafely overlapping'.format(
                       reverse_method, reverse_class.name(),
