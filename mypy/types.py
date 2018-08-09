c812c01b10ae0b0d4b9a4ec1f36336ba3b2603c3
Michael J. Sullivan <sully@msully.net>
2018-08-09 13:02:12 -0700

    Move TypeQuery to mypy.type_visitor also

--- mypy/types.py
+++ mypy/types.py
@@ -29,7 +29,7 @@ MYPY = False
 # at the top, it breaks at runtime because of import cycle issues, so we do it at different
 # times in different places.
 if MYPY:
-    from mypy.type_visitor import TypeVisitor, SyntheticTypeVisitor, TypeTranslator
+    from mypy.type_visitor import TypeVisitor, SyntheticTypeVisitor, TypeTranslator, TypeQuery
 
 
 def deserialize_type(data: Union[JsonDict, str]) -> 'Type':
@@ -1547,7 +1547,9 @@ class ForwardRef(Type):
 # Import them here, after the types are defined.
 # This is intended as a re-export also.
 if not MYPY:
-    from mypy.type_visitor import TypeVisitor, SyntheticTypeVisitor, TypeTranslator  # noqa
+    from mypy.type_visitor import (  # noqa
+        TypeVisitor, SyntheticTypeVisitor, TypeTranslator, TypeQuery
+    )
 
 
 class TypeStrVisitor(SyntheticTypeVisitor[str]):
@@ -1733,99 +1735,6 @@ class TypeStrVisitor(SyntheticTypeVisitor[str]):
             else:
                 res.append(str(t))
         return ', '.join(res)
-
-
-class TypeQuery(SyntheticTypeVisitor[T]):
-    """Visitor for performing queries of types.
-
-    strategy is used to combine results for a series of types
-
-    Common use cases involve a boolean query using `any` or `all`
-    """
-
-    def __init__(self, strategy: Callable[[Iterable[T]], T]) -> None:
-        self.strategy = strategy
-        self.seen = []  # type: List[Type]
-
-    def visit_unbound_type(self, t: UnboundType) -> T:
-        return self.query_types(t.args)
-
-    def visit_type_list(self, t: TypeList) -> T:
-        return self.query_types(t.items)
-
-    def visit_callable_argument(self, t: CallableArgument) -> T:
-        return t.typ.accept(self)
-
-    def visit_any(self, t: AnyType) -> T:
-        return self.strategy([])
-
-    def visit_uninhabited_type(self, t: UninhabitedType) -> T:
-        return self.strategy([])
-
-    def visit_none_type(self, t: NoneTyp) -> T:
-        return self.strategy([])
-
-    def visit_erased_type(self, t: ErasedType) -> T:
-        return self.strategy([])
-
-    def visit_deleted_type(self, t: DeletedType) -> T:
-        return self.strategy([])
-
-    def visit_type_var(self, t: TypeVarType) -> T:
-        return self.strategy([])
-
-    def visit_partial_type(self, t: PartialType) -> T:
-        return self.query_types(t.inner_types)
-
-    def visit_instance(self, t: Instance) -> T:
-        return self.query_types(t.args)
-
-    def visit_callable_type(self, t: CallableType) -> T:
-        # FIX generics
-        return self.query_types(t.arg_types + [t.ret_type])
-
-    def visit_tuple_type(self, t: TupleType) -> T:
-        return self.query_types(t.items)
-
-    def visit_typeddict_type(self, t: TypedDictType) -> T:
-        return self.query_types(t.items.values())
-
-    def visit_star_type(self, t: StarType) -> T:
-        return t.type.accept(self)
-
-    def visit_union_type(self, t: UnionType) -> T:
-        return self.query_types(t.items)
-
-    def visit_overloaded(self, t: Overloaded) -> T:
-        return self.query_types(t.items())
-
-    def visit_type_type(self, t: TypeType) -> T:
-        return t.item.accept(self)
-
-    def visit_forwardref_type(self, t: ForwardRef) -> T:
-        if t.resolved:
-            return t.resolved.accept(self)
-        else:
-            return t.unbound.accept(self)
-
-    def visit_ellipsis_type(self, t: EllipsisType) -> T:
-        return self.strategy([])
-
-    def query_types(self, types: Iterable[Type]) -> T:
-        """Perform a query for a list of types.
-
-        Use the strategy to combine the results.
-        Skip types already visited types to avoid infinite recursion.
-        Note: types can be recursive until they are fully analyzed and "unentangled"
-        in patches after the semantic analysis.
-        """
-        res = []  # type: List[T]
-        for t in types:
-            if any(t is s for s in self.seen):
-                continue
-            self.seen.append(t)
-            res.append(t.accept(self))
-        return self.strategy(res)
 
 
 def strip_type(typ: Type) -> Type:
