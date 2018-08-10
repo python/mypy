d70cc193cfc71b08f2c2a1fba468b6c48caf8cf9
Michael J. Sullivan <sully@msully.net>
2018-08-10 13:23:43 -0700

    Support __str__ and __repr__

--- mypyc/emitclass.py
+++ mypyc/emitclass.py
@@ -37,6 +37,8 @@ SlotTable = Mapping[str, Tuple[str, SlotGenerator]]
 SLOT_DEFS = {
     '__init__': ('tp_init', lambda c, t, e: generate_init_for_class(c, t, e)),
     '__call__': ('tp_call', wrapper_slot),
+    '__str__': ('tp_str', native_slot),
+    '__repr__': ('tp_repr', native_slot),
     '__next__': ('tp_iternext', native_slot),
     '__iter__': ('tp_iter', native_slot),
     '__hash__': ('tp_hash', generate_hash_wrapper),
