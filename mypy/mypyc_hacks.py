16e3d6ff235fe0a2ce10a63c0c2b3ed2fcd475ec
Michael J. Sullivan <sully@msully.net>
2018-08-15 17:21:41 -0700

    Move DecodeError back to util.py

--- mypy/mypyc_hacks.py
+++ mypy/mypyc_hacks.py
@@ -1,14 +1,6 @@
 """Stuff that we had to move out of its right place because of mypyc limitations."""
 
 
-# Moved from util.py, because it inherits from Exception
-class DecodeError(Exception):
-    """Exception raised when a file cannot be decoded due to an unknown encoding type.
-
-    Essentially a wrapper for the LookupError raised by `bytearray.decode`
-    """
-
-
 # Moved from types.py, because it inherits from Enum, which uses a
 # metaclass in a nontrivial way.
 from enum import Enum
