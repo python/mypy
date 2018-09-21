2319cfecb80e15e5926a0b76b0ce141851fdfbf0
Michael J. Sullivan <sully@msully.net>
2018-09-21 14:17:37 -0700

    Try automatically triggering builds

---
+++ misc/trigger_wheel_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+REPO=travis-testing
+
+set -x
+git clone --recurse-submodules https://msullivan:${GH_TOKEN}@github.com/$REPO.git
+
+git config --global user.email "nobody"
+git config --global user.name "mypy wheels autopush"
+
+COMMIT=$(git rev-parse HEAD)
+cd $REPO/mypy
+git fetch
+git checkout $COMMIT
+V=$(python3 -m mypy --version| cut -d" " -f2)
+cd ..
+git commit -am "Build wheels for mypy $V"
+git tag v$V
+git push --tags origin master
