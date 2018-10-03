06490838d1d943ebbf079a0f8e1d98e2199a7f2f
Guido van Rossum <guido@python.org>
2018-10-03 16:23:53 -0700

    Use a raw string for a regex

--- mypy/options.py
+++ mypy/options.py
@@ -341,7 +341,7 @@ class Options:
         parts = s.split('.')
         expr = re.escape(parts[0]) if parts[0] != '*' else '.*'
         for part in parts[1:]:
-            expr += re.escape('.' + part) if part != '*' else '(\..*)?'
+            expr += re.escape('.' + part) if part != '*' else r'(\..*)?'
         return re.compile(expr + '\\Z')
 
     def select_options_affecting_cache(self) -> Mapping[str, object]:
