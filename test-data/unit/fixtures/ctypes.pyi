15d305603708afcb16a8f062a0fe43b11349df04
dgelessus <dgelessus@users.noreply.github.com>
2018-11-12 15:59:27 +0100

    Add test cases for ctypes plugin

---
+++ test-data/unit/fixtures/ctypes.pyi
@@ -0,0 +1,22 @@
+# Builtins stub used in ctypes-related test cases.
+
+from typing import TypeVar, Generic, Mapping, Sequence
+
+T = TypeVar('T')
+K = TypeVar('K')
+V = TypeVar('V')
+
+class object:
+    def __init__(self): pass
+
+class type: pass
+
+class bytes: pass
+class dict(Mapping[K, V]): pass
+class ellipsis: pass
+class function: pass
+class int: pass
+class list(Sequence[T]): pass
+class slice: pass
+class str: pass
+class tuple(Generic[T]): pass
