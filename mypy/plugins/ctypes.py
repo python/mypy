199ebac8b1bfbd732d0749b95368be0ee0737d93
dgelessus <dgelessus@users.noreply.github.com>
2018-11-14 14:47:45 +0100

    Handle ctypes.Array.value correctly if element type is a union

--- mypy/plugins/ctypes.py
+++ mypy/plugins/ctypes.py
@@ -6,7 +6,7 @@ from typing import Optional
 import mypy.plugin
 from mypy.maptype import map_instance_to_supertype
 from mypy.subtypes import is_subtype
-from mypy.types import CallableType, Instance, Type, UnionType
+from mypy.types import CallableType, Instance, Type, UnionType, union_items
 
 
 def _find_simplecdata_base_arg(tp: Instance, api: 'mypy.plugin.CheckerPluginInterface') -> Optional[Type]:
@@ -149,18 +149,21 @@ def array_value_callback(ctx: 'mypy.plugin.AttributeContext') -> Type:
     """Callback to provide an accurate type for ctypes.Array.value."""
     et = _get_array_element_type(ctx.type)
     if et is not None:
-        if isinstance(et, Instance) and et.type.fullname() == 'ctypes.c_char':
-            return ctx.api.named_generic_type('builtins.bytes', [])
-        elif isinstance(et, Instance) and et.type.fullname() == 'ctypes.c_wchar':
-            return ctx.api.named_generic_type(
-                'builtins.str' if ctx.api.options.python_version >= (3,) else 'builtins.unicode',
-                [])
-        else:
-            ctx.api.msg.fail(
-                'ctypes.Array attribute "value" is only available'
-                ' with element type c_char or c_wchar, not "{}"'
-                .format(et),
-                ctx.context)
+        types = []
+        for tp in union_items(et):
+            if isinstance(tp, Instance) and tp.type.fullname() == 'ctypes.c_char':
+                types.append(ctx.api.named_generic_type('builtins.bytes', []))
+            elif isinstance(tp, Instance) and tp.type.fullname() == 'ctypes.c_wchar':
+                types.append(ctx.api.named_generic_type(
+                    'builtins.str' if ctx.api.options.python_version >= (3,) else 'builtins.unicode',
+                    []))
+            else:
+                ctx.api.msg.fail(
+                    'ctypes.Array attribute "value" is only available'
+                    ' with element type c_char or c_wchar, not "{}"'
+                    .format(et),
+                    ctx.context)
+        return UnionType.make_simplified_union(types)
     return ctx.default_attr_type
 
 
