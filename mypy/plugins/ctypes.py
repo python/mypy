2e2888b861bf34b337a3baa2d81f1258af44225a
dgelessus <dgelessus@users.noreply.github.com>
2018-11-19 00:51:22 +0100

    Fix ctypes.Array.(value|raw) callbacks if element type is Any

--- mypy/plugins/ctypes.py
+++ mypy/plugins/ctypes.py
@@ -152,7 +152,9 @@ def array_value_callback(ctx: 'mypy.plugin.AttributeContext') -> Type:
     if et is not None:
         types = []  # type: List[Type]
         for tp in union_items(et):
-            if isinstance(tp, Instance) and tp.type.fullname() == 'ctypes.c_char':
+            if isinstance(tp, AnyType):
+                types.append(AnyType(TypeOfAny.from_another_any, source_any=tp))
+            elif isinstance(tp, Instance) and tp.type.fullname() == 'ctypes.c_char':
                 types.append(ctx.api.named_generic_type('builtins.bytes', []))
             elif isinstance(tp, Instance) and tp.type.fullname() == 'ctypes.c_wchar':
                 types.append(ctx.api.named_generic_type(
@@ -174,12 +176,15 @@ def array_raw_callback(ctx: 'mypy.plugin.AttributeContext') -> Type:
     """Callback to provide an accurate type for ctypes.Array.raw."""
     et = _get_array_element_type(ctx.type)
     if et is not None:
-        if isinstance(et, Instance) and et.type.fullname() == 'ctypes.c_char':
-            return ctx.api.named_generic_type('builtins.bytes', [])
-        else:
-            ctx.api.msg.fail(
-                'ctypes.Array attribute "raw" is only available'
-                ' with element type c_char, not "{}"'
-                .format(et),
-                ctx.context)
+        types = []  # type: List[Type]
+        for tp in union_items(et):
+            if isinstance(tp, AnyType) or isinstance(tp, Instance) and tp.type.fullname() == 'ctypes.c_char':
+                types.append(ctx.api.named_generic_type('builtins.bytes', []))
+            else:
+                ctx.api.msg.fail(
+                    'ctypes.Array attribute "raw" is only available'
+                    ' with element type c_char, not "{}"'
+                    .format(et),
+                    ctx.context)
+        return UnionType.make_simplified_union(types)
     return ctx.default_attr_type
