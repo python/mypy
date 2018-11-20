3c311c24323d20bbf7e13084ba16bcb99e8934d7
Guido van Rossum <guido@python.org>
2018-11-19 17:15:49 -0800

    Fix crash when reveal_locals() encounters a class definition (#5920)
    The root cause were some improper casts when creating the `local_nodes` list. These were easy to fix by adding `isinstance(node, Var)` checks to the list comprehensions.
    
    Fixes #5915

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -2880,23 +2880,24 @@ class SemanticAnalyzerPass2(NodeVisitor[None],
                 # Each SymbolTableNode has an attribute node that is nodes.Var
                 # look for variable nodes that marked as is_inferred
                 # Each symboltable node has a Var node as .node
-                local_nodes = cast(
-                    List[Var],
-                    [
-                        n.node for name, n in self.globals.items()
-                        if getattr(n.node, 'is_inferred', False)
-                    ]
-                )
+                local_nodes = [n.node
+                               for name, n in self.globals.items()
+                               if getattr(n.node, 'is_inferred', False)
+                               and isinstance(n.node, Var)]
             elif self.is_class_scope():
                 # type = None  # type: Optional[TypeInfo]
                 if self.type is not None:
-                    local_nodes = cast(List[Var], [st.node for st in self.type.names.values()])
+                    local_nodes = [st.node
+                                   for st in self.type.names.values()
+                                   if isinstance(st.node, Var)]
             elif self.is_func_scope():
                 # locals = None  # type: List[Optional[SymbolTable]]
                 if self.locals is not None:
                     symbol_table = self.locals[-1]
                     if symbol_table is not None:
-                        local_nodes = cast(List[Var], [st.node for st in symbol_table.values()])
+                        local_nodes = [st.node
+                                       for st in symbol_table.values()
+                                       if isinstance(st.node, Var)]
             expr.analyzed = RevealExpr(kind=REVEAL_LOCALS, local_nodes=local_nodes)
             expr.analyzed.line = expr.line
             expr.analyzed.column = expr.column
