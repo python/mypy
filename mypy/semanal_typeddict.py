3d68701367d96cdbd45fef586e1285984fc1741d
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2018-11-21 10:46:30 +0000

    Change TypedDict fallback to `Mapping[str, object]`
    The previous fallback mapping item type was join of the value
    types, which is unsafe because of structural subtyping.
    
    Fixes #5927.

--- mypy/semanal_typeddict.py
+++ mypy/semanal_typeddict.py
@@ -283,16 +283,6 @@ class TypedDictAnalyzer:
         info = self.api.basic_new_typeinfo(name, fallback)
         info.typeddict_type = TypedDictType(OrderedDict(zip(items, types)), required_keys,
                                             fallback)
-
-        def patch() -> None:
-            # Calculate the correct value type for the fallback Mapping.
-            assert info.typeddict_type, "TypedDict type deleted before calling the patch"
-            fallback.args[1] = join.join_type_list(list(info.typeddict_type.items.values()))
-
-        # We can't calculate the complete fallback type until after semantic
-        # analysis, since otherwise MROs might be incomplete. Postpone a callback
-        # function that patches the fallback.
-        self.api.schedule_patch(PRIORITY_FALLBACKS, patch)
         return info
 
     # Helpers
