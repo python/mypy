2a57ce111ed27bf52741378c746040397afa692d
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2018-12-27 23:36:16 -0800

    lint

--- mypy/plugins/pynamodb.py
+++ mypy/plugins/pynamodb.py
@@ -23,16 +23,21 @@ def _function_hook_callback(ctx: MethodContext) -> Type:
         return ctx.default_return_type
 
     attr_type = ctx.default_return_type
-    base_types = [base for base in attr_type.type.bases if base.type.fullname() == 'pynamodb.attributes.Attribute']
+    base_types = [base for base in attr_type.type.bases
+                  if base.type.fullname() == 'pynamodb.attributes.Attribute']
     if not base_types:
         return ctx.default_return_type
     base_type = base_types[0]
     underlying_type = base_type.args[0]
 
-    # If initializer gets named arg null=True, wrap in _NullableAttribute to make the underlying type optional
+    # If initializer gets named arg null=True,
+    # wrap in _NullableAttribute to make the underlying type optional
     for arg_name, arg_expr in zip(ctx.context.arg_names, ctx.context.args):
         if isinstance(arg_expr, NameExpr) and arg_expr.fullname == 'builtins.True':
-            return ctx.api.named_generic_type('pynamodb.attributes._NullableAttribute', [attr_type, underlying_type])
+            return ctx.api.named_generic_type('pynamodb.attributes._NullableAttribute', [
+                attr_type,
+                underlying_type,
+            ])
 
     return ctx.default_return_type
 
