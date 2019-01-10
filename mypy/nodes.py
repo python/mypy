ef1e1ae6bfc7e6bfd5bbb125f0552b0e08e85a89
Jonathan Striebel <jstriebel@users.noreply.github.com>
2019-01-10 12:37:36 +0100

    fix python/mypy#6108

--- mypy/nodes.py
+++ mypy/nodes.py
@@ -443,11 +443,11 @@ class OverloadedFuncDef(FuncBase, SymbolNode, Statement):
 
     def __init__(self, items: List['OverloadPart']) -> None:
         super().__init__()
-        assert len(items) > 0
         self.items = items
         self.unanalyzed_items = items.copy()
         self.impl = None
-        self.set_line(items[0].line)
+        if len(items) > 0:
+            self.set_line(items[0].line)
         self.is_final = False
 
     def name(self) -> str:
