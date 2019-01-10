f8139355bfafe9f3724f63b28d8fdb64bd6f308e
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2019-01-10 16:55:08 +0000

    Add star imported modules as dependencies

--- mypy/build.py
+++ mypy/build.py
@@ -1775,9 +1775,13 @@ class State:
     def semantic_analysis(self) -> None:
         assert self.tree is not None, "Internal error: method must be called on parsed file only"
         patches = []  # type: List[Tuple[int, Callable[[], None]]]
+        self.manager.semantic_analyzer.imports.clear()
         with self.wrap_context():
             self.manager.semantic_analyzer.visit_file(self.tree, self.xpath, self.options, patches)
         self.patches = patches
+        for dep in self.manager.semantic_analyzer.imports:
+            self.dependencies.append(dep)
+            self.priorities[dep] = PRI_LOW
 
     def semantic_analysis_pass_three(self) -> None:
         assert self.tree is not None, "Internal error: method must be called on parsed file only"
