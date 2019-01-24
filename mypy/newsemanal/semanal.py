9fc3229a7c8d72aaa872a9d98fc80bc4d5d39a4d
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2019-01-24 14:34:06 +0000

    New semantic analyzer: Refactor deferred/incompleteness handling (#6247)
    The implementation of deferred/incompleteness state remains
    the same but now we have a cleaner API for accessing it.

--- mypy/newsemanal/semanal.py
+++ mypy/newsemanal/semanal.py
@@ -153,6 +153,10 @@ SUGGESTED_TEST_FIXTURES = {
 CORE_BUILTIN_CLASSES = ['object', 'bool']  # type: Final
 
 
+# Used for tracking incomplete references
+Tag = int
+
+
 class NewSemanticAnalyzer(NodeVisitor[None],
                           SemanticAnalyzerInterface,
                           SemanticAnalyzerPluginInterface):
@@ -218,7 +222,7 @@ class NewSemanticAnalyzer(NodeVisitor[None],
         Args:
             modules: Global modules dictionary
             incomplete_namespaces: Namespaces that are being populated during semantic analysis
-                (can contain modules and classes within the current SCC)
+                (can contain modules and classes within the current SCC; mutated by the caller)
             errors: Report analysis errors using this instance
         """
         self.locals = [None]
@@ -233,6 +237,9 @@ class NewSemanticAnalyzer(NodeVisitor[None],
         self.modules = modules
         self.msg = MessageBuilder(errors, modules)
         self.missing_modules = missing_modules
+        # These namespaces are still in process of being populated. If we encounter a
+        # missing name in these namespaces, we need to defer the current analysis target,
+        # since it's possible that the name will be there once the namespace is complete.
         self.incomplete_namespaces = incomplete_namespaces
         self.postpone_nested_functions_stack = [FUNCTION_BOTH_PHASES]
         self.postponed_functions_stack = []
@@ -1640,12 +1647,10 @@ class NewSemanticAnalyzer(NodeVisitor[None],
                 self.add_symbol(imported_id, symbol, imp)
             elif module and not missing:
                 # Missing attribute.
-                if import_id in self.incomplete_namespaces:
+                if self.is_incomplete_namespace(import_id):
                     # We don't know whether the name will be there, since the namespace
                     # is incomplete. Defer the current target.
-                    self.deferred = True
-                    # Mark the current module namespace as incomplete.
-                    self.incomplete = True
+                    self.mark_incomplete()
                     return
                 message = "Module '{}' has no attribute '{}'".format(import_id, id)
                 extra = self.undefined_name_extra_info('{}.{}'.format(import_id, id))
@@ -1834,9 +1839,9 @@ class NewSemanticAnalyzer(NodeVisitor[None],
                                allow_tuple_literal=allow_tuple_literal,
                                report_invalid_types=report_invalid_types,
                                third_pass=third_pass)
-        prev_incomplete = self.num_incomplete_refs
+        tag = self.track_incomplete_refs()
         typ = t.accept(a)
-        if self.num_incomplete_refs != prev_incomplete:
+        if self.found_incomplete_ref(tag):
             # Something could not be bound yet.
             return None
         self.add_type_alias_deps(a.aliases_used)
@@ -1859,12 +1864,11 @@ class NewSemanticAnalyzer(NodeVisitor[None],
 
     def visit_assignment_stmt(self, s: AssignmentStmt) -> None:
         s.is_final_def = self.unwrap_final(s)
-        prev_incomplete = self.num_incomplete_refs
+        tag = self.track_incomplete_refs()
         s.rvalue.accept(self)
-        if self.num_incomplete_refs != prev_incomplete:
+        if self.found_incomplete_ref(tag):
             # Initializer couldn't be fully analyzed. Defer the current node and give up.
-            self.deferred = True
-            self.incomplete = True
+            self.mark_incomplete()
             return
         self.analyze_lvalues(s)
         self.check_final_implicit_def(s)
@@ -3263,9 +3267,8 @@ class NewSemanticAnalyzer(NodeVisitor[None],
                 expr.fullname = '{}.{}'.format(file.fullname(), expr.name)
                 expr.node = Var(expr.name, type=typ)
             else:
-                if file.fullname() in self.incomplete_namespaces:
-                    self.deferred = True
-                    self.num_incomplete_refs += 1
+                if self.is_incomplete_namespace(file.fullname()):
+                    self.record_incomplete_ref()
                     return
                 # We only catch some errors here; the rest will be
                 # caught during type checking.
@@ -3639,6 +3642,36 @@ class NewSemanticAnalyzer(NodeVisitor[None],
                 return n
             return None
 
+    def defer(self) -> None:
+        """Defer current analysis target to be analyzed again."""
+        self.deferred = True
+
+    def track_incomplete_refs(self) -> Tag:
+        """Return tag that can be used for tracking references to incomplete names."""
+        return self.num_incomplete_refs
+
+    def found_incomplete_ref(self, tag: Tag) -> bool:
+        """Have we encountered an incomplete reference since starting tracking?"""
+        return self.num_incomplete_refs != tag
+
+    def record_incomplete_ref(self) -> None:
+        """Record the encounter of an incomplete reference and defer current analysis target."""
+        self.defer()
+        self.num_incomplete_refs += 1
+
+    def mark_incomplete(self) -> None:
+        """Mark the current module namespace as incomplete (and defer current analysis target)."""
+        self.defer()
+        self.incomplete = True
+
+    def is_incomplete_namespace(self, fullname: str) -> bool:
+        """Is a module or class namespace potentially missing some definitions?
+
+        If a name is missing from an incomplete namespace, we'll need to defer the
+        current analysis target.
+        """
+        return fullname in self.incomplete_namespaces
+
     def create_getattr_var(self, getattr_defn: SymbolTableNode,
                            name: str, fullname: str) -> Optional[SymbolTableNode]:
         """Create a dummy global symbol using __getattr__ return type.
@@ -3844,11 +3877,10 @@ class NewSemanticAnalyzer(NodeVisitor[None],
 
     def name_not_defined(self, name: str, ctx: Context) -> None:
         # TODO: Reference to another namespace
-        if self.cur_mod_id in self.incomplete_namespaces:
+        if self.is_incomplete_namespace(self.cur_mod_id):
             # Target namespace is incomplete, so it's possible that the name will be defined
             # later on. Defer current target.
-            self.deferred = True
-            self.num_incomplete_refs += 1
+            self.record_incomplete_ref()
             return
         message = "Name '{}' is not defined".format(name)
         extra = self.undefined_name_extra_info(name)
