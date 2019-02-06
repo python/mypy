9ef077bb5d340ba45f4bde3e2bc0baab1ff3b486
Michael J. Sullivan <sully@msully.net>
2019-02-06 10:23:53 -0800

    Disable LTCG on windows in multi-file mode (mypyc/mypyc#527)
    This makes compilation faster and less memory-intensive.
    In some unscientific measurements, it seemed to slow down
    a compiled mypy self check by less than 1%

--- mypyc/build.py
+++ mypyc/build.py
@@ -401,6 +401,14 @@ def mypycify(paths: List[str],
             '/wd4101',  # unreferenced local variable
             '/wd4146',  # negating unsigned int
         ]
+        if multi_file:
+            # Disable whole program optimization in multi-file mode so
+            # that we actually get the compilation speed and memory
+            # use wins that multi-file mode is intended for.
+            cflags += [
+                '/GL-',
+                '/wd9025',  # warning about overriding /GL
+            ]
 
     # Copy the runtime library in
     rt_file = os.path.join(build_dir, 'CPy.c')
