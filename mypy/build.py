bc3b88dee8a37b99cde0c4b1768fad783dd66c7c
Ethan Smith <ethan@ethanhs.me>
2019-02-06 22:01:08 -0800

    Respond to review

--- mypy/build.py
+++ mypy/build.py
@@ -829,12 +829,13 @@ def _load_json_file(file: str, manager: BuildManager,
         result = json.loads(data)
     except ValueError:  # TODO: JSONDecodeError in 3.5
         manager.errors.set_file(file, None)
-        manager.errors.report(0, 0, "Error reading JSON file;"
-                                    " you likely have a bad cache.\n"
-                                    "Try removing the {cache_dir} directory"
-                                    " and run mypy again.".format(
-                                        cache_dir=manager.options.cache_dir
-                                    ),
+        manager.errors.report(-1, -1,
+                              "Error reading JSON file;"
+                              " you likely have a bad cache.\n"
+                              "Try removing the {cache_dir} directory"
+                              " and run mypy again.".format(
+                                  cache_dir=manager.options.cache_dir
+                              ),
                               blocker=True)
         return None
     else:
