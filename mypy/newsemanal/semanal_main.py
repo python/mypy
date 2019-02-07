f10744d116e12c9555c3b8b102dc14705c5b113b
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2019-02-07 16:07:19 +0000

    Better comments

--- mypy/newsemanal/semanal_main.py
+++ mypy/newsemanal/semanal_main.py
@@ -39,6 +39,7 @@ if MYPY:
 # Perform up to this many semantic analysis iterations until giving up trying to bind all names.
 MAX_ITERATIONS = 10
 
+# Number of passes over core modules before going on to the rest of the builtin SCC.
 CORE_WARMUP = 2
 core_modules = ['typing', 'builtins', 'abc', 'collections']
 
@@ -68,7 +69,8 @@ def process_top_levels(graph: 'Graph', scc: List[str]) -> None:
     state.manager.incomplete_namespaces.update(scc)
 
     worklist = scc[:]
-    # HACK: process core stuff first.
+    # HACK: process core stuff first. This is mostly needed to support defining
+    # named tuples in builtin SCC.
     if all(m in worklist for m in core_modules):
         worklist += list(reversed(core_modules)) * CORE_WARMUP
     iteration = 0
