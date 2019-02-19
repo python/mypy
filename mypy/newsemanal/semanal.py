cc240c9fe673f8f7406f18e9dd3bcf9847e66fe4
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2019-02-19 19:25:09 +0000

    Fix long line

--- mypy/newsemanal/semanal.py
+++ mypy/newsemanal/semanal.py
@@ -2639,7 +2639,8 @@ class NewSemanticAnalyzer(NodeVisitor[None],
                         # NOTE: It is safe to not call self.defer() here, because the only way
                         # we can get None from self.anal_type() is if self.found_incomplete_refs()
                         # returned True. In turn, the only way it can happen is if someone called
-                        # self.record_incomplete_ref(), and the latter unconditionally calls self.defer().
+                        # self.record_incomplete_ref(), and the latter unconditionally calls
+                        # self.defer().
                         analyzed = AnyType(TypeOfAny.special_form)
                     upper_bound = analyzed
                     if isinstance(upper_bound, AnyType) and upper_bound.is_from_error:
