d2cf9c64f5cf1e716b8bd22f23353fd99d25c014
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2019-04-02 17:11:53 +0100

    New semantic analyzer: fix type promotions special cases (#6609)
    Set up type promotions after the main semantic analyzer pass to
    avoid the need to defer. This fixes an issue with test stubs,
    but the existing approach probably worked with typeshed.
    Another benefit of this approach is that things are easier to
    reason about, as the main semantic analysis pass is a bit
    simpler.
    
    This fixes the testDivmod type checker test case.

--- mypy/test/hacks.py
+++ mypy/test/hacks.py
@@ -8,7 +8,6 @@ if MYPY:
 # Files to not run with new semantic analyzer.
 new_semanal_blacklist = [
     'check-async-await.test',
-    'check-expressions.test',
     'check-flags.test',
     'check-incremental.test',
     'check-literal.test',
