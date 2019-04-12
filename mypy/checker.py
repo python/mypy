9a4cfae8f61aff8cf5696b8d502a3baebe0aad4f
Ekin Dursun <ekindursun@gmail.com>
2019-04-12 15:41:02 +0300

    Fix crash when a star expression is used in isinstance (#6659)
    Fixes #4986.

--- mypy/checker.py
+++ mypy/checker.py
@@ -3945,6 +3945,8 @@ def flatten(t: Expression) -> List[Expression]:
     """Flatten a nested sequence of tuples/lists into one list of nodes."""
     if isinstance(t, TupleExpr) or isinstance(t, ListExpr):
         return [b for a in t.items for b in flatten(a)]
+    elif isinstance(t, StarExpr):
+        return flatten(t.expr)
     else:
         return [t]
 
