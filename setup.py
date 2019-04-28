e954da02e76fe50c450f378108c5ce4e92ae33e2
Guido van Rossum <guido@dropbox.com>
2019-04-28 16:04:02 -0700

    Bump minimal required typed-ast version from 1.3.1 to 1.3.5 (#6737)
    - 1.3.2 fixed two out of bounds reads
    - 1.3.3 was a dud
    - 1.3.4 made it compile under newer Python 3.8 alphas
    - 1.3.5 fixed a crash with unicode names on Python 3.4/3.5
    
    Also keep test-requirements.txt in sync with setup.py, and add reminder comments to the latter.

--- setup.py
+++ setup.py
@@ -173,9 +173,11 @@ setup(name='mypy',
                                         ]},
       classifiers=classifiers,
       cmdclass=cmdclass,
-      install_requires = ['typed-ast >= 1.3.1, < 1.4.0',
+      # When changing this, also update test-requirements.txt.
+      install_requires = ['typed-ast >= 1.3.5, < 1.4.0',
                           'mypy_extensions >= 0.4.0, < 0.5.0',
                           ],
+      # Same here.
       extras_require = {
           'dmypy': 'psutil >= 5.4.0, < 5.5.0; sys_platform!="win32"',
       },
