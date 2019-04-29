d387906ebe6afc6d683c1564d3a1a5391254bd57
Michael J. Sullivan <sully@msully.net>
2019-04-29 15:06:52 -0700

    Fix mypyc (#6743)
    Which doesn't support **kwargs

--- mypy/dmypy_server.py
+++ mypy/dmypy_server.py
@@ -517,11 +517,15 @@ class Server:
     def cmd_suggest(self,
                     function: str,
                     callsites: bool,
-                    **kwargs: bool) -> Dict[str, object]:
+                    # We'd like to just use **kwargs here and save some duplication but
+                    # mypyc doesn't support it yet...
+                    json: bool,
+                    no_errors: bool,
+                    no_any: bool) -> Dict[str, object]:
         """Suggest a signature for a function."""
         if not self.fine_grained_manager:
             return {'error': "Command 'suggest' is only valid after a 'check' command"}
-        engine = SuggestionEngine(self.fine_grained_manager, **kwargs)
+        engine = SuggestionEngine(self.fine_grained_manager, json, no_errors, no_any)
         try:
             if callsites:
                 out = engine.suggest_callsites(function)
