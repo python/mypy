7bc2435c443dff7719ad57ab8394eff3b3842b98
Brandt Bucher <brandtbucher@gmail.com>
2019-05-25 21:41:01 -0700

    Fix type annotation.

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -3411,9 +3411,9 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             item_type = join.join_type_list(subexpr_type.items)
             any_type = AnyType(TypeOfAny.special_form)
             iter_type = self.chk.named_generic_type('typing.Generator',
-                                                    [item_type, any_type, any_type])
+                                                    [item_type, any_type, any_type])  # type: Type
         elif isinstance(subexpr_type, AnyType):
-            iter_type = AnyType(TypeOfAny.from_another_any, source_any=subexpr_type)  # type: Type
+            iter_type = AnyType(TypeOfAny.from_another_any, source_any=subexpr_type)
         elif self.chk.type_is_iterable(subexpr_type):
             if is_async_def(subexpr_type) and not has_coroutine_decorator(return_type):
                 self.chk.msg.yield_from_invalid_operand_type(subexpr_type, e)
