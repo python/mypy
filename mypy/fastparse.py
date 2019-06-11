9455c132fe1b466dcbc3f14249baabfb55ac990a
Sanjit Kalapatapu <skalapatapu@skalapatapu-mbp.corp.dropbox.com>
2019-06-10 17:51:34 -0700

    spelling mistake..

--- mypy/fastparse.py
+++ mypy/fastparse.py
@@ -1115,7 +1115,7 @@ class ASTConverter:
         # A FormattedValue is a component of a JoinedStr, or it can exist
         # on its own. We translate them to individual '{}'.format(value)
         # calls. Format specifier and conversion information is passed along
-        # to allow mypyc to support f-strings with format specifiers and converisons.
+        # to allow mypyc to support f-strings with format specifiers and conversions.
         val_exp = self.visit(n.value)
         val_exp.set_line(n.lineno, n.col_offset)
         conv_str = '' if n.conversion is None or n.conversion < 0 else '!' + chr(n.conversion)
