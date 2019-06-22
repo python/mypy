95ac93fdec0f0ae5a9b9ebe2519f7c8eaf1feeb1
Chad Birch <deimos@tildes.net>
2019-06-21 21:00:31 -0600

    Fix anchor on docs link for using a dev build (#7043)

--- mypy/errors.py
+++ mypy/errors.py
@@ -616,7 +616,7 @@ def report_internal_error(err: Exception,
     # Print "INTERNAL ERROR" message.
     print('{}error: INTERNAL ERROR --'.format(prefix),
           'Please try using mypy master on Github:\n'
-          'https://mypy.rtfd.io/en/latest/common_issues.html#using-development-mypy-build',
+          'https://mypy.rtfd.io/en/latest/common_issues.html#using-a-development-mypy-build',
           file=stderr)
     if options.show_traceback:
         print('Please report a bug at https://github.com/python/mypy/issues',
