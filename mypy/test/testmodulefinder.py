6a4cab9d91c67d5ac0abaf6ca2e8311985dbc442
Arne Welzel <arne.welzel@quoscient.io>
2019-07-01 13:08:46 +0200

    testmodulefinder: rename pkg{4,5} to pkg{1,2}
    ...and some more explaining comments.

--- mypy/test/testmodulefinder.py
+++ mypy/test/testmodulefinder.py
@@ -19,8 +19,8 @@ class ModuleFinderSuite(Suite):
                 os.path.join(data_path, "nsx-pkg3"),
                 os.path.join(data_path, "nsy-pkg1"),
                 os.path.join(data_path, "nsy-pkg2"),
-                os.path.join(data_path, "pkg4"),
-                os.path.join(data_path, "pkg5"),
+                os.path.join(data_path, "pkg1"),
+                os.path.join(data_path, "pkg2"),
             ),
             package_path=(),
             typeshed_path=(),
@@ -47,17 +47,17 @@ class ModuleFinderSuite(Suite):
         found_module = self.fmc_nons.find_module("nsx.a")
         self.assertIsNone(found_module)
 
-    def test__no_namespace_packages__find_a_in_pkg4(self) -> None:
+    def test__no_namespace_packages__find_a_in_pkg1(self) -> None:
         """
-        Find find pkg4/a.py for "a" with namespace_packages False.
+        Find find pkg1/a.py for "a" with namespace_packages False.
         """
         found_module = self.fmc_nons.find_module("a")
-        expected = os.path.join(data_path, "pkg4", "a.py")
+        expected = os.path.join(data_path, "pkg1", "a.py")
         assert_equal(expected, found_module)
 
-    def test__no_namespace_packages__find_b_in_pkg5(self) -> None:
+    def test__no_namespace_packages__find_b_in_pkg2(self) -> None:
         found_module = self.fmc_ns.find_module("b")
-        expected = os.path.join(data_path, "pkg5", "b", "__init__.py")
+        expected = os.path.join(data_path, "pkg2", "b", "__init__.py")
         assert_equal(expected, found_module)
 
     def test__find_nsx_as_namespace_pkg_in_pkg1(self) -> None:
@@ -70,23 +70,32 @@ class ModuleFinderSuite(Suite):
         assert_equal(expected, found_module)
 
     def test__find_nsx_a_init_in_pkg1(self) -> None:
+        """
+        Find nsx-pkg1/nsx/a/__init__.py for "nsx.a" in namespace mode.
+        """
         found_module = self.fmc_ns.find_module("nsx.a")
         expected = os.path.join(data_path, "nsx-pkg1", "nsx", "a", "__init__.py")
         assert_equal(expected, found_module)
 
     def test__find_nsx_b_init_in_pkg2(self) -> None:
+        """
+        Find nsx-pkg2/nsx/b/__init__.py for "nsx.b" in namespace mode.
+        """
         found_module = self.fmc_ns.find_module("nsx.b")
         expected = os.path.join(data_path, "nsx-pkg2", "nsx", "b", "__init__.py")
         assert_equal(expected, found_module)
 
     def test__find_nsx_c_c_in_pkg3(self) -> None:
+        """
+        Find nsx-pkg3/nsx/c/c.py for "nsx.c.c" in namespace mode.
+        """
         found_module = self.fmc_ns.find_module("nsx.c.c")
         expected = os.path.join(data_path, "nsx-pkg3", "nsx", "c", "c.py")
         assert_equal(expected, found_module)
 
     def test__find_nsy_a__init_pyi(self) -> None:
         """
-        Prefer a __init__.pyi file over a __init__.py
+        Prefer nsy-pkg1/a/__init__.pyi file over __init__.py.
         """
         found_module = self.fmc_ns.find_module("nsy.a")
         expected = os.path.join(data_path, "nsy-pkg1", "nsy", "a", "__init__.pyi")
@@ -112,14 +121,14 @@ class ModuleFinderSuite(Suite):
         expected = os.path.join(data_path, "nsy-pkg2", "nsy", "c.pyi")
         assert_equal(expected, found_module)
 
-    def test__find_a_in_pkg4(self) -> None:
+    def test__find_a_in_pkg1(self) -> None:
         found_module = self.fmc_ns.find_module("a")
-        expected = os.path.join(data_path, "pkg4", "a.py")
+        expected = os.path.join(data_path, "pkg1", "a.py")
         assert_equal(expected, found_module)
 
-    def test__find_b_init_in_pkg5(self) -> None:
+    def test__find_b_init_in_pkg2(self) -> None:
         found_module = self.fmc_ns.find_module("b")
-        expected = os.path.join(data_path, "pkg5", "b", "__init__.py")
+        expected = os.path.join(data_path, "pkg2", "b", "__init__.py")
         assert_equal(expected, found_module)
 
     def test__find_d_nowhere(self) -> None:
