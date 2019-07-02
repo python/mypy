8b9fd06ba1a9321f17c8ffbfa8da34923719c4b2
Ivan Levkivskyi <levkivskyi@gmail.com>
2019-07-02 17:13:42 +0100

    Don't do any tricky inference in deferred nodes

--- mypy/checker.py
+++ mypy/checker.py
@@ -2410,6 +2410,10 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
                                                            reinferred_rvalue_type, context,
                                                            infer_lvalue_type)
                     return
+                if isinstance(reinferred_rvalue_type, AnyType) and self.current_node_deferred:
+                    # Doing more inference in deferred nodes can be hard, so give up for now.
+                    # TODO: should we try harder?
+                    return
                 assert isinstance(reinferred_rvalue_type, TupleType)
                 rvalue_type = reinferred_rvalue_type
 
