36846dfa2038cb5eeb19e00a9f5553d30d0ef49c
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2019-07-24 12:44:18 +0100

    Work around mypyc failure

--- mypy/report.py
+++ mypy/report.py
@@ -837,7 +837,8 @@ class LinePrecisionReporter(AbstractReporter):
         width = max(4, max(len(info.module) for info in output_files))
         titles = ('Lines', 'Precise', 'Imprecise', 'Any', 'Empty', 'Unanalyzed')
         widths = (width,) + tuple(len(t) for t in titles)
-        fmt = '{:%d}  {:%d}  {:%d}  {:%d}  {:%d}  {:%d}  {:%d}\n' % widths
+        # TODO: Need mypyc mypy pin move
+        fmt = '{:%d}  {:%d}  {:%d}  {:%d}  {:%d}  {:%d}  {:%d}\n' % widths  # type: ignore
         with open(report_file, 'w') as f:
             f.write(
                 fmt.format('Name', *titles))
