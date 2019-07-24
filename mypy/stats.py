3f898fa4000a589cac51b096505e82dd08f8e642
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2019-07-24 13:45:28 +0100

    Fix unreachable code

--- mypy/stats.py
+++ mypy/stats.py
@@ -19,7 +19,7 @@ from mypy.nodes import (
     Expression, FuncDef, TypeApplication, AssignmentStmt, NameExpr, CallExpr, MypyFile,
     MemberExpr, OpExpr, ComparisonExpr, IndexExpr, UnaryExpr, YieldFromExpr, RefExpr, ClassDef,
     ImportFrom, Import, ImportAll, PassStmt, BreakStmt, ContinueStmt, StrExpr, BytesExpr,
-    IntExpr, FloatExpr, ComplexExpr, EllipsisExpr
+    UnicodeExpr, IntExpr, FloatExpr, ComplexExpr, EllipsisExpr, ExpressionStmt, Node
 )
 from mypy.util import correct_relative_import
 
@@ -183,21 +183,28 @@ class StatisticsVisitor(TraverserVisitor):
                             self.type(self.typemap.get(item))
         super().visit_assignment_stmt(o)
 
+    def visit_expression_stmt(self, o: ExpressionStmt) -> None:
+        if isinstance(o.expr, (StrExpr, UnicodeExpr, BytesExpr)):
+            # Docstring
+            self.record_line(o.line, TYPE_EMPTY)
+        else:
+            super().visit_expression_stmt(o)
+
     def visit_pass_stmt(self, o: PassStmt) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     def visit_break_stmt(self, o: BreakStmt) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     def visit_continue_stmt(self, o: ContinueStmt) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     def visit_name_expr(self, o: NameExpr) -> None:
         if o.fullname in ('builtins.None',
                           'builtins.True',
                           'builtins.False',
                           'builtins.Ellipsis'):
-            self.record_precise_if_checked_scope(o.line)
+            self.record_precise_if_checked_scope(o)
         else:
             self.process_node(o)
             super().visit_name_expr(o)
@@ -236,22 +243,22 @@ class StatisticsVisitor(TraverserVisitor):
         super().visit_unary_expr(o)
 
     def visit_str_expr(self, o: StrExpr) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     def visit_bytes_expr(self, o: BytesExpr) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     def visit_int_expr(self, o: IntExpr) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     def visit_float_expr(self, o: FloatExpr) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     def visit_complex_expr(self, o: ComplexExpr) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     def visit_ellipsis(self, o: EllipsisExpr) -> None:
-        self.record_precise_if_checked_scope(o.line)
+        self.record_precise_if_checked_scope(o)
 
     # Helpers
 
@@ -261,8 +268,14 @@ class StatisticsVisitor(TraverserVisitor):
                 self.line = node.line
                 self.type(self.typemap.get(node))
 
-    def record_precise_if_checked_scope(self, line: int) -> None:
-        self.record_line(line, TYPE_PRECISE if self.is_checked_scope() else TYPE_ANY)
+    def record_precise_if_checked_scope(self, node: Node) -> None:
+        if isinstance(node, Expression) and self.typemap and node not in self.typemap:
+            kind = TYPE_UNANALYZED
+        elif self.is_checked_scope():
+            kind = TYPE_PRECISE
+        else:
+            kind = TYPE_ANY
+        self.record_line(node.line, kind)
 
     def type(self, t: Optional[Type]) -> None:
         if not t:
