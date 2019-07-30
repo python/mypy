e254285d6dedef182ccadac0a5dd24542016e620
Michael J. Sullivan <sully@msully.net>
2019-07-30 12:31:50 -0700

    [mypyc] Move all test-data under mypyc

--- mypyc/test/config.py
+++ mypyc/test/config.py
@@ -4,4 +4,4 @@ this_file_dir = os.path.dirname(os.path.realpath(__file__))
 prefix = os.path.dirname(os.path.dirname(this_file_dir))
 
 # Locations of test data files such as test case descriptions (.test).
-test_data_prefix = os.path.join(prefix, 'test-data')
+test_data_prefix = os.path.join(prefix, 'mypyc', 'test-data')
