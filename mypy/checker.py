28a8cbd5e2093a3abed32f9943fdcfd90b4a0986
denis <gantsevdenis@gmail.com>
2019-08-21 12:50:29 -0400

    move comment to where it was

--- mypy/checker.py
+++ mypy/checker.py
@@ -2982,11 +2982,11 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
         if isinstance(s.expr, TupleExpr) and len(s.expr.items) > 0:
             self.fail(message_registry.MALFORMED_ASSERT, s)
 
+        # If this is asserting some isinstance check, bind that type in the following code
         true_map, else_map = self.find_isinstance_check(s.expr)
         if s.msg is not None:
             with self.binder.frame_context(can_skip=False, fall_through=1):
                 self.expr_checker.analyze_cond_branch(else_map, s.msg, None)
-        # If this is asserting some isinstance check, bind that type in the following code
         self.push_type_map(true_map)
 
     def visit_raise_stmt(self, s: RaiseStmt) -> None:
