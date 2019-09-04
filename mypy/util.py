3d33f52a78968cd45d7e0383eb13d9619c8efb2a
Ethan Smith <ethan@ethanhs.me>
2019-09-04 11:01:10 -0700

    Add support for ouput styling on Windows 10

--- mypy/util.py
+++ mypy/util.py
@@ -338,38 +338,61 @@ class FancyFormatter:
                  show_error_codes: bool) -> None:
         self.show_error_codes = show_error_codes
         # Check if we are in a human-facing terminal on a supported platform.
-        if sys.platform not in ('linux', 'darwin'):
+        if sys.platform not in ('linux', 'darwin', 'win32'):
             self.dummy_term = True
             return
         force_color = int(os.getenv('MYPY_FORCE_COLOR', '0'))
         if not force_color and (not f_out.isatty() or not f_err.isatty()):
             self.dummy_term = True
             return
+        if sys.platform == 'win32':
+            # Windows ANSI escape sequences are only supported on Threshold 2 and above.
+            if sys.getwindowsversion().build < 10586:
+                self.dummy_term = True
+                return
+            self.dummy_term = False
+            import ctypes
+            kernel32 = ctypes.windll.kernel32
+            ENABLE_PROCESSED_OUTPUT = 0x1
+            ENABLE_WRAP_AT_EOL_OUTPUT = 0x2
+            ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x4
+            STD_OUTPUT_HANDLE = -11
+            kernel32.SetConsoleMode(kernel32.GetStdHandle(STD_OUTPUT_HANDLE),
+                                                                ENABLE_PROCESSED_OUTPUT
+                                                                | ENABLE_WRAP_AT_EOL_OUTPUT
+                                                                | ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+            self.BOLD = '\033[1m'
+            self.UNDER = '\033[4m'
+            self.BLUE = '\033[94m'
+            self.GREEN = '\033[92m'
+            self.RED = '\033[91m'
+            self.YELLOW = '\033[93m'
+            self.NORMAL = '\033[0m'
+        else:
+            # We in a human-facing terminal, check if it supports enough styling.
+            if not CURSES_ENABLED:
+                self.dummy_term = True
+                return
+            try:
+                curses.setupterm()
+            except curses.error:
+                # Most likely terminfo not found.
+                self.dummy_term = True
+                return
+            bold = curses.tigetstr('bold')
+            under = curses.tigetstr('smul')
+            set_color = curses.tigetstr('setaf')
+            self.dummy_term = not (bold and under and set_color)
+            if self.dummy_term:
+                return
 
-        # We in a human-facing terminal, check if it supports enough styling.
-        if not CURSES_ENABLED:
-            self.dummy_term = True
-            return
-        try:
-            curses.setupterm()
-        except curses.error:
-            # Most likely terminfo not found.
-            self.dummy_term = True
-            return
-        bold = curses.tigetstr('bold')
-        under = curses.tigetstr('smul')
-        set_color = curses.tigetstr('setaf')
-        self.dummy_term = not (bold and under and set_color)
-        if self.dummy_term:
-            return
-
-        self.BOLD = bold.decode()
-        self.UNDER = under.decode()
-        self.BLUE = curses.tparm(set_color, curses.COLOR_BLUE).decode()
-        self.GREEN = curses.tparm(set_color, curses.COLOR_GREEN).decode()
-        self.RED = curses.tparm(set_color, curses.COLOR_RED).decode()
-        self.YELLOW = curses.tparm(set_color, curses.COLOR_YELLOW).decode()
-        self.NORMAL = curses.tigetstr('sgr0').decode()
+            self.BOLD = bold.decode()
+            self.UNDER = under.decode()
+            self.BLUE = curses.tparm(set_color, curses.COLOR_BLUE).decode()
+            self.GREEN = curses.tparm(set_color, curses.COLOR_GREEN).decode()
+            self.RED = curses.tparm(set_color, curses.COLOR_RED).decode()
+            self.YELLOW = curses.tparm(set_color, curses.COLOR_YELLOW).decode()
+            self.NORMAL = curses.tigetstr('sgr0').decode()
         self.colors = {'red': self.RED, 'green': self.GREEN,
                        'blue': self.BLUE, 'yellow': self.YELLOW,
                        'none': ''}
