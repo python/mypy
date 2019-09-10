python/mypy

commit 995829514cf469dcc21a10644a4c5cd78cf57b4e

Bind self correctly when mapping class methods from supertype (#7474)
Fixes #2511

mypy/checker.py
@@ -1530,7 +1530,11 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
         """
         if (isinstance(sym.node, (FuncDef, OverloadedFuncDef, Decorator))
                 and not is_static(sym.node)):
-            bound = bind_self(typ, self.scope.active_self_type())
+            if isinstance(sym.node, Decorator):
+                is_class_method = sym.node.func.is_class
+            else:
+                is_class_method = sym.node.is_class
+            bound = bind_self(typ, self.scope.active_self_type(), is_class_method)
         else:
             bound = typ
         return cast(FunctionLike, map_type_from_supertype(bound, sub_info, super_info))

test-data/unit/check-generics.test
@@ -2062,3 +2062,52 @@ class A:
 x, y = A()
 reveal_type(x)  # N: Revealed type is 'Any'
 reveal_type(y)  # N: Revealed type is 'Any'
+
+[case testSubclassingGenericSelfClassMethod]
+from typing import TypeVar, Type
+
+AT = TypeVar('AT', bound='A')
+
+class A:
+    @classmethod
+    def from_config(cls: Type[AT]) -> AT:
+        ...
+
+class B(A):
+    @classmethod
+    def from_config(cls: Type[B]) -> B:
+        return B()
+[builtins fixtures/classmethod.pyi]
+
+[case testSubclassingGenericSelfClassMethodOptional]
+# flags: --strict-optional
+from typing import TypeVar, Type, Optional
+
+AT = TypeVar('AT', bound='A')
+
+class A:
+    @classmethod
+    def from_config(cls: Type[AT]) -> Optional[AT]:
+        return None
+
+class B(A):
+    @classmethod
+    def from_config(cls: Type[B]) -> Optional[B]:
+        return B()
+[builtins fixtures/classmethod.pyi]
+
+[case testSubclassingGenericSelfClassMethodNonAnnotated]
+from typing import TypeVar, Type
+
+AT = TypeVar('AT', bound='A')
+
+class A:
+    @classmethod
+    def from_config(cls: Type[AT]) -> AT:
+        ...
+
+class B(A):
+    @classmethod
+    def from_config(cls) -> B:
+        return B()
+[builtins fixtures/classmethod.pyi]

commit 41db9a0c570a3e190f3749cf0b681a31823dc0f7

Pass is_classmethod to bind_self() also for superype (#7491)
Fix a regression caused by https://github.com/python/mypy/pull/7474

The original PR correctly added `is_classmethod=...` in `bind_self()` for supertype, but I didn't notice the same problem (flag not passed) was also present for _subtype_. The fix is trivial.

mypy/checker.py
@@ -1454,14 +1454,17 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
             if isinstance(defn, (FuncDef, OverloadedFuncDef)):
                 typ = self.function_type(defn)  # type: Type
                 override_class_or_static = defn.is_class or defn.is_static
+                override_class = defn.is_class
             else:
                 assert defn.var.is_ready
                 assert defn.var.type is not None
                 typ = defn.var.type
                 override_class_or_static = defn.func.is_class or defn.func.is_static
+                override_class = defn.func.is_class
             typ = get_proper_type(typ)
             if isinstance(typ, FunctionLike) and not is_static(context):
-                typ = bind_self(typ, self.scope.active_self_type())
+                typ = bind_self(typ, self.scope.active_self_type(),
+                                is_classmethod=override_class)
             # Map the overridden method type to subtype context so that
             # it can be checked for compatibility.
             original_type = get_proper_type(base_attr.type)

test-data/unit/check-classes.test
@@ -6141,3 +6141,18 @@ class C(B[int, T]):
     def __init__(self) -> None:
         # TODO: error message could be better.
         self.x: Tuple[str, T]  # E: Incompatible types in assignment (expression has type "Tuple[str, T]", base class "A" defined the type as "Tuple[int, T]")
+
+[case testOverrideGenericSelfClassMethod]
+from typing import Generic, TypeVar, Type, List
+
+T = TypeVar('T', bound=A)
+
+class A:
+    @classmethod
+    def meth(cls: Type[T]) -> List[T]: ...
+
+class B(A):
+    @classmethod
+    def meth(cls: Type[T]) -> List[T]: ...
+
+[builtins fixtures/isinstancelist.pyi]

test-data/unit/fixtures/isinstancelist.pyi
@@ -11,6 +11,7 @@ class type:
 
 class function: pass
 class ellipsis: pass
+class classmethod: pass
 
 def isinstance(x: object, t: Union[type, Tuple]) -> bool: pass
 def issubclass(x: object, t: Union[type, Tuple]) -> bool: pass