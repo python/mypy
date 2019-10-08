52e9427c82d1f61acbdb8a6362882aebd7e9989d
Samyak Jain <samyak.jn11@gmail.com>
2019-10-08 06:58:37 +0530

    Update attrs.py

--- mypy/plugins/attrs.py
+++ mypy/plugins/attrs.py
@@ -559,7 +559,7 @@ def _parse_converter(ctx: 'mypy.plugin.ClassDefContext',
 
         # Signal that we have an unsupported converter.
         ctx.api.fail(
-            "Unsupported converter, only types and named functions with an annotated return type are currently supported",
+            "Unsupported converter, only types and named functions with annotated return type are supported",
             converter
         )
         return Converter('')
