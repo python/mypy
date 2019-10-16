7506cbe4f1970bbc703a92cc1abd94b1424fa63a
Tudor Brindus <me@tbrindus.ca>
2019-10-16 14:37:13 -0400

    Don't restrict mixins to abstract properties

--- mypy/checker.py
+++ mypy/checker.py
@@ -1886,7 +1886,7 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
                 ok = is_subtype(first_sig, second_sig, ignore_pos_arg_names=True)
         elif first_type and second_type:
             ok = is_equivalent(first_type, second_type)
-            if not ok and name in base2.abstract_attributes:
+            if not ok:
                 second_node = base2[name].node
                 if isinstance(second_node, Decorator) and second_node.func.is_property:
                     ok = is_subtype(first_type, cast(CallableType, second_type).ret_type)
