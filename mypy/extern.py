ee48fb436c60702e802294e6ab03d68e1137e71d
Denis Gantsev <gantsevdenis@gmail.com>
2019-11-04 20:48:32 +0100

    comment

--- mypy/extern.py
+++ mypy/extern.py
@@ -1,7 +1,7 @@
 from typing import Union, Tuple, Any, Callable
 
 
-# copy pasted from typeshed
+# signature is partially copy pasted from typeshed isinstance
 def narrow_cast(T: Union[type, Tuple[Union[type, Tuple[Any, ...]], ...]]) \
         -> Callable[..., Callable[..., bool]]:
     def narrow_cast_inner(f: Callable[..., bool]) -> Callable[..., bool]:
