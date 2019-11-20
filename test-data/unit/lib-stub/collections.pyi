b5dd37af514e5e8a7a0f251f75eef2ff700cf44c
TH3CHARLie <th3charlie@gmail.com>
2019-11-20 12:28:53 +0800

    fix test

--- test-data/unit/lib-stub/collections.pyi
+++ test-data/unit/lib-stub/collections.pyi
@@ -10,8 +10,49 @@ def namedtuple(
     defaults: Optional[Iterable[Any]] = ...
 ) -> Any: ...
 
+from typing import (
+    Callable as Callable,
+    Container as Container,
+    Hashable as Hashable,
+    ItemsView as ItemsView,
+    Iterable as Iterable,
+    Iterator as Iterator,
+    KeysView as KeysView,
+    Mapping as Mapping,
+    MappingView as MappingView,
+    MutableMapping as MutableMapping,
+    MutableSequence as MutableSequence,
+    MutableSet as MutableSet,
+    Sequence as Sequence,
+    AbstractSet as Set,
+    Sized as Sized,
+    ValuesView as ValuesView,
+)
+
+_S = TypeVar('_S')
+_T = TypeVar('_T')
 _KT = TypeVar('_KT')
 _VT = TypeVar('_VT')
 
 class defaultdict(Dict[_KT, _VT], Generic[_KT, _VT]):
-    pass
+    default_factory: Callable[[], _VT]
+    @overload
+    def __init__(self, **kwargs: _VT) -> None: ...
+    @overload
+    def __init__(self, default_factory: Optional[Callable[[], _VT]]) -> None: ...
+    @overload
+    def __init__(self, default_factory: Optional[Callable[[], _VT]], **kwargs: _VT) -> None: ...
+    @overload
+    def __init__(self, default_factory: Optional[Callable[[], _VT]],
+                 map: Mapping[_KT, _VT]) -> None: ...
+    @overload
+    def __init__(self, default_factory: Optional[Callable[[], _VT]],
+                 map: Mapping[_KT, _VT], **kwargs: _VT) -> None: ...
+    @overload
+    def __init__(self, default_factory: Optional[Callable[[], _VT]],
+                 iterable: Iterable[Tuple[_KT, _VT]]) -> None: ...
+    @overload
+    def __init__(self, default_factory: Optional[Callable[[], _VT]],
+                 iterable: Iterable[Tuple[_KT, _VT]], **kwargs: _VT) -> None: ...
+    def __missing__(self, key: _KT) -> _VT: ...
+    def copy(self: _S) -> _S: ...
