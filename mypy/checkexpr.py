130663b634508d353b49a1291c250e6dc109ee02
Ivan Levkivskyi <levkivskyi@gmail.com>
2019-12-05 11:01:32 +0000

    Properly push scope when checking lambda expressions (#8080)
    Fixes #8079.
    
    Previously function scope was pushed inconsistently in two branches of 
    `visit_lambda_expr()` (it was only pushed in the second branch by 
    `check_func_item()`). I believe there is still a small inconsistency, although 
    I wasn't able to trigger a corresponding crash, I added a TODO about this.

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -3236,12 +3236,13 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             self.chk.return_types.append(AnyType(TypeOfAny.special_form))
             # Type check everything in the body except for the final return
             # statement (it can contain tuple unpacking before return).
-            for stmt in e.body.body[:-1]:
-                stmt.accept(self.chk)
-            # Only type check the return expression, not the return statement.
-            # This is important as otherwise the following statements would be
-            # considered unreachable. There's no useful type context.
-            ret_type = self.accept(e.expr(), allow_none_return=True)
+            with self.chk.scope.push_function(e):
+                for stmt in e.body.body[:-1]:
+                    stmt.accept(self.chk)
+                # Only type check the return expression, not the return statement.
+                # This is important as otherwise the following statements would be
+                # considered unreachable. There's no useful type context.
+                ret_type = self.accept(e.expr(), allow_none_return=True)
             fallback = self.named_type('builtins.function')
             self.chk.return_types.pop()
             return callable_type(e, fallback, ret_type)
@@ -3250,6 +3251,7 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             self.chk.return_types.append(inferred_type.ret_type)
             self.chk.check_func_item(e, type_override=type_override)
             if e.expr() not in self.chk.type_map:
+                # TODO: return expression must be accepted before exiting function scope.
                 self.accept(e.expr(), allow_none_return=True)
             ret_type = self.chk.type_map[e.expr()]
             if isinstance(get_proper_type(ret_type), NoneType):
