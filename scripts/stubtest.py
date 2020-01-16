be5c715b529e0e10f0e5cfcd2efb005fb9f20e38
hauntsaninja <>
2020-01-16 10:52:32 -0800

    stubtest: more improvements to logging

--- scripts/stubtest.py
+++ scripts/stubtest.py
@@ -47,21 +47,30 @@ MISSING = Missing()
 T = TypeVar("T")
 MaybeMissing = Union[T, Missing]
 
+_formatter = FancyFormatter(sys.stdout, sys.stderr, False)
+
+
+def _style(message: str, **kwargs: Any) -> str:
+    kwargs.setdefault("color", "none")
+    return _formatter.style(message, **kwargs)
+
 
 class Error:
     def __init__(
         self,
+        object_path: List[str],
         message: str,
         stub_object: MaybeMissing[nodes.Node],
         runtime_object: MaybeMissing[Any],
         stub_printer: Optional[Callable[[nodes.Node], str]] = None,
         runtime_printer: Optional[Callable[[Any], str]] = None,
     ) -> None:
+        self.object_desc = ".".join(object_path)
         self.message = message
         self.stub_object = stub_object
         self.runtime_object = runtime_object
         if stub_printer is None:
-            stub_printer = lambda stub: str(stub.type)
+            stub_printer = lambda stub: str(getattr(stub, "type", stub))
         self.stub_printer = lambda s: s if isinstance(s, Missing) else stub_printer(s)
         if runtime_printer is None:
             runtime_printer = lambda runtime: str(runtime)
@@ -100,16 +109,22 @@ class Error:
         if runtime_file:
             runtime_loc_str += f" in file {runtime_file}"
 
-        return "".join(
-            [
-                formatter.style("error:", "red", bold=True),
-                f" {self.message}\n",
-                f"Stub{stub_loc_str}:\n",
-                f"{self.stub_printer(self.stub_object)}\n",
-                f"Runtime{runtime_loc_str}:\n",
-                f"{self.runtime_printer(self.runtime_object)}\n",
-            ]
-        )
+        output = [
+            _style("error: ", color="red", bold=True),
+            _style(self.object_desc, bold=True),
+            f" {self.message}\n",
+            "Stub:",
+            _style(stub_loc_str, dim=True),
+            "\n",
+            _style(f"{self.stub_printer(self.stub_object)}\n", color="blue", dim=True),
+            "Runtime:",
+            _style(runtime_loc_str, dim=True),
+            "\n",
+            _style(
+                f"{self.runtime_printer(self.runtime_object)}\n", color="blue", dim=True
+            ),
+        ]
+        return "".join(output)
 
 
 def test_module(
@@ -123,7 +138,7 @@ def test_module(
 
     for mod, stub in stubs.items():
         runtime = importlib.import_module(mod)
-        yield from verify(stub, runtime)
+        yield from verify(stub, runtime, [mod])
 
 
 def trace(fn):
@@ -137,20 +152,24 @@ def trace(fn):
 
 
 @singledispatch
-def verify(stub: nodes.Node, runtime: MaybeMissing[Any]) -> Iterator[Error]:
-    yield Error("unknown mypy node", stub, runtime)
+def verify(
+    stub: nodes.Node, runtime: MaybeMissing[Any], object_path: List[str]
+) -> Iterator[Error]:
+    yield Error(object_path, "is an unknown mypy node", stub, runtime)
 
 
 @verify.register(nodes.MypyFile)
 @trace
 def verify_mypyfile(
-    stub: nodes.MypyFile, runtime: MaybeMissing[types.ModuleType]
+    stub: nodes.MypyFile,
+    runtime: MaybeMissing[types.ModuleType],
+    object_path: List[str],
 ) -> Iterator[Error]:
     if isinstance(runtime, Missing):
-        yield Error("not_in_runtime", stub, runtime)
+        yield Error(object_path, "is not present at runtime", stub, runtime)
         return
     if not isinstance(runtime, types.ModuleType):
-        yield Error("type_mismatch", stub, runtime)
+        yield Error(object_path, "is not a module", stub, runtime)
         return
 
     # Check all things in the stub
@@ -167,19 +186,20 @@ def verify_mypyfile(
         yield from verify(
             getattr(stub.names.get(entry, MISSING), "node", MISSING),
             getattr(runtime, entry, MISSING),
+            object_path + [entry],
         )
 
 
 @verify.register(nodes.TypeInfo)
 @trace
 def verify_typeinfo(
-    stub: nodes.TypeInfo, runtime: MaybeMissing[Type[Any]]
+    stub: nodes.TypeInfo, runtime: MaybeMissing[Type[Any]], object_path: List[str]
 ) -> Iterator[Error]:
     if isinstance(runtime, Missing):
-        yield Error("not_in_runtime", stub, runtime)
+        yield Error(object_path, "is not present at runtime", stub, runtime)
         return
     if not isinstance(runtime, type):
-        yield Error("type_mismatch", stub, runtime)
+        yield Error(object_path, "is not a type", stub, runtime)
         return
 
     to_check = set(stub.names)
@@ -189,19 +209,22 @@ def verify_typeinfo(
         yield from verify(
             getattr(stub.names.get(entry, MISSING), "node", MISSING),
             getattr(runtime, entry, MISSING),
+            object_path + [entry],
         )
 
 
 @verify.register(nodes.FuncItem)
 @trace
 def verify_funcitem(
-    stub: nodes.FuncItem, runtime: MaybeMissing[types.FunctionType]
+    stub: nodes.FuncItem,
+    runtime: MaybeMissing[types.FunctionType],
+    object_path: List[str],
 ) -> Iterator[Error]:
     if isinstance(runtime, Missing):
-        yield Error("not_in_runtime", stub, runtime)
+        yield Error(object_path, "is not present at runtime", stub, runtime)
         return
     if not isinstance(runtime, (types.FunctionType, types.BuiltinFunctionType)):
-        yield Error("type_mismatch", stub, runtime)
+        yield Error(object_path, "is not a function", stub, runtime)
         return
 
     try:
@@ -219,7 +242,8 @@ def verify_funcitem(
             # listed out the keyword parameters this function takes
             if runtime_args[j].kind != inspect.Parameter.VAR_KEYWORD:
                 yield Error(
-                    f"arg_mismatch: {stub.name}, stub missing {runtime_args[j].name}",
+                    object_path,
+                    f'is inconsistent, stub does not have argument "{runtime_args[j].name}"',
                     stub,
                     runtime,
                     runtime_printer=lambda s: str(inspect.signature(s)),
@@ -228,7 +252,8 @@ def verify_funcitem(
             continue
         if j >= len(runtime_args):
             yield Error(
-                f"arg_mismatch: {stub.name}, runtime missing {stub_args[i].variable.name}",
+                object_path,
+                f"is inconsistent, runtime does not have argument {stub_args[i].variable.name}",
                 stub,
                 runtime,
                 runtime_printer=lambda s: str(inspect.signature(s)),
@@ -267,7 +292,9 @@ def verify_funcitem(
         if stub_arg.variable.name != runtime_arg.name:
             # TODO: _asdict takes _self and self
             yield Error(
-                f"arg_mismatch: {stub.name}, {stub_arg.variable.name} != {runtime_arg.name}",
+                object_path,
+                f'is inconsistent, stub argument "{stub_arg.variable.name}" differs from '
+                f'runtime argument "{runtime_arg.name}"',
                 stub,
                 runtime,
                 runtime_printer=lambda s: str(inspect.signature(s)),
@@ -278,38 +305,43 @@ def verify_funcitem(
 
 @verify.register(Missing)
 @trace
-def verify_none(stub: Missing, runtime: MaybeMissing[Any]) -> Iterator[Error]:
-    yield Error("not_in_stub", stub, runtime)
+def verify_none(
+    stub: Missing, runtime: MaybeMissing[Any], object_path: List[str]
+) -> Iterator[Error]:
+    yield Error(object_path, "is not present in stub", stub, runtime)
     if isinstance(runtime, Missing):
         raise RuntimeError
 
 
 @verify.register(nodes.Var)
 @trace
-def verify_var(stub: nodes.Var, runtime: MaybeMissing[Any]) -> Iterator[Error]:
+def verify_var(
+    stub: nodes.Var, runtime: MaybeMissing[Any], object_path: List[str]
+) -> Iterator[Error]:
     if isinstance(runtime, Missing):
-        # Don't yield an error here, because we often can't find instance variables
-        # yield Error("not_in_runtime", stub, runtime)
+        # Don't always yield an error here, because we often can't find instance variables
+        if len(object_path) <= 1:
+            yield Error(object_path, "is not present at runtime", stub, runtime)
         return
     # TODO: Make this better
     if isinstance(stub, mypy.types.Instance):
         if stub.type.type.name != runtime.__name__:
-            yield Error(f"var_mismatch", stub, runtime)
+            yield Error(object_path, "var_mismatch", stub, runtime)
 
 
 @verify.register(nodes.OverloadedFuncDef)
 @trace
 def verify_overloadedfuncdef(
-    stub: nodes.OverloadedFuncDef, runtime: MaybeMissing[Any]
+    stub: nodes.OverloadedFuncDef, runtime: MaybeMissing[Any], object_path: List[str]
 ) -> Iterator[Error]:
     for func in stub.items:
-        yield from verify(func, runtime)
+        yield from verify(func, runtime, object_path)
 
 
 @verify.register(nodes.TypeVarExpr)
 @trace
 def verify_typevarexpr(
-    stub: nodes.TypeVarExpr, runtime: MaybeMissing[Any]
+    stub: nodes.TypeVarExpr, runtime: MaybeMissing[Any], object_path: List[str]
 ) -> Iterator[Error]:
     if False:
         yield None
@@ -318,20 +350,20 @@ def verify_typevarexpr(
 @verify.register(nodes.Decorator)
 @trace
 def verify_decorator(
-    stub: nodes.Decorator, runtime: MaybeMissing[Any]
+    stub: nodes.Decorator, runtime: MaybeMissing[Any], object_path: List[str]
 ) -> Iterator[Error]:
     if (
         len(stub.decorators) == 1
         and isinstance(stub.decorators[0], nodes.NameExpr)
         and stub.decorators[0].fullname == "typing.overload"
     ):
-        yield from verify(stub.func, runtime)
+        yield from verify(stub.func, runtime, object_path)
 
 
 @verify.register(nodes.TypeAlias)
 @trace
 def verify_typealias(
-    stub: nodes.TypeAlias, runtime: MaybeMissing[Any]
+    stub: nodes.TypeAlias, runtime: MaybeMissing[Any], object_path: List[str]
 ) -> Iterator[Error]:
     if False:
         yield None
@@ -371,6 +403,5 @@ def main() -> Iterator[Error]:
 
 
 if __name__ == "__main__":
-    formatter = FancyFormatter(sys.stdout, sys.stderr, False)
     for err in main():
         print(err)
