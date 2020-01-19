c32dfe52cc1e01d12fb10e74eda083effa0412a4
Ville Skyttä <ville.skytta@iki.fi>
2020-01-19 12:33:53 +0200

    Improve strict not supported in inline config error message

--- mypy/config_parser.py
+++ mypy/config_parser.py
@@ -343,7 +343,11 @@ def parse_mypy_comments(
         if reports:
             errors.append((lineno, "Reports not supported in inline configuration"))
         if strict_found:
-            errors.append((lineno, "Setting 'strict' not supported in inline configuration"))
+            errors.append((lineno,
+                           "Setting 'strict' not supported in inline configuration: specify it in "
+                           "a configuration file instead, or set individual inline flags "
+                           "(see 'mypy -h' for the list of flags enabled in strict mode)"))
+
         sections.update(new_sections)
 
     return sections, errors
