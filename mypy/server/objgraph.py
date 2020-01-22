af2eaee419e6b5f370e590539afbf9578e549123
Michael J. Sullivan <sully@msully.net>
2020-01-22 13:25:50 -0800

    Make the fine-grained mergechecker work again
    This fixes some stuff in the mergechecker that breaks with current
    mypy's and also some bugs that the mergechecker caught. The entire
    fine-grained test suite now passes with the merge checker on, which I
    don't think has ever been true before.
    
    This means that we could turn it on by default, but it doubles the
    runtime of the fine-grained tests (from 90s CPU time to 180s CPU time
    on my laptop), so I've left it off for now.
    
    The motivation here is that I knew intersect_callable's creation of
    types during typechecking used to run afoul of the consistency checker
    and so I was nervous that #8305 would cause more problems by adding
    more logic of that kind. It no longer does, probably as a result of
    the semantic analyzer rewrite, so I think we are in the clear on that.

--- mypy/server/objgraph.py
+++ mypy/server/objgraph.py
@@ -54,12 +54,18 @@ def isproperty(o: object, attr: str) -> bool:
 
 
 def get_edge_candidates(o: object) -> Iterator[Tuple[object, object]]:
+    # use getattr because mypyc expects dict, not mappingproxy
+    if '__getattribute__' in getattr(type(o), '__dict__'):  # noqa
+        return
     if type(o) not in COLLECTION_TYPE_BLACKLIST:
         for attr in dir(o):
-            if attr not in ATTR_BLACKLIST and hasattr(o, attr) and not isproperty(o, attr):
-                e = getattr(o, attr)
-                if not type(e) in ATOMIC_TYPE_BLACKLIST:
-                    yield attr, e
+            try:
+                if attr not in ATTR_BLACKLIST and hasattr(o, attr) and not isproperty(o, attr):
+                    e = getattr(o, attr)
+                    if not type(e) in ATOMIC_TYPE_BLACKLIST:
+                        yield attr, e
+            except AssertionError:
+                pass
     if isinstance(o, Mapping):
         for k, v in o.items():
             yield k, v
@@ -78,7 +84,7 @@ def get_edges(o: object) -> Iterator[Tuple[object, object]]:
                 yield (s, '__closure__'), e.__closure__  # type: ignore
             if hasattr(e, '__self__'):
                 se = e.__self__  # type: ignore
-                if se is not o and se is not type(o):
+                if se is not o and se is not type(o) and hasattr(s, '__self__'):
                     yield s.__self__, se  # type: ignore
         else:
             if not type(e) in TYPE_BLACKLIST:
