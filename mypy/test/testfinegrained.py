2c18d8d128c5bbad5f8c7c4fdd182ab2a5c13182
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-02-24 16:57:16 +0000

    WIP add test case

--- mypy/test/testfinegrained.py
+++ mypy/test/testfinegrained.py
@@ -153,7 +153,8 @@ class FineGrainedSuite(DataSuite):
         options.use_fine_grained_cache = self.use_cache and not build_cache
         options.cache_fine_grained = self.use_cache
         options.local_partial_types = True
-        if options.follow_imports == 'normal':
+        if re.search('flags:.*--follow-imports', source) is None:
+            # Override the default for follow_imports
             options.follow_imports = 'error'
 
         for name, _ in testcase.files:
