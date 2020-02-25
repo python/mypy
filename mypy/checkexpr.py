09cdab4ffd34f89cc02f512fc35fdef57cb4c267
Shantanu <hauntsaninja@users.noreply.github.com>
2020-02-25 09:39:16 -0800

    mypy: remove has_member (#8438)
    In particular:
    - The test case mentioned in the code passes without it
    - The test case changed seems to have more desirable behaviour now,
      consider:
    
    ```
    from typing import Any
    
    """
    class C:
        def __radd__(self, other) -> float:
            return 1.234
    """
    C: Any
    class D(C):
        pass
    
    reveal_type("str" + D())
    ```

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -2410,15 +2410,6 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             """Looks up the given operator and returns the corresponding type,
             if it exists."""
             local_errors = make_local_errors()
-
-            # TODO: Remove this call and rely just on analyze_member_access
-            # Currently, it seems we still need this to correctly deal with
-            # things like metaclasses?
-            #
-            # E.g. see the pythoneval.testMetaclassOpAccessAny test case.
-            if not self.has_member(base_type, op_name):
-                return None
-
             member = analyze_member_access(
                 name=op_name,
                 typ=base_type,
@@ -3798,43 +3789,6 @@ class ExpressionChecker(ExpressionVisitor[Type]):
                     'typing.Mapping',
                     [self.named_type('builtins.unicode'),
                      AnyType(TypeOfAny.special_form)])))
-
-    def has_member(self, typ: Type, member: str) -> bool:
-        """Does type have member with the given name?"""
-        # TODO: refactor this to use checkmember.analyze_member_access, otherwise
-        # these two should be carefully kept in sync.
-        typ = get_proper_type(typ)
-
-        if isinstance(typ, TypeVarType):
-            typ = get_proper_type(typ.upper_bound)
-        if isinstance(typ, TupleType):
-            typ = tuple_fallback(typ)
-        if isinstance(typ, LiteralType):
-            typ = typ.fallback
-        if isinstance(typ, Instance):
-            return typ.type.has_readable_member(member)
-        if isinstance(typ, CallableType) and typ.is_type_obj():
-            return typ.fallback.type.has_readable_member(member)
-        elif isinstance(typ, AnyType):
-            return True
-        elif isinstance(typ, UnionType):
-            result = all(self.has_member(x, member) for x in typ.relevant_items())
-            return result
-        elif isinstance(typ, TypeType):
-            # Type[Union[X, ...]] is always normalized to Union[Type[X], ...],
-            # so we don't need to care about unions here.
-            item = typ.item
-            if isinstance(item, TypeVarType):
-                item = get_proper_type(item.upper_bound)
-            if isinstance(item, TupleType):
-                item = tuple_fallback(item)
-            if isinstance(item, Instance) and item.type.metaclass_type is not None:
-                return self.has_member(item.type.metaclass_type, member)
-            if isinstance(item, AnyType):
-                return True
-            return False
-        else:
-            return False
 
     def not_ready_callback(self, name: str, context: Context) -> None:
         """Called when we can't infer the type of a variable because it's not ready yet.
