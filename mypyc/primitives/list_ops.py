9c3abe872ceaff365cc350c33745a063a1964b0a
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-03-21 13:53:06 +0000

    Document more primitives

--- mypyc/primitives/list_ops.py
+++ mypyc/primitives/list_ops.py
@@ -12,12 +12,14 @@ from mypyc.primitives.registry import (
 )
 
 
+# Get the 'builtins.list' type object.
 name_ref_op('builtins.list',
             result_type=object_rprimitive,
             error_kind=ERR_NEVER,
             emit=simple_emit('{dest} = (PyObject *)&PyList_Type;'),
             is_borrowed=True)
 
+# list(obj)
 to_list = func_op(
     name='builtins.list',
     arg_types=[object_rprimitive],
@@ -35,6 +37,7 @@ def emit_new(emitter: EmitterInterface, args: List[str], dest: str) -> None:
     emitter.emit_line('}')
 
 
+# Construct a list from values: [item1, item2, ....]
 new_list_op = custom_op(arg_types=[object_rprimitive],
                         result_type=list_rprimitive,
                         is_var_arg=True,
@@ -44,13 +47,13 @@ new_list_op = custom_op(arg_types=[object_rprimitive],
                         emit=emit_new)
 
 
+# list[index] (for an integer index)
 list_get_item_op = method_op(
     name='__getitem__',
     arg_types=[list_rprimitive, int_rprimitive],
     result_type=object_rprimitive,
     error_kind=ERR_MAGIC,
     emit=call_emit('CPyList_GetItem'))
-
 
 # Version with no int bounds check for when it is known to be short
 method_op(
@@ -71,7 +74,7 @@ list_get_item_unsafe_op = custom_op(
     format_str='{dest} = {args[0]}[{args[1]}] :: unsafe list',
     emit=simple_emit('{dest} = CPyList_GetItemUnsafe({args[0]}, {args[1]});'))
 
-
+# list[index] = obj
 list_set_item_op = method_op(
     name='__setitem__',
     arg_types=[list_rprimitive, int_rprimitive, object_rprimitive],
@@ -81,6 +84,7 @@ list_set_item_op = method_op(
     emit=call_emit('CPyList_SetItem'))
 
 
+# list.append(obj)
 list_append_op = method_op(
     name='append',
     arg_types=[list_rprimitive, object_rprimitive],
@@ -88,6 +92,7 @@ list_append_op = method_op(
     error_kind=ERR_FALSE,
     emit=call_negative_bool_emit('PyList_Append'))
 
+# list.extend(obj)
 list_extend_op = method_op(
     name='extend',
     arg_types=[list_rprimitive, object_rprimitive],
@@ -95,6 +100,7 @@ list_extend_op = method_op(
     error_kind=ERR_MAGIC,
     emit=simple_emit('{dest} = _PyList_Extend((PyListObject *) {args[0]}, {args[1]});'))
 
+# list.pop()
 list_pop_last = method_op(
     name='pop',
     arg_types=[list_rprimitive],
@@ -102,6 +108,7 @@ list_pop_last = method_op(
     error_kind=ERR_MAGIC,
     emit=call_emit('CPyList_PopLast'))
 
+# list.pop(index)
 list_pop = method_op(
     name='pop',
     arg_types=[list_rprimitive, int_rprimitive],
@@ -109,6 +116,7 @@ list_pop = method_op(
     error_kind=ERR_MAGIC,
     emit=call_emit('CPyList_Pop'))
 
+# list.count(obj)
 method_op(
     name='count',
     arg_types=[list_rprimitive, object_rprimitive],
@@ -135,6 +143,7 @@ def emit_multiply_reversed(emitter: EmitterInterface, args: List[str], dest: str
     emit_multiply_helper(emitter, dest, args[1], args[0])
 
 
+# list * int
 binary_op(op='*',
           arg_types=[list_rprimitive, int_rprimitive],
           result_type=list_rprimitive,
@@ -142,6 +151,7 @@ binary_op(op='*',
           format_str='{dest} = {args[0]} * {args[1]} :: list',
           emit=emit_multiply)
 
+# int * list
 binary_op(op='*',
           arg_types=[int_rprimitive, list_rprimitive],
           result_type=list_rprimitive,
@@ -157,6 +167,7 @@ def emit_len(emitter: EmitterInterface, args: List[str], dest: str) -> None:
     emitter.emit_line('%s = CPyTagged_ShortFromSsize_t(%s);' % (dest, temp))
 
 
+# len(list)
 list_len_op = func_op(name='builtins.len',
                       arg_types=[list_rprimitive],
                       result_type=short_int_rprimitive,
