c95d8b4e6709d6418cc08a3b61dd2115a23733f5
Ville Skyttä <ville.skytta@iki.fi>
2020-04-08 19:52:29 +0300

    Make strict in config a boolean (#8636)
    Note that setting strict = False does not invert any of the options
    touched by strict; instead it is the same as if strict was not set at
    all.
    
    Closes #8633.

--- mypy/config_parser.py
+++ mypy/config_parser.py
@@ -86,6 +86,7 @@ config_types = {
     'package_root': lambda s: [p.strip() for p in s.split(',')],
     'cache_dir': expand_path,
     'python_executable': expand_path,
+    'strict': bool,
 }  # type: Final
 
 
@@ -209,7 +210,7 @@ def parse_section(prefix: str, template: Options,
                     options_key = key[3:]
                     invert = True
                 elif key == 'strict':
-                    set_strict_flags()
+                    pass  # Special handling below
                 else:
                     print("%sUnrecognized option: %s = %s" % (prefix, key, section[key]),
                           file=stderr)
@@ -239,6 +240,10 @@ def parse_section(prefix: str, template: Options,
                 continue
         except ValueError as err:
             print("%s%s: %s" % (prefix, key, err), file=stderr)
+            continue
+        if key == 'strict':
+            if v:
+                set_strict_flags()
             continue
         if key == 'silent_imports':
             print("%ssilent_imports has been replaced by "
