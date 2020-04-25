2432d2876a886acedc020d54b638dbde83bc898f
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2020-04-25 23:27:37 +0100

    Fix dict subclasses

--- mypyc/irbuild/builder.py
+++ mypyc/irbuild/builder.py
@@ -24,6 +24,7 @@ from mypy.nodes import (
 from mypy.types import (
     Type, Instance, TupleType, UninhabitedType, get_proper_type
 )
+from mypy.maptype import map_instance_to_supertype
 from mypy.visitor import ExpressionVisitor, StatementVisitor
 from mypy.util import split_target
 
@@ -604,17 +605,24 @@ class IRBuilder:
         else:
             return self.type_to_rtype(target_type.args[0])
 
-    def get_dict_key_type(self, expr: Expression) -> RType:
+    def get_dict_base_type(self, expr: Expression) -> Instance:
+        """Find dict type of a dict-like expression.
+
+        This is useful for dict subclasses like SymbolTable.
+        """
         target_type = get_proper_type(self.types[expr])
         assert isinstance(target_type, Instance)
-        # TODO: be careful with subclasses.
-        return self.type_to_rtype(target_type.args[0])
+        dict_base = next(base for base in target_type.type.mro
+                         if base.fullname == 'builtins.dict')
+        return map_instance_to_supertype(target_type, dict_base)
+
+    def get_dict_key_type(self, expr: Expression) -> RType:
+        dict_base_type = self.get_dict_base_type(expr)
+        return self.type_to_rtype(dict_base_type.args[0])
 
     def get_dict_value_type(self, expr: Expression) -> RType:
-        target_type = get_proper_type(self.types[expr])
-        assert isinstance(target_type, Instance)
-        # TODO: be careful with subclasses.
-        return self.type_to_rtype(target_type.args[1])
+        dict_base_type = self.get_dict_base_type(expr)
+        return self.type_to_rtype(dict_base_type.args[1])
 
     def get_dict_item_type(self, expr: Expression) -> RType:
         key_type = self.get_dict_key_type(expr)
