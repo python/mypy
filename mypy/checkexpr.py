f5e83418d83d97d59d15c738ef2186adda0256a0
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2020-05-05 13:14:32 +0100

    Use a guaranteed object

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -3126,7 +3126,7 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             if alias_definition:
                 return AnyType(TypeOfAny.special_form)
             # This type is invalid in most runtime contexts, give it an 'object' type.
-            return join.object_or_any_from_type(item)
+            return self.named_type('builtins.object')
 
     def apply_type_arguments_to_callable(self, tp: Type, args: List[Type], ctx: Context) -> Type:
         """Apply type arguments to a generic callable type coming from a type object.
