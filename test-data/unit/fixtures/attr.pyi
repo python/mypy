d8f0602c8d8ab087f8e8c9a213dd6a282467b783
Markus Schmaus <markus.schmaus@web.de>
2020-05-23 16:30:53 +0200

    revert changes to tuple fixture to avoid issues with other test

--- test-data/unit/fixtures/attr.pyi
+++ test-data/unit/fixtures/attr.pyi
@@ -1,5 +1,7 @@
 # Builtins stub used to support @attr.s tests.
-from typing import Union, overload
+from typing import Union, overload, Sequence, Generic, TypeVar, Iterable, \
+    Tuple, Iterator
+
 
 class object:
     def __init__(self) -> None: pass
@@ -21,6 +23,24 @@ class complex:
     def __init__(self, real: float = ..., im: float = ...) -> None: ...
     @overload
     def __init__(self, real: str = ...) -> None: ...
+
+Tco = TypeVar('Tco', covariant=True)
+
+class tuple(Sequence[Tco], Generic[Tco]):
+    @overload
+    def __init__(self) -> None: pass
+    @overload
+    def __init__(self, x: Iterable[Tco]) -> None: pass
+    def __iter__(self) -> Iterator[Tco]: pass
+    def __contains__(self, item: object) -> bool: pass
+    def __getitem__(self, x: int) -> Tco: pass
+    def __rmul__(self, n: int) -> Tuple[Tco, ...]: pass
+    def __add__(self, x: Tuple[Tco, ...]) -> Tuple[Tco, ...]: pass
+    def count(self, obj: object) -> int: pass
+
+T = TypeVar('T')
+
+class list(Sequence[T], Generic[T]): pass
 
 class str: pass
 class unicode: pass
