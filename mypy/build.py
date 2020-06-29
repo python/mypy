33f65add7c4b1e8d02e7bd0258bc28a9d28af15b
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-06-29 12:47:40 +0100

    Fix error messages for submodules of third-party packages

--- mypy/build.py
+++ mypy/build.py
@@ -2516,12 +2516,13 @@ def module_not_found(manager: BuildManager, line: int, caller_state: State,
     else:
         msg, notes = reason.error_message_templates()
         errors.report(line, 0, msg.format(target), code=codes.IMPORT)
+        top_level = target.partition('.')[0]
         for note in notes:
             if '{}' in note:
-                note = note.format(legacy_bundled_packages[target])
+                note = note.format(legacy_bundled_packages[top_level])
             errors.report(line, 0, note, severity='note', only_once=True, code=codes.IMPORT)
         if reason is ModuleNotFoundReason.STUBS_NOT_INSTALLED:
-            manager.missing_stub_packages.add(legacy_bundled_packages[target])
+            manager.missing_stub_packages.add(legacy_bundled_packages[top_level])
     errors.set_import_context(save_import_context)
 
 
