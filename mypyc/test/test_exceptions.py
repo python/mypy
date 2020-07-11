a8aae84aa1c2f14f8ddd66f4a6b0461815a74d17
Xuanda Yang <th3charlie@gmail.com>
2020-07-11 15:01:55 +0800

    fix

--- mypyc/test/test_exceptions.py
+++ mypyc/test/test_exceptions.py
@@ -9,7 +9,7 @@ from mypy.test.config import test_temp_dir
 from mypy.test.data import DataDrivenTestCase
 from mypy.errors import CompileError
 
-from mypyc.common import TOP_LEVEL_NAME
+from mypyc.common import TOP_LEVEL_NAME, IS_32_BIT_PLATFORM
 from mypyc.ir.func_ir import format_func
 from mypyc.transform.uninit import insert_uninit_checks
 from mypyc.transform.exceptions import insert_exception_handling
@@ -32,7 +32,9 @@ class TestExceptionTransform(MypycDataSuite):
         """Perform a runtime checking transformation test case."""
         with use_custom_builtins(os.path.join(self.data_prefix, ICODE_GEN_BUILTINS), testcase):
             expected_output = remove_comment_lines(testcase.output)
-
+            # replace native_int with platform specific ints
+            int_format_str = 'int32' if IS_32_BIT_PLATFORM else 'int64'
+            expected_output = [s.replace('native_int', int_format_str) for s in expected_output]
             try:
                 ir = build_ir_for_single_file(testcase.input)
             except CompileError as e:
