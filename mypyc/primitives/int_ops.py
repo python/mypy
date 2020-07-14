9fb5888d9cc3f650ae1b8b6dc4fa235c3d4cbd02
Xuanda Yang <th3charlie@gmail.com>
2020-07-15 02:17:30 +0800

    merge GT

--- mypyc/primitives/int_ops.py
+++ mypyc/primitives/int_ops.py
@@ -173,5 +173,6 @@ int_less_than_ = c_custom_op(
 int_logical_op_mapping = {
     '==': IntLogicalOpDescrption(BinaryIntOp.EQ, int_equal_, False, False),
     '!=': IntLogicalOpDescrption(BinaryIntOp.NEQ, int_equal_, True, False),
-    '<': IntLogicalOpDescrption(BinaryIntOp.LT, int_less_than_, False, False)
+    '<': IntLogicalOpDescrption(BinaryIntOp.LT, int_less_than_, False, False),
+    '>': IntLogicalOpDescrption(BinaryIntOp.GT, int_less_than_, False, True),
 }  # type: Dict[str, IntLogicalOpDescrption]
