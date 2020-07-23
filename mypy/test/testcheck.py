fc1452830f410bd2eb09bc436ace74a97a6c78eb
Kamil Turek <kamil.turek@nokia.com>
2020-07-23 20:13:48 +0200

    Create test for __setattr__ usage with property

--- mypy/test/testcheck.py
+++ mypy/test/testcheck.py
@@ -89,6 +89,7 @@ typecheck_files = [
     'check-reports.test',
     'check-errorcodes.test',
     'check-annotated.test',
+    'check-setattr.test',
 ]
 
 # Tests that use Python 3.8-only AST features (like expression-scoped ignores):
