python/mypy

commit af00d01754e8afddaf9445db703d51076ccf6fae

Fix type annotation for AugmentedHelpFormatter (#6982)
This fixes a crash under mypyc.
Closes #6980.

mypy/main.py
@@ -6,7 +6,7 @@ import subprocess
 import sys
 import time
 
-from typing import Dict, List, Optional, Tuple, TextIO
+from typing import Any, Dict, List, Optional, Tuple, TextIO
 
 from mypy import build
 from mypy import defaults
@@ -124,7 +124,9 @@ class AugmentedHelpFormatter(argparse.RawDescriptionHelpFormatter):
     def __init__(self, prog: str) -> None:
         super().__init__(prog=prog, max_help_position=28)
 
-    def _fill_text(self, text: str, width: int, indent: int) -> str:
+    # FIXME: typeshed incorrectly has the type of indent as int when
+    # it should be str. Make it Any to avoid rusing mypyc.
+    def _fill_text(self, text: str, width: int, indent: Any) -> str:
         if '\n' in text:
             # Assume we want to manually format the text
             return super()._fill_text(text, width, indent)

commit e3ceefff093036859f1f63f45c0399dffb5e1a76

Restore the correct type of AugmentedHelpFormatter._fill_text()
This reverts commit af00d01754e8afddaf9445db703d51076ccf6fae which
introduced a workaround for an incorrect type definition in typeshed.

The type of AugmentedHelpFormatter._fill_text() got fixed in commit
python/typeshed@686e21d330ce9a2ca16fb925554fd5eba082fb8f so we can
remove the workaround.

mypy/main.py
@@ -134,9 +134,7 @@ class AugmentedHelpFormatter(argparse.RawDescriptionHelpFormatter):
     def __init__(self, prog: str) -> None:
         super().__init__(prog=prog, max_help_position=28)
 
-    # FIXME: typeshed incorrectly has the type of indent as int when
-    # it should be str. Make it Any to avoid rusing mypyc.
-    def _fill_text(self, text: str, width: int, indent: Any) -> str:
+    def _fill_text(self, text: str, width: int, indent: str) -> str:
         if '\n' in text:
             # Assume we want to manually format the text
             return super()._fill_text(text, width, indent)