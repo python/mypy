c4f10eb9596298eb4d19583da506973647a695b5
Septatrix <24257556+Septatrix@users.noreply.github.com>
2020-09-18 22:04:57 +0200

    Flush streams and close stderr

--- mypy/__main__.py
+++ mypy/__main__.py
@@ -7,7 +7,10 @@ from mypy.main import main
 def console_entry() -> None:
     try:
         main(None, sys.stdout, sys.stderr)
+        sys.stdout.flush()
+        sys.stderr.flush()
     except BrokenPipeError:
+        sys.stderr.close()
         sys.exit(2)
 
 
