3fbddafdf61aceb5140e3b7a5be7446fdef28dc3
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-09-26 19:13:58 +0100

    Add remaining bitwise ops

--- mypyc/lib-rt/CPy.h
+++ mypyc/lib-rt/CPy.h
@@ -135,6 +135,8 @@ CPyTagged CPyTagged_Multiply(CPyTagged left, CPyTagged right);
 CPyTagged CPyTagged_FloorDivide(CPyTagged left, CPyTagged right);
 CPyTagged CPyTagged_Remainder(CPyTagged left, CPyTagged right);
 CPyTagged CPyTagged_And(CPyTagged left, CPyTagged right);
+CPyTagged CPyTagged_Or(CPyTagged left, CPyTagged right);
+CPyTagged CPyTagged_Xor(CPyTagged left, CPyTagged right);
 bool CPyTagged_IsEq_(CPyTagged left, CPyTagged right);
 bool CPyTagged_IsLt_(CPyTagged left, CPyTagged right);
 PyObject *CPyTagged_Str(CPyTagged n);
