1b34ae183e5773178a5a796e644cb0eecde574f2
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-09-26 18:40:37 +0100

    Add separate test case variant for 32-bit platforms
    It should fail right now. I'll update it later after I've validated
    that it fails as expected.

--- mypyc/test/test_irbuild.py
+++ mypyc/test/test_irbuild.py
@@ -6,7 +6,7 @@ from mypy.test.config import test_temp_dir
 from mypy.test.data import DataDrivenTestCase
 from mypy.errors import CompileError
 
-from mypyc.common import TOP_LEVEL_NAME
+from mypyc.common import TOP_LEVEL_NAME, IS_32_BIT_PLATFORM
 from mypyc.ir.func_ir import format_func
 from mypyc.test.testutil import (
     ICODE_GEN_BUILTINS, use_custom_builtins, MypycDataSuite, build_ir_for_single_file,
@@ -46,6 +46,12 @@ class TestGenOps(MypycDataSuite):
             expected_output = remove_comment_lines(testcase.output)
             expected_output = replace_native_int(expected_output)
             expected_output = replace_word_size(expected_output)
+            name = testcase.name
+            # If this is specific to some bit width, always pass if platform doesn't match.
+            if name.endswith('_64bit') and IS_32_BIT_PLATFORM:
+                return
+            if name.endswith('_32bit') and not IS_32_BIT_PLATFORM:
+                return
             try:
                 ir = build_ir_for_single_file(testcase.input, options)
             except CompileError as e:
@@ -54,7 +60,7 @@ class TestGenOps(MypycDataSuite):
                 actual = []
                 for fn in ir:
                     if (fn.name == TOP_LEVEL_NAME
-                            and not testcase.name.endswith('_toplevel')):
+                            and not name.endswith('_toplevel')):
                         continue
                     actual.extend(format_func(fn))
 
