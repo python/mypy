0de6f47a768142fc9b1b3e70247904f0ed174722
Greg Compestine <gcompestine@bloomberg.net>
2020-12-16 11:07:18 -0800

    tests: Avoid side effect of COLUMNS env var
    Several tests rely on getting the default width of a terminal for
    rendering test output. Some PTY environments (in my case the Emacs
    shell) set the env var `COLUMNS` to the terminal width, which can
    cause several tests to fail due to formatting mismatches.
    
    Prior to running thesee tests, mock out `sys.environ`, removing the
    `COLUMNS` env var when present.

--- mypy/test/testutil.py
+++ mypy/test/testutil.py
@@ -8,5 +8,8 @@ class TestGetTerminalSize(TestCase):
     def test_get_terminal_size_in_pty_defaults_to_80(self) -> None:
         # when run using a pty, `os.get_terminal_size()` returns `0, 0`
         ret = os.terminal_size((0, 0))
+        mock_environ = os.environ.copy()
+        mock_environ.pop('COLUMNS', None)
         with mock.patch.object(os, 'get_terminal_size', return_value=ret):
-            assert get_terminal_width() == 80
+            with mock.patch.dict(os.environ, values=mock_environ, clear=True):
+                assert get_terminal_width() == 80
