990b6a6f03ad82c10cb8edbe70508b943336f7f3
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-12-16 18:54:07 +0000

    Add error code for name mistaches in named tuples and TypedDicts (#9811)
    This gives a new error code for code like this:
    
    ```
    # Foo and Bar don't match
    Foo = NamedTuple("Bar", [...])
    ```
    
    Since these errors generally don't cause runtime issues, some users may want to
    disable these errors. It's now easy to do using the error code name-match.

--- mypy/errorcodes.py
+++ mypy/errorcodes.py
@@ -117,6 +117,9 @@ REDUNDANT_EXPR = ErrorCode(
     "Warn about redundant expressions",
     'General',
     default_enabled=False)  # type: Final
+NAME_MATCH = ErrorCode(
+    'name-match', "Check that type definition has consistent naming", 'General')  # type: Final
+
 
 # Syntax errors are often blocking.
 SYNTAX = ErrorCode(
