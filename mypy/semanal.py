f242a678a1857706ff15157da7e90a7a1458cbce
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-12-16 17:43:30 +0000

    Give specific error code for named tuple naming mismatch
    This can generate many errors in large codebases, so make it possible
    to disable it.

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -2205,7 +2205,7 @@ class SemanticAnalyzer(NodeVisitor[None],
             return False
         if internal_name != name:
             self.fail("First argument to namedtuple() should be '{}', not '{}'".format(
-                name, internal_name), s.rvalue)
+                name, internal_name), s.rvalue, code=codes.NAME_MATCH)
             return True
         # Yes, it's a valid namedtuple, but defer if it is not ready.
         if not info:
