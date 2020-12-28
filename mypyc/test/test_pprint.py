7833a1d38eb3b3dc39545c0effd659d413419f15
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-12-28 12:46:23 +0000

    Fixes

--- mypyc/test/test_pprint.py
+++ mypyc/test/test_pprint.py
@@ -3,7 +3,7 @@ from typing import List
 
 from mypyc.ir.ops import BasicBlock, Register, Op, LoadInt, BinaryIntOp, Unreachable, Assign
 from mypyc.ir.rtypes import int_rprimitive
-from mypyc.ir.pprint import generate_names_for_env
+from mypyc.ir.pprint import generate_names_for_ir
 
 
 def register(name: str) -> Register:
@@ -18,18 +18,18 @@ def make_block(ops: List[Op]) -> BasicBlock:
 
 class TestGenerateNames(unittest.TestCase):
     def test_empty(self) -> None:
-        assert generate_names_for_env([], []) == {}
+        assert generate_names_for_ir([], []) == {}
 
     def test_arg(self) -> None:
         reg = register('foo')
-        assert generate_names_for_env([reg], []) == {reg: 'foo'}
+        assert generate_names_for_ir([reg], []) == {reg: 'foo'}
 
     def test_int_op(self) -> None:
         op1 = LoadInt(2)
         op2 = LoadInt(4)
         op3 = BinaryIntOp(int_rprimitive, op1, op2, BinaryIntOp.ADD)
         block = make_block([op1, op2, op3, Unreachable()])
-        assert generate_names_for_env([], [block]) == {op1: 'i0', op2: 'i1', op3: 'r0'}
+        assert generate_names_for_ir([], [block]) == {op1: 'i0', op2: 'i1', op3: 'r0'}
 
     def test_assign(self) -> None:
         reg = register('foo')
@@ -37,4 +37,4 @@ class TestGenerateNames(unittest.TestCase):
         op2 = Assign(reg, op1)
         op3 = Assign(reg, op1)
         block = make_block([op1, op2, op3])
-        assert generate_names_for_env([reg], [block]) == {op1: 'i0', reg: 'foo'}
+        assert generate_names_for_ir([reg], [block]) == {op1: 'i0', reg: 'foo'}
