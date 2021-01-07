9ceabe0e7268e68c2cb19fa84a0e7ad6bafa09f0
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2021-01-07 07:03:36 -0800

    Add Python 3.9 to trove classifiers (#9887)
    Since my checklist in https://github.com/python/mypy/issues/9761 is
    almost complete, it's time to check off this box as well.
    
    Co-authored-by: hauntsaninja <>

--- setup.py
+++ setup.py
@@ -165,6 +165,7 @@ classifiers = [
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Topic :: Software Development',
 ]
 
