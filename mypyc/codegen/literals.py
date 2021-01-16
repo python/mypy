57b9073b997d8aec7346106267b0d5dcac3c3728
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2021-01-16 17:00:23 +0000

    Start work on str literal tables

--- mypyc/codegen/literals.py
+++ mypyc/codegen/literals.py
@@ -1,4 +1,6 @@
-from typing import Dict, Type, Optional
+from typing import Dict, Type, List
+
+from mypyc.codegen.cstring import encode_bytes_as_c_string
 
 
 class Literals:
@@ -8,6 +10,7 @@ class Literals:
         self.literals = {}  # type: Dict[Type[object], Dict[str, int]]
 
     def record_literal(self, value: str) -> None:
+        """Ensure that the literal value is available in generated code."""
         literals = self.literals
         t = type(value)
         if t not in literals:
@@ -17,4 +20,36 @@ class Literals:
             d[value] = len(d)
 
     def literal_index(self, value: str) -> int:
+        """Return the index to the literals array for given value."""
         return self.literals[str][value]
+
+    def encoded_str_values(self) -> List[bytes]:
+        return encode_str_values(self.literals[str])
+
+
+def encode_str_values(values: Dict[str, int]) -> List[bytes]:
+    value_by_index = {}
+    for value, index in values.items():
+        value_by_index[index] = value
+    result = []
+    for i in range(len(values)):
+        value = value_by_index[i]
+        c_literal = format_str_literal(value)
+        result.append(c_literal)
+    return result
+
+
+def format_int(n: int) -> bytes:
+    if n < 128:
+        return bytes([n])
+    result = b''
+    while n >= 128:
+        result += bytes([(n & 0x7f) | 128])
+        n >>= 7
+    result += bytes([n])
+    return result
+
+
+def format_str_literal(s: str) -> bytes:
+    utf8 = s.encode('utf-8')
+    return format_int(len(utf8)) + utf8
