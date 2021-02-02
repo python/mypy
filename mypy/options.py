1c0b5c33729eac3bb3d8d051f45433e3ed2a7798
hauntsaninja <>
2021-02-01 22:50:30 -0800

    flake8

--- mypy/options.py
+++ mypy/options.py
@@ -98,7 +98,9 @@ class Options:
         # top-level __init__.py to your packages.
         self.explicit_package_bases = False
         # File names, directory names or subpaths to avoid checking
-        self.exclude = "/(__pycache__|site-packages|node_modules)/|/(\\.[^/]+|[^/]+~)/?$"  # type: str
+        self.exclude = (
+            "/(__pycache__|site-packages|node_modules)/|/(\\.[^/]+|[^/]+~)/?$"
+        ) # type: str
 
         # disallow_any options
         self.disallow_any_generics = False
