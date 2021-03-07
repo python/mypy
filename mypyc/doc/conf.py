7e9b57ce6cee69e2b05c29f089a223aa46774e29
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2021-03-07 10:29:16 +0000

    [mypyc] Make mypyc docs use the same theme as mypy docs (#10176)
    Consistency is good.
    
    Work on mypyc/mypyc#807.

--- mypyc/doc/conf.py
+++ mypyc/doc/conf.py
@@ -43,8 +43,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+try:
+    import sphinx_rtd_theme
+except:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
