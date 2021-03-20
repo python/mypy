baf1d63919a86da90f0a000017c2f1900086ea3b
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2021-03-20 12:41:42 +0000

    Fix enums

--- mypyc/irbuild/builder.py
+++ mypyc/irbuild/builder.py
@@ -345,8 +345,12 @@ class IRBuilder:
         # Currently the stack always has at least two items: dummy and top-level.
         return len(self.fn_infos) <= 2
 
-    def init_final_static(self, lvalue: Lvalue, rvalue_reg: Value,
-                          class_name: Optional[str] = None) -> None:
+    def init_final_static(self,
+                          lvalue: Lvalue,
+                          rvalue_reg: Value,
+                          class_name: Optional[str] = None,
+                          *,
+                          type_override: Optional[RType] = None) -> None:
         assert isinstance(lvalue, NameExpr)
         assert isinstance(lvalue.node, Var)
         if lvalue.node.final_value is None:
@@ -355,7 +359,7 @@ class IRBuilder:
             else:
                 name = '{}.{}'.format(class_name, lvalue.name)
             assert name is not None, "Full name not set for variable"
-            coerced = self.coerce(rvalue_reg, self.node_type(lvalue), lvalue.line)
+            coerced = self.coerce(rvalue_reg, type_override or self.node_type(lvalue), lvalue.line)
             self.final_names.append((name, coerced.type))
             self.add(InitStatic(coerced, name, self.module_name))
 
