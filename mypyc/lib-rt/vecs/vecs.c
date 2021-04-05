c49c01922bce5c9585df72dfbb85505d8dbfa56b
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2021-04-05 15:14:30 +0100

    Can construct empty vec

--- mypyc/lib-rt/vecs/vecs.c
+++ mypyc/lib-rt/vecs/vecs.c
@@ -1,20 +1,58 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+static PyObject *vec_new(PyTypeObject *self, PyObject *args, PyObject *kw);
+
 typedef struct {
-    PyObject_HEAD
-    /* Type-specific fields go here. */
+    PyObject_VAR_HEAD
+    Py_ssize_t capacity;
+    long long item[1];
 } VecObject;
 
 static PyTypeObject VecType = {
     PyVarObject_HEAD_INIT(NULL, 0)
     .tp_name = "vecs.vec",
     .tp_doc = "vec doc",
-    .tp_basicsize = sizeof(VecObject),
-    .tp_itemsize = 0,
+    .tp_basicsize = sizeof(VecObject) - sizeof(long long),
+    .tp_itemsize = sizeof(long long),
     .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_new = PyType_GenericNew,
+    .tp_new = vec_new,
+    .tp_free = PyObject_Del,
 };
+
+static VecObject *
+vec_alloc(Py_ssize_t size)
+{
+    VecObject *v;
+    /* TODO: Check for overflow */
+    v = PyObject_NewVar(VecObject, &VecType, size);
+    if (v == NULL)
+        return NULL;
+    return v;
+}
+
+PyObject *
+Vec_New(Py_ssize_t size)
+{
+    VecObject *v;
+    v = vec_alloc(size);
+    if (v == NULL)
+        return NULL;
+    for (Py_ssize_t i = 0; i < size; i++) {
+        v->item[i] = 0;
+    }
+    v->capacity = size;
+    return (PyObject *)v;
+}
+
+PyObject *vec_new(PyTypeObject *self, PyObject *args, PyObject *kw) {
+    static char *kwlist[] = {"", NULL};
+    PyObject *t;
+    if (!PyArg_ParseTupleAndKeywords(args, kw, "O:vec", kwlist, &t)) {
+        return NULL;
+    }
+    return Vec_New(0);
+}
 
 static PyObject *
 vecs_append(PyObject *self, PyObject *args)
