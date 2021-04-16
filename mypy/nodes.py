2d3364bc4fff3fd13cb2ce1cfee083966ba5cb3e
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-04-15 22:36:45 -0500

    __annotations__ now implicit module attr

--- mypy/nodes.py
+++ mypy/nodes.py
@@ -100,7 +100,8 @@ inverse_node_kinds = {_kind: _name for _name, _kind in node_kinds.items()}  # ty
 implicit_module_attrs = {'__name__': '__builtins__.str',
                          '__doc__': None,  # depends on Python version, see semanal.py
                          '__file__': '__builtins__.str',
-                         '__package__': '__builtins__.str'}  # type: Final
+                         '__package__': '__builtins__.str',
+                         '__annotations__': '__builtins__.dict'}  # type: Final
 
 
 # These aliases exist because built-in class objects are not subscriptable.
