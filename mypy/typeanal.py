79fff93d72cf44d5dd7947bff0ec7994919bf925
David Foster <david@dafoster.net>
2021-04-24 21:14:19 -0700

    Treat Required[X] the same as just X.

--- mypy/typeanal.py
+++ mypy/typeanal.py
@@ -345,6 +345,11 @@ class TypeAnalyser(SyntheticTypeVisitor[Type], TypeAnalyzerPluginInterface):
                           " and at least one annotation", t)
                 return AnyType(TypeOfAny.from_error)
             return self.anal_type(t.args[0])
+        elif fullname in ('typing_extensions.Required', 'typing.Required'):
+            if len(t.args) != 1:
+                self.fail("Required[...] must have exactly one type argument", t)
+                return AnyType(TypeOfAny.from_error)
+            return self.anal_type(t.args[0])
         elif self.anal_type_guard_arg(t, fullname) is not None:
             # In most contexts, TypeGuard[...] acts as an alias for bool (ignoring its args)
             return self.named_type('builtins.bool')
