9637f992fbd9f0d8e5ae4a0fed91c6aa3fdb10f2
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2021-06-21 18:49:37 +0100

    Fix crash with assignment to variable guarded with TypeGuard (#10683)
    This is a quick fix to unblock the 0.910 release.
    
    The type guard type can be unrelated to the original type, so we
    shouldn't join it. I'm not sure whether this is right from first
    principles, but it seems to address the issue.
    
    Fixes #10671.

--- mypy/binder.py
+++ mypy/binder.py
@@ -5,7 +5,7 @@ from typing import Dict, List, Set, Iterator, Union, Optional, Tuple, cast
 from typing_extensions import DefaultDict
 
 from mypy.types import (
-    Type, AnyType, PartialType, UnionType, TypeOfAny, NoneType, get_proper_type
+    Type, AnyType, PartialType, UnionType, TypeOfAny, NoneType, TypeGuardType, get_proper_type
 )
 from mypy.subtypes import is_subtype
 from mypy.join import join_simple
@@ -202,7 +202,9 @@ class ConditionalTypeBinder:
             else:
                 for other in resulting_values[1:]:
                     assert other is not None
-                    type = join_simple(self.declarations[key], type, other)
+                    # Ignore the error about using get_proper_type().
+                    if not isinstance(other, TypeGuardType):  # type: ignore[misc]
+                        type = join_simple(self.declarations[key], type, other)
             if current_value is None or not is_same_type(type, current_value):
                 self._put(key, type)
                 changed = True
