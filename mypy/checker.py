a747da3fdd49cd394b3e5813eeb50cd063f80129
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2021-07-06 15:30:45 +0100

    Experiment

--- mypy/checker.py
+++ mypy/checker.py
@@ -2027,6 +2027,11 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
             with self.enter_final_context(s.is_final_def):
                 self.check_assignment(s.lvalues[-1], s.rvalue, s.type is None, s.new_syntax)
 
+        if s.is_alias_def:
+            # We do this mostly for compatibility with old semantic analyzer.
+            # TODO: should we get rid of this?
+            self.store_type(s.lvalues[-1], self.expr_checker.accept(s.rvalue))
+
         if (s.type is not None and
                 self.options.disallow_any_unimported and
                 has_any_from_unimported_type(s.type)):
