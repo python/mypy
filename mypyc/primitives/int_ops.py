66cae4b99e585735e3767623991d5f64b8269377
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2021-07-11 15:25:51 +0100

    [mypyc] Refactor: rename c_unary_op to unary_op (#10803)
    We've been getting rid of the `c_` prefixes.

--- mypyc/primitives/int_ops.py
+++ mypyc/primitives/int_ops.py
@@ -15,7 +15,7 @@ from mypyc.ir.rtypes import (
     str_rprimitive, bit_rprimitive, RType
 )
 from mypyc.primitives.registry import (
-    load_address_op, c_unary_op, CFunctionDescription, function_op, binary_op, custom_op
+    load_address_op, unary_op, CFunctionDescription, function_op, binary_op, custom_op
 )
 
 # These int constructors produce object_rprimitives that then need to be unboxed
@@ -114,11 +114,11 @@ int_binary_op('<<=', 'CPyTagged_Lshift', error_kind=ERR_MAGIC)
 
 
 def int_unary_op(name: str, c_function_name: str) -> CFunctionDescription:
-    return c_unary_op(name=name,
-                      arg_type=int_rprimitive,
-                      return_type=int_rprimitive,
-                      c_function_name=c_function_name,
-                      error_kind=ERR_NEVER)
+    return unary_op(name=name,
+                    arg_type=int_rprimitive,
+                    return_type=int_rprimitive,
+                    c_function_name=c_function_name,
+                    error_kind=ERR_NEVER)
 
 
 int_neg_op = int_unary_op('-', 'CPyTagged_Negate')
