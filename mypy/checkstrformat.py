f32ae8d50ebc858c473a24fa51999068ed875899
Jingchen Ye <97littleleaf11@gmail.com>
2021-07-28 23:38:30 +0800

    Replace /d with [0-9]

--- mypy/checkstrformat.py
+++ mypy/checkstrformat.py
@@ -53,7 +53,7 @@ def compile_format_re() -> Pattern[str]:
     key_re = r'(\((?P<key>[^)]*)\))?'  # (optional) parenthesised sequence of characters.
     flags_re = r'(?P<flag>[#0\-+ ]*)'  # (optional) sequence of flags.
     width_re = r'(?P<width>[1-9][0-9]*|\*)?'  # (optional) minimum field width (* or numbers).
-    precision_re = r'(?:\.(?P<precision>\*|\d+)?)?'  # (optional) . followed by * of numbers.
+    precision_re = r'(?:\.(?P<precision>\*|[0-9]+)?)?'  # (optional) . followed by * of numbers.
     length_mod_re = r'[hlL]?'  # (optional) length modifier (unused).
     type_re = r'(?P<type>.)?'  # conversion type.
     format_re = '%' + key_re + flags_re + width_re + precision_re + length_mod_re + type_re
