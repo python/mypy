869f7b447d850f8b9607ea93e6f891954a781237
Michael J. Sullivan <sully@msully.net>
2021-08-04 21:27:16 -0700

    Add an xfailed test for issue #9852 (#10932)
    As part of doing this I upgraded the pep561 tests to support
    incremental tests, and removed at least a little of the duplication
    between different incremental test suites.

--- mypy/test/data.py
+++ mypy/test/data.py
@@ -335,7 +335,6 @@ def module_from_path(path: str) -> str:
     path = re.sub(r'\.pyi?$', '', path)
     # We can have a mix of Unix-style and Windows-style separators.
     parts = re.split(r'[/\\]', path)
-    assert parts[0] == test_temp_dir
     del parts[0]
     module = '.'.join(parts)
     module = re.sub(r'\.__init__$', '', module)
