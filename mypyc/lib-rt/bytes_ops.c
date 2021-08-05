97a1b3f81f90ebc2739773cd949b92d891f47c4d
jhance <jhance@users.noreply.github.com>
2021-08-04 23:11:18 -0700

    Add primitive for bytes join() method (#10929)

--- mypyc/lib-rt/bytes_ops.c
+++ mypyc/lib-rt/bytes_ops.c
@@ -4,3 +4,13 @@
 
 #include <Python.h>
 #include "CPy.h"
+
+// Like _PyBytes_Join but fallback to dynamic call if 'sep' is not bytes
+// (mostly commonly, for bytearrays)
+PyObject *CPyBytes_Join(PyObject *sep, PyObject *iter) {
+    if (PyBytes_CheckExact(sep)) {
+        return _PyBytes_Join(sep, iter);
+    } else {
+        return PyObject_CallMethod(sep, "join", "(O)", iter);
+    }
+}
