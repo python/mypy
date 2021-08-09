7e16789a265cb85997d6ccfb4d669101a4c65b8f
Jingchen Ye <97littleleaf11@gmail.com>
2021-08-09 18:56:46 +0800

    Discard setitem

--- mypyc/lib-rt/CPy.h
+++ mypyc/lib-rt/CPy.h
@@ -401,8 +401,6 @@ Py_ssize_t CPyStr_Size_size_t(PyObject *str);
 
 
 CPyTagged CPyBytes_GetItem(PyObject *o, CPyTagged index);
-CPyTagged CPyBytes_GetItemShort(PyObject *o, CPyTagged index);
-bool CPyByteArray_SetItem(PyObject *o, CPyTagged index, CPyTagged num);
 PyObject *CPyBytes_Concat(PyObject *a, PyObject *b);
 PyObject *CPyBytes_Join(PyObject *sep, PyObject *iter);
 
--- mypyc/lib-rt/bytes_ops.c
+++ mypyc/lib-rt/bytes_ops.c
@@ -22,51 +22,7 @@ CPyTagged CPyBytes_GetItem(PyObject *o, CPyTagged index) {
         return num << 1;
     } else {
         PyErr_SetString(PyExc_OverflowError, CPYTHON_LARGE_INT_ERRMSG);
-        return CPY_INT_TAG;
-    }
-}
-
-CPyTagged CPyBytes_GetItemShort(PyObject *o, CPyTagged index) {
-    Py_ssize_t n = CPyTagged_ShortAsSsize_t(index);
-    Py_ssize_t size = ((PyVarObject *)o)->ob_size;
-    if ((n >= 0 && n >= size) || (n < 0 && n + size < 0)) {
-        PyErr_SetString(PyExc_IndexError, "index out of range");
-        return CPY_INT_TAG;
-    }
-    if (n < 0)
-        n += size;
-    int num = PyBytes_Check(o) ? ((PyBytesObject *)o)->ob_sval[n]
-                               : ((PyByteArrayObject *)o)->ob_bytes[n];
-    if (num < 0)
-        num += 256;
-    return num << 1;
-}
-
-bool CPyByteArray_SetItem(PyObject *o, CPyTagged index, CPyTagged num) {
-    if (PyByteArray_Check(o)) {
-        if (CPyTagged_CheckShort(index) && CPyTagged_CheckShort(num)) {
-            Py_ssize_t n = CPyTagged_ShortAsSsize_t(index);
-            Py_ssize_t size = ((PyVarObject *)o)->ob_size;
-            if ((n >= 0 && n >= size) || (n < 0 && n + size < 0)) {
-                PyErr_SetString(PyExc_IndexError, "index out of range");
-                return false;
-            }
-            if (n < 0)
-                n += size;
-            Py_ssize_t num = CPyTagged_ShortAsSsize_t(index);
-            if (num < 0 || n > 255) {
-                PyErr_SetString(PyExc_ValueError, "byte must be in range(0, 256)");
-                return false;
-            }
-            ((PyByteArrayObject *)o)->ob_bytes[n] = num;
-            return true;
-        } else {
-            PyErr_SetString(PyExc_OverflowError, CPYTHON_LARGE_INT_ERRMSG);
-            return false;
-        }
-    } else {
-        PyErr_SetString(PyExc_TypeError, "'bytes' object does not support item assignment");
-        return false;
+        return -1;
     }
 }
 
