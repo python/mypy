ba7b50c07886f744268d664adb5a6f026ce7be37
97littleleaf11 <97littleleaf11@users.noreply.github.com>
2021-08-11 17:34:52 +0800

    [mypyc] Add bytes index primitive (#10950)
    Implements part of mypyc/mypyc#880.

--- mypyc/lib-rt/CPy.h
+++ mypyc/lib-rt/CPy.h
@@ -400,6 +400,7 @@ Py_ssize_t CPyStr_Size_size_t(PyObject *str);
 // Bytes operations
 
 
+CPyTagged CPyBytes_GetItem(PyObject *o, CPyTagged index);
 PyObject *CPyBytes_Concat(PyObject *a, PyObject *b);
 PyObject *CPyBytes_Join(PyObject *sep, PyObject *iter);
 
