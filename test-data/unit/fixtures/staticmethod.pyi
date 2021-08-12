e76637ec05ced178e412e89b0048972fdf318a74
Niklas Gustafson <niklas.erik.gustafson@gmail.com>
2021-08-12 15:41:34 +0200

    Add dict stub to staticmethod fixture

--- test-data/unit/fixtures/staticmethod.pyi
+++ test-data/unit/fixtures/staticmethod.pyi
@@ -1,5 +1,8 @@
 import typing
 
+_KT = typing.TypeVar('_KT')
+_VT = typing.TypeVar('_VT')
+
 class object:
     def __init__(self) -> None: pass
 
@@ -7,6 +10,8 @@ class type:
     def __init__(self, x) -> None: pass
 
 class function: pass
+
+class dict(typing.Mapping[_KT, _VT]): pass
 
 staticmethod = object() # Dummy definition.
 property = object()  # Dummy definition
