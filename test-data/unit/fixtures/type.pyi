5c07a1385bc3d743e62aff52b934531f98b66bcf
Niklas Gustafson <niklas.erik.gustafson@gmail.com>
2021-08-12 15:19:33 +0200

    Add dict stub to type fixture

--- test-data/unit/fixtures/type.pyi
+++ test-data/unit/fixtures/type.pyi
@@ -1,14 +1,17 @@
 # builtins stub used in type-related test cases.
 
-from typing import Generic, TypeVar, List, Union
+from typing import Generic, TypeVar, List, Union, Mapping
 
 T = TypeVar('T')
+KT = TypeVar('KT')
+VT = TypeVar('VT')
 
 class object:
     def __init__(self) -> None: pass
     def __str__(self) -> 'str': pass
 
 class list(Generic[T]): pass
+class dict(Mapping[KT, VT]): pass
 
 class type(Generic[T]):
     __name__: str
