762991c87d48bd8dce58be617e2d23451c040ac0
Niklas Gustafson <niklas.erik.gustafson@gmail.com>
2021-08-13 15:09:33 +0200

    Add dict to complex fixture

--- test-data/unit/fixtures/complex.pyi
+++ test-data/unit/fixtures/complex.pyi
@@ -1,6 +1,11 @@
 # Builtins stub used for some float/complex test cases.
 # Please don't add tuple to this file, it is used to test incomplete fixtures.
 
+from typing import TypeVar, Mapping
+
+_KT = TypeVar('_KT')
+_VT = TypeVar('_VT')
+
 class object:
     def __init__(self): pass
 
@@ -10,3 +15,4 @@ class int: pass
 class float: pass
 class complex: pass
 class str: pass
+class dict(Mapping[_KT, _VT]): pass
