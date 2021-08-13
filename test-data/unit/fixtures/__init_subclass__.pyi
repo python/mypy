f3a2fd4f934cd81f9ee987a3bef2872f7bd09ed5
Niklas Gustafson <niklas.erik.gustafson@gmail.com>
2021-08-13 19:23:53 +0200

    Add dict stub to __init_subclass__ fixture

--- test-data/unit/fixtures/__init_subclass__.pyi
+++ test-data/unit/fixtures/__init_subclass__.pyi
@@ -1,4 +1,7 @@
 # builtins stub with object.__init_subclass__
+from typing import TypeVar, Mapping
+KT = TypeVar('KT')
+VT = TypeVar('VT')
 
 class object:
     def __init_subclass__(cls) -> None: pass
@@ -9,3 +12,4 @@ class int: pass
 class bool: pass
 class str: pass
 class function: pass
+class dict(Mapping[KT, VT]): pass
