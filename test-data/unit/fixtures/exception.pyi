5a5af459788e4f025903e017c51cc64cd91932a2
Niklas Gustafson <niklas.erik.gustafson@gmail.com>
2021-08-13 15:46:26 +0200

    Add dict stub to exception fixture

--- test-data/unit/fixtures/exception.pyi
+++ test-data/unit/fixtures/exception.pyi
@@ -1,11 +1,14 @@
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Mapping
 T = TypeVar('T')
+KT = TypeVar('KT')
+VT = TypeVar('VT')
 
 class object:
     def __init__(self): pass
 
 class type: pass
 class tuple(Generic[T]): pass
+class dict(Mapping[KT, VT]): pass
 class function: pass
 class int: pass
 class str: pass
