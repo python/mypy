c413c4d47cad11bc78f7cf629f33cc98be93d6df
Niklas Gustafson <niklas.erik.gustafson@gmail.com>
2021-08-13 19:46:12 +0200

    Add dict stub to slice fixture

--- test-data/unit/fixtures/slice.pyi
+++ test-data/unit/fixtures/slice.pyi
@@ -1,12 +1,15 @@
 # Builtins stub used in slicing test cases.
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Mapping
 T = TypeVar('T')
+KT = TypeVar('KT')
+VT = TypeVar('VT')
 
 class object:
     def __init__(self): pass
 
 class type: pass
 class tuple(Generic[T]): pass
+class dict(Mapping[KT, VT]): pass
 class function: pass
 
 class int: pass
