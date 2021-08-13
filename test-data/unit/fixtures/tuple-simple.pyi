7a68cc72d61503958a6911a5336648c24b198215
Niklas Gustafson <niklas.erik.gustafson@gmail.com>
2021-08-13 21:19:47 +0200

    Add dict stub to tuple-simple fixture

--- test-data/unit/fixtures/tuple-simple.pyi
+++ test-data/unit/fixtures/tuple-simple.pyi
@@ -3,9 +3,11 @@
 # This is a simpler version of tuple.py which is useful
 # and makes some test cases easier to write/debug.
 
-from typing import Iterable, TypeVar, Generic
+from typing import Iterable, TypeVar, Generic, Mapping
 
 T = TypeVar('T')
+KT = TypeVar('KT')
+VT = TypeVar('VT')
 
 class object:
     def __init__(self): pass
@@ -13,6 +15,7 @@ class object:
 class type: pass
 class tuple(Generic[T]):
     def __getitem__(self, x: int) -> T: pass
+class dict(Mapping[KT, VT]): pass
 class function: pass
 
 # We need int for indexing tuples.
