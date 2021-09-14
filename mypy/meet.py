74f04f4c00574f93b73e4cfd78679f2fa8648373
hauntsaninja <>
2021-09-13 21:58:25 -0700

    .

--- mypy/meet.py
+++ mypy/meet.py
@@ -146,7 +146,7 @@ def is_overlapping_types(left: Type,
     If 'prohibit_none_typevar_overlap' is True, we disallow None from overlapping with
     TypeVars (in both strict-optional and non-strict-optional mode).
     """
-    if isinstance(left, TypeGuardedType) or isinstance(right, TypeGuardedType):
+    if isinstance(left, TypeGuardedType) or isinstance(right, TypeGuardedType):  # type: ignore[misc]
         # A type guard forces the new type even if it doesn't overlap the old.
         return True
 
