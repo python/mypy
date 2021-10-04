cf27233dc9da678db36674def2aa380060ed6f7c
sobolevn <mail@sobolevn.me>
2021-10-04 16:55:11 +0300

    Adds check for unique enum keys

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -2748,6 +2748,13 @@ class SemanticAnalyzer(NodeVisitor[None],
         existing = names.get(name)
 
         outer = self.is_global_or_nonlocal(name)
+        if not outer and isinstance(self.type, TypeInfo) and self.type.is_enum:
+            # Special case: we need to be sure that `Enum` keys are unique.
+            if self.type.names.get(name) is not None:
+                self.fail('Attempted to reuse key "{}" in Enum definition "{}"'.format(
+                    name, self.type.name,
+                ), lvalue)
+
         if (not existing or isinstance(existing.node, PlaceholderNode)) and not outer:
             # Define new variable.
             var = self.make_name_lvalue_var(lvalue, kind, not explicit_type)
