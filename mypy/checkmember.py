8e01ad930015889ea8be1f26556fcc4e4d918e82
Nikita Sobolev <mail@sobolevn.me>
2021-10-11 23:55:47 +0300

    `None.__bool__` returns `Literal[False]` (#11290)
    Closes #11287

--- mypy/checkmember.py
+++ mypy/checkmember.py
@@ -317,17 +317,18 @@ def analyze_union_member_access(name: str, typ: UnionType, mx: MemberContext) ->
 
 
 def analyze_none_member_access(name: str, typ: NoneType, mx: MemberContext) -> Type:
-    if mx.chk.should_suppress_optional_error([typ]):
-        return AnyType(TypeOfAny.from_error)
     is_python_3 = mx.chk.options.python_version[0] >= 3
     # In Python 2 "None" has exactly the same attributes as "object". Python 3 adds a single
     # extra attribute, "__bool__".
     if is_python_3 and name == '__bool__':
+        literal_false = LiteralType(False, fallback=mx.named_type('builtins.bool'))
         return CallableType(arg_types=[],
                             arg_kinds=[],
                             arg_names=[],
-                            ret_type=mx.named_type('builtins.bool'),
+                            ret_type=literal_false,
                             fallback=mx.named_type('builtins.function'))
+    elif mx.chk.should_suppress_optional_error([typ]):
+        return AnyType(TypeOfAny.from_error)
     else:
         return _analyze_member_access(name, mx.named_type('builtins.object'), mx)
 
