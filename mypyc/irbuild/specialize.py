aeb65a6ff8cace69d4f2a04dcf511cce2cd8bf21
97littleleaf11 <97littleleaf11@users.noreply.github.com>
2021-11-12 17:00:29 +0800

    [mypyc] Faster max (#11530)
    Description
    Closes mypyc/mypyc#773, follows up to #10265

--- mypyc/irbuild/specialize.py
+++ mypyc/irbuild/specialize.py
@@ -186,27 +186,29 @@ def translate_set_from_generator_call(
 
 
 @specialize_function('builtins.min')
-def faster_min(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> Optional[Value]:
+@specialize_function('builtins.max')
+def faster_min_max(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> Optional[Value]:
     if expr.arg_kinds == [ARG_POS, ARG_POS]:
         x, y = builder.accept(expr.args[0]), builder.accept(expr.args[1])
         result = Register(builder.node_type(expr))
-        comparison = builder.binary_op(y, x, '<', expr.line)
-        true = BasicBlock()
-        false = BasicBlock()
-        next_block = BasicBlock()
+        # CPython evaluates arguments reversely when calling min(...) or max(...)
+        if callee.fullname == 'builtins.min':
+            comparison = builder.binary_op(y, x, '<', expr.line)
+        else:
+            comparison = builder.binary_op(y, x, '>', expr.line)
 
-        builder.add_bool_branch(comparison, true, false)
+        true_block, false_block, next_block = BasicBlock(), BasicBlock(), BasicBlock()
+        builder.add_bool_branch(comparison, true_block, false_block)
 
-        builder.activate_block(true)
+        builder.activate_block(true_block)
         builder.assign(result, builder.coerce(y, result.type, expr.line), expr.line)
         builder.goto(next_block)
 
-        builder.activate_block(false)
+        builder.activate_block(false_block)
         builder.assign(result, builder.coerce(x, result.type, expr.line), expr.line)
         builder.goto(next_block)
 
         builder.activate_block(next_block)
-
         return result
     return None
 
