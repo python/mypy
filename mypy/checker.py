cc6c8a99eb7ee40679135bde5700f030fa327d47
Nikita Sobolev <mail@sobolevn.me>
2021-11-18 06:18:12 +0300

    Now we can override attributes with methods, refs #10134 (#11561)
    Now we can override attributes with methods.
    
    Closes #10134
    Closes #11556
    
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- mypy/checker.py
+++ mypy/checker.py
@@ -1536,6 +1536,16 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
                     original_type = self.function_type(original_node)
                 elif isinstance(original_node, Decorator):
                     original_type = self.function_type(original_node.func)
+                elif isinstance(original_node, Var):
+                    # Super type can define method as an attribute.
+                    # See https://github.com/python/mypy/issues/10134
+
+                    # We also check that sometimes `original_node.type` is None.
+                    # This is the case when we use something like `__hash__ = None`.
+                    if original_node.type is not None:
+                        original_type = get_proper_type(original_node.type)
+                    else:
+                        original_type = NoneType()
                 else:
                     assert False, str(base_attr.node)
             if isinstance(original_node, (FuncDef, OverloadedFuncDef)):
