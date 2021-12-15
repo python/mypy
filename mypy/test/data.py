b4df2b341c345bb009210783dddeb454e7b45fc5
Nikita Sobolev <mail@sobolevn.me>
2021-12-15 13:49:17 +0300

    Enforces having unique test ids in a single namespace (#11687)
    I found lots of duplicate test names. It might hide important errors, be hard to reproduce 
    or rerun.
    
    Refs #11662
    
    Co-authored-by: Shantanu <12621235+hauntsaninja@users.noreply.github.com>

--- mypy/test/data.py
+++ mypy/test/data.py
@@ -578,8 +578,13 @@ def split_test_cases(parent: 'DataFileCollector', suite: 'DataSuite',
                      data,
                      flags=re.DOTALL | re.MULTILINE)
     line_no = cases[0].count('\n') + 1
+    test_names = set()
     for i in range(1, len(cases), NUM_GROUPS):
         name, writescache, only_when, platform_flag, skip, xfail, data = cases[i:i + NUM_GROUPS]
+        if name in test_names:
+            raise RuntimeError('Found a duplicate test name "{}" in {} on line {}'.format(
+                name, parent.name, line_no,
+            ))
         platform = platform_flag[1:] if platform_flag else None
         yield DataDrivenTestCase.from_parent(
             parent=parent,
@@ -595,6 +600,9 @@ def split_test_cases(parent: 'DataFileCollector', suite: 'DataSuite',
             line=line_no,
         )
         line_no += data.count('\n') + 1
+
+        # Record existing tests to prevent duplicates:
+        test_names.update({name})
 
 
 class DataSuiteCollector(pytest.Class):
