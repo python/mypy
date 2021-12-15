69dbb47da0af74af25e1b60a50d4ba2b0d8b2683
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2021-12-14 16:50:37 -0800

    Add Python 3.10 to trove classifiers (#11747)
    Co-authored-by: hauntsaninja <>

--- setup.py
+++ setup.py
@@ -166,6 +166,7 @@ classifiers = [
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Software Development',
 ]
 
