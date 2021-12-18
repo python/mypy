6f39906ba06d2ad48c0dca241ac0d06d6d0faa30
Peter Law <PeterJCLaw@gmail.com>
2021-12-18 13:03:47 +0000

    Tweak error message to avoid misleading the user
    Users who turn ignore-without-code on are likely at first to
    encounter cases where their existing ignores are too broad.
    The previous hint spelling was useful, however could encourage
    users to just ignore everything which was already ignored rather
    than actually reviewing the code. This change removes the implication
    that the correct course is to blindly update the ignore comment
    while still containing the useful information about what is ignored.

--- mypy/errors.py
+++ mypy/errors.py
@@ -511,7 +511,7 @@ class Errors:
             codes_hint = ''
             ignored_codes = used_ignored_lines[line]
             if ignored_codes:
-                codes_hint = f' (hint: add [{", ".join(ignored_codes)}])'
+                codes_hint = f' (currently ignored: [{", ".join(ignored_codes)}])'
 
             message = f'"type: ignore" comment without error code{codes_hint}'
             # Don't use report since add_error_info will ignore the error!
