50dbb3fe6a7e46442d63582e5916fb1052321b88
97littleleaf11 <97littleleaf11@gmail.com>
2022-01-13 13:24:33 +0800

    Add argument explanation

--- mypyc/codegen/emit.py
+++ mypyc/codegen/emit.py
@@ -436,6 +436,9 @@ class Emitter:
             declare_dest: If True, also declare the variable 'dest'
             error: What happens on error
             raise_exception: If True, also raise TypeError on failure
+            optional: Set True when it is an optional argument
+            src_type: The type of the source C variable, help speed up
+                      casting optional types
             likely: If the cast is likely to succeed (can be False for unions)
         """
         error = error or AssignHandler()
@@ -610,6 +613,7 @@ class Emitter:
                            declare_dest=False,
                            raise_exception=False,
                            optional=False,
+                           src_type=src_type,
                            likely=False)
             self.emit_line('if ({} != NULL) goto {};'.format(dest, good_label))
         # Handle cast failure.
@@ -681,8 +685,8 @@ class Emitter:
             declare_dest: If True, also declare the variable 'dest'
             error: What happens on error
             raise_exception: If True, also raise TypeError on failure
+            optional: Set True when it is an optional argument
             borrow: If True, create a borrowed reference
-
         """
         error = error or AssignHandler()
         # TODO: Verify refcount handling.
