6e147e702a4c961ff513723ff2be1d14d0830d80
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2022-01-25 14:18:27 +0000

    Update mypy/test/typefixture.py
    Co-authored-by: Nikita Sobolev <mail@sobolevn.me>

--- mypy/test/typefixture.py
+++ mypy/test/typefixture.py
@@ -157,7 +157,7 @@ class TypeFixture:
         self.lit1 = LiteralType(1, self.a)
         self.lit2 = LiteralType(2, self.a)
         self.lit3 = LiteralType("foo", self.d)
-        self.lit4 = LiteralType(3, self.a)
+        self.lit4 = LiteralType(4, self.a)
         self.lit1_inst = Instance(self.ai, [], last_known_value=self.lit1)
         self.lit2_inst = Instance(self.ai, [], last_known_value=self.lit2)
         self.lit3_inst = Instance(self.di, [], last_known_value=self.lit3)
