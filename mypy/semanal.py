6f04070077a88cbe9dca0afce57c072d7e1ab5d1
hauntsaninja <>
2022-02-15 19:16:20 -0800

    fffff

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -4806,32 +4806,34 @@ class SemanticAnalyzer(NodeVisitor[None],
 
         symbol_node: Optional[SymbolNode] = node.node
 
-        # I promise this type checks; I'm just making mypyc issues go away.
-        # mypyc is absolutely convinced that `symbol_node` narrows to a Var in the following,
-        # when it can also be a FuncBase. Once fixed, the casts in the following can be removed.
-        # See also https://github.com/mypyc/mypyc/issues/892
-        if self.is_class_scope() and isinstance((lambda x: x)(symbol_node), (FuncBase, Var)):
-            assert isinstance(symbol_node, SymbolNode)
-            # For imports in class scope, we construct a new node to represent the symbol and set
-            # its `info` attribute to `self.type`.
-            existing = self.current_symbol_table().get(name)
-            if (
-                # The redefinition checks in `add_symbol_table_node` don't work for our
-                # constructed Var / FuncBase, so check for possible redefinitions here.
-                existing is not None
-                and isinstance(existing.node, (FuncBase, Var))
-                and existing.type == cast(Any, symbol_node).type
-            ):
-                symbol_node = cast(SymbolNode, existing.node)
-            else:
-                # Construct the new node
-                constructed_node = cast(Any, copy.copy(symbol_node))
-                assert self.type is not None  # guaranteed by is_class_scope
-                constructed_node.line = context.line
-                constructed_node.column = context.column
-                constructed_node.info = self.type
-                constructed_node._fullname = self.qualified_name(name)
-                symbol_node = constructed_node
+        if self.is_class_scope():
+            # I promise this type checks; I'm just making mypyc issues go away.
+            # mypyc is absolutely convinced that `symbol_node` narrows to a Var in the following,
+            # when it can also be a FuncBase. Once fixed, casts in the following can be removed.
+            # See also https://github.com/mypyc/mypyc/issues/892
+            f = cast(Any, lambda x: x)
+            if isinstance(f(symbol_node), (FuncBase, Var)):
+                assert symbol_node is not None
+                # For imports in class scope, we construct a new node to represent the symbol and set
+                # its `info` attribute to `self.type`.
+                existing = self.current_symbol_table().get(name)
+                if (
+                    # The redefinition checks in `add_symbol_table_node` don't work for our
+                    # constructed Var / FuncBase, so check for possible redefinitions here.
+                    existing is not None
+                    and isinstance(f(existing.node), (FuncBase, Var))
+                    and f(existing.type) == f(symbol_node).type
+                ):
+                    symbol_node = existing.node
+                else:
+                    # Construct the new node
+                    constructed_node = f(copy.copy(symbol_node))
+                    assert self.type is not None  # guaranteed by is_class_scope
+                    constructed_node.line = context.line
+                    constructed_node.column = context.column
+                    constructed_node.info = self.type
+                    constructed_node._fullname = self.qualified_name(name)
+                    symbol_node = constructed_node
 
         symbol = SymbolTableNode(node.kind, symbol_node,
                                  module_public=module_public,
