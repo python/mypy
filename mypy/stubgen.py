21d957afc6241c57eaa364c70920e3927ff94fdd
citruz <3756270+citruz@users.noreply.github.com>
2022-03-18 02:57:02 +0100

    stubgen: fix handling of Protocol and add testcase (#12129)
    ### Description
    
    This PR fixes #12072 by correctly handling `Protocol` definitions. Previously, the `Protocol` base class was removed when generating type stubs which causes problems with other packages that want to use type definition (because they see it as a regular class, not as a `Protocol`).
    
    ## Test Plan
    
    Added a testcase to the stubgen testset.
    
    Co-authored-by: 97littleleaf11 <11172084+97littleleaf11@users.noreply.github.com>

--- mypy/stubgen.py
+++ mypy/stubgen.py
@@ -851,6 +851,12 @@ class StubGenerator(mypy.traverser.TraverserVisitor):
             base_types.append('metaclass=abc.ABCMeta')
             self.import_tracker.add_import('abc')
             self.import_tracker.require_name('abc')
+        elif self.analyzed and o.info.is_protocol:
+            type_str = 'Protocol'
+            if o.info.type_vars:
+                type_str += f'[{", ".join(o.info.type_vars)}]'
+            base_types.append(type_str)
+            self.add_typing_import('Protocol')
         if base_types:
             self.add('(%s)' % ', '.join(base_types))
         self.add(':\n')
