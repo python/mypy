222029b07e0fdcb3c174f15b61915b3b2e1665ca
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2022-04-07 19:16:02 +0100

    Minor proper subtype check optimization (#12536)
    Mypyc is bad at compiling nested functions. In one use case
    we were spending 7% of the mypy runtime just creating closure
    objects for `check_argument`. Here I manually inline the nested
    function to avoid this overhead.
    
    Work on #12526.
    
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- mypy/subtypes.py
+++ mypy/subtypes.py
@@ -1370,13 +1370,6 @@ class ProperSubtypeVisitor(TypeVisitor[bool]):
                         return True
 
             if left.type.has_base(right.type.fullname):
-                def check_argument(leftarg: Type, rightarg: Type, variance: int) -> bool:
-                    if variance == COVARIANT:
-                        return self._is_proper_subtype(leftarg, rightarg)
-                    elif variance == CONTRAVARIANT:
-                        return self._is_proper_subtype(rightarg, leftarg)
-                    else:
-                        return mypy.sametypes.is_same_type(leftarg, rightarg)
                 # Map left type to corresponding right instances.
                 left = map_instance_to_supertype(left, right.type)
                 if self.erase_instances:
@@ -1387,9 +1380,17 @@ class ProperSubtypeVisitor(TypeVisitor[bool]):
                 nominal = True
                 for ta, ra, tvar in zip(left.args, right.args, right.type.defn.type_vars):
                     if isinstance(tvar, TypeVarType):
-                        nominal = nominal and check_argument(ta, ra, tvar.variance)
+                        variance = tvar.variance
+                        if variance == COVARIANT:
+                            nominal = self._is_proper_subtype(ta, ra)
+                        elif variance == CONTRAVARIANT:
+                            nominal = self._is_proper_subtype(ra, ta)
+                        else:
+                            nominal = mypy.sametypes.is_same_type(ta, ra)
                     else:
-                        nominal = nominal and mypy.sametypes.is_same_type(ta, ra)
+                        nominal = mypy.sametypes.is_same_type(ta, ra)
+                    if not nominal:
+                        break
 
                 if nominal:
                     TypeState.record_subtype_cache_entry(self._subtype_kind, left, right)
