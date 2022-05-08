df4edc43ec18befda35eb9dcb56291b0b1539652
Tomoki Nakagawa <poilkjmnb0824@gmail.com>
2022-05-08 12:51:27 +0900

    Fix error message

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -1104,7 +1104,7 @@ class SemanticAnalyzer(NodeVisitor[None],
         if dec.decorators and dec.var.is_property:
             self.fail('Decorated property not supported', dec)
         if dec.func.is_abstract and dec.func.is_final:
-            self.fail(f"Final method {dec.func.name} has abstract attributes", dec)
+            self.fail(f"Method {dec.func.name} is both abstract and final", dec)
 
     def check_decorated_function_is_method(self, decorator: str,
                                            context: Context) -> None:
