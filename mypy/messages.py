927a9ba05d7e1ca495c62e555b937e74a01c4e57
Ramazan Elsunakev <35545693+relsunkaev@users.noreply.github.com>
2022-05-20 19:35:31 -0700

    fix: "variable arguments" error wording (#12827)
    Fixes #12508

--- mypy/messages.py
+++ mypy/messages.py
@@ -944,7 +944,7 @@ class MessageBuilder:
             self.fail('Cannot infer function type argument', context)
 
     def invalid_var_arg(self, typ: Type, context: Context) -> None:
-        self.fail('List or tuple expected as variable arguments', context)
+        self.fail('List or tuple expected as variadic arguments', context)
 
     def invalid_keyword_var_arg(self, typ: Type, is_mapping: bool, context: Context) -> None:
         typ = get_proper_type(typ)
