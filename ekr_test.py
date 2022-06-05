5a13590ea3f9fca23d2cfc3e72c3674bd2796cad
Edward K. Ream <edreamleo@gmail.com>
2022-06-05 06:46:31 -0500

    Restore .mypy.ini

--- ekr_test.py
+++ ekr_test.py
@@ -2,9 +2,11 @@
 #@+node:ekr.20220603080610.1: * @file ekr_test.py
 """Stand-alone test file for issue #12352"""
 
-# def f1_str(ekr_a: str="abc") -> None:
-    # pass
+# Passes with legacy mypy.
+def f1_str(ekr_a: str="abc") -> None:
+    pass
 
-def f1_str(ekr_a="abc") -> None:
+# Fails with legacy mypy.
+def f2_str(ekr_a="abc") -> None:
     pass
 #@-leo
