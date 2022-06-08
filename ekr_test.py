91a9edc6a5287d2b109b05178d3c43cab778bd00
Edward K. Ream <edreamleo@gmail.com>
2022-06-08 08:58:41 -0500

    Convert ekr_test to @clean

--- ekr_test.py
+++ ekr_test.py
@@ -1,5 +1,4 @@
-#@+leo-ver=5-thin
-#@+node:ekr.20220603080610.1: * @file ekr_test.py
+
 """Stand-alone test file for issue #12352"""
 
 # Passes with legacy mypy.
@@ -9,4 +8,3 @@ def f1_str_annotated(ekr_a: str="abc") -> None:
 # Fails with legacy mypy.
 def f2_str_not_annotated(ekr_b="abc") -> None:
     pass
-#@-leo
