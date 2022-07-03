86aefb14ffb92975ccd312f12c65919b26002c8d
Richard Si <63936253+ichard26@users.noreply.github.com>
2022-07-03 17:15:14 -0400

    [mypyc] Add LoadAddress primitive op for PySet_Type & PyFrozenSet_Type (#13057)
    This also fixes https://github.com/mypyc/mypyc/issues/917
    
    RE above, the root issue is that mypyc didn't know builtins.set was a
    built-in name, so it guessed it comes from the module globals. This
    didn't blow up anything up somehow... until the dataclasses
    commit[^1] which made the `__annotations__` logic for dataclasses try to
    better preserve the type annotations (previously they would be erased
    to builtins.type). This new logic would use `load_type` to load
    `builtins.set` (so it can be put in `__annotations__`) which went poorly
    as only types registered with `load_address_op` are considered
    built-ins.
    
    [^1]: https://github.com/python/mypy/commit/1bcfc041bb767ee93e90676b0a61f3e40267e858

--- mypyc/test-data/fixtures/ir.py
+++ mypyc/test-data/fixtures/ir.py
@@ -3,7 +3,7 @@
 
 from typing import (
     TypeVar, Generic, List, Iterator, Iterable, Dict, Optional, Tuple, Any, Set,
-    overload, Mapping, Union, Callable, Sequence,
+    overload, Mapping, Union, Callable, Sequence, FrozenSet
 )
 
 T = TypeVar('T')
@@ -211,7 +211,13 @@ class set(Generic[T]):
     def clear(self) -> None: pass
     def pop(self) -> T: pass
     def update(self, x: Iterable[S]) -> None: pass
-    def __or__(self, s: Set[S]) -> Set[Union[T, S]]: ...
+    def __or__(self, s: Union[Set[S], FrozenSet[S]]) -> Set[Union[T, S]]: ...
+
+class frozenset(Generic[T]):
+    def __init__(self, i: Optional[Iterable[T]] = None) -> None: pass
+    def __iter__(self) -> Iterator[T]: pass
+    def __len__(self) -> int: pass
+    def __or__(self, s: Union[Set[S], FrozenSet[S]]) -> FrozenSet[Union[T, S]]: ...
 
 class slice: pass
 
