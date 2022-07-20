KotlinIsland/basedmypy

error codes for reveal and any

mypy/semanal.py
@@ -1563,7 +1563,7 @@ class SemanticAnalyzer(NodeVisitor[None],
                         msg = 'Class cannot subclass "{}" (has type "Any")'.format(base_expr.name)
                     else:
                         msg = 'Class cannot subclass value of type "Any"'
-                    self.fail(msg, base_expr)
+                    self.fail(msg, base_expr, code=codes.DYNAMIC_TYPING)
                 info.fallback_to_any = True
             else:
                 msg = 'Invalid base class'

python/mypy

mypy/semanal.py
@@ -1583,7 +1583,7 @@ class SemanticAnalyzer(NodeVisitor[None],
                         msg = f'Class cannot subclass "{base_expr.name}" (has type "Any")'
                     else:
                         msg = 'Class cannot subclass value of type "Any"'
-                    self.fail(msg, base_expr)
+                    self.fail(msg, base_expr, code=codes.NO_SUBCLASS_ANY)
                 info.fallback_to_any = True
             else:
                 msg = 'Invalid base class'
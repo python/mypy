b3b5bf60865091c0ddb42e13d6d0a22646592a01
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-02-17 22:56:55 +1000

    error codes for reveal and any

--- mypy/errorcodes.py
+++ mypy/errorcodes.py
@@ -162,6 +162,14 @@ UNUSED_AWAITABLE: Final = ErrorCode(
 UNSAFE_VARIANCE: Final = ErrorCode("unsafe-variance", "Incorrect usages of variance", "General")
 UNUSED_IGNORE: Final = ErrorCode("unused-ignore", "Ignore comment is unused", "General")
 
+NO_ANY_EXPR: Final = ErrorCode("no-any-expr", "An expression contains Any", "General")
+NO_ANY_EXPLICIT: Final = ErrorCode("no-any-explicit", "Usage of the Any type", "General")
+NO_SUBCLASS_ANY: Final = ErrorCode("no-subclass-any", "Usage of Any as a class base", "General")
+NO_ANY_DECORATED: Final = ErrorCode(
+    "no-any-decorated", "Decorated function contains Any", "General")
+
+REVEAL: Final = ErrorCode("reveal", "Reveal types at check time", "General")
+
 # Syntax errors are often blocking.
 SYNTAX: Final = ErrorCode("syntax", "Report syntax errors", "General")
 
--- mypy/messages.py
+++ mypy/messages.py
@@ -1164,14 +1164,14 @@ class MessageBuilder:
                   context)
 
     def reveal_type(self, typ: Type, context: Context) -> None:
-        self.note(f'Revealed type is "{typ}"', context)
+        self.note(f'Revealed type is "{typ}"', context, code=codes.REVEAL)
 
     def reveal_locals(self, type_map: Dict[str, Optional[Type]], context: Context) -> None:
         # To ensure that the output is predictable on Python < 3.6,
         # use an ordered dictionary sorted by variable name
         sorted_locals = OrderedDict(sorted(type_map.items(), key=lambda t: t[0]))
         if sorted_locals:
-            self.note("Revealed local types are:", context)
+            self.note("Revealed local types are:", context, code=codes.REVEAL)
             for k, v in sorted_locals.items():
                 self.note(f'    {k}: {v}', context)
         else:
@@ -1219,7 +1219,7 @@ class MessageBuilder:
                   code=codes.VAR_ANNOTATED)
 
     def explicit_any(self, ctx: Context) -> None:
-        self.fail('Explicit "Any" is not allowed', ctx)
+        self.fail('Explicit "Any" is not allowed', ctx, code=codes.NO_ANY_EXPLICIT)
 
     def unexpected_typeddict_keys(
             self,
@@ -1319,7 +1319,7 @@ class MessageBuilder:
             message = 'Expression has type "Any"'
         else:
             message = f'Expression type contains "Any" (has type {format_type(typ)})'
-        self.fail(message, context)
+        self.fail(message, context, code=codes.NO_ANY_EXPR)
 
     def incorrectly_returning_any(self, typ: Type, context: Context) -> None:
         message = f'Returning Any from function declared to return {format_type(typ)}'
@@ -1340,10 +1340,11 @@ class MessageBuilder:
     def untyped_decorated_function(self, typ: Type, context: Context) -> None:
         typ = get_proper_type(typ)
         if isinstance(typ, AnyType):
-            self.fail("Function is untyped after decorator transformation", context)
+            self.fail("Function is untyped after decorator transformation", context,
+                      code=codes.NO_ANY_DECORATED)
         else:
             self.fail('Type of decorated function contains type "Any" ({})'.format(
-                format_type(typ)), context)
+                format_type(typ)), context, code=codes.NO_ANY_DECORATED)
 
     def typed_function_untyped_decorator(self, func_name: str, context: Context) -> None:
         self.fail(f'Untyped decorator makes function "{func_name}" untyped', context)
