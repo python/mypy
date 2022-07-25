2a519c06903b93c371199f87b20e8b4963859af9
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2022-07-25 10:28:30 -0700

    Fix site package on MYPYPATH check (#13223)
    This was a regression in 0.971
    
    Fixes #13214

--- mypy/main.py
+++ mypy/main.py
@@ -1051,11 +1051,10 @@ def process_options(args: List[str],
     # Set target.
     if special_opts.modules + special_opts.packages:
         options.build_type = BuildType.MODULE
-        search_dirs = get_search_dirs(options.python_executable)
-        search_paths = SearchPaths((os.getcwd(),),
-                                   tuple(mypy_path() + options.mypy_path),
-                                   tuple(search_dirs),
-                                   ())
+        sys_path, _ = get_search_dirs(options.python_executable)
+        search_paths = SearchPaths(
+            (os.getcwd(),), tuple(mypy_path() + options.mypy_path), tuple(sys_path), ()
+        )
         targets = []
         # TODO: use the same cache that the BuildManager will
         cache = FindModuleCache(search_paths, fscache, options)
