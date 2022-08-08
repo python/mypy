82b22fb616a6a84748e46d6580ed552b6f974495
sobolevn <mail@sobolevn.me>
2022-08-08 12:47:32 +0300

    Revert pyinfo

--- mypy/pyinfo.py
+++ mypy/pyinfo.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 """Utilities to find the site and prefix information of a Python executable, which may be Python 2.
 
 This file MUST remain compatible with Python 2. Since we cannot make any assumptions about the
@@ -47,11 +49,11 @@ def getsyspath():
     stdlib_zip = os.path.join(
         sys.base_exec_prefix,
         getattr(sys, "platlibdir", "lib"),
-        f"python{sys.version_info.major}{sys.version_info.minor}.zip",
+        "python{}{}.zip".format(sys.version_info.major, sys.version_info.minor),
     )
     stdlib = sysconfig.get_path("stdlib")
     stdlib_ext = os.path.join(stdlib, "lib-dynload")
-    excludes = {stdlib_zip, stdlib, stdlib_ext}
+    excludes = set([stdlib_zip, stdlib, stdlib_ext])
 
     # Drop the first entry of sys.path
     # - If pyinfo.py is executed as a script (in a subprocess), this is the directory
