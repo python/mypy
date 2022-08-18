b8852f86d7632c54980717b78ab01a0f4ac54244
AlexWaygood <alex.waygood@gmail.com>
2022-08-18 07:51:26 +0100

    Fix name collision in mypy/test/testinfer.py

--- mypy/test/testinfer.py
+++ mypy/test/testinfer.py
@@ -92,7 +92,7 @@ class MapActualsToFormalsSuite(Suite):
     def assert_map(
         self,
         caller_kinds_: list[ArgKind | str],
-        callee_kinds_: list[ArgKind | tuple[ArgKind, str]],
+        callee_kinds_: list[ArgKind | Tuple[ArgKind, str]],
         expected: list[list[int]],
     ) -> None:
         caller_kinds, caller_names = expand_caller_kinds(caller_kinds_)
