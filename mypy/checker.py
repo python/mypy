11bae277726adaea9e5d5a297e67eae516129618
sobolevn <mail@sobolevn.me>
2022-08-21 11:47:43 +0300

    Try removing `builtins.callable` special casing, refs #13443

--- mypy/checker.py
+++ mypy/checker.py
@@ -4677,122 +4677,6 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
 
         return self.intersect_instance_callable(typ, callable_type)
 
-    def partition_by_callable(
-        self, typ: Type, unsound_partition: bool
-    ) -> tuple[list[Type], list[Type]]:
-        """Partitions a type into callable subtypes and uncallable subtypes.
-
-        Thus, given:
-        `callables, uncallables = partition_by_callable(type)`
-
-        If we assert `callable(type)` then `type` has type Union[*callables], and
-        If we assert `not callable(type)` then `type` has type Union[*uncallables]
-
-        If unsound_partition is set, assume that anything that is not
-        clearly callable is in fact not callable. Otherwise we generate a
-        new subtype that *is* callable.
-
-        Guaranteed to not return [], [].
-        """
-        typ = get_proper_type(typ)
-
-        if isinstance(typ, FunctionLike) or isinstance(typ, TypeType):
-            return [typ], []
-
-        if isinstance(typ, AnyType):
-            return [typ], [typ]
-
-        if isinstance(typ, NoneType):
-            return [], [typ]
-
-        if isinstance(typ, UnionType):
-            callables = []
-            uncallables = []
-            for subtype in typ.items:
-                # Use unsound_partition when handling unions in order to
-                # allow the expected type discrimination.
-                subcallables, subuncallables = self.partition_by_callable(
-                    subtype, unsound_partition=True
-                )
-                callables.extend(subcallables)
-                uncallables.extend(subuncallables)
-            return callables, uncallables
-
-        if isinstance(typ, TypeVarType):
-            # We could do better probably?
-            # Refine the the type variable's bound as our type in the case that
-            # callable() is true. This unfortunately loses the information that
-            # the type is a type variable in that branch.
-            # This matches what is done for isinstance, but it may be possible to
-            # do better.
-            # If it is possible for the false branch to execute, return the original
-            # type to avoid losing type information.
-            callables, uncallables = self.partition_by_callable(
-                erase_to_union_or_bound(typ), unsound_partition
-            )
-            uncallables = [typ] if len(uncallables) else []
-            return callables, uncallables
-
-        # A TupleType is callable if its fallback is, but needs special handling
-        # when we dummy up a new type.
-        ityp = typ
-        if isinstance(typ, TupleType):
-            ityp = tuple_fallback(typ)
-
-        if isinstance(ityp, Instance):
-            method = ityp.type.get_method("__call__")
-            if method and method.type:
-                callables, uncallables = self.partition_by_callable(
-                    method.type, unsound_partition=False
-                )
-                if len(callables) and not len(uncallables):
-                    # Only consider the type callable if its __call__ method is
-                    # definitely callable.
-                    return [typ], []
-
-            if not unsound_partition:
-                fake = self.make_fake_callable(ityp)
-                if isinstance(typ, TupleType):
-                    fake.type.tuple_type = TupleType(typ.items, fake)
-                    return [fake.type.tuple_type], [typ]
-                return [fake], [typ]
-
-        if unsound_partition:
-            return [], [typ]
-        else:
-            # We don't know how properly make the type callable.
-            return [typ], [typ]
-
-    def conditional_callable_type_map(
-        self, expr: Expression, current_type: Type | None
-    ) -> tuple[TypeMap, TypeMap]:
-        """Takes in an expression and the current type of the expression.
-
-        Returns a 2-tuple: The first element is a map from the expression to
-        the restricted type if it were callable. The second element is a
-        map from the expression to the type it would hold if it weren't
-        callable.
-        """
-        if not current_type:
-            return {}, {}
-
-        if isinstance(get_proper_type(current_type), AnyType):
-            return {}, {}
-
-        callables, uncallables = self.partition_by_callable(current_type, unsound_partition=False)
-
-        if len(callables) and len(uncallables):
-            callable_map = {expr: UnionType.make_union(callables)} if len(callables) else None
-            uncallable_map = (
-                {expr: UnionType.make_union(uncallables)} if len(uncallables) else None
-            )
-            return callable_map, uncallable_map
-
-        elif len(callables):
-            return {}, None
-
-        return None, {}
-
     def _is_truthy_type(self, t: ProperType) -> bool:
         return (
             (
@@ -4958,12 +4842,6 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
                     return {}, {}
                 if literal(expr) == LITERAL_TYPE:
                     return self.infer_issubclass_maps(node, expr)
-            elif refers_to_fullname(node.callee, "builtins.callable"):
-                if len(node.args) != 1:  # the error will be reported elsewhere
-                    return {}, {}
-                if literal(expr) == LITERAL_TYPE:
-                    vartype = self.lookup_type(expr)
-                    return self.conditional_callable_type_map(expr, vartype)
             elif isinstance(node.callee, RefExpr):
                 if node.callee.type_guard is not None:
                     # TODO: Follow keyword args or *args, **kwargs
