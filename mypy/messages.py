0dff5105a11f32aca641e059c9130b39db577db3
hauntsaninja <hauntsaninja@gmail.com>
2022-09-02 12:45:14 -0700

    blacken, fix nit

--- mypy/messages.py
+++ mypy/messages.py
@@ -1526,25 +1526,25 @@ class MessageBuilder:
     ) -> None:
         hint = ""
         has_variable_annotations = not python_version or python_version >= (3, 6)
-        pep604_supported = python_version and python_version >= (3, 10)
+        pep604_supported = not python_version or python_version >= (3, 10)
         # type to recommend the user adds
         recommended_type = None
         # Only gives hint if it's a variable declaration and the partial type is a builtin type
         if python_version and isinstance(node, Var) and isinstance(node.type, PartialType):
-            type_dec = '<type>'
+            type_dec = "<type>"
             if not node.type.type:
                 # partial None
                 if pep604_supported:
-                    recommended_type = f'{type_dec} | None'
+                    recommended_type = f"{type_dec} | None"
                 else:
-                    recommended_type = f'Optional[{type_dec}]'
+                    recommended_type = f"Optional[{type_dec}]"
             elif node.type.type.fullname in reverse_builtin_aliases:
                 # partial types other than partial None
                 alias = reverse_builtin_aliases[node.type.type.fullname]
-                alias = alias.split('.')[-1]
-                if alias == 'Dict':
-                    type_dec = f'{type_dec}, {type_dec}'
-                recommended_type = f'{alias}[{type_dec}]'
+                alias = alias.split(".")[-1]
+                if alias == "Dict":
+                    type_dec = f"{type_dec}, {type_dec}"
+                recommended_type = f"{alias}[{type_dec}]"
         if recommended_type is not None:
             if has_variable_annotations:
                 hint = f' (hint: "{node.name}: {recommended_type} = ...")'
