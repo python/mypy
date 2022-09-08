7797c1ef0aec258b11586b9480fb4d9636ea80af
Stas Ilinskiy <stas@dropbox.com>
2022-09-08 11:23:59 -0700

    add support for jump statements

--- mypy/partially_defined.py
+++ mypy/partially_defined.py
@@ -2,9 +2,13 @@ from __future__ import annotations
 
 from typing import NamedTuple
 
+from mypy import checker
 from mypy.messages import MessageBuilder
 from mypy.nodes import (
+    AssertStmt,
     AssignmentStmt,
+    BreakStmt,
+    ContinueStmt,
     ForStmt,
     FuncDef,
     FuncItem,
@@ -12,10 +16,13 @@ from mypy.nodes import (
     ListExpr,
     Lvalue,
     NameExpr,
+    RaiseStmt,
+    ReturnStmt,
     TupleExpr,
-    WhileStmt,
+    WhileStmt, ExpressionStmt, Expression, Block, GeneratorExpr,
 )
-from mypy.traverser import TraverserVisitor
+from mypy.traverser import ExtendedTraverserVisitor
+from mypy.types import Type, UninhabitedType
 
 
 class DefinedVars(NamedTuple):
@@ -30,52 +37,58 @@ class DefinedVars(NamedTuple):
     must_be_defined: set[str]
 
 
+class BranchState:
+    def __init__(self, already_defined: DefinedVars) -> None:
+        self.vars = DefinedVars(
+            may_be_defined=set(), must_be_defined=set(already_defined.must_be_defined)
+        )
+        self.skipped = False
+
+
 class BranchStatement:
     def __init__(self, already_defined: DefinedVars) -> None:
         self.already_defined = already_defined
-        self.defined_by_branch: list[DefinedVars] = [
-            DefinedVars(may_be_defined=set(), must_be_defined=set(already_defined.must_be_defined))
-        ]
+        self.branches: list[BranchState] = [BranchState(self.already_defined)]
 
     def next_branch(self) -> None:
-        self.defined_by_branch.append(
-            DefinedVars(
-                may_be_defined=set(), must_be_defined=set(self.already_defined.must_be_defined)
-            )
-        )
+        self.branches.append(BranchState(self.already_defined))
 
     def record_definition(self, name: str) -> None:
-        assert len(self.defined_by_branch) > 0
-        self.defined_by_branch[-1].must_be_defined.add(name)
-        self.defined_by_branch[-1].may_be_defined.discard(name)
+        assert len(self.branches) > 0
+        self.branches[-1].vars.must_be_defined.add(name)
+        self.branches[-1].vars.may_be_defined.discard(name)
 
     def record_nested_branch(self, vars: DefinedVars) -> None:
-        assert len(self.defined_by_branch) > 0
-        current_branch = self.defined_by_branch[-1]
-        current_branch.must_be_defined.update(vars.must_be_defined)
-        current_branch.may_be_defined.update(vars.may_be_defined)
-        current_branch.may_be_defined.difference_update(current_branch.must_be_defined)
+        assert len(self.branches) > 0
+        current_branch = self.branches[-1]
+        current_branch.vars.must_be_defined.update(vars.must_be_defined)
+        current_branch.vars.may_be_defined.update(vars.may_be_defined)
+        current_branch.vars.may_be_defined.difference_update(current_branch.vars.must_be_defined)
+
+    def skip_branch(self) -> None:
+        assert len(self.branches) > 0
+        self.branches[-1].skipped = True
 
     def is_possibly_undefined(self, name: str) -> bool:
-        assert len(self.defined_by_branch) > 0
-        return name in self.defined_by_branch[-1].may_be_defined
+        assert len(self.branches) > 0
+        return name in self.branches[-1].vars.may_be_defined
 
     def done(self) -> DefinedVars:
-        assert len(self.defined_by_branch) > 0
-        if len(self.defined_by_branch) == 1:
-            # If there's only one branch, then we just return current.
-            # Note that this case is a different case when an empty branch is omitted (e.g. `if` without `else`).
-            return self.defined_by_branch[0]
+        branch_vars = [b.vars for b in self.branches if not b.skipped]
+        if len(branch_vars) == 0:
+            return DefinedVars(must_be_defined=set(), may_be_defined=set())
+        if len(branch_vars) == 1:
+            return branch_vars[0]
 
         # must_be_defined is a union of must_be_defined of all branches.
-        must_be_defined = set(self.defined_by_branch[0].must_be_defined)
-        for branch_vars in self.defined_by_branch[1:]:
-            must_be_defined.intersection_update(branch_vars.must_be_defined)
+        must_be_defined = set(branch_vars[0].must_be_defined)
+        for vars in branch_vars[1:]:
+            must_be_defined.intersection_update(vars.must_be_defined)
         # may_be_defined are all variables that are not must be defined.
         all_vars = set()
-        for branch_vars in self.defined_by_branch:
-            all_vars.update(branch_vars.may_be_defined)
-            all_vars.update(branch_vars.must_be_defined)
+        for vars in branch_vars:
+            all_vars.update(vars.may_be_defined)
+            all_vars.update(vars.must_be_defined)
         may_be_defined = all_vars.difference(must_be_defined)
         return DefinedVars(may_be_defined=may_be_defined, must_be_defined=must_be_defined)
 
@@ -95,14 +108,14 @@ class DefinedVariableTracker:
 
     def enter_scope(self) -> None:
         assert len(self._scope()) > 0
-        self.scopes.append([BranchStatement(self._scope()[-1].defined_by_branch[-1])])
+        self.scopes.append([BranchStatement(self._scope()[-1].branches[-1].vars)])
 
     def exit_scope(self) -> None:
         self.scopes.pop()
 
     def start_branch_statement(self) -> None:
         assert len(self._scope()) > 0
-        self._scope().append(BranchStatement(self._scope()[-1].defined_by_branch[-1]))
+        self._scope().append(BranchStatement(self._scope()[-1].branches[-1].vars))
 
     def next_branch(self) -> None:
         assert len(self._scope()) > 1
@@ -112,6 +125,11 @@ class DefinedVariableTracker:
         assert len(self._scope()) > 1
         result = self._scope().pop().done()
         self._scope()[-1].record_nested_branch(result)
+
+    def skip_branch(self) -> None:
+        # Only skip branch if we're outside of "root" branch statement.
+        if len(self._scope()) > 1:
+            self._scope()[-1].skip_branch()
 
     def record_declaration(self, name: str) -> None:
         assert len(self.scopes) > 0
@@ -125,7 +143,7 @@ class DefinedVariableTracker:
         return self._scope()[-1].is_possibly_undefined(name)
 
 
-class PartiallyDefinedVariableVisitor(TraverserVisitor):
+class PartiallyDefinedVariableVisitor(ExtendedTraverserVisitor):
     """Detect variables that are defined only part of the time.
 
     This visitor detects the following case:
@@ -137,9 +155,10 @@ class PartiallyDefinedVariableVisitor(TraverserVisitor):
     handled by the semantic analyzer.
     """
 
-    def __init__(self, msg: MessageBuilder) -> None:
+    def __init__(self, msg: MessageBuilder, type_map: dict[Expression, Type]) -> None:
         self.msg = msg
         self.tracker = DefinedVariableTracker()
+        self.type_map = type_map
 
     def process_lvalue(self, lvalue: Lvalue) -> None:
         if isinstance(lvalue, NameExpr):
@@ -185,6 +204,32 @@ class PartiallyDefinedVariableVisitor(TraverserVisitor):
         if o.else_body:
             o.else_body.accept(self)
         self.tracker.end_branch_statement()
+
+    def visit_return_stmt(self, o: ReturnStmt) -> None:
+        super().visit_return_stmt(o)
+        self.tracker.skip_branch()
+
+    def visit_assert_stmt(self, o: AssertStmt) -> None:
+        super().visit_assert_stmt(o)
+        if checker.is_false_literal(o.expr):
+            self.tracker.skip_branch()
+
+    def visit_raise_stmt(self, o: RaiseStmt) -> None:
+        super().visit_raise_stmt(o)
+        self.tracker.skip_branch()
+
+    def visit_continue_stmt(self, o: ContinueStmt) -> None:
+        super().visit_continue_stmt(o)
+        self.tracker.skip_branch()
+
+    def visit_break_stmt(self, o: BreakStmt) -> None:
+        super().visit_break_stmt(o)
+        self.tracker.skip_branch()
+
+    def visit_expression_stmt(self, o: ExpressionStmt) -> None:
+        if isinstance(self.type_map.get(o.expr, None), UninhabitedType):
+            self.tracker.skip_branch()
+        super().visit_expression_stmt(o)
 
     def visit_while_stmt(self, o: WhileStmt) -> None:
         o.expr.accept(self)
