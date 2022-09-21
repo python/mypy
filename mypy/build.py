a677f49420871679f80c6952c228ae0d64691551
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2022-09-21 22:33:55 +0100

    Suggest additional types-* packages from typeshed (#13698)
    Now we have suggestions for almost all the currently available
    third-party packages in typeshed that don't have a release that includes
    PEP 561 type information. `sqlalchemy` is not included, since it has a
    few alternatives (some outside typeshed), and I don't know which we
    should suggest.
    
    These stubs were never bundled with mypy, so `--ignore-missing-imports`
    works with these, unlike the existing packages for which we have
    suggestions.
    
    Here's an example mypy output:
    ```
    t.py:1: error: Library stubs not installed for "tree_sitter"
    t.py:1: note: Hint: "python3 -m pip install types-tree-sitter"
    t.py:1: note: (or run "mypy --install-types" to install all missing stub packages)
    t.py:1: note: See https://mypy.readthedocs.io/en/stable/running_mypy.html#missing-imports
    ```
    
    I manually tested that `--install-types` works for these.

--- mypy/build.py
+++ mypy/build.py
@@ -92,7 +92,12 @@ from mypy.plugin import ChainedPlugin, Plugin, ReportConfigContext
 from mypy.plugins.default import DefaultPlugin
 from mypy.renaming import LimitedVariableRenameVisitor, VariableRenameVisitor
 from mypy.stats import dump_type_stats
-from mypy.stubinfo import is_legacy_bundled_package, legacy_bundled_packages
+from mypy.stubinfo import (
+    is_legacy_bundled_package,
+    legacy_bundled_packages,
+    non_bundled_packages,
+    stub_package_name,
+)
 from mypy.types import Type
 from mypy.typestate import TypeState, reset_global_state
 from mypy.version import __version__
@@ -2740,14 +2745,14 @@ def module_not_found(
         msg, notes = reason.error_message_templates(daemon)
         errors.report(line, 0, msg.format(module=target), code=codes.IMPORT)
         top_level, second_level = get_top_two_prefixes(target)
-        if second_level in legacy_bundled_packages:
+        if second_level in legacy_bundled_packages or second_level in non_bundled_packages:
             top_level = second_level
         for note in notes:
             if "{stub_dist}" in note:
-                note = note.format(stub_dist=legacy_bundled_packages[top_level])
+                note = note.format(stub_dist=stub_package_name(top_level))
             errors.report(line, 0, note, severity="note", only_once=True, code=codes.IMPORT)
         if reason is ModuleNotFoundReason.APPROVED_STUBS_NOT_INSTALLED:
-            manager.missing_stub_packages.add(legacy_bundled_packages[top_level])
+            manager.missing_stub_packages.add(stub_package_name(top_level))
     errors.set_import_context(save_import_context)
 
 
