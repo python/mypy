0b3701b80ccdba6b6d98edb9fe78e102e3457f54
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2022-10-15 09:31:18 -0400

    add missing assert_type fixture

--- test-data/unit/fixtures/typing-typeddict.pyi
+++ test-data/unit/fixtures/typing-typeddict.pyi
@@ -26,35 +26,44 @@ NoReturn = 0
 Required = 0
 NotRequired = 0
 
-T = TypeVar('T')
-T_co = TypeVar('T_co', covariant=True)
-V = TypeVar('V')
+T = TypeVar("T")
+T_co = TypeVar("T_co", covariant=True)
+V = TypeVar("V")
 
 # Note: definitions below are different from typeshed, variances are declared
 # to silence the protocol variance checks. Maybe it is better to use type: ignore?
 
 class Sized(Protocol):
-    def __len__(self) -> int: pass
+    def __len__(self) -> int:
+        pass
 
 class Iterable(Protocol[T_co]):
-    def __iter__(self) -> 'Iterator[T_co]': pass
+    def __iter__(self) -> "Iterator[T_co]":
+        pass
 
 class Iterator(Iterable[T_co], Protocol):
-    def __next__(self) -> T_co: pass
+    def __next__(self) -> T_co:
+        pass
 
 class Sequence(Iterable[T_co]):
     # misc is for explicit Any.
-    def __getitem__(self, n: Any) -> T_co: pass # type: ignore[misc]
+    def __getitem__(self, n: Any) -> T_co:
+        pass  # type: ignore[misc]
 
 class Mapping(Iterable[T], Generic[T, T_co], metaclass=ABCMeta):
-    def __getitem__(self, key: T) -> T_co: pass
+    def __getitem__(self, key: T) -> T_co:
+        pass
     @overload
-    def get(self, k: T) -> Optional[T_co]: pass
+    def get(self, k: T) -> Optional[T_co]:
+        pass
     @overload
-    def get(self, k: T, default: Union[T_co, V]) -> Union[T_co, V]: pass
-    def values(self) -> Iterable[T_co]: pass  # Approximate return type
+    def get(self, k: T, default: Union[T_co, V]) -> Union[T_co, V]:
+        pass
+    def values(self) -> Iterable[T_co]:
+        pass  # Approximate return type
     def __len__(self) -> int: ...
-    def __contains__(self, arg: object) -> int: pass
+    def __contains__(self, arg: object) -> int:
+        pass
 
 # Fallback type for all typed dicts (does not exist at runtime).
 class _TypedDict(Mapping[str, object]):
@@ -68,3 +77,5 @@ class _TypedDict(Mapping[str, object]):
     def pop(self, k: NoReturn, default: T = ...) -> object: ...
     def update(self: T, __m: T) -> None: ...
     def __delitem__(self, k: NoReturn) -> None: ...
+
+def assert_type(o, t): ...
