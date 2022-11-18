1561ed8c4308fdc0e61cd9fb5432169fa34a5aea
Ivan Levkivskyi <levkivskyi@gmail.com>
2022-11-18 20:20:27 +0000

    Allow function arguments as base classes

--- mypy/stubtest.py
+++ mypy/stubtest.py
@@ -354,7 +354,7 @@ def _verify_final(
 ) -> Iterator[Error]:
     try:
 
-        class SubClass(runtime):  # type: ignore[misc,valid-type]
+        class SubClass(runtime):
             pass
 
     except TypeError:
