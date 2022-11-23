da7392de848c1e71addbd9d206aaf26538dac818
Stas Ilinskiy <stas@dropbox.com>
2022-11-23 15:43:04 -0600

    Revert

--- mypy/errorcodes.py
+++ mypy/errorcodes.py
@@ -193,7 +193,10 @@ REDUNDANT_SELF_TYPE = ErrorCode(
     default_enabled=False,
 )
 USE_BEFORE_DEF: Final[ErrorCode] = ErrorCode(
-    "use-before-def", "Warn about variables that are used before they are defined", "General"
+    "use-before-def",
+    "Warn about variables that are used before they are defined",
+    "General",
+    default_enabled=False,
 )
 
 
