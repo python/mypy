ceb976f1b9bc4c3ae7cd07a5222ec076f6c7ab72
Nikita Sobolev <mail@sobolevn.me>
2022-12-01 23:36:00 +0300

    [stubtest] associate exported symbol error with __all__ object_path (#14217)

--- mypy/stubtest.py
+++ mypy/stubtest.py
@@ -261,10 +261,10 @@ def _verify_exported_names(
     if not (names_in_runtime_not_stub or names_in_stub_not_runtime):
         return
     yield Error(
-        object_path,
+        object_path + ["__all__"],
         (
             "names exported from the stub do not correspond to the names exported at runtime. "
-            "This is probably due to an inaccurate `__all__` in the stub or things being missing from the stub."
+            "This is probably due to things being missing from the stub, or if present, an inaccurate `__all__` in the stub"
         ),
         # Pass in MISSING instead of the stub and runtime objects, as the line numbers aren't very
         # relevant here, and it makes for a prettier error message
