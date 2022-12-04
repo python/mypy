2f3ba621357e715569048a6aba89124b504720f8
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2022-12-04 16:54:18 +0000

    Fix lint

--- mypyc/irbuild/builder.py
+++ mypyc/irbuild/builder.py
@@ -90,7 +90,6 @@ from mypyc.ir.rtypes import (
     c_pyssize_t_rprimitive,
     dict_rprimitive,
     int_rprimitive,
-    is_fixed_width_rtype,
     is_list_rprimitive,
     is_none_rprimitive,
     is_object_rprimitive,
