ea334c75f67a73f4298623418b266fa25dfb92d1
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2023-01-15 12:15:35 +0000

    Print a status message before copying files

--- misc/docker/run-wrapper.sh
+++ misc/docker/run-wrapper.sh
@@ -2,6 +2,7 @@
 # Internal wrapper script used to run commands in a container
 
 # Copy all the files we need from the directory shared with the host to a local directory
+echo "copying files to the container..."
 cp -R /repo/{mypy,mypyc,test-data,misc} .
 cp /repo/{pytest.ini,conftest.py,runtests.py,pyproject.toml,setup.cfg} .
 cp /repo/{mypy_self_check.ini,mypy_bootstrap.ini} .
