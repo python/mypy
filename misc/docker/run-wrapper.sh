9a8d7126351dc9c0e0ca3183566b55878dc9330d
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2023-01-15 12:20:20 +0000

    Update comment

--- misc/docker/run-wrapper.sh
+++ misc/docker/run-wrapper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Internal wrapper script used to run commands in a container
 
-# Copy all the files we need from the directory shared with the host to a local directory
+# Copy all the files we need from the mypy repo directory shared with
+# the host to a local directory. Accessing files using a shared
+# directory on a mac can be *very* slow.
 echo "copying files to the container..."
 cp -R /repo/{mypy,mypyc,test-data,misc} .
 cp /repo/{pytest.ini,conftest.py,runtests.py,pyproject.toml,setup.cfg} .
