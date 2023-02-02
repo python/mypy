54b319e3e88f9cdc3d013e0d369c3897971b4718
MestreLion <linux@rodrigosilva.com>
2023-02-02 19:33:36 -0300

    rephasing message as requested by @hauntsaninja

--- mypy/semanal_enum.py
+++ mypy/semanal_enum.py
@@ -220,14 +220,14 @@ class EnumCallAnalyzer:
                     items.append(field)
             else:
                 return self.fail_enum_call_arg(
-                    "Non-literal string, tuple, list or dict as the second argument of %s() is not supported"
+                    "Second argument of %s() must be string, tuple, list or dict literal for mypy to determine Enum members"
                     % class_name,
                     call,
                 )
         else:
             # TODO: Allow dict(x=1, y=2) as a substitute for {'x': 1, 'y': 2}?
             return self.fail_enum_call_arg(
-                "Non-literal string, tuple, list or dict as the second argument of %s() is not supported"
+                "Second argument of %s() must be string, tuple, list or dict literal for mypy to determine Enum members"
                 % class_name,
                 call,
             )
