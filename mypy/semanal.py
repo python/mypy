a1bf1bfe9d98e8f200f427553265a7e564ba83c0
davfsa <davfsa@gmail.com>
2023-02-15 22:29:05 +0100

    Follow PEP 673 instead of PEP 484 when dealing with implicit `self`

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -972,11 +972,7 @@ class SemanticAnalyzer(
             elif isinstance(functype, CallableType):
                 self_type = get_proper_type(functype.arg_types[0])
                 if isinstance(self_type, AnyType):
-                    if has_self_type:
-                        assert self.type is not None and self.type.self_type is not None
-                        leading_type: Type = self.type.self_type
-                    else:
-                        leading_type = fill_typevars(info)
+                    leading_type = info.self_type if info.self_type else fill_typevars(info)
                     if func.is_class or func.name == "__new__":
                         leading_type = self.class_type(leading_type)
                     func.type = replace_implicit_first_type(functype, leading_type)
