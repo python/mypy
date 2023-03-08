46914f9a8e2cc228c43bf8f1ec445e5d88901b0b
AlexWaygood <alex.waygood@gmail.com>
2023-03-08 19:23:45 +0000

    Better solution in `semanal_namedtuple`

--- mypy/semanal_namedtuple.py
+++ mypy/semanal_namedtuple.py
@@ -373,8 +373,7 @@ class NamedTupleAnalyzer:
         if not isinstance(args[0], StrExpr):
             self.fail(f'"{type_name}()" expects a string literal as the first argument', call)
             return None
-        assert isinstance(call.args[0], StrExpr)
-        typename = call.args[0].value
+        typename = args[0].value
         types: list[Type] = []
         tvar_defs = []
         if not isinstance(args[1], (ListExpr, TupleExpr)):
