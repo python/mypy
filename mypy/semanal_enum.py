9f12cae24cef4c390faafb37330ef8f180874ccd
AlexWaygood <alex.waygood@gmail.com>
2023-03-08 19:20:16 +0000

    Revert changes to `semanal_enum`

--- mypy/semanal_enum.py
+++ mypy/semanal_enum.py
@@ -108,8 +108,7 @@ class EnumCallAnalyzer:
             # Error. Construct dummy return value.
             info = self.build_enum_call_typeinfo(var_name, [], fullname, node.line)
         else:
-            assert isinstance(call.args[0], StrExpr)
-            name = call.args[0].value
+            name = cast(StrExpr, call.args[0]).value
             if name != var_name or is_func_scope:
                 # Give it a unique name derived from the line number.
                 name += "@" + str(call.line)
