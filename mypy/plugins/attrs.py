cbd71ee8cd816e8a0d3d227768b7b7f54d801f94
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2023-04-07 23:42:44 -0400

    streamline

--- mypy/plugins/attrs.py
+++ mypy/plugins/attrs.py
@@ -967,35 +967,18 @@ def evolve_function_sig_callback(ctx: mypy.plugin.FunctionSigContext) -> Callabl
         return ctx.default_signature  # evolve(Any, ....) -> Any
     # We stringify it first, so that TypeVars maintain their name.
     inst_type_str = format_type_bare(inst_type)
-    if isinstance(inst_type, TypeVarType):
-        attrs_type = inst_type.upper_bound
-        if not isinstance(attrs_type, Instance):
-            ctx.api.fail(
-                f'Argument 1 to "evolve" has a variable type "{inst_type_str}" with unexpected upper bounds',
-                ctx.context,
-            )
-            return ctx.default_signature  # TODO: is this possible?
+    attrs_type = inst_type.upper_bound if isinstance(inst_type, TypeVarType) else inst_type
+    attrs_init_type = None
+    if isinstance(attrs_type, Instance):
         attrs_init_type = _get_attrs_init_type(attrs_type)
-        if attrs_init_type is None:
-            ctx.api.fail(
-                f'Argument 1 to "evolve" has a variable type "{inst_type_str}" not bound to an attrs class',
-                ctx.context,
-            )
-            return ctx.default_signature
-    else:
-        attrs_type = inst_type
-        if not isinstance(attrs_type, Instance):
-            ctx.api.fail(
-                f'Argument 1 to "evolve" has incompatible type "{inst_type_str}"', ctx.context
-            )
-            return ctx.default_signature  # TODO: is this possible?
-        attrs_init_type = _get_attrs_init_type(attrs_type)
-        if attrs_init_type is None:
-            ctx.api.fail(
-                f'Argument 1 to "evolve" has incompatible type "{inst_type_str}"; expected an attrs class',
-                ctx.context,
-            )
-            return ctx.default_signature  # TODO: is this possible?
+    if attrs_init_type is None:
+        ctx.api.fail(
+            f'Argument 1 to "evolve" has a variable type "{inst_type_str}" not bound to an attrs class'
+            if isinstance(inst_type, TypeVarType)
+            else f'Argument 1 to "evolve" has incompatible type "{inst_type_str}"; expected an attrs class',
+            ctx.context,
+        )
+        return ctx.default_signature
 
     attrs_init_type = expand_type_by_instance(attrs_init_type, attrs_type)
 
