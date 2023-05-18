1c3dac540b53d534d45d7d29e328c0e084a3cbc7
Ilya Priven <ilya.konstantinov@gmail.com>
2023-05-17 22:18:23 -0400

    signature_incompatible_with_supertype: show non-callables

--- mypy/checker.py
+++ mypy/checker.py
@@ -1988,7 +1988,7 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
                 # If the attribute is read-only, allow covariance
                 pass
             else:
-                self.msg.signature_incompatible_with_supertype(defn.name, name, base.name, context)
+                self.msg.signature_incompatible_with_supertype(defn.name, name, base.name, context, original_type, typ)
         return False
 
     def bind_and_map_method(
