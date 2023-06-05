e0d20638f6d541de63587b37275722c1dc8dedfe
Ilya Priven <ilya.konstantinov@gmail.com>
2023-06-04 22:44:43 -0400

    Add get_expression_type to CheckerPluginInterface

--- test-data/unit/plugins/function_sig_hook.py
+++ test-data/unit/plugins/function_sig_hook.py
@@ -1,5 +1,5 @@
-from mypy.plugin import CallableType, CheckerPluginInterface, FunctionSigContext, Plugin
-from mypy.types import Instance, Type
+from mypy.plugin import CallableType, FunctionSigContext, Plugin
+
 
 class FunctionSigPlugin(Plugin):
     def get_function_signature_hook(self, fullname):
@@ -7,20 +7,17 @@ class FunctionSigPlugin(Plugin):
             return my_hook
         return None
 
-def _str_to_int(api: CheckerPluginInterface, typ: Type) -> Type:
-    if isinstance(typ, Instance):
-        if typ.type.fullname == 'builtins.str':
-            return api.named_generic_type('builtins.int', [])
-        elif typ.args:
-            return typ.copy_modified(args=[_str_to_int(api, t) for t in typ.args])
-
-    return typ
 
 def my_hook(ctx: FunctionSigContext) -> CallableType:
+    arg1_args = ctx.args[0]
+    if len(arg1_args) != 1:
+        return ctx.default_signature
+    arg1_type = ctx.api.get_expression_type(arg1_args[0])
     return ctx.default_signature.copy_modified(
-        arg_types=[_str_to_int(ctx.api, t) for t in ctx.default_signature.arg_types],
-        ret_type=_str_to_int(ctx.api, ctx.default_signature.ret_type),
+        arg_types=[arg1_type],
+        ret_type=arg1_type,
     )
+
 
 def plugin(version):
     return FunctionSigPlugin
