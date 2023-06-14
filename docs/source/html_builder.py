16fe5da0bd9be4d0669f00a47d260894c988cf87
Ivan Levkivskyi <levkivskyi@gmail.com>
2023-06-14 16:18:09 +0100

    Fix readthedocs build (#15437)
    I initially included an error (removed allowlisted error code that is
    not covered), to verify it will be caught.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/source/html_builder.py
+++ docs/source/html_builder.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+import os
 import textwrap
 from pathlib import Path
 from typing import Any
@@ -8,8 +9,6 @@ from typing import Any
 from sphinx.addnodes import document
 from sphinx.application import Sphinx
 from sphinx.builders.html import StandaloneHTMLBuilder
-
-from mypy.errorcodes import error_codes
 
 
 class MypyHTMLBuilder(StandaloneHTMLBuilder):
@@ -21,7 +20,9 @@ class MypyHTMLBuilder(StandaloneHTMLBuilder):
         super().write_doc(docname, doctree)
         self._ref_to_doc.update({_id: docname for _id in doctree.ids})
 
-    def _write_ref_redirector(self) -> None:
+    def _verify_error_codes(self) -> None:
+        from mypy.errorcodes import error_codes
+
         known_missing = {
             # TODO: fix these before next release
             "annotation-unchecked",
@@ -39,6 +40,10 @@ class MypyHTMLBuilder(StandaloneHTMLBuilder):
             raise ValueError(
                 f"Some error codes are not documented: {', '.join(sorted(missing_error_codes))}"
             )
+
+    def _write_ref_redirector(self) -> None:
+        if os.getenv("VERIFY_MYPY_ERROR_CODES"):
+            self._verify_error_codes()
         p = Path(self.outdir) / "_refs.html"
         data = f"""
         <html>
