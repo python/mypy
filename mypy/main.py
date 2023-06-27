1faca091f1bf6756bf422f0b0cd6b83e9a587e5c
hauntsaninja <hauntsaninja@gmail.com>
2023-06-27 13:01:56 -0700

    remove assert

--- mypy/main.py
+++ mypy/main.py
@@ -1337,7 +1337,6 @@ def process_options(
     # Set build flags.
     if special_opts.find_occurrences:
         _find_occurrences = tuple(special_opts.find_occurrences.split("."))
-        assert _find_occurrences is not None
         if len(_find_occurrences) < 2:
             parser.error("Can only find occurrences of class members.")
         if len(_find_occurrences) != 2:
