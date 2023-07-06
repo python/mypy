e0b159e0bb6bd414d2999bfcecbb5432541ec3fd
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-07-06 10:07:13 +0200

    Enable strict optional for more test files (6) (#15603)

--- mypy/test/testcheck.py
+++ mypy/test/testcheck.py
@@ -51,9 +51,6 @@ if sys.platform not in ("darwin", "win32"):
 
 # TODO: Enable strict optional in test cases by default. Remove files here, once test cases are updated
 no_strict_optional_files = {
-    "check-functions.test",
-    "check-generic-subtyping.test",
-    "check-generics.test",
     "check-inference-context.test",
     "check-inference.test",
     "check-isinstance.test",
