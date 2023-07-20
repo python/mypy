99eda138789d26aeac006cb4df9512252c676fd7
Charlie Denton <charlie@meshy.co.uk>
2023-07-20 15:24:52 +0100

    Fix disappearing errors when re-running dmypy check
    This which fixes issue
    https://github.com/python/mypy/issues/9655 wherein some types of error
    would be lost when a file was re-processed by dmypy.
    
    This also fixes another error where sometimes files would not be
    re-processed by dmypy if the only error in the file was either "unused
    type ignore" or "ignore without code".

--- mypy/errors.py
+++ mypy/errors.py
@@ -725,6 +725,8 @@ class Errors:
                 blocker=False,
                 only_once=False,
                 allow_dups=False,
+                origin=(self.file, [line]),
+                target=self.target_module,
             )
             self._add_error_info(file, info)
 
@@ -777,6 +779,8 @@ class Errors:
                 blocker=False,
                 only_once=False,
                 allow_dups=False,
+                origin=(self.file, [line]),
+                target=self.target_module,
             )
             self._add_error_info(file, info)
 
