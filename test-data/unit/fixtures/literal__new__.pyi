dad9b05dabd6ab233a6c6c0624a9943503b2d4f0
Gobot1234 <gobot1234yt@gmail.com>
2023-08-11 00:25:19 +0100

    Push what I've got

---
+++ test-data/unit/fixtures/literal__new__.pyi
@@ -0,0 +1,19 @@
+from typing import Literal, Protocol, overload
+
+class str: pass
+class dict: pass
+class float: pass
+class int:
+    def __new__(cls) -> Literal[0]: pass
+
+class _Truthy(Protocol):
+    def __bool__(self) -> Literal[True]: pass
+
+class _Falsy(Protocol):
+    def __bool__(self) -> Literal[False]: pass
+
+class bool(int):
+    @overload
+    def __new__(cls, __o: _Truthy) -> Literal[True]: pass
+    @overload
+    def __new__(cls, __o: _Falsy) -> Literal[False]: pass
