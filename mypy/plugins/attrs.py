010da0b2f48dc92be2f79495fd4551c92351868f
Ilya Priven <ilya.konstantinov@gmail.com>
2023-08-28 04:03:50 -0400

    attrs, dataclasses: don't enforce slots when base doesn't (#15976)
    Doing the same thing we do for regular classes.
    
    Fixes #15975

--- mypy/plugins/attrs.py
+++ mypy/plugins/attrs.py
@@ -893,6 +893,11 @@ def _add_attrs_magic_attribute(
 
 
 def _add_slots(ctx: mypy.plugin.ClassDefContext, attributes: list[Attribute]) -> None:
+    if any(p.slots is None for p in ctx.cls.info.mro[1:-1]):
+        # At least one type in mro (excluding `self` and `object`)
+        # does not have concrete `__slots__` defined. Ignoring.
+        return
+
     # Unlike `@dataclasses.dataclass`, `__slots__` is rewritten here.
     ctx.cls.info.slots = {attr.name for attr in attributes}
 
