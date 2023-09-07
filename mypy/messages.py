c19236f2a1fae1a5ac70e5c5c285c99aaa874e06
Randolf Scholz <randolf.scholz@gmail.com>
2023-09-07 16:25:48 +0200

    removed comment

--- mypy/messages.py
+++ mypy/messages.py
@@ -1601,8 +1601,8 @@ class MessageBuilder:
 
     def overloaded_signatures_overlap(self, index1: int, index2: int, context: Context) -> None:
         self.fail(
-            f"Overloaded function signatures {index1} and {index2} overlap with incompatible return types",
-            # "See https://mypy.readthedocs.io/en/stable/more_types.html#type-checking-the-variants.",
+            "Overloaded function signatures {} and {} overlap with "
+            "incompatible return types".format(index1, index2),
             context,
             code=codes.UNSAFE_OVERLOAD,
         )
