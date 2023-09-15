44999065054885c58ed7dbf5cb3fd2483c4a8c4c
hauntsaninja <hauntsaninja@gmail.com>
2023-09-15 01:21:07 -0700

    comment

--- mypy/subtypes.py
+++ mypy/subtypes.py
@@ -466,10 +466,11 @@ class SubtypeVisitor(TypeVisitor[bool]):
                 if all(isinstance(get_proper_type(a), AnyType) for a in mapped.args):
                     return not self.proper_subtype
                 if mapped.type.tuple_type:
-                    expanded = expand_type_by_instance(mapped.type.tuple_type, mapped)
-                    assert isinstance(expanded, TupleType)
+                    # similar to logic inside map_instance_to_supertype
+                    tuple_type = expand_type_by_instance(mapped.type.tuple_type, mapped)
+                    assert isinstance(tuple_type, TupleType)
                     return self._is_subtype(
-                        expanded, right.copy_modified(fallback=expanded.partial_fallback)
+                        tuple_type, right.copy_modified(fallback=tuple_type.partial_fallback)
                     ) and self._is_subtype(left, mypy.typeops.tuple_fallback(right))
             return False
         if isinstance(right, TypeVarTupleType):
