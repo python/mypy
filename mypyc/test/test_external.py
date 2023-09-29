6248f7296636dedd7a7026e4848a401a4213bef9
Eli Schwartz <eschwartz93@gmail.com>
2023-09-28 20:31:54 -0400

    tests: run the external test in a tempdir
    Previously it ran inside the source tree, and left files behind itself.

--- mypyc/test/test_external.py
+++ mypyc/test/test_external.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import os
 import subprocess
 import sys
+import tempfile
 import unittest
 
 base_dir = os.path.join(os.path.dirname(__file__), "..", "..")
@@ -22,19 +23,27 @@ class TestExternal(unittest.TestCase):
             cppflags += ["-mmacosx-version-min=10.10", "-stdlib=libc++"]
         env["CPPFLAGS"] = " ".join(cppflags)
         # Build Python wrapper for C unit tests.
-        status = subprocess.check_call(
-            [sys.executable, "setup.py", "build_ext", "--inplace"],
-            env=env,
-            cwd=os.path.join(base_dir, "mypyc", "lib-rt"),
-        )
-        # Run C unit tests.
-        env = os.environ.copy()
-        if "GTEST_COLOR" not in os.environ:
-            env["GTEST_COLOR"] = "yes"  # Use fancy colors
-        status = subprocess.call(
-            [sys.executable, "-c", "import sys, test_capi; sys.exit(test_capi.run_tests())"],
-            env=env,
-            cwd=os.path.join(base_dir, "mypyc", "lib-rt"),
-        )
-        if status != 0:
-            raise AssertionError("make test: C unit test failure")
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            status = subprocess.check_call(
+                [
+                    sys.executable,
+                    "setup.py",
+                    "build_ext",
+                    f"--build-lib={tmpdir}",
+                    f"--build-temp={tmpdir}",
+                ],
+                env=env,
+                cwd=os.path.join(base_dir, "mypyc", "lib-rt"),
+            )
+            # Run C unit tests.
+            env = os.environ.copy()
+            if "GTEST_COLOR" not in os.environ:
+                env["GTEST_COLOR"] = "yes"  # Use fancy colors
+            status = subprocess.call(
+                [sys.executable, "-c", "import sys, test_capi; sys.exit(test_capi.run_tests())"],
+                env=env,
+                cwd=tmpdir,
+            )
+            if status != 0:
+                raise AssertionError("make test: C unit test failure")
