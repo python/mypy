1da116ce62769ea92bc2310989088d49ad28708d
Ivan Levkivskyi <levkivskyi@gmail.com>
2023-09-30 21:53:55 +0100

    Fix mypyc test case

--- mypyc/irbuild/mapper.py
+++ mypyc/irbuild/mapper.py
@@ -19,6 +19,7 @@ from mypy.types import (
     UnboundType,
     UninhabitedType,
     UnionType,
+    UnpackType,
     get_proper_type,
 )
 from mypyc.ir.class_ir import ClassIR
@@ -112,8 +113,10 @@ class Mapper:
                 return object_rprimitive
         elif isinstance(typ, TupleType):
             # Use our unboxed tuples for raw tuples but fall back to
-            # being boxed for NamedTuple.
-            if typ.partial_fallback.type.fullname == "builtins.tuple":
+            # being boxed for NamedTuple or for variadic tuples.
+            if typ.partial_fallback.type.fullname == "builtins.tuple" and not any(
+                isinstance(t, UnpackType) for t in typ.items
+            ):
                 return RTuple([self.type_to_rtype(t) for t in typ.items])
             else:
                 return tuple_rprimitive
