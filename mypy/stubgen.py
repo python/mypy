python/mypy

commit d98cc5a162729727c5c90e98b27404eebff14bb5

stubgen: fixes and simplifications (#15232)
This PR refactors name resolution in stubgen to make it simple to use
and fixes a bunch of related bugs especially when run in `--parse-only`
mode. It mainly does the following:
1. Adds a `get_fullname` method that resolves the full name of both
`NameExpr` and `MemberExpr` taking into account import aliases
2. Use this method everywhere manual resolution was done including the
decorator processors
3. Simplify decorator processing by consolidating `process_decorator`,
`process_name_expr_decorator`, and `process_member_expr_decorator` into
a single method thanks to the common name resolution using
`get_fullname`
4. Fix occurrences of some hard-coded implicitly-added names (like
`Incomplete` from `_typeshed`) by using `self.typing_name()` and
`self.add_typing_import` which take into account objects of the same
name defined in the file
5. Fix some inconsistencies in generated empty lines. This improves the
consistency within stubgen as well as with the black code style for
stubs.

Out of the 9 test cases added, only `testAbstractPropertyImportAlias`
passes on master (added it any way because it was not tested). The other
8 all fail without this PR. The last test case `testUseTypingName`
demonstrates point `4.`. In existing test cases, only white space
changes in four of the cases due to point `5.` above were made.

This will allow easier additions in the future as no manual name
resolution has to be made any more.

test-data/unit/stubgen.test
@@ -664,7 +664,6 @@ Y = typing.NamedTuple('Y', [])
 from typing import NamedTuple
 
 class X(NamedTuple): ...
-
 class Y(NamedTuple): ...
 
 [case testNamedtupleAltSyntax]
@@ -786,6 +785,7 @@ from _typeshed import Incomplete
 
 N: Incomplete
 M: Incomplete
+
 class X(Incomplete): ...
 
 [case testNamedTupleInClassBases]
@@ -822,6 +822,29 @@ X: Incomplete
 Y: Incomplete
 Z: Incomplete
 W: Incomplete
+
+[case testNamedTupleFromImportAlias]
+import collections as c
+import typing as t
+import typing_extensions as te
+X = c.namedtuple('X', ['a', 'b'])
+Y = t.NamedTuple('Y', [('a', int), ('b', str)])
+Z = te.NamedTuple('Z', [('a', int), ('b', str)])
+[out]
+from _typeshed import Incomplete
+from typing import NamedTuple
+
+class X(NamedTuple):
+    a: Incomplete
+    b: Incomplete
+
+class Y(NamedTuple):
+    a: int
+    b: str
+
+class Z(NamedTuple):
+    a: int
+    b: str
 
 [case testArbitraryBaseClass]
 import x
@@ -867,6 +890,12 @@ class D(Generic[T]): ...
 
 [case testObjectBaseClass]
 class A(object): ...
+[out]
+class A: ...
+
+[case testObjectBaseClassWithImport]
+import builtins as b
+class A(b.object): ...
 [out]
 class A: ...
 
@@ -1017,6 +1046,42 @@ tv = TypeVar('tv', bound=bool, covariant=True)
 from typing import TypeVar
 
 tv = TypeVar('tv', bound=bool, covariant=True)
+
+[case TypeVarImportAlias]
+from typing import TypeVar as t_TV
+from typing_extensions import TypeVar as te_TV
+from x import TypeVar as x_TV
+
+T = t_TV('T')
+U = te_TV('U')
+V = x_TV('V')
+
+[out]
+from _typeshed import Incomplete
+from typing import TypeVar as t_TV
+from typing_extensions import TypeVar as te_TV
+
+T = t_TV('T')
+U = te_TV('U')
+V: Incomplete
+
+[case testTypeVarFromImportAlias]
+import typing as t
+import typing_extensions as te
+import x
+
+T = t.TypeVar('T')
+U = te.TypeVar('U')
+V = x.TypeVar('V')
+
+[out]
+import typing as t
+import typing_extensions as te
+from _typeshed import Incomplete
+
+T = t.TypeVar('T')
+U = te.TypeVar('U')
+V: Incomplete
 
 [case testTypeAliasPreserved]
 alias = str
@@ -1902,6 +1967,36 @@ class A: ...
 
 class C(A):
     def f(self) -> None: ...
+
+[case testAbstractPropertyImportAlias]
+import abc as abc_alias
+
+class A:
+    @abc_alias.abstractproperty
+    def x(self): pass
+
+[out]
+import abc as abc_alias
+
+class A:
+    @property
+    @abc_alias.abstractmethod
+    def x(self): ...
+
+[case testAbstractPropertyFromImportAlias]
+from abc import abstractproperty as ap
+
+class A:
+    @ap
+    def x(self): pass
+
+[out]
+import abc
+
+class A:
+    @property
+    @abc.abstractmethod
+    def x(self): ...
 
 [case testAbstractProperty1_semanal]
 import other
@@ -2814,6 +2909,27 @@ def g(x: int, y: int) -> int: ...
 @te.overload
 def g(x: t.Tuple[int, int]) -> int: ...
 
+[case testOverloadFromImportAlias]
+from typing import overload as t_overload
+from typing_extensions import overload as te_overload
+
+@t_overload
+def f(x: int, y: int) -> int:
+    ...
+
+@te_overload
+def g(x: int, y: int) -> int:
+    ...
+
+[out]
+from typing import overload as t_overload
+from typing_extensions import overload as te_overload
+
+@t_overload
+def f(x: int, y: int) -> int: ...
+@te_overload
+def g(x: int, y: int) -> int: ...
+
 [case testProtocol_semanal]
 from typing import Protocol, TypeVar
 
@@ -2969,9 +3085,7 @@ Z = TypedDict('X', {'a': int, 'in': str})
 from typing import TypedDict
 
 X = TypedDict('X', {'a-b': int, 'c': str})
-
 Y = TypedDict('X', {'a-b': int, 'c': str}, total=False)
-
 Z = TypedDict('X', {'a': int, 'in': str})
 
 [case testEmptyTypeddict]
@@ -2984,11 +3098,8 @@ W = typing.TypedDict('W', total=False)
 from typing_extensions import TypedDict
 
 class X(TypedDict): ...
-
 class Y(TypedDict, total=False): ...
-
 class Z(TypedDict): ...
-
 class W(TypedDict, total=False): ...
 
 [case testTypeddictAliased]
@@ -3012,6 +3123,22 @@ class Y(TypedDict):
     b: str
 
 def g() -> None: ...
+
+[case testTypeddictFromImportAlias]
+import typing as t
+import typing_extensions as te
+X = t.TypedDict('X', {'a': int, 'b': str})
+Y = te.TypedDict('Y', {'a': int, 'b': str})
+[out]
+from typing_extensions import TypedDict
+
+class X(TypedDict):
+    a: int
+    b: str
+
+class Y(TypedDict):
+    a: int
+    b: str
 
 [case testNotTypeddict]
 from x import TypedDict
@@ -3041,3 +3168,65 @@ T: Incomplete
 U: Incomplete
 V: Incomplete
 W: Incomplete
+
+[case testUseTypingName]
+import collections
+import typing
+from typing import NamedTuple, TypedDict
+
+class Incomplete: ...
+class Generator: ...
+class NamedTuple: ...
+class TypedDict: ...
+
+nt = collections.namedtuple("nt", "a b")
+NT = typing.NamedTuple("NT", [("a", int), ("b", str)])
+NT1 = typing.NamedTuple("NT1", [("a", int)] + [("b", str)])
+NT2 = typing.NamedTuple("NT2", [(xx, int), ("b", str)])
+NT3 = typing.NamedTuple(xx, [("a", int), ("b", str)])
+TD = typing.TypedDict("TD", {"a": int, "b": str})
+TD1 = typing.TypedDict("TD1", {"a": int, "b": str}, totale=False)
+TD2 = typing.TypedDict("TD2", {xx: int, "b": str})
+TD3 = typing.TypedDict(xx, {"a": int, "b": str})
+
+def gen():
+    y = yield x
+    return z
+
+class X(unknown_call("X", "a b")): ...
+class Y(collections.namedtuple("Y", xx)): ...
+[out]
+from _typeshed import Incomplete as _Incomplete
+from collections.abc import Generator as _Generator
+from typing import NamedTuple as _NamedTuple
+from typing_extensions import TypedDict as _TypedDict
+
+class Incomplete: ...
+class Generator: ...
+class NamedTuple: ...
+class TypedDict: ...
+
+class nt(_NamedTuple):
+    a: _Incomplete
+    b: _Incomplete
+
+class NT(_NamedTuple):
+    a: int
+    b: str
+
+NT1: _Incomplete
+NT2: _Incomplete
+NT3: _Incomplete
+
+class TD(_TypedDict):
+    a: int
+    b: str
+
+TD1: _Incomplete
+TD2: _Incomplete
+TD3: _Incomplete
+
+def gen() -> _Generator[_Incomplete, _Incomplete, _Incomplete]: ...
+
+class X(_Incomplete): ...
+class Y(_Incomplete): ...

commit 2c0748d58e188a1c925a1214ebe04d59075713ff

stubgen: fix missing property setter in semantic analysis mode
The semantic analyzer treats properties as overloaded functions.
This was previously ignored by stubgen but regressed in #15232.

Fixes #16300

mypy/stubgen.py
@@ -633,6 +633,7 @@ class ASTStubGenerator(BaseStubGenerator, mypy.traverser.TraverserVisitor):
 
         Only preserve certain special decorators such as @abstractmethod.
         """
+        o.func.is_overload = False
         for decorator in o.original_decorators:
             if not isinstance(decorator, (NameExpr, MemberExpr)):
                 continue

mypy/stubutil.py
@@ -669,8 +669,6 @@ class BaseStubGenerator:
                 self.add_name(f"{pkg}.{t}", require=False)
 
     def check_undefined_names(self) -> None:
-        print(self._all_)
-        print(self._toplevel_names)
         undefined_names = [name for name in self._all_ or [] if name not in self._toplevel_names]
         if undefined_names:
             if self._output:

test-data/unit/stubgen.test
@@ -377,6 +377,24 @@ class A:
     def f(self, x) -> None: ...
     def h(self) -> None: ...
 
+[case testProperty_semanal]
+class A:
+    @property
+    def f(self):
+        return 1
+    @f.setter
+    def f(self, x): ...
+
+    def h(self):
+        self.f = 1
+[out]
+class A:
+    @property
+    def f(self): ...
+    @f.setter
+    def f(self, x) -> None: ...
+    def h(self) -> None: ...
+
 -- a read/write property is treated the same as an attribute
 [case testProperty_inspect]
 class A: