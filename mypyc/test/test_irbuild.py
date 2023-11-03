6ee9521c5a533ca7d25271db70f1e241b2635e3c
Richard Si <sichard26@gmail.com>
2023-11-03 17:52:21 -0400

    Fix tests (and indicate when a test is skipped due to mismatched architecture)

--- mypyc/test/test_irbuild.py
+++ mypyc/test/test_irbuild.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 import os.path
 import sys
 
+import pytest
+
 from mypy.errors import CompileError
 from mypy.test.config import test_temp_dir
 from mypy.test.data import DataDrivenTestCase
@@ -67,8 +69,7 @@ class TestGenOps(MypycDataSuite):
         """Perform a runtime checking transformation test case."""
         options = infer_ir_build_options_from_test_name(testcase.name)
         if options is None:
-            # Skipped test case
-            return
+            pytest.skip()
         with use_custom_builtins(os.path.join(self.data_prefix, ICODE_GEN_BUILTINS), testcase):
             expected_output = remove_comment_lines(testcase.output)
             expected_output = replace_word_size(expected_output)
