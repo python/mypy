6c8e0cc47c014894ea41621a10f3d1b465322362
Ivan Levkivskyi <levkivskyi@gmail.com>
2023-11-13 16:59:50 +0000

    Ignore position if imprecise arguments are matched by name (#16471)
    Fixes https://github.com/python/mypy/issues/16405
    Fixes https://github.com/python/mypy/issues/16412
    
    Imprecise argument kinds inference was added a while ago to support
    various edge cases with `ParamSpec`. This feature required mapping
    actual kinds to formal kinds, which is in general undecidable. At that
    time we decided to not add much special-casing, and wait for some real
    use-cases. So far there are two relevant issues, and it looks like both
    of them can be fixed with simple special-casing: ignore argument
    positions in subtyping if arguments can be matched by name. This adds
    minor unsafety, and generally doesn't look bad, so I think we should go
    ahead with it.
    
    ---------
    
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- mypy/subtypes.py
+++ mypy/subtypes.py
@@ -1651,7 +1651,12 @@ def are_parameters_compatible(
                 continue
             return False
         if not are_args_compatible(
-            left_arg, right_arg, ignore_pos_arg_names, allow_partial_overlap, is_compat
+            left_arg,
+            right_arg,
+            is_compat,
+            ignore_pos_arg_names=ignore_pos_arg_names,
+            allow_partial_overlap=allow_partial_overlap,
+            allow_imprecise_kinds=right.imprecise_arg_kinds,
         ):
             return False
 
@@ -1676,9 +1681,9 @@ def are_parameters_compatible(
             if not are_args_compatible(
                 left_by_position,
                 right_by_position,
-                ignore_pos_arg_names,
-                allow_partial_overlap,
                 is_compat,
+                ignore_pos_arg_names=ignore_pos_arg_names,
+                allow_partial_overlap=allow_partial_overlap,
             ):
                 return False
             i += 1
@@ -1711,7 +1716,11 @@ def are_parameters_compatible(
                 continue
 
             if not are_args_compatible(
-                left_by_name, right_by_name, ignore_pos_arg_names, allow_partial_overlap, is_compat
+                left_by_name,
+                right_by_name,
+                is_compat,
+                ignore_pos_arg_names=ignore_pos_arg_names,
+                allow_partial_overlap=allow_partial_overlap,
             ):
                 return False
 
@@ -1735,6 +1744,7 @@ def are_parameters_compatible(
             and right_by_name != right_by_pos
             and (right_by_pos.required or right_by_name.required)
             and strict_concatenate_check
+            and not right.imprecise_arg_kinds
         ):
             return False
 
@@ -1749,9 +1759,11 @@ def are_parameters_compatible(
 def are_args_compatible(
     left: FormalArgument,
     right: FormalArgument,
+    is_compat: Callable[[Type, Type], bool],
+    *,
     ignore_pos_arg_names: bool,
     allow_partial_overlap: bool,
-    is_compat: Callable[[Type, Type], bool],
+    allow_imprecise_kinds: bool = False,
 ) -> bool:
     if left.required and right.required:
         # If both arguments are required allow_partial_overlap has no effect.
@@ -1779,7 +1791,7 @@ def are_args_compatible(
             return False
 
     # If right is at a specific position, left must have the same:
-    if is_different(left.pos, right.pos):
+    if is_different(left.pos, right.pos) and not allow_imprecise_kinds:
         return False
 
     # If right's argument is optional, left's must also be
