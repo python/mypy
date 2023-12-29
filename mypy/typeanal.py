8016d332162e6007048510bc8d0c5cedd2d5173b
Christoph Tyralla <c.tyralla@bjoernsen.de>
2023-12-29 23:47:45 +0100

    use `/` instead of `__` to mark positional-only method parameters

--- mypy/typeanal.py
+++ mypy/typeanal.py
@@ -2016,7 +2016,7 @@ TypeVarLikeList = list[tuple[str, TypeVarLikeExpr]]
 
 
 class MsgCallback(Protocol):
-    def __call__(self, __msg: str, __ctx: Context, *, code: ErrorCode | None = None) -> None: ...
+    def __call__(self, msg: str, ctx: Context, /, *, code: ErrorCode | None = None) -> None: ...
 
 
 def get_omitted_any(
