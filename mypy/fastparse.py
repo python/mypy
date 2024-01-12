52a7623754467bbbe935dd9b3a940ea92a6174d7
hauntsaninja <hauntsaninja@gmail.com>
2024-01-11 18:42:59 -0800

    .

--- mypy/fastparse.py
+++ mypy/fastparse.py
@@ -318,14 +318,9 @@ def parse_type_string(
     string expression "blah" using this function.
     """
     try:
-        try:
-            _, node = parse_type_comment(
-                expr_string.strip(), line=line, column=column, errors=None
-            )
-        except SyntaxError:
-            _, node = parse_type_comment(
-                f"({expr_string.strip()})", line=line, column=column, errors=None
-            )
+        _, node = parse_type_comment(
+            f"({expr_string})", line=line, column=column, errors=None
+        )
         if isinstance(node, UnboundType) and node.original_str_expr is None:
             node.original_str_expr = expr_string
             node.original_str_fallback = expr_fallback_name
