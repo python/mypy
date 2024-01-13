a8741d8fda2695e189add60b1cfc4adebd2c3e1e
Fabian Keller <bluenote10@users.noreply.github.com>
2024-01-13 16:14:16 +0100

    Improve stubgen tests (#16760)
    Improve test cases around #16486
    
    This PR does not change any actual mypy behavior, only hardens the
    stubgen tests. The specific changes are:
    
    - **dedicated test cases**: The existing pybind11 test cases originate
    from a pybind11 demo. They cover a specific topic involving geometry
    types and semi-implemented logic related to it. This somehow distracts
    from the aspects we are trying to test here from the mypy stubgen
    perspective, because it hides the actual intent of the bindings. I've
    simply started adding new test cases that clearly express via their name
    what the test case is addressing. I've kept the original demo stuff for
    now, so that the new cases are just an addition (overhead is
    negligible).
    - **running mypy on the generated stubs**: In general it is crucial that
    the output produced by the stubgen can actually be type checked by mypy
    (this was the regression in #18486). This wasn't covered by the CI check
    so far. I've added check now, which would have avoided the regression.
    My goal for follow up PRs would be that we can use `mypy
    --disallow-untyped-defs` or even `mypy --strict` on the output.
    - **minor directory restructuring**: So far the expected stub outputs
    were stored in folder names `stubgen` and `stubgen-include-docs`. This
    was a bit confusing, because the folder `stubgen` suggested it contains
    _the_ stubgen (implementation). I went for `expected_stubs_no_docs` and
    `expected_stubs_with_docs` to make the role of the two folders more
    explicit.
    - **minor script bugfix**: Fix a bug in `test-stubgen.sh`: The
    pre-delete functionality was broken, because the `*` was quoted and
    therefore did not match.

--- test-data/pybind11_fixtures/expected_stubs_no_docs/pybind11_fixtures/demo.pyi
+++ test-data/pybind11_fixtures/expected_stubs_no_docs/pybind11_fixtures/demo.pyi
@@ -3,17 +3,6 @@ from typing import ClassVar, List, overload
 PI: float
 __version__: str
 
-class Foo:
-    def __init__(self, *args, **kwargs) -> None: ...
-    @overload
-    @staticmethod
-    def overloaded_static_method(value: int) -> int: ...
-    @overload
-    @staticmethod
-    def overloaded_static_method(value: float) -> float: ...
-    @staticmethod
-    def some_static_method(a: int, b: int) -> int: ...
-
 class Point:
     class AngleUnit:
         __members__: ClassVar[dict] = ...  # read-only
--- test-data/pybind11_fixtures/expected_stubs_with_docs/pybind11_fixtures/demo.pyi
+++ test-data/pybind11_fixtures/expected_stubs_with_docs/pybind11_fixtures/demo.pyi
@@ -3,36 +3,6 @@ from typing import ClassVar, List, overload
 PI: float
 __version__: str
 
-class Foo:
-    def __init__(self, *args, **kwargs) -> None:
-        """Initialize self.  See help(type(self)) for accurate signature."""
-    @overload
-    @staticmethod
-    def overloaded_static_method(value: int) -> int:
-        """overloaded_static_method(*args, **kwargs)
-        Overloaded function.
-
-        1. overloaded_static_method(value: int) -> int
-
-        2. overloaded_static_method(value: float) -> float
-        """
-    @overload
-    @staticmethod
-    def overloaded_static_method(value: float) -> float:
-        """overloaded_static_method(*args, **kwargs)
-        Overloaded function.
-
-        1. overloaded_static_method(value: int) -> int
-
-        2. overloaded_static_method(value: float) -> float
-        """
-    @staticmethod
-    def some_static_method(a: int, b: int) -> int:
-        """some_static_method(a: int, b: int) -> int
-
-        None
-        """
-
 class Point:
     class AngleUnit:
         __members__: ClassVar[dict] = ...  # read-only
@@ -40,15 +10,15 @@ class Point:
         degree: ClassVar[Point.AngleUnit] = ...
         radian: ClassVar[Point.AngleUnit] = ...
         def __init__(self, value: int) -> None:
-            """__init__(self: pybind11_mypy_demo.basics.Point.AngleUnit, value: int) -> None"""
+            """__init__(self: pybind11_fixtures.demo.Point.AngleUnit, value: int) -> None"""
         def __eq__(self, other: object) -> bool:
             """__eq__(self: object, other: object) -> bool"""
         def __hash__(self) -> int:
             """__hash__(self: object) -> int"""
         def __index__(self) -> int:
-            """__index__(self: pybind11_mypy_demo.basics.Point.AngleUnit) -> int"""
+            """__index__(self: pybind11_fixtures.demo.Point.AngleUnit) -> int"""
         def __int__(self) -> int:
-            """__int__(self: pybind11_mypy_demo.basics.Point.AngleUnit) -> int"""
+            """__int__(self: pybind11_fixtures.demo.Point.AngleUnit) -> int"""
         def __ne__(self, other: object) -> bool:
             """__ne__(self: object, other: object) -> bool"""
         @property
@@ -63,15 +33,15 @@ class Point:
         mm: ClassVar[Point.LengthUnit] = ...
         pixel: ClassVar[Point.LengthUnit] = ...
         def __init__(self, value: int) -> None:
-            """__init__(self: pybind11_mypy_demo.basics.Point.LengthUnit, value: int) -> None"""
+            """__init__(self: pybind11_fixtures.demo.Point.LengthUnit, value: int) -> None"""
         def __eq__(self, other: object) -> bool:
             """__eq__(self: object, other: object) -> bool"""
         def __hash__(self) -> int:
             """__hash__(self: object) -> int"""
         def __index__(self) -> int:
-            """__index__(self: pybind11_mypy_demo.basics.Point.LengthUnit) -> int"""
+            """__index__(self: pybind11_fixtures.demo.Point.LengthUnit) -> int"""
         def __int__(self) -> int:
-            """__int__(self: pybind11_mypy_demo.basics.Point.LengthUnit) -> int"""
+            """__int__(self: pybind11_fixtures.demo.Point.LengthUnit) -> int"""
         def __ne__(self, other: object) -> bool:
             """__ne__(self: object, other: object) -> bool"""
         @property
@@ -90,38 +60,38 @@ class Point:
         """__init__(*args, **kwargs)
         Overloaded function.
 
-        1. __init__(self: pybind11_mypy_demo.basics.Point) -> None
+        1. __init__(self: pybind11_fixtures.demo.Point) -> None
 
-        2. __init__(self: pybind11_mypy_demo.basics.Point, x: float, y: float) -> None
+        2. __init__(self: pybind11_fixtures.demo.Point, x: float, y: float) -> None
         """
     @overload
     def __init__(self, x: float, y: float) -> None:
         """__init__(*args, **kwargs)
         Overloaded function.
 
-        1. __init__(self: pybind11_mypy_demo.basics.Point) -> None
+        1. __init__(self: pybind11_fixtures.demo.Point) -> None
 
-        2. __init__(self: pybind11_mypy_demo.basics.Point, x: float, y: float) -> None
+        2. __init__(self: pybind11_fixtures.demo.Point, x: float, y: float) -> None
         """
     def as_list(self) -> List[float]:
-        """as_list(self: pybind11_mypy_demo.basics.Point) -> List[float]"""
+        """as_list(self: pybind11_fixtures.demo.Point) -> List[float]"""
     @overload
     def distance_to(self, x: float, y: float) -> float:
         """distance_to(*args, **kwargs)
         Overloaded function.
 
-        1. distance_to(self: pybind11_mypy_demo.basics.Point, x: float, y: float) -> float
+        1. distance_to(self: pybind11_fixtures.demo.Point, x: float, y: float) -> float
 
-        2. distance_to(self: pybind11_mypy_demo.basics.Point, other: pybind11_mypy_demo.basics.Point) -> float
+        2. distance_to(self: pybind11_fixtures.demo.Point, other: pybind11_fixtures.demo.Point) -> float
         """
     @overload
     def distance_to(self, other: Point) -> float:
         """distance_to(*args, **kwargs)
         Overloaded function.
 
-        1. distance_to(self: pybind11_mypy_demo.basics.Point, x: float, y: float) -> float
+        1. distance_to(self: pybind11_fixtures.demo.Point, x: float, y: float) -> float
 
-        2. distance_to(self: pybind11_mypy_demo.basics.Point, other: pybind11_mypy_demo.basics.Point) -> float
+        2. distance_to(self: pybind11_fixtures.demo.Point, other: pybind11_fixtures.demo.Point) -> float
         """
     @property
     def length(self) -> float: ...
