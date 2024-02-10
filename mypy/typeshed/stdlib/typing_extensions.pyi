25a9c79010b47c1eb93354c7f02c48723fb6b464
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-02-09 16:53:29 -0800

    temp change typeshed

--- mypy/typeshed/stdlib/typing_extensions.pyi
+++ mypy/typeshed/stdlib/typing_extensions.pyi
@@ -309,6 +309,8 @@ else:
     TypeGuard: _SpecialForm
     def is_typeddict(tp: object) -> bool: ...
 
+TypeNarrower: _SpecialForm
+
 # New and changed things in 3.11
 if sys.version_info >= (3, 11):
     from typing import (
