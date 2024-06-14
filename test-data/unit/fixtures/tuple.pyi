781f2a5c3a392a87eeba77d8d6223d9d4c79c800
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2024-06-14 11:47:27 +0100

    Prefix type variables with underscore in builtins fixtures

--- test-data/unit/fixtures/tuple.pyi
+++ test-data/unit/fixtures/tuple.pyi
@@ -3,8 +3,8 @@
 import _typeshed
 from typing import Iterable, Iterator, TypeVar, Generic, Sequence, Optional, overload, Tuple, Type
 
-T = TypeVar("T")
-Tco = TypeVar('Tco', covariant=True)
+_T = TypeVar("_T")
+_Tco = TypeVar('_Tco', covariant=True)
 
 class object:
     def __init__(self) -> None: pass
@@ -12,17 +12,17 @@ class object:
 class type:
     def __init__(self, *a: object) -> None: pass
     def __call__(self, *a: object) -> object: pass
-class tuple(Sequence[Tco], Generic[Tco]):
-    def __new__(cls: Type[T], iterable: Iterable[Tco] = ...) -> T: ...
-    def __iter__(self) -> Iterator[Tco]: pass
+class tuple(Sequence[_Tco], Generic[_Tco]):
+    def __new__(cls: Type[_T], iterable: Iterable[_Tco] = ...) -> _T: ...
+    def __iter__(self) -> Iterator[_Tco]: pass
     def __contains__(self, item: object) -> bool: pass
     @overload
-    def __getitem__(self, x: int) -> Tco: pass
+    def __getitem__(self, x: int) -> _Tco: pass
     @overload
-    def __getitem__(self, x: slice) -> Tuple[Tco, ...]: ...
-    def __mul__(self, n: int) -> Tuple[Tco, ...]: pass
-    def __rmul__(self, n: int) -> Tuple[Tco, ...]: pass
-    def __add__(self, x: Tuple[Tco, ...]) -> Tuple[Tco, ...]: pass
+    def __getitem__(self, x: slice) -> Tuple[_Tco, ...]: ...
+    def __mul__(self, n: int) -> Tuple[_Tco, ...]: pass
+    def __rmul__(self, n: int) -> Tuple[_Tco, ...]: pass
+    def __add__(self, x: Tuple[_Tco, ...]) -> Tuple[_Tco, ...]: pass
     def count(self, obj: object) -> int: pass
 class function:
     __name__: str
@@ -40,13 +40,13 @@ class str: pass # For convenience
 class bytes: pass
 class bytearray: pass
 
-class list(Sequence[T], Generic[T]):
+class list(Sequence[_T], Generic[_T]):
     @overload
-    def __getitem__(self, i: int) -> T: ...
+    def __getitem__(self, i: int) -> _T: ...
     @overload
-    def __getitem__(self, s: slice) -> list[T]: ...
+    def __getitem__(self, s: slice) -> list[_T]: ...
     def __contains__(self, item: object) -> bool: ...
-    def __iter__(self) -> Iterator[T]: ...
+    def __iter__(self) -> Iterator[_T]: ...
 
 def isinstance(x: object, t: type) -> bool: pass
 
