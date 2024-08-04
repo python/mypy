32675dddfacccef616557916cb872757605ab493
Ivan Levkivskyi <levkivskyi@gmail.com>
2024-08-04 12:50:05 +0100

    Revert "Fix Literal strings containing pipe characters" (#17638)
    Reverts python/mypy#17148
    
    (cherry picked from commit bc39f1714e2e5f39eb90f05f74575a7c397f068b)

--- mypy/stubutil.py
+++ mypy/stubutil.py
@@ -17,16 +17,7 @@ import mypy.options
 from mypy.modulefinder import ModuleNotFoundReason
 from mypy.moduleinspect import InspectError, ModuleInspect
 from mypy.stubdoc import ArgSig, FunctionSig
-from mypy.types import (
-    AnyType,
-    NoneType,
-    RawExpressionType,
-    Type,
-    TypeList,
-    TypeStrVisitor,
-    UnboundType,
-    UnionType,
-)
+from mypy.types import AnyType, NoneType, Type, TypeList, TypeStrVisitor, UnboundType, UnionType
 
 # Modules that may fail when imported, or that may have side effects (fully qualified).
 NOT_IMPORTABLE_MODULES = ()
@@ -302,11 +293,12 @@ class AnnotationPrinter(TypeStrVisitor):
         The main difference from list_str is the preservation of quotes for string
         arguments
         """
+        types = ["builtins.bytes", "builtins.str"]
         res = []
         for arg in args:
             arg_str = arg.accept(self)
-            if isinstance(arg, RawExpressionType):
-                res.append(repr(arg.literal_value))
+            if isinstance(arg, UnboundType) and arg.original_str_fallback in types:
+                res.append(f"'{arg_str}'")
             else:
                 res.append(arg_str)
         return ", ".join(res)
