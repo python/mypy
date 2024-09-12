eb5c51b0dc34f96f6b9a2ea2d6bef223c5675cfa
subnix <gh@subnix.io>
2024-09-12 15:26:59 +0400

    fix: unpack unions inside tuples in except handlers

--- mypy/checker.py
+++ mypy/checker.py
@@ -4858,8 +4858,13 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
         """Helper for check_except_handler_test to retrieve handler types."""
         typ = get_proper_type(typ)
         if isinstance(typ, TupleType):
-            return typ.items
+            # avoid recursion here, since we can accidentally unpack tuples inside
+            merged_type = make_simplified_union(typ.items)
+            if isinstance(merged_type, UnionType):
+                return merged_type.relevant_items()
+            return [merged_type]
         elif isinstance(typ, UnionType):
+            # recursion is fine here for top-level Union
             return [
                 union_typ
                 for item in typ.relevant_items()
@@ -4867,7 +4872,11 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
             ]
         elif is_named_instance(typ, "builtins.tuple"):
             # variadic tuple
-            return [typ.args[0]]
+            # avoid recursion here too
+            merged_type = make_simplified_union((typ.args[0],))
+            if isinstance(merged_type, UnionType):
+                return merged_type.relevant_items()
+            return [merged_type]
         else:
             return [typ]
 
