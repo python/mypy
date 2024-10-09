d4afd6df2bb30685e0f395b98c09af8f27eb1159
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2024-10-09 13:24:37 +0100

    Fix nested unions

--- mypy/plugins/functools.py
+++ mypy/plugins/functools.py
@@ -131,18 +131,17 @@ def partial_new_callback(ctx: mypy.plugin.FunctionContext) -> Type:
     if isinstance(get_proper_type(ctx.arg_types[0][0]), Overloaded):
         # TODO: handle overloads, just fall back to whatever the non-plugin code does
         return ctx.default_return_type
-    callee = ctx.arg_types[0][0]
-    if isinstance(callee_proper := get_proper_type(callee), UnionType):
-        return UnionType.make_union(
-            [handle_partial_with_callee(ctx, item) for item in callee_proper.items]
-        )
-    else:
-        return handle_partial_with_callee(ctx, callee)
+    return handle_partial_with_callee(ctx, callee=ctx.arg_types[0][0])
 
 
 def handle_partial_with_callee(ctx: mypy.plugin.FunctionContext, callee: Type) -> Type:
     if not isinstance(ctx.api, mypy.checker.TypeChecker):  # use internals
         return ctx.default_return_type
+
+    if isinstance(callee_proper := get_proper_type(callee), UnionType):
+        return UnionType.make_union(
+            [handle_partial_with_callee(ctx, item) for item in callee_proper.items]
+        )
 
     fn_type = ctx.api.extract_callable_type(callee, ctx=ctx.default_return_type)
     if fn_type is None:
