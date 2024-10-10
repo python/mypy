d3029ae52ad5dfe4e88ebe57fb0cb2f981a55100
changhoetyng <hoetyng@gmail.com>
2024-10-10 11:20:31 +0100

    removed TODOS

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -6461,8 +6461,6 @@ class SemanticAnalyzer(
         Note that this can't be used for names nested in class namespaces.
         """
         # TODO: unify/clean-up/simplify lookup methods, see #4157.
-        # TODO: support nested classes (but consider performance impact,
-        #       we might keep the module level only lookup for thing like 'builtins.int').
         module, name = fullname.rsplit(".", maxsplit=1)
 
         if module in self.modules:
