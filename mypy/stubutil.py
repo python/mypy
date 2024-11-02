1f200dde451493a070b9464011998dd837df4bc2
Chad Dombrova <chadrik@gmail.com>
2024-11-02 07:47:48 -0700

    stubgen:  fix FunctionContext.fullname for nested classes (#17963)
    This fixes an issue with the computation of `FunctionContext.fullname`
    for nested classes.
    
    For a module named `spangle`, with the following classes:
    
    ```python
    class Foo:
        class Bar:
            pass
    ```
    
    The previous output for the class `Bar` was `"spangle.Bar"` and with
    this fix it is now `"spangle.Foo.Bar"`.

--- mypy/stubutil.py
+++ mypy/stubutil.py
@@ -307,12 +307,18 @@ class AnnotationPrinter(TypeStrVisitor):
 
 class ClassInfo:
     def __init__(
-        self, name: str, self_var: str, docstring: str | None = None, cls: type | None = None
+        self,
+        name: str,
+        self_var: str,
+        docstring: str | None = None,
+        cls: type | None = None,
+        parent: ClassInfo | None = None,
     ) -> None:
         self.name = name
         self.self_var = self_var
         self.docstring = docstring
         self.cls = cls
+        self.parent = parent
 
 
 class FunctionContext:
@@ -335,7 +341,13 @@ class FunctionContext:
     def fullname(self) -> str:
         if self._fullname is None:
             if self.class_info:
-                self._fullname = f"{self.module_name}.{self.class_info.name}.{self.name}"
+                parents = []
+                class_info: ClassInfo | None = self.class_info
+                while class_info is not None:
+                    parents.append(class_info.name)
+                    class_info = class_info.parent
+                namespace = ".".join(reversed(parents))
+                self._fullname = f"{self.module_name}.{namespace}.{self.name}"
             else:
                 self._fullname = f"{self.module_name}.{self.name}"
         return self._fullname
