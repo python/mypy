2842e8f27993e303d757a916acdf278bfd6fd682
Advait Dixit <48302999+advait-dixit@users.noreply.github.com>
2024-11-27 14:38:45 -0800

    [mypyc] Fixing check for enum classes. (#18178)
    Fixes [mypyc/mypyc#1065](https://github.com/mypyc/mypyc/issues/1065)
    Fixes [mypyc/mypyc#1059](https://github.com/mypyc/mypyc/issues/1059)
    Fixes [mypyc/mypyc#1022](https://github.com/mypyc/mypyc/issues/1022)
    
    * Checking for enum classes using `is_enum` flag instead of `fullname`
    of base class. That way, mypyc recognizes classes derived from `IntEnum`
    as enum classes too.
    * After fixing the above bug, test failures revealed that mypyc was
    sending all MRO classes to `__prepare__` function. For example, for the
    `Player` test class added in this PR, mypyc was generating C code to
    call `__prepare__` of the base class with `(IntEnum, int, Enum)` instead
    of just `(IntEnum,)`. This bug has been fixed in
    `classdef.py:populate_non_ext_bases`.

--- mypyc/irbuild/classdef.py
+++ mypyc/irbuild/classdef.py
@@ -513,7 +513,7 @@ def populate_non_ext_bases(builder: IRBuilder, cdef: ClassDef) -> Value:
     is_named_tuple = cdef.info.is_named_tuple
     ir = builder.mapper.type_to_ir[cdef.info]
     bases = []
-    for cls in cdef.info.mro[1:]:
+    for cls in (b.type for b in cdef.info.bases):
         if cls.fullname == "builtins.object":
             continue
         if is_named_tuple and cls.fullname in (
@@ -682,7 +682,7 @@ def add_non_ext_class_attr(
         # are final.
         if (
             cdef.info.bases
-            and cdef.info.bases[0].type.fullname == "enum.Enum"
+            and cdef.info.bases[0].type.is_enum
             # Skip these since Enum will remove it
             and lvalue.name not in EXCLUDED_ENUM_ATTRIBUTES
         ):
