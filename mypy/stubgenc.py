1fc81b2add459cc231c34994c8bd906de7ef7276
Michael Carlstrom <rmc@carlstrom.com>
2024-12-08 19:32:02 -0500

    Update stubgenc.py

--- mypy/stubgenc.py
+++ mypy/stubgenc.py
@@ -254,10 +254,18 @@ class InspectionStubGenerator(BaseStubGenerator):
                         "List",
                         "Literal",
                         "NamedTuple",
+                        "Never",
+                        "NoReturn",
+                        "Set",
                         "Optional",
                         "Tuple",
+                        "Type",
+                        "TypeIs",
+                        "TypeGuard",
                         "Union",
-                    ]
+                    ],
+                    "os" [:"PathLike"],
+                    "pathlib": ["Path"]
                 }
             )
 
