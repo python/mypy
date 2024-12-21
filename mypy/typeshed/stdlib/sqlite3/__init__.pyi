6605d1023fa6524bbfba42cdd3226b9d4b1cec24
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-12-21 22:36:38 +0100

    Revert Remove redundant inheritances from Iterator in builtins

--- mypy/typeshed/stdlib/sqlite3/__init__.pyi
+++ mypy/typeshed/stdlib/sqlite3/__init__.pyi
@@ -426,7 +426,7 @@ class Connection:
     ) -> Literal[False]: ...
 
 @disjoint_base
-class Cursor:
+class Cursor(Iterator[Any]):
     arraysize: int
     @property
     def connection(self) -> Connection: ...
