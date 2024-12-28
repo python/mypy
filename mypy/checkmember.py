193e605f6889ffe071ab45ef98d7e215f679e066
Shantanu Jain <hauntsaninja@gmail.com>
2024-12-27 19:44:52 -0800

    more

--- mypy/checkmember.py
+++ mypy/checkmember.py
@@ -1085,13 +1085,23 @@ def analyze_class_attribute_access(
             def_vars = set(node.node.info.defn.type_vars)
             if not node.node.is_classvar and node.node.info.self_type:
                 def_vars.add(node.node.info.self_type)
+
             typ_vars = set(get_type_vars(t))
-            if any(tv for tv in def_vars & typ_vars if tv.upper_bound != itype):
+            for tv in def_vars & typ_vars:
+                if (
+                    # Forgive access to variables generic over Self
+                    isinstance(tv.upper_bound, Instance)
+                    and super_info
+                    and tv.upper_bound.type.fullname == super_info.fullname
+                ):
+                    continue
                 if node.node.is_classvar:
                     message = message_registry.GENERIC_CLASS_VAR_ACCESS
                 else:
                     message = message_registry.GENERIC_INSTANCE_VAR_CLASS_ACCESS
                 mx.msg.fail(message, mx.context)
+                break
+
             t = expand_self_type_if_needed(t, mx, node.node, itype, is_class=True)
             # Erase non-mapped variables, but keep mapped ones, even if there is an error.
             # In the above example this means that we infer following types:
