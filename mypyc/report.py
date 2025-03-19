d062c6a70f217282e6c2a928cc71f26e4bfcc552
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-03-19 15:22:18 +0000

    Tag dynamic getattr using red

--- mypyc/report.py
+++ mypyc/report.py
@@ -1,4 +1,5 @@
 import os.path
+import re
 import sys
 from html import escape
 
@@ -6,6 +7,8 @@ from mypy.build import BuildResult
 from mypy.nodes import MypyFile
 from mypy.util import FancyFormatter
 from mypyc.ir.module_ir import ModuleIR
+from mypyc.ir.func_ir import FuncIR
+from mypyc.ir.ops import CallC
 
 
 class AnnotatedSource:
@@ -31,21 +34,47 @@ def generate_report(result: BuildResult, modules: dict[str, ModuleIR]) -> None:
 
 
 def generate_annotations(path: str, tree: MypyFile, ir: ModuleIR) -> AnnotatedSource:
-    return AnnotatedSource(path, {})
+    anns = {}
+    for func_ir in ir.functions:
+        anns.update(function_annotations(func_ir))
+    return AnnotatedSource(path, anns)
+
+
+def function_annotations(func_ir: FuncIR) -> dict[int, str]:
+    # TODO: check if func_ir.line is -1
+    anns = {}
+    for block in func_ir.blocks:
+        for op in block.ops:
+            if isinstance(op, CallC):
+                name = op.function_name
+                if name == "CPyObject_GetAttr":
+                    anns[op.line] = "bad"
+                    print(op.line, op.function_name)
+    return anns
 
 
 def generate_html_report(sources: list[AnnotatedSource]) -> str:
     html = []
     html.append("<html><head></head>\n")
     html.append("<body>\n")
-    html.append("<h1>Mypyc Report</h1>\n")
     for src in sources:
-        html.append(f"<h2>{src.path}</h2>\n")
+        html.append(f"<h2><tt>{src.path}</tt></h2>\n")
         html.append("<pre>")
+        anns = src.annotations
         with open(src.path) as f:
-            text = f.read()
-        html.append(escape(text))
+            lines = f.readlines()
+        for i, s in enumerate(lines):
+            s = escape(s)
+            line = i + 1
+            if line in anns:
+                s = colorize_line(s)
+            html.append(s)
         html.append("</pre>")
 
     html.append("</body></html>\n")
     return "".join(html)
+
+
+def colorize_line(s: str) -> str:
+    init = re.match("[ \t]*", s).group()
+    return init + f'<span style="background-color: #fcc">{s[len(init):]}</span>'
