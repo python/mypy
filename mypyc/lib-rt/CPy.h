a8a2142fa5d8e828aab72363729d24f0261f3b76
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-04-05 16:24:39 +0200

    [mypyc] Optimize list.__imul__, tuple.__mul__

--- mypyc/lib-rt/CPy.h
+++ mypyc/lib-rt/CPy.h
@@ -664,6 +664,7 @@ int CPyList_Remove(PyObject *list, PyObject *obj);
 CPyTagged CPyList_Index(PyObject *list, PyObject *obj);
 PyObject *CPySequence_Multiply(PyObject *seq, CPyTagged t_size);
 PyObject *CPySequence_RMultiply(CPyTagged t_size, PyObject *seq);
+PyObject *CPySequence_InPlaceMultiply(PyObject *seq, CPyTagged t_size);
 PyObject *CPyList_GetSlice(PyObject *obj, CPyTagged start, CPyTagged end);
 PyObject *CPyList_Copy(PyObject *list);
 int CPySequence_Check(PyObject *obj);
