06f6b7321bdf28ab966162674ecfc62f2b7cbb3e
Jdwashin9 <Jdwashin9@github.com>
2025-04-19 18:06:13 -0400

    general code_cleanup 1806

--- mypy/test_decorator_suggestion.py
+++ mypy/test_decorator_suggestion.py
@@ -1,11 +1,13 @@
-from typing import Callable, TypeVar, ParamSpec 
+from typing import Callable, ParamSpec, TypeVar
 
 R = TypeVar("R")
 P = ParamSpec("P")
 
+
 def dec(f: Callable[P, R]) -> Callable[P, R]:
     return f
 
+
 @dec
 def f():
-    print('hello world')
+    print("hello world")
