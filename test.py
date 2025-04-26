c979af9ab1bbb47b8de4c27c6cc4438c791d5ae2
Christina <tchristina1607@gmail.com>
2025-04-25 23:06:52 -0400

    Extend check for format string specifiers and added more unit tests

--- test.py
+++
@@ -1,7 +0,0 @@
-class GoodFormat:
-    def __format__(self, format_spec):
-        return f"<Formatted:{format_spec}>"
-"{:*^15}".format(GoodFormat())
-class Foo1:
-    def __str__(self): return "hello"
-"{:*^15}".format(Foo1())
