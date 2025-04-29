2866660d4445e1228c9f6e679b2d34e4db41d36f
Arnav Jain <arnavjain20042@gmail.com>
2025-04-29 01:17:38 -0700

    Updated the magic_method.py to pass the self check

--- test-data/unit/plugins/magic_method.py
+++ test-data/unit/plugins/magic_method.py
@@ -1,29 +1,33 @@
-from mypy.types import LiteralType, AnyType, TypeOfAny
-from mypy.plugin import Plugin
+from mypy.types import LiteralType, AnyType, TypeOfAny, Type
+from mypy.plugin import Plugin, MethodContext
+from typing import Callable
 
 # If radd exists, there shouldn't be an error. If it doesn't exist, then there will be an error
-def type_add(ctx, *args):
-    lhs = ctx.type
-    rhs = ctx.arg_types[0][0]
-    ret = lhs.value + rhs.value
+def type_add(ctx: MethodContext, *args) -> Type:
     ctx.api.fail("test", ctx.context)
     return AnyType(TypeOfAny.from_error)
 
-def type_radd(ctx, *args):
+def type_radd(ctx: MethodContext, *args) -> Type:
     lhs = ctx.type
-    rhs = ctx.arg_types[0][0]
+    rhs = ctx.arg_types[-1][0]
+    if not (isinstance(lhs, LiteralType) and isinstance(rhs, LiteralType)):
+        ctx.api.fail("operands not literals", ctx.context)
+        return AnyType(TypeOfAny.from_error)
+    if not (isinstance(lhs.value, int) and isinstance(rhs.value, int)):
+        ctx.api.fail("operands not literal ints", ctx.context)
+        return AnyType(TypeOfAny.from_error)
     ret = lhs.value + rhs.value
     return LiteralType(ret, fallback=ctx.api.named_generic_type('builtins.int', []))
 
 
 class TestPlugin(Plugin):
+    def get_method_hook(self, fullname: str) -> Callable[[MethodContext], Type] | None:
 
-    def get_method_hook(self, fullname):
         if fullname == 'builtins.int.__add__':
             return type_add
         if fullname == 'builtins.int.__radd__':
             return type_radd
         return None
 
-def plugin(version: str):
+def plugin(version: str) -> type[TestPlugin]:
     return TestPlugin
