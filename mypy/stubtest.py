39e315a754a2925e30dca8b32bba149393cf1a89
STerliakov <terlya.stas@gmail.com>
2025-05-13 01:46:51 +0200

    Make it work with replaced stdout

--- mypy/stubtest.py
+++ mypy/stubtest.py
@@ -2107,11 +2107,12 @@ def safe_print(text: str) -> None:
     # If `sys.stdout` encoding is not the same as out (usually UTF8) string,
     # if may cause painful crashes. I don't want to reconfigure `sys.stdout`
     # to do `errors = "replace"` as that sounds scary.
-    print(
-        text.encode(sys.stdout.encoding, errors="replace").decode(
-            sys.stdout.encoding, errors="replace"
-        )
-    )
+    out_encoding = sys.stdout.encoding
+    if out_encoding is not None:
+        # Can be None if stdout is replaced (including our own tests). This should be
+        # safe to omit if the actual stream doesn't care about encoding.
+        text = text.encode(out_encoding, errors="replace").decode(out_encoding, errors="replace")
+    print(text)
 
 
 def parse_options(args: list[str]) -> _Arguments:
