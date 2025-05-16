c5e8931beef3607daf9983dae4655dc0a7457988
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-16 16:36:51 -0400

    feat: mypyc weakref_ops.py
    new primitives for weakref.ref and weakref.proxy

---
+++ mypyc/primitives/weakref_ops.py
@@ -0,0 +1,22 @@
+from mypyc.ir.rtypes import (
+    object_rprimitive,
+)
+from mypyc.primitives.registry import (
+    function_op,
+)
+
+# Weakref operations
+
+py_weakref_new_ref_op = function_op(
+    name="weakref.weakref",
+    arg_types=[object_rprimitive],
+    result_type=object_rprimitive,
+    c_function_name="PyWeakref_NewRef",
+)
+
+py_weakref_new_ref_op = function_op(
+    name="weakref.proxy",
+    arg_types=[object_rprimitive],
+    result_type=object_rprimitive,
+    c_function_name="PyWeakref_NewProxy",
+)
