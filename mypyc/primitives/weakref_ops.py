c4d117f72d366fe0e08d997ec39b258a7233eaa7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-16 23:58:04 -0400

    Update weakref_ops.py

--- mypyc/primitives/weakref_ops.py
+++ mypyc/primitives/weakref_ops.py
@@ -6,13 +6,13 @@ from mypyc.primitives.registry import function_op
 py_weakref_new_ref_op = function_op(
     name="weakref.weakref",
     arg_types=[object_rprimitive, object_rprimitive],
-    result_type=object_rprimitive,
+    return_type=object_rprimitive,
     c_function_name="PyWeakref_NewRef",
 )
 
 py_weakref_new_ref_op = function_op(
     name="weakref.proxy",
     arg_types=[object_rprimitive, object_rprimitive],
-    result_type=object_rprimitive,
+    return_type=object_rprimitive,
     c_function_name="PyWeakref_NewProxy",
 )
