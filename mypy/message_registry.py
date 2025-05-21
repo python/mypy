7b89fd9808ade98f26e53016efdd380113d5cd30
Charulata Lodha <clodha@bloomberg.net>
2025-05-21 11:37:22 -0400

    fix err code for ARGUMENT_TYPE_EXPECTED NO_UNTYPED_DEF

--- mypy/message_registry.py
+++ mypy/message_registry.py
@@ -126,7 +126,8 @@ RETURN_TYPE_EXPECTED: Final = ErrorMessage(
     "Function is missing a return type annotation", codes.NO_UNTYPED_DEF
 )
 ARGUMENT_TYPE_EXPECTED: Final = ErrorMessage(
-    "Function is missing a type annotation for one or more arguments: {}", code=codes.ASSIGNMENT
+    "Function is missing a type annotation for one or more arguments: {}",
+    code=codes.NO_UNTYPED_DEF,
 )
 KEYWORD_ARGUMENT_REQUIRES_STR_KEY_TYPE: Final = ErrorMessage(
     'Keyword argument only valid with "str" key type in call to "dict"'
