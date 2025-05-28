3801b7fef670eae73a72b903e93330d3e2dbdbd5
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-05-28 10:35:12 +0200

    Drop support for --python-version 3.8 (#19157)
    Drop last remaining support for Python 3.8. Support for running with 3.8
    was removed in #17492 already. This PR removes the option to use 3.8
    with `--python-version` since the type stubs only support 3.9+, see
    https://github.com/python/mypy/pull/18930.

--- mypy/modulefinder.py
+++ mypy/modulefinder.py
@@ -995,6 +995,6 @@ def parse_version(version: str) -> tuple[int, int]:
 
 def typeshed_py_version(options: Options) -> tuple[int, int]:
     """Return Python version used for checking whether module supports typeshed."""
-    # Typeshed no longer covers Python 3.x versions before 3.8, so 3.8 is
+    # Typeshed no longer covers Python 3.x versions before 3.9, so 3.9 is
     # the earliest we can support.
-    return max(options.python_version, (3, 8))
+    return max(options.python_version, (3, 9))
