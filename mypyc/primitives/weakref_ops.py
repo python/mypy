9e1375df84d55c884df65be84b05fe3a968ee0df
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-06-03 13:00:35 -0400

    Update weakref_ops.py

--- mypyc/primitives/weakref_ops.py
+++ mypyc/primitives/weakref_ops.py
@@ -20,20 +20,3 @@ new_ref__with_callback_op = function_op(
     c_function_name="PyWeakref_NewRef",
     error_kind=ERR_MAGIC,
 )
-
-new_proxy_op = function_op(
-    name="weakref.ProxyType",
-    arg_types=[object_rprimitive],
-    return_type=object_rprimitive,
-    c_function_name="PyWeakref_NewProxy",
-    extra_int_constants=[(0, pointer_rprimitive)],
-    error_kind=ERR_MAGIC,
-)
-
-new_proxy_with_callback_op = function_op(
-    name="weakref.ProxyType",
-    arg_types=[object_rprimitive, object_rprimitive],
-    return_type=object_rprimitive,
-    c_function_name="PyWeakref_NewProxy",
-    error_kind=ERR_MAGIC,
-)
