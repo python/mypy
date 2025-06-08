f1b496c0f93fe16c07538e4d825a92373c2d7015
Guy Wilson <guywilsonjr@gmail.com>
2025-06-08 00:01:05 -0700

    Fix help message url for "None and Optional handling" section (#19252)
    Fixes #19251

--- mypy/main.py
+++ mypy/main.py
@@ -785,7 +785,7 @@ def process_options(
         title="None and Optional handling",
         description="Adjust how values of type 'None' are handled. For more context on "
         "how mypy handles values of type 'None', see: "
-        "https://mypy.readthedocs.io/en/stable/kinds_of_types.html#no-strict-optional",
+        "https://mypy.readthedocs.io/en/stable/kinds_of_types.html#optional-types-and-the-none-type",
     )
     add_invertible_flag(
         "--implicit-optional",
