ad570933924b3810ba61d2e4a13eac596f74672b
Ivan Levkivskyi <levkivskyi@gmail.com>
2025-06-19 17:27:14 +0100

    Cleanup generic class variable access (#19292)
    Fixes https://github.com/python/mypy/issues/5144
    Fixes https://github.com/python/mypy/issues/15223
    
    This is related to the work on
    https://github.com/python/mypy/issues/7724
    
    Now that all attribute access goes through `checkmember.py` there is not
    much benefit in giving an error at the definition site, especially that
    it prohibits some valid (and common) use cases, see comments in
    https://github.com/python/mypy/issues/5144. While looking at this I
    discovered a bunch of defects in the _implementation_, that I also fix
    (I am keeping unsafe self-type related logic as is):
    * We used to erase type vars of the definition class instead of the use
    class. This caused type variables leaks.
    * The erasure was inconsistent, so that in some cases we silently erased
    type variables to `Any` even in allowed use cases
    * `TypeVarTuple` and `ParamSpec` were not handled as equal to regular
    type variables (I guess because of old problems with erasing them)

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -5079,14 +5079,6 @@ class SemanticAnalyzer(
                 node.is_classvar = True
             analyzed = self.anal_type(s.type)
             assert self.type is not None
-            if analyzed is not None and set(get_type_vars(analyzed)) & set(
-                self.type.defn.type_vars
-            ):
-                # This means that we have a type var defined inside of a ClassVar.
-                # This is not allowed by PEP526.
-                # See https://github.com/python/mypy/issues/11538
-
-                self.fail(message_registry.CLASS_VAR_WITH_TYPEVARS, s)
             if (
                 analyzed is not None
                 and self.type.self_type in get_type_vars(analyzed)
