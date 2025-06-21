a4801f928aaadb19f9893fe45af8e69ab6b509d0
Charlie Denton <charlie@meshy.co.uk>
2025-06-21 01:08:46 +0100

    Type ignore comments erroneously marked as unused by dmypy (#15043)
    There is currently a misbehaviour where "type: ignore" comments are
    erroneously marked as unused in re-runs of dmypy. There are also cases
    where errors disappear on the re-run.
    
    As far as I can tell, this only happens in modules which contain an
    import that we don't know how to type (such as a module which does not
    exist), and a submodule which is unused.
    
    There was a lot of commenting and investigation on this PR, but I hope
    that the committed tests and fixes illustrate and address the issue.
    
    Related to https://github.com/python/mypy/issues/9655
    
    ---------
    
    Co-authored-by: David Seddon <david@seddonym.me>
    Co-authored-by: Shantanu <12621235+hauntsaninja@users.noreply.github.com>
    Co-authored-by: Ivan Levkivskyi <levkivskyi@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- mypy/errors.py
+++ mypy/errors.py
@@ -786,6 +786,8 @@ class Errors:
                 code=codes.UNUSED_IGNORE,
                 blocker=False,
                 only_once=False,
+                origin=(self.file, [line]),
+                target=self.target_module,
             )
             self._add_error_info(file, info)
 
@@ -837,6 +839,8 @@ class Errors:
                 code=codes.IGNORE_WITHOUT_CODE,
                 blocker=False,
                 only_once=False,
+                origin=(self.file, [line]),
+                target=self.target_module,
             )
             self._add_error_info(file, info)
 
