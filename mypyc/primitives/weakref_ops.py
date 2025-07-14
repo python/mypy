e6e4a6bfb47b8ef72e5215e93db43116814ecd94
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-07-14 20:23:23 +0000

    feat: new primitive for `weakref.__call__`

--- mypyc/primitives/weakref_ops.py
+++ mypyc/primitives/weakref_ops.py
@@ -1,6 +1,6 @@
 from mypyc.ir.ops import ERR_MAGIC
-from mypyc.ir.rtypes import object_rprimitive, pointer_rprimitive
-from mypyc.primitives.registry import function_op
+from mypyc.ir.rtypes import object_rprimitive, pointer_rprimitive, weakref_rprimitive
+from mypyc.primitives.registry import ERR_NEG_INT, function_op, method_op
 
 # Weakref operations
 
@@ -16,7 +16,15 @@ new_ref_op = function_op(
 new_ref__with_callback_op = function_op(
     name="weakref.ReferenceType",
     arg_types=[object_rprimitive, object_rprimitive],
-    return_type=object_rprimitive,
+    return_type=weakref_rprimitive,
     c_function_name="PyWeakref_NewRef",
     error_kind=ERR_MAGIC,
+)
+
+deref_op = method_op(
+    name="__call__",
+    arg_types=[weakref_rprimitive],
+    return_type=object_rprimitive,
+    c_function_name="PyWeakref_GetRef",
+    error_kind=ERR_NEG_INT,
 )
