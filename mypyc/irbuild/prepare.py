68e28bb1ddd9ba3792cb7ffac2eca00aa7a45184
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-07-14 20:24:10 +0000

    Update prepare.py

--- mypyc/irbuild/prepare.py
+++ mypyc/irbuild/prepare.py
@@ -326,9 +326,9 @@ def prepare_class_def(
     if attrs.get("serializable") is True:
         # Supports copy.copy and pickle (including subclasses)
         ir._serializable = True
-    if attrs.get("support_weakrefs") is True:
-        # Supports weakrefs (including subclasses)
-        ir.support_weakrefs = True
+    if attrs.get("supports_weakref") is True:
+        # Has a tp_weakrefoffset slot allowing the creation of weak references (including subclasses)
+        ir.supports_weakref = True
 
     # Check for subclassing from builtin types
     for cls in info.mro:
