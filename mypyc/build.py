02a472a0dbb7d21810ef2c56626eba2113e5a049
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-07-16 13:43:40 +0100

    [mypyc] Optionally log a sampled operation trace to a file (#19457)
    Logging executed ops is useful for performance analysis. For example, we
    can look for functions which perform many slow operations and try to
    optimize them. I've already used this successfully to implement several
    optimizations. A typical optimization that this helps with is replacing
    a generic Python function call operation with a native call. This has
    also helped me identify inefficient code generated by mypyc.
    
    Compile using `MYPYC_LOG_TRACE=1 mypyc ...` to enable trace logging. The
    log will be written to `mypyc_trace.txt`. Roughly 1/1000 of ops of
    certain kinds (e.g. primitive calls) are logged.
    
    This can also be enabled by passing `log_trace=True` to `mypycify`.
    
    Compared to profiling, this logging data is frequency-oriented instead
    of CPU time oriented, and it's mostly helpful for micro-optimizations.
    It also needs some understanding of mypyc internals to be useful. It's
    not generally possible to reconstruct call stacks from the event data
    (but we could improve this). However, there is very little noise in the
    data and even small improvements can be visible.
    
    Logging isn't impacted by C compiler optimizations, so for a faster
    iteration loop, optimizations can be disabled.
    
    In the future this could possibly be used for profile-guided
    optimizations, but we'd probably need to adjust the data collection a
    bit for this use case.
    
    This is currently not documented and mostly intended for mypy or mypyc
    maintainers for now. Also no tests yet, since this is not a user-evel
    feature and it's disabled by default.
    
    Random example of log entries from mypy self check:
    ```
    mypy.typeops.TypeVarExtractor._merge:1146:call_c:CPyList_Extend
    mypy.semanal.SemanticAnalyzer.lookup::primitive_op:list_get_item_unsafe
    mypy.expandtype.ExpandTypeVisitor.visit_type_var__TypeVisitor_glue:239:call:mypy.expandtype.ExpandTypeVisitor.visit_type_var
    mypy.applytype.apply_generic_arguments:111:call_c:CPy_NoErrOccurred
    mypy.indirection.TypeIndirectionVisitor.visit_callable_type__TypeVisitor_glue:118:call:mypy.indirection.TypeIndirectionVisitor.visit_callable_type
    mypy.fastparse.ASTConverter.visit_Call::primitive_op:buf_init_item
    mypy.semanal.SemanticAnalyzer.is_func_scope::primitive_op:int_eq
    mypy.meet.is_overlapping_types:397:call:mypy.meet._is_subtype_is_overlapping_types_obj
    mypy.types.CallableType.serialize:2287:call_c:CPyList_SetItemUnsafe
    mypy.checkexpr.ExpressionChecker.check_argument_types:2576:call_c:CPyList_SetItemUnsafe
    ```
    
    For example, let's look at this line:
    ```
    mypy.typeops.TypeVarExtractor._merge:1146:call_c:CPyList_Extend
    ```
    In method `TypeVarExtractor._merge`, on line 1146 of `mypy/typeops.py`,
    the C primitive CPyList_Extend was called (corresponds to
    `list.extend`).
    
    I'll later add some documentation to the wiki or other developer docs
    and give examples of using and analyzing the data.

--- mypyc/build.py
+++ mypyc/build.py
@@ -485,6 +485,7 @@ def mypycify(
     include_runtime_files: bool | None = None,
     strict_dunder_typing: bool = False,
     group_name: str | None = None,
+    log_trace: bool = False,
 ) -> list[Extension]:
     """Main entry point to building using mypyc.
 
@@ -531,6 +532,10 @@ def mypycify(
                     the hash of module names. This is used for the names of the
                     output C files and the shared library. This is only supported
                     if there is a single group. [Experimental]
+        log_trace: If True, compiled code writes a trace log of events in
+                   mypyc_trace.txt (derived from executed operations). This is
+                   useful for performance analysis, such as analyzing which
+                   primitive ops are used the most and on which lines.
     """
 
     # Figure out our configuration
@@ -543,6 +548,7 @@ def mypycify(
         include_runtime_files=include_runtime_files,
         strict_dunder_typing=strict_dunder_typing,
         group_name=group_name,
+        log_trace=log_trace,
     )
 
     # Generate all the actual important C code
@@ -583,6 +589,8 @@ def mypycify(
             # See https://github.com/mypyc/mypyc/issues/956
             "-Wno-cpp",
         ]
+        if log_trace:
+            cflags.append("-DMYPYC_LOG_TRACE")
     elif compiler.compiler_type == "msvc":
         # msvc doesn't have levels, '/O2' is full and '/Od' is disable
         if opt_level == "0":
@@ -607,6 +615,8 @@ def mypycify(
             # that we actually get the compilation speed and memory
             # use wins that multi-file mode is intended for.
             cflags += ["/GL-", "/wd9025"]  # warning about overriding /GL
+        if log_trace:
+            cflags.append("/DMYPYC_LOG_TRACE")
 
     # If configured to (defaults to yes in multi-file mode), copy the
     # runtime library in. Otherwise it just gets #included to save on
