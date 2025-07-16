a0b037eec1405a69528a8de531ac98324a598831
Randolf Scholz <randolf.scholz@gmail.com>
2025-07-16 17:42:19 +0200

    Update mypy/subtypes.py
    Co-authored-by: Stanislav Terliakov <50529348+sterliakov@users.noreply.github.com>

--- mypy/subtypes.py
+++ mypy/subtypes.py
@@ -2208,7 +2208,7 @@ def infer_variance(info: TypeInfo, i: int) -> bool:
                     settable = False
 
             # TODO: handle settable properties with setter type different from getter.
-            plain_self = Instance(info.mro[0], [])  # self-type without type variables
+            plain_self = fill_typevars_with_any(info)  # self-type without type variables
             typ = find_member(member, self_type, plain_self)
             if typ:
                 # It's okay for a method in a generic class with a contravariant type
