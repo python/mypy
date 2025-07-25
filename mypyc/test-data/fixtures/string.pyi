19842e1343e310a351c9e79eb400fe0eebe7d488
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-07-25 13:44:17 -0400

    Create string.pyi

---
+++ mypyc/test-data/fixtures/string.pyi
@@ -0,0 +1,10 @@
+from typing import Optional
+
+class int: pass
+class str: pass
+    def count(
+        self,
+        substr: str,
+        start: Optional[int] = None,
+        end: Optional[int] = None,
+    ) -> int: pass
