298e5db8df73ba46d138adafb8a3f1a653bb97e2
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-07-25 15:01:53 -0400

    Delete mypyc/test-data/fixtures/tuple.pyi

--- mypyc/test-data/fixtures/tuple.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Iterator
-
-class bytes:
-    def __iter__(self) -> Iterator[int]: pass
-class dict: pass
-class int:
-    def __add__(self, other: int) -> int: pass
-class str: pass
-class tuple: pass
