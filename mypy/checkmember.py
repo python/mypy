06e28f8707baf8eab69eac84a2e4202060420e04
Stanislav Terliakov <50529348+sterliakov@users.noreply.github.com>
2025-08-03 15:01:59 +0200

    Follow-up after #19025: test and cleanup (#19294)
    As requested by @ilevkivskyi in #19025
    
    ---------
    
    Co-authored-by: Ivan Levkivskyi <levkivskyi@gmail.com>

--- mypy/checkmember.py
+++ mypy/checkmember.py
@@ -1249,9 +1249,6 @@ def analyze_class_attribute_access(
             or isinstance(node.node, Var)
             and node.node.is_classmethod
         )
-        is_staticmethod = (is_decorated and cast(Decorator, node.node).func.is_static) or (
-            isinstance(node.node, SYMBOL_FUNCBASE_TYPES) and node.node.is_static
-        )
         t = get_proper_type(t)
         is_trivial_self = False
         if isinstance(node.node, Decorator):
@@ -1274,7 +1271,7 @@ def analyze_class_attribute_access(
             original_vars=original_vars,
             is_trivial_self=is_trivial_self,
         )
-        if is_decorated and not is_staticmethod:
+        if is_decorated:
             t = expand_self_type_if_needed(
                 t, mx, cast(Decorator, node.node).var, itype, is_class=is_classmethod
             )
