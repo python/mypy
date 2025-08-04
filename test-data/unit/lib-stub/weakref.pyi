d0ee2f0c65e692b032525153c25b4a700e96b7e0
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-08-04 02:19:00 +0000

    fix: error: "ReferenceType[object]" not callable

--- test-data/unit/lib-stub/weakref.pyi
+++ test-data/unit/lib-stub/weakref.pyi
@@ -7,5 +7,6 @@ _T = TypeVar("_T")
 class ReferenceType(Generic[_T]):  # "weakref"
     __callback__: Callable[[Self], Any]
     def __new__(cls, o: _T, callback: Callable[[Self], Any] | None = ..., /) -> Self: ...
+    def __call__(self) -> _T: ...
 
 ref = ReferenceType
