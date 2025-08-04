9f950880d2962ec83a6bcf257f006078d70a6841
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-08-04 02:17:10 +0000

    Update errorcodes.py

--- mypy/errorcodes.py
+++ mypy/errorcodes.py
@@ -326,7 +326,7 @@ DEPRECATED: Final = ErrorCode(
     default_enabled=False,
 )
 
-INDEX_RANGE = ErrorCode("index-range", "index out of statically known range", "Index Range", True)
+INDEX_RANGE: Final[ErrorCode] = ErrorCode("index-range", "index out of statically known range", "Index Range", True)
 
 # This copy will not include any error codes defined later in the plugins.
 mypy_error_codes = error_codes.copy()
