12e0a9d0b60895a53b59900867016d5fed81fdcb
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-08-07 19:42:20 -0400

    WIP [mypyc] optimize f-string building

--- mypyc/irbuild/specialize.py
+++ mypyc/irbuild/specialize.py
@@ -710,6 +710,15 @@ def translate_fstring(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> Va
                 format_ops.append(FormatOp.STR)
                 exprs.append(item.args[0])
 
+        for i in range(len(exprs)):
+            # TODO: instead of checking isinstance StrExpr, check with some new is_literal fn.
+            # This can include IntExpr, BytesExpr, Final string RefExpr, and more future cases.
+            while isinstance(exprs[i], StrExpr) and isinstance(exprs[i+1], StrExpr):
+                first = exprs[i]
+                concatenated = first.value + exprs[i+1].value
+                exprs = [exprs[:i] + [StrExpr(concatenated, first.line)] + exprs[i+2:]]
+                format_ops = format_ops[:i] + [format_ops[i+1]] + [format_ops[i+2:]
+
         substitutions = convert_format_expr_to_str(builder, format_ops, exprs, expr.line)
         if substitutions is None:
             return None
