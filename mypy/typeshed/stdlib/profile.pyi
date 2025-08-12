c9606a8add05cbc4d6f422fa65bcf9a47750be6f
Alex <gkristoffersen83@hotmail.com>
2025-08-12 14:25:30 +0200

    Update profile.pyi

--- mypy/typeshed/stdlib/profile.pyi
+++ mypy/typeshed/stdlib/profile.pyi
@@ -1,31 +1,27 @@
-from _typeshed import StrOrBytesPath
-from collections.abc import Callable, Mapping
-from typing import Any, TypeVar
-from typing_extensions import ParamSpec, Self, TypeAlias
+# ~/.profile: executed by the command interpreter for login shells.
+# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
+# exists.
+# see /usr/share/doc/bash/examples/startup-files for examples.
+# the files are located in the bash-doc package.
 
-__all__ = ["run", "runctx", "Profile"]
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+#umask 022
 
-def run(statement: str, filename: str | None = None, sort: str | int = -1) -> None: ...
-def runctx(
-    statement: str, globals: dict[str, Any], locals: Mapping[str, Any], filename: str | None = None, sort: str | int = -1
-) -> None: ...
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+	. "$HOME/.bashrc"
+    fi
+fi
 
-_T = TypeVar("_T")
-_P = ParamSpec("_P")
-_Label: TypeAlias = tuple[str, int, str]
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
 
-class Profile:
-    bias: int
-    stats: dict[_Label, tuple[int, int, int, int, dict[_Label, tuple[int, int, int, int]]]]  # undocumented
-    def __init__(self, timer: Callable[[], float] | None = None, bias: int | None = None) -> None: ...
-    def set_cmd(self, cmd: str) -> None: ...
-    def simulate_call(self, name: str) -> None: ...
-    def simulate_cmd_complete(self) -> None: ...
-    def print_stats(self, sort: str | int = -1) -> None: ...
-    def dump_stats(self, file: StrOrBytesPath) -> None: ...
-    def create_stats(self) -> None: ...
-    def snapshot_stats(self) -> None: ...
-    def run(self, cmd: str) -> Self: ...
-    def runctx(self, cmd: str, globals: dict[str, Any], locals: Mapping[str, Any]) -> Self: ...
-    def runcall(self, func: Callable[_P, _T], /, *args: _P.args, **kw: _P.kwargs) -> _T: ...
-    def calibrate(self, m: int, verbose: int = 0) -> float: ...
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
