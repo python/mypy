d63100f05e8429ec24c3e5227e210149dde7e24f
Ivan Levkivskyi <levkivskyi@gmail.com>
2025-08-26 15:27:51 +0100

    Fix specializations

--- mypyc/primitives/misc_ops.py
+++ mypyc/primitives/misc_ops.py
@@ -425,17 +425,17 @@ function_op(
 )
 
 function_op(
-    name="native_internal.write_native_internal",
+    name="native_internal.write_tag",
     arg_types=[object_rprimitive, int_rprimitive],
     return_type=none_rprimitive,
-    c_function_name="write_native_internal_internal",
+    c_function_name="write_tag_internal",
     error_kind=ERR_MAGIC,
 )
 
 function_op(
-    name="native_internal.read_native_internal",
+    name="native_internal.read_tag",
     arg_types=[object_rprimitive],
     return_type=int_rprimitive,
-    c_function_name="read_native_internal_internal",
+    c_function_name="read_tag_internal",
     error_kind=ERR_MAGIC,
 )
