6bbb9cf82ebb6946c11a06c4d338365b81310e79
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-17 08:51:58 +0000

    fix name err

--- mypyc/irbuild/expression.py
+++ mypyc/irbuild/expression.py
@@ -115,6 +115,8 @@ from mypyc.primitives.set_ops import set_add_op, set_in_op, set_update_op
 from mypyc.primitives.str_ops import str_slice_op
 from mypyc.primitives.tuple_ops import list_tuple_op, tuple_slice_op
 
+ConstantValueTuple = ConstantValue | tuple["ConstantValueTuple", ...]
+
 # Name and attribute references
 
 
