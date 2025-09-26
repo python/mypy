bfe24e3b0b727c9d0b9da73c6d3048708354990c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-26 08:01:46 +0000

    fix: tuple get

--- mypyc/irbuild/for_helpers.py
+++ mypyc/irbuild/for_helpers.py
@@ -241,7 +241,7 @@ def sequence_from_generator_preallocate_helper(
             # If input is RTuple, box it to tuple_rprimitive for generic iteration
             # TODO: this can be optimized a bit better with an unrolled ForRTuple helper
             sequence = builder.coerce(sequence, tuple_rprimitive, line, force=True)
-            items = [builder.add(TupleGet(sequence, i, line) for i in range(len(rtype.types)))]
+            items = [builder.add(TupleGet(sequence, i, line)) for i in range(len(rtype.types))]
             sequence = builder.new_tuple(items, line)
 
         target_op = empty_op_llbuilder(length, line)
