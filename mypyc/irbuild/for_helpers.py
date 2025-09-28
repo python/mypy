1197e4e7fdd7d516691dff211b3b3519a5a9c026
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-28 15:43:40 -0400

    fix ir generaton

--- mypyc/irbuild/for_helpers.py
+++ mypyc/irbuild/for_helpers.py
@@ -1242,8 +1242,8 @@ class ForFilter(ForGenerator):
             # I put this here to prevent a circular import
             # from mypyc.irbuild.expression import transform_call_expr
 
-            # result = transform_call_expr(builder, fake_call_expr)
-            result = builder.accept(fake_call_expr)
+            result = transform_call_expr(builder, fake_call_expr)
+            # result = builder.accept(fake_call_expr)
 
         # Now, filter: only enter the body if func(item) is truthy
         cont_block, rest_block = BasicBlock(), BasicBlock()
