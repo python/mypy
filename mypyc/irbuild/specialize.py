02d47392cf3f572f0d3128df65c20325a776938b
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-28 18:01:53 -0400

    Update specialize.py

--- mypyc/irbuild/specialize.py
+++ mypyc/irbuild/specialize.py
@@ -433,7 +433,7 @@ def translate_any_call(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> V
 
             index_type = builder._analyze_iterable_item_type(arg)
             index = _create_iterable_lexpr(index_name, index_type)
-            index_reg = builder.add_local_reg(index.node, builder.type_to_rtype(index_type))
+            index_reg = builder.add_local_reg(index.node, builder.type_to_rtype(index_type))  # type: ignore [arg-type]
 
             for_loop_helper(builder, index, arg, body_insts, None, is_async=False, line=expr.line)
             builder.goto_and_activate(loop_exit)
@@ -471,7 +471,7 @@ def translate_all_call(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> V
 
             index_type = builder._analyze_iterable_item_type(arg)
             index = _create_iterable_lexpr(index_name, index_type)
-            index_reg = builder.add_local_reg(index.node, builder.type_to_rtype(index_type))
+            index_reg = builder.add_local_reg(index.node, builder.type_to_rtype(index_type))  # type: ignore [arg-type]
 
             for_loop_helper(builder, index, arg, body_insts, None, is_async=False, line=expr.line)
             builder.goto_and_activate(loop_exit)
