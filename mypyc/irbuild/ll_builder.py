9a358fcfa8e49a8b0f78eab336a7f6733d2b30fa
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-30 19:08:10 +0000

    [mypyc] fix: isinstance_native fast path conflict w/ subclasses

--- mypyc/irbuild/ll_builder.py
+++ mypyc/irbuild/ll_builder.py
@@ -695,11 +695,15 @@ class LowLevelIRBuilder:
         """Fast isinstance() check for a native class.
 
         If there are three or fewer concrete (non-trait) classes among the class
-        and all its children, use even faster type comparison checks `type(obj)
-        is typ`.
+        and all its children, and none of them allow interpreted subclasses, use
+        even faster type comparison checks `type(obj) is typ`.
         """
         concrete = all_concrete_classes(class_ir)
-        if concrete is None or len(concrete) > FAST_ISINSTANCE_MAX_SUBCLASSES + 1:
+        if concrete is None or len(concrete) > FAST_ISINSTANCE_MAX_SUBCLASSES + 1 or any(
+            # we can only take the fast path if we know
+            # an end user won't inherit from the class
+            cls.allow_interpreted_subclasses for cls in concrete
+        ):
             return self.primitive_op(
                 fast_isinstance_op, [obj, self.get_native_type(class_ir)], line
             )
