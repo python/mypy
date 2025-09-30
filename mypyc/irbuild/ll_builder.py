f84f576699c77e8284f81364bd73875822c507ba
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-30 15:43:39 -0400

    handle non-native final

--- mypyc/irbuild/ll_builder.py
+++ mypyc/irbuild/ll_builder.py
@@ -703,9 +703,10 @@ class LowLevelIRBuilder:
             concrete is None
             or len(concrete) > FAST_ISINSTANCE_MAX_SUBCLASSES + 1
             or any(
-                # we can only take the fast path if we know
-                # an end user won't inherit from the class
+                # we can only use fast path if we know end user won't inherit from the class
                 cls.allow_interpreted_subclasses
+                # a non-native class is only suitable for fast path if final
+                or not cls.is_ext_class and not cls.is_final_class
                 for cls in concrete
             )
         ):
