6a25595bf6c406975168d279312dade1e48a9bd1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-02 15:30:43 -0400

    Update constant_fold.py

--- mypyc/irbuild/constant_fold.py
+++ mypyc/irbuild/constant_fold.py
@@ -10,7 +10,7 @@ to other compiled modules in the same compilation unit.
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Final, Union
+from typing import TYPE_CHECKING, Callable, Final, Union
 
 from mypy.constant_fold import constant_fold_binary_op, constant_fold_unary_op
 from mypy.nodes import (
