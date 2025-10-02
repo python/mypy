59c1826fd6a9f582a43395abec0f2821da4b3072
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-01 20:35:30 -0400

    Update specialize.py

--- mypyc/irbuild/specialize.py
+++ mypyc/irbuild/specialize.py
@@ -1059,10 +1059,9 @@ def translate_float(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> Valu
 def translate_ord(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> Value | None:
     if len(expr.args) != 1 or expr.arg_kinds[0] != ARG_POS:
         return None
-    arg = expr.args[0]
-    folded_arg = constant_fold_expr(builder, arg)
-    if isinstance(folded_arg, (str, bytes)) and len(folded_arg) == 1:
-        return Integer(ord(folded_arg))
+    arg = constant_fold_expr(builder, expr.args[0])
+    if isinstance(arg, (str, bytes)) and len(arg) == 1:
+        return Integer(ord(arg))
     return None
 
 
