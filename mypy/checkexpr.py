a642928e558e383483cfdfe236faff9d63d057fe
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-10-02 01:04:56 +0000

    [mypyc] feat: support constant folding in `ExpressionChecker.check_str_format_call`

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -659,8 +659,9 @@ class ExpressionChecker(ExpressionVisitor[Type], ExpressionCheckerSharedApi):
         """More precise type checking for str.format() calls on literals."""
         assert isinstance(e.callee, MemberExpr)
         format_value = None
-        if isinstance(e.callee.expr, StrExpr):
-            format_value = e.callee.expr.value
+        folded_callee_expr = constant_fold_expr(e.callee.expr, "unused")
+        if isinstance(folded_callee_expr, str):
+            format_value = folded_callee_expr
         elif self.chk.has_type(e.callee.expr):
             typ = get_proper_type(self.chk.lookup_type(e.callee.expr))
             if (
