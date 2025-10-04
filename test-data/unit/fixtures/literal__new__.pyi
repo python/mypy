535a7b1923601f4c40d6d28f61e5677a6cd5e1ba
Ivan Levkivskyi <levkivskyi@gmail.com>
2025-10-04 02:17:43 +0100

    Add type as well

--- test-data/unit/fixtures/literal__new__.pyi
+++ test-data/unit/fixtures/literal__new__.pyi
@@ -3,6 +3,9 @@ from typing import Literal, Protocol, overload
 class object:
     def __init__(self) -> None: pass
 
+class type:
+    def __init__(self, x) -> None: pass
+
 class str: pass
 class dict: pass
 class float: pass
