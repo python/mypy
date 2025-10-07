f21d222932fcf6ff4c3f977329a9f2bc36cea63c
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-07 08:39:02 -0400

    Update specialize.py

--- mypyc/irbuild/specialize.py
+++ mypyc/irbuild/specialize.py
@@ -520,7 +520,7 @@ def translate_sum_call(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> V
         return None
 
     arg = expr.args[0]
-    if not isinstance(arg, GeneratorExpr) and not _is_supported_forloop_iter(builder, arg):
+    if not isinstance(arg, GeneratorExpr) and not expr_has_specialized_for_helper(builder, arg):
         return None
 
     # handle 'start' argument, if given
@@ -572,7 +572,7 @@ def translate_sum_call(builder: IRBuilder, expr: CallExpr, callee: RefExpr) -> V
             builder.assign(retval, total, expr.line)
 
         index_type = builder._analyze_iterable_item_type(arg)
-        index = _create_iterable_lexpr(index_name, index_type)
+        index = create_synthetic_nameexpr(index_name, index_type)
         index_reg = builder.add_local_reg(index.node, builder.type_to_rtype(index_type))  # type: ignore [arg-type]
 
         for_loop_helper(builder, index, arg, body_insts, None, is_async=False, line=expr.line)
