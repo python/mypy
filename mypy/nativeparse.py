8c0f90648228eba7a234d33c73db8bfeab9201f2
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-10-11 16:14:42 +0100

    [WIP] First steps towards parsing something

--- mypy/nativeparse.py
+++ mypy/nativeparse.py
@@ -17,14 +17,53 @@ from __future__ import annotations
 import os
 import subprocess
 
-from mypy.nodes import MypyFile
+from mypy import nodes
+from mypy.cache import Buffer, read_int, read_str, read_tag
+from mypy.nodes import (
+    ARG_POS,
+    CallExpr,
+    Expression,
+    ExpressionStmt,
+    MypyFile,
+    NameExpr,
+    Statement,
+    StrExpr,
+)
 
 
 def native_parse(filename: str) -> MypyFile:
-    assert False
+    b = parse_to_binary_ast(filename)
+    print(repr(b))
+    data = Buffer(b)
+    stmt = read_statement(data)
+    return MypyFile([stmt], [])
 
 
 def parse_to_binary_ast(filename: str) -> bytes:
     binpath = os.path.expanduser("~/src/ruff/target/release/mypy_parser")
     result = subprocess.run([binpath, "serialize-ast", filename], capture_output=True, check=True)
     return result.stdout
+
+
+def read_statement(data: Buffer) -> Statement:
+    tag = read_tag(data)
+    if tag == nodes.EXPR_STMT:
+        return ExpressionStmt(read_expression(data))
+    else:
+        assert False
+
+
+def read_expression(data: Buffer) -> Expression:
+    tag = read_tag(data)
+    if tag == nodes.CALL_EXPR:
+        callee = read_expression(data)
+        n = read_int(data)
+        args = [read_expression(data) for i in range(n)]
+        return CallExpr(callee, args, [ARG_POS] * n, [None] * n)
+    elif tag == nodes.NAME_EXPR:
+        n = read_str(data)
+        return NameExpr(n)
+    elif tag == nodes.STR_EXPR:
+        return StrExpr(read_str(data))
+    else:
+        assert False
--- mypy/test/test_nativeparse.py
+++ mypy/test/test_nativeparse.py
@@ -1,19 +1,32 @@
+import contextlib
 import os
 import tempfile
 import unittest
+from collections.abc import Iterator
 
 from mypy import nodes
-from mypy.nativeparse import parse_to_binary_ast
+from mypy.nativeparse import native_parse, parse_to_binary_ast
+from mypy.nodes import MypyFile
 
 
 class TestNativeParse(unittest.TestCase):
-    def test_hello_world_binary(self) -> None:
-        with tempfile.TemporaryDirectory() as temp_dir:
-            temp_path = os.path.join(temp_dir, "t.py")
-            with open(temp_path, "w") as f:
-                f.write("print('hello')")
-
-            b = parse_to_binary_ast(temp_path)
-            assert list(b) == [nodes.EXPR_STMT, nodes.CALL_EXPR, nodes.NAME_EXPR, 5] + list(
+    def test_trivial_binary_data(self) -> None:
+        with temp_source("print('hello')") as fnam:
+            b = parse_to_binary_ast(fnam)
+            assert list(b) == [nodes.EXPR_STMT, nodes.CALL_EXPR, nodes.NAME_EXPR, 10] + list(
                 b"print"
-            ) + [1, nodes.STR_EXPR, 5] + list(b"hello")
+            ) + [22, nodes.STR_EXPR, 10] + list(b"hello")
+
+    def test_deserialize_hello(self) -> None:
+        with temp_source("print('hello')") as fnam:
+            node = native_parse(fnam)
+            assert isinstance(node, MypyFile)
+
+
+@contextlib.contextmanager
+def temp_source(text: str) -> Iterator[str]:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_path = os.path.join(temp_dir, "t.py")
+        with open(temp_path, "w") as f:
+            f.write(text)
+        yield temp_path
