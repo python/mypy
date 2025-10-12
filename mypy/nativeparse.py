32d855e9e6df88d6fb3bcaeaa5bf99edc1b70e43
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-10-12 12:56:41 +0100

    Fix self check, update docstring

--- mypy/nativeparse.py
+++ mypy/nativeparse.py
@@ -5,11 +5,14 @@ to a mypy AST.
 
 NOTE: This is heavily work in progress.
 
-Key planned features compared the mypy.fastparse:
+Expected benefits over mypy.fastparse:
  * No intermediate non-mypyc AST created, to improve performance
- * Support all Python syntax even if running mypy on older Python versions
- * Parsing doesn't need GIL => multithreading to produce serialized AST in parallel
- * Produce import dependencies without having to build an AST (helps parallel type checking)
+ * Parsing doesn't need GIL => use multithreading to construct serialized ASTs in parallel
+ * Produce import dependencies without having to build an AST => helps parallel type checking
+ * Support all Python syntax even if running mypy on an older Python version
+ * Generate an AST even if there are syntax errors
+ * Potential to support incremental parsing (quickly process modified sections in a file)
+ * Stripping function bodies in third-party code can happen earlier, for extra performance
 """
 
 from __future__ import annotations
@@ -71,8 +74,8 @@ def read_expression(data: Buffer) -> Expression:
         read_loc(data, ce)
         return ce
     elif tag == nodes.NAME_EXPR:
-        n = read_str(data)
-        ne = NameExpr(n)
+        s = read_str(data)
+        ne = NameExpr(s)
         read_loc(data, ne)
         return ne
     elif tag == nodes.STR_EXPR:
