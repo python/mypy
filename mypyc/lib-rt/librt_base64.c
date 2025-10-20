d7f66e042e9c36e69c8be500db7e930f17993cca
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-10-20 16:37:39 +0100

    WIP beginning of C extension

---
+++ mypyc/lib-rt/librt_base64.c
@@ -0,0 +1,60 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+static PyObject *
+b64encode_internal(PyObject *) {
+    return 0;
+}
+
+static PyObject*
+b64encode(PyObject *self, PyObject *const *args, size_t nargs, PyObject *kwnames) {
+    return 0;
+}
+
+static PyMethodDef librt_base64_module_methods[] = {
+    {"b64encode", (PyCFunction)b64encode, METH_FASTCALL | METH_KEYWORDS, PyDoc_STR("Encode bytes-like object using Base64.")},
+    {NULL, NULL, 0, NULL}
+};
+
+static int
+base64_abi_version(void) {
+    return 1;
+}
+
+static int
+librt_internal_module_exec(PyObject *m)
+{
+    // Export mypy internal C API, be careful with the order!
+    static void *base64_api[2] = {
+        (void *)base64_abi_version,
+        (void *)b64encode_internal,
+    };
+    PyObject *c_api_object = PyCapsule_New((void *)base64_api, "librt.base64._C_API", NULL);
+    if (PyModule_Add(m, "_C_API", c_api_object) < 0) {
+        return -1;
+    }
+    return 0;
+}
+
+static PyModuleDef_Slot librt_base64_module_slots[] = {
+    {Py_mod_exec, librt_base64_module_exec},
+#ifdef Py_MOD_GIL_NOT_USED
+    {Py_mod_gil, Py_MOD_GIL_NOT_USED},
+#endif
+    {0, NULL}
+};
+
+static PyModuleDef librt_base64_module = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "base64",
+    .m_doc = "base64 encoding and decoding optimized for mypyc",
+    .m_size = 0,
+    .m_methods = librt_base64_module_methods,
+    .m_slots = librt_base64_module_slots,
+};
+
+PyMODINIT_FUNC
+PyInit_base64(void)
+{
+    return PyModuleDef_Init(&librt_base64_module);
+}
