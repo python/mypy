31773bf4dbf5c0fa8f8ccbc47e573fe1df95b2c8
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-10-21 17:23:02 +0100

    WIP benchmark

--- mypyc/build.py
+++ mypyc/build.py
@@ -588,6 +588,7 @@ def mypycify(
     build_dir = compiler_options.target_dir
 
     cflags: list[str] = []
+    opt_level = 3
     if compiler.compiler_type == "unix":
         cflags += [
             f"-O{opt_level}",
