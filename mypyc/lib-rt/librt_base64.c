8dc85c0b44a9cac9a55761808b95d52c0e18eba0
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-11-12 13:40:41 +0000

    Add initial b64encode implementation from CPython

--- mypyc/lib-rt/librt_base64.c
+++ mypyc/lib-rt/librt_base64.c
@@ -1,10 +1,74 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include "librt_base64.h"
+#include "pythoncapi_compat.h"
+
+// The b64encode_interal is lightly modified from CPython binascii module
+
+static const unsigned char table_b2a_base64[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+#define BASE64_PAD '='
+
+/* Max binary chunk size; limited only by available memory */
+#define BASE64_MAXBIN ((PY_SSIZE_T_MAX - 3) / 2)
 
 static PyObject *
 b64encode_internal(PyObject *obj) {
-    return PyBytes_FromStringAndSize("xyz", 3);
+    unsigned char *ascii_data;
+    const unsigned char *bin_data;
+    int leftbits = 0;
+    unsigned char this_ch;
+    unsigned int leftchar = 0;
+    Py_ssize_t bin_len, out_len;
+    PyBytesWriter *writer;
+    int newline = 0; // TODO
+
+    bin_data = (const unsigned char *)PyBytes_AS_STRING(obj);
+    bin_len = PyBytes_GET_SIZE(obj);
+
+    assert(bin_len >= 0);
+
+    if ( bin_len > BASE64_MAXBIN ) {
+        PyErr_SetString(PyExc_ValueError, "Too much data for base64 line");
+        return NULL;
+    }
+
+    /* We're lazy and allocate too much (fixed up later).
+       "+2" leaves room for up to two pad characters.
+       Note that 'b' gets encoded as 'Yg==\n' (1 in, 5 out). */
+    out_len = bin_len*2 + 2;
+    if (newline)
+        out_len++;
+    writer = PyBytesWriter_Create(out_len);
+    ascii_data = PyBytesWriter_GetData(writer);
+    if (writer == NULL)
+        return NULL;
+
+    for( ; bin_len > 0 ; bin_len--, bin_data++ ) {
+        /* Shift the data into our buffer */
+        leftchar = (leftchar << 8) | *bin_data;
+        leftbits += 8;
+
+        /* See if there are 6-bit groups ready */
+        while ( leftbits >= 6 ) {
+            this_ch = (leftchar >> (leftbits-6)) & 0x3f;
+            leftbits -= 6;
+            *ascii_data++ = table_b2a_base64[this_ch];
+        }
+    }
+    if ( leftbits == 2 ) {
+        *ascii_data++ = table_b2a_base64[(leftchar&3) << 4];
+        *ascii_data++ = BASE64_PAD;
+        *ascii_data++ = BASE64_PAD;
+    } else if ( leftbits == 4 ) {
+        *ascii_data++ = table_b2a_base64[(leftchar&0xf) << 2];
+        *ascii_data++ = BASE64_PAD;
+    }
+    if (newline)
+        *ascii_data++ = '\n';       /* Append a courtesy newline */
+
+    return PyBytesWriter_FinishWithSize(writer, ascii_data - (unsigned char *)PyBytesWriter_GetData(writer));
 }
 
 static PyObject*
