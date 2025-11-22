e6ac51b46853c1f9e45f0f9a1b957eaad7eabc0c
A5rocks <git@helvetica.moe>
2025-11-22 04:51:32 -0500

    Provide a fullname for `__module__`s

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -6505,7 +6505,9 @@ class SemanticAnalyzer(
                 implicit_node = node
         # 2b. Class attributes __qualname__ and __module__
         if self.type and not self.is_func_scope() and name in {"__qualname__", "__module__"}:
-            return SymbolTableNode(MDEF, Var(name, self.str_type()))
+            v = Var(name, self.str_type())
+            v._fullname = name
+            return SymbolTableNode(MDEF, v)
         # 3. Local (function) scopes
         for table in reversed(self.locals):
             if table is not None and name in table:
