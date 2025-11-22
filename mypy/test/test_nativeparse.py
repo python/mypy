cf9aefd2393c2627e7aecf297dedba6e7dc73cb5
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-11-22 14:26:51 +0000

    WIP add parse/deserialize benchmark

--- mypy/test/test_nativeparse.py
+++ mypy/test/test_nativeparse.py
@@ -8,6 +8,10 @@ from mypy import nodes
 from mypy.nativeparse import native_parse, parse_to_binary_ast
 from mypy.nodes import MypyFile
 
+import gc
+
+gc.set_threshold(200 * 1000, 30, 30)
+
 
 class TestNativeParse(unittest.TestCase):
     def test_trivial_binary_data(self) -> None:
@@ -40,6 +44,40 @@ class TestNativeParse(unittest.TestCase):
     def test_deserialize_hello(self) -> None:
         with temp_source("print('hello')") as fnam:
             node = native_parse(fnam)
+            assert isinstance(node, MypyFile)
+
+    def test_deserialize_bench(self) -> None:
+        with temp_source("print('hello')\n" * 4000) as fnam:
+            import time
+            for i in range(10):
+                native_parse(fnam)
+            t0 = time.time()
+            for i in range(25):
+                node = native_parse(fnam)
+            assert isinstance(node, MypyFile)
+            print(len(node.defs))
+            print((time.time() - t0) * 1000)
+            assert False, 1 / ((time.time() - t0) / 100000)
+
+    def test_parse_bench(self) -> None:
+        with temp_source("print('hello')\n" * 4000) as fnam:
+            import time
+            from mypy.parse import parse
+            from mypy.errors import Errors
+            from mypy.options import Options
+            o = Options()
+
+            for i in range(10):
+                with open(fnam, "rb") as f:
+                    data = f.read()
+                node = parse(data, fnam, "__main__", Errors(o), o)
+
+            t0 = time.time()
+            for i in range(25):
+                with open(fnam, "rb") as f:
+                    data = f.read()
+                node = parse(data, fnam, "__main__", Errors(o), o)
+            assert False, 1 / ((time.time() - t0) / 100000)
             assert isinstance(node, MypyFile)
 
 
