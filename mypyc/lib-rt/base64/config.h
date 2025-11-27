cb0c4eda21a35437b0f1be64f239270f5f9301cb
Michael R. Crusoe <michael.crusoe@gmail.com>
2025-11-27 12:05:22 +0100

    [mypyc] librt base64: check for SSE before enabling the rest, to fix macOS universal builds

--- mypyc/lib-rt/base64/config.h
+++ mypyc/lib-rt/base64/config.h
@@ -1,7 +1,7 @@
 #ifndef BASE64_CONFIG_H
 #define BASE64_CONFIG_H
 
-#if (defined(__x86_64__) && defined(__LP64__)) || defined(_M_X64)
+#if defined(__SSE__) && ((defined(__x86_64__) && defined(__LP64__)) || defined(_M_X64))
   #define HAVE_SSSE3 1
   #define HAVE_SSE41 1
   #define HAVE_SSE42 1
