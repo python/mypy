a6a19a96864168a00924544b417110a8544a9832
LeoLomardo <leolomardo@aluno.puc-rio.br>
2025-12-08 20:22:41 -0300

    feature: new error message for decorator incompatible type

--- mypy/messages.py
+++ mypy/messages.py
@@ -45,6 +45,7 @@ from mypy.nodes import (
     ClassDef,
     Context,
     Expression,
+    Decorator,
     FuncDef,
     IndexExpr,
     MypyFile,
@@ -772,13 +773,27 @@ class MessageBuilder:
                 actual_type_str, expected_type_str
             )
         else:
-            if self.prefer_simple_messages():
+            try:
+                expected_type = callee.arg_types[m - 1]
+            except IndexError:  # Varargs callees
+                expected_type = callee.arg_types[-1]
+
+            decorator_context = callee_name is None and isinstance(outer_context, Decorator)
+            simple_message = self.prefer_simple_messages() and not decorator_context
+
+            if decorator_context:
+                arg_type_str, expected_type_str = format_type_distinctly(
+                    arg_type, expected_type, bare=True, options=self.options
+                )
+                func_name = outer_context.func.name
+                msg = (
+                    f'Decorated function "{func_name}" has incompatible type '
+                    f'{quote_type_string(arg_type_str)}; expected '
+                    f'{quote_type_string(expected_type_str)}'
+                )
+            elif simple_message:
                 msg = "Argument has incompatible type"
             else:
-                try:
-                    expected_type = callee.arg_types[m - 1]
-                except IndexError:  # Varargs callees
-                    expected_type = callee.arg_types[-1]
                 arg_type_str, expected_type_str = format_type_distinctly(
                     arg_type, expected_type, bare=True, options=self.options
                 )
@@ -822,6 +837,7 @@ class MessageBuilder:
                         quote_type_string(arg_type_str),
                         quote_type_string(expected_type_str),
                     )
+            if not simple_message:
                 expected_type = get_proper_type(expected_type)
                 if isinstance(expected_type, UnionType):
                     expected_types = list(expected_type.items)
