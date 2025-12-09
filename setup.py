284ba5534bf0dd46c0834f8290d1c34aa402e969
Ivan Levkivskyi <levkivskyi@gmail.com>
2025-12-09 14:08:13 +0000

    Fail with an explicit error on PyPy (#20384)
    Fixes https://github.com/mypyc/librt/issues/21
    
    Fail with an explicit user-friendly error on PyPy.

--- setup.py
+++ setup.py
@@ -5,11 +5,20 @@ from __future__ import annotations
 import glob
 import os
 import os.path
+import platform
 import sys
 from typing import TYPE_CHECKING, Any
 
 if sys.version_info < (3, 10, 0):  # noqa: UP036, RUF100
     sys.stderr.write("ERROR: You need Python 3.10 or later to use mypy.\n")
+    exit(1)
+
+if platform.python_implementation() == "PyPy":
+    sys.stderr.write(
+        "ERROR: Running mypy on PyPy is not supported yet.\n"
+        "To type-check a PyPy library please use an equivalent CPython version,\n"
+        "see https://github.com/mypyc/librt/issues/16 for possible workarounds.\n"
+    )
     exit(1)
 
 # we'll import stuff from the source tree, let's ensure is on the sys path
