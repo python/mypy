8c6166a2611e633b7622519820a611593d32bbaf
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-06 06:01:24 -0400

    Update format_str_tokenizer.py

--- mypyc/irbuild/format_str_tokenizer.py
+++ mypyc/irbuild/format_str_tokenizer.py
@@ -144,11 +144,10 @@ def convert_format_expr_to_str(
     for x, format_op in zip(exprs, format_ops):
         node_type = builder.node_type(x)
         if format_op == FormatOp.STR:
-            if is_str_rprimitive(node_type) or isinstance(x, StrExpr):
-                # NOTE: why does mypyc think our fake StrExprs are not str rprimitives?
-                var_str = builder.accept(x)
-            elif (folded := constant_fold_expr(builder, x)) is not None:
+            if (folded := constant_fold_expr(builder, x)) is not None:
                 var_str = builder.accept(StrExpr(str(folded)))
+            elif is_str_rprimitive(node_type):
+                var_str = builder.accept(x)
             elif is_int_rprimitive(node_type) or is_short_int_rprimitive(node_type):
                 var_str = builder.primitive_op(int_to_str_op, [builder.accept(x)], line)
             else:
