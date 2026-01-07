5a620d0bd81e0bd84d1bde343ba434493e68f724
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2026-01-07 19:10:40 +0000

    Replace non-ascii character in test

--- mypy/test/testcheck.py
+++ mypy/test/testcheck.py
@@ -62,7 +62,7 @@ class TypeCheckSuite(DataSuite):
             with tempfile.NamedTemporaryFile(prefix="test", dir=".") as temp_file:
                 temp_path = Path(temp_file.name)
                 if not temp_path.with_name(temp_path.name.upper()).exists():
-                    pytest.skip("File system is not case‐insensitive")
+                    pytest.skip("File system is not case-insensitive")
         if lxml is None and os.path.basename(testcase.file) == "check-reports.test":
             pytest.skip("Cannot import lxml. Is it installed?")
         incremental = (
