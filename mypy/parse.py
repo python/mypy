2072f9eb3a9e9b7269769548c6b520cb41c59967
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2026-01-17 13:58:31 +0000

    WIP: basic integration of native parser

--- mypy/parse.py
+++ mypy/parse.py
@@ -1,8 +1,28 @@
 from __future__ import annotations
 
 from mypy.errors import Errors
-from mypy.nodes import MypyFile
+from mypy.nodes import MypyFile, ImportBase, Import, ImportFrom, ImportAll
 from mypy.options import Options
+from mypy.traverser import TraverserVisitor
+
+
+class ImportCollector(TraverserVisitor):
+    """Visitor that collects all import nodes from an AST."""
+
+    def __init__(self) -> None:
+        self.imports: list[ImportBase] = []
+
+    def visit_import(self, node: Import) -> None:
+        self.imports.append(node)
+        super().visit_import(node)
+
+    def visit_import_from(self, node: ImportFrom) -> None:
+        self.imports.append(node)
+        super().visit_import_from(node)
+
+    def visit_import_all(self, node: ImportAll) -> None:
+        self.imports.append(node)
+        super().visit_import_all(node)
 
 
 def parse(
@@ -20,6 +40,24 @@ def parse(
 
     The python_version (major, minor) option determines the Python syntax variant.
     """
+    if options.native_parser:
+        import mypy.nativeparse
+
+        errors.set_file(fnam, module, options=options)
+        tree, parse_errors, type_ignores = mypy.nativeparse.native_parse(fnam)
+        # Convert type ignores list to dict
+        tree.ignored_lines = dict(type_ignores)
+        # Set is_stub based on file extension
+        tree.is_stub = fnam.endswith(".pyi")
+        # Collect all import nodes from the tree
+        collector = ImportCollector()
+        tree.accept(collector)
+        tree.imports = collector.imports
+        # TODO: Report parse_errors to errors object
+        if raise_on_error and errors.is_errors():
+            errors.raise_error()
+        return tree
+
     if options.transform_source is not None:
         source = options.transform_source(source)
     import mypy.fastparse
