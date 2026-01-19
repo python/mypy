723213388a965d83643af5f842da3cb9ed1734d4
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2026-01-19 19:28:08 +0000

    WIP TEMPORARY ignore some errors caused be native parser issues

--- mypy/test/teststubgen.py
+++ mypy/test/teststubgen.py
@@ -1153,7 +1153,7 @@ class StubgencSuite(unittest.TestCase):
     def test_generate_c_type_classmethod(self) -> None:
         class TestClass:
             @classmethod
-            def test(cls, arg0: str) -> None:
+            def test(cls, arg0: str) -> None:  # type: ignore[no-redef]  # HAX
                 pass
 
         output: list[str] = []
@@ -1171,7 +1171,7 @@ class StubgencSuite(unittest.TestCase):
     def test_generate_c_type_classmethod_with_overloads(self) -> None:
         class TestClass:
             @classmethod
-            def test(cls, arg0: str) -> None:
+            def test(cls, arg0: str) -> None:  # type: ignore[no-redef]  # HAX
                 """
                 test(cls, arg0: str)
                 test(cls, arg0: int)
@@ -1370,11 +1370,11 @@ class StubgencSuite(unittest.TestCase):
                 self._attribute = 0
 
             @property
-            def attribute(self) -> int:
+            def attribute(self) -> int:  # type: ignore[no-redef]  # HAX
                 return self._attribute
 
             @attribute.setter
-            def attribute(self, value: int) -> None:
+            def attribute(self, value: int) -> None:  # type: ignore[no-redef]  # HAX
                 self._attribute = value
 
         readwrite_properties: list[str] = []
