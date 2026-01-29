8bf049f7556f188af468dd6537035e2774be7592
hauntsaninja <hauntsaninja@gmail.com>
2026-01-29 10:55:23 -0800

    .

--- mypy/checker.py
+++ mypy/checker.py
@@ -8685,7 +8685,7 @@ def flatten_types_if_tuple(t: Type) -> list[Type]:
     """Flatten a nested sequence of tuples into one list of nodes."""
     t = get_proper_type(t)
     if isinstance(t, UnionType):
-        return [b for a in t.items for b in flatten_types(a)]
+        return [b for a in t.items for b in flatten_types_if_tuple(a)]
     if isinstance(t, TupleType):
         return [b for a in t.items for b in flatten_types_if_tuple(a)]
     elif is_named_instance(t, "builtins.tuple"):
