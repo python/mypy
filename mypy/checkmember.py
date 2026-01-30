bebd6558234a7edf29040338ce999ae0f233a0fb
KevinRK29 <kevinrk2000@gmail.com>
2026-01-30 01:42:13 -0500

    added fuzzy matching for missing enum member

--- mypy/checkmember.py
+++ mypy/checkmember.py
@@ -1159,6 +1159,8 @@ def analyze_class_attribute_access(
                 return itype.extra_attrs.attrs[name]
         if info.fallback_to_any or info.meta_fallback_to_any:
             return apply_class_attr_hook(mx, hook, AnyType(TypeOfAny.special_form))
+        if info.is_enum:
+            return report_missing_attribute(mx.original_type, itype, name, mx)
         return None
 
     if (
