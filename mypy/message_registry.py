79f7b1126c0f4b24c0e86d845fd266fbd4d315ce
Kai (Kazuya Ito) <kazya.ito.dream@gmail.com>
2026-02-02 01:59:31 +0900

    Update message_registry.py

--- mypy/message_registry.py
+++ mypy/message_registry.py
@@ -312,7 +312,7 @@ ELLIPSIS_WITH_OTHER_TYPEARGS: Final = ErrorMessage(
     "Ellipses cannot accompany other parameter types in function type signature", codes.SYNTAX
 )
 TYPE_SIGNATURE_TOO_MANY_ARGS: Final = ErrorMessage(
-    "Type signature has too many arguments", codes.SYNTAX
+    "Type signature has too many parameter", codes.SYNTAX
 )
 TYPE_SIGNATURE_TOO_FEW_ARGS: Final = ErrorMessage(
     "Type signature has too few arguments", codes.SYNTAX
