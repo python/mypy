7df1be72a33345a8dc92410c7af955bbce2469f7
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2026-02-02 11:50:58 +0000

    Add missing librt_vercs_ops.py file

---
+++ mypyc/primitives/librt_vecs_ops.py
@@ -0,0 +1,13 @@
+from mypyc.ir.ops import ERR_NEVER
+from mypyc.ir.rtypes import bit_rprimitive, object_rprimitive
+from mypyc.primitives.registry import function_op
+
+# isinstance(obj, vec)
+isinstance_vec = function_op(
+    name="builtins.isinstance",
+    arg_types=[object_rprimitive],
+    return_type=bit_rprimitive,
+    c_function_name="CPyVec_Check",
+    error_kind=ERR_NEVER,
+    experimental=True,
+)
