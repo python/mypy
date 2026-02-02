77c0646ac6516bb44445b6ccf0a740f4fe218001
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2026-02-02 11:53:40 +0000

    Fix primitive dep

--- mypyc/primitives/librt_vecs_ops.py
+++ mypyc/primitives/librt_vecs_ops.py
@@ -1,3 +1,4 @@
+from mypyc.ir.deps import LIBRT_VECS
 from mypyc.ir.ops import ERR_NEVER
 from mypyc.ir.rtypes import bit_rprimitive, object_rprimitive
 from mypyc.primitives.registry import function_op
@@ -10,4 +11,5 @@ isinstance_vec = function_op(
     c_function_name="CPyVec_Check",
     error_kind=ERR_NEVER,
     experimental=True,
+    dependencies=[LIBRT_VECS],
 )
