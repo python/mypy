7a45cc543101a393261eff4787de32d67223aed9
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2026-02-11 11:41:34 +0000

    Update apply-cache-diff.py to support .ff cache files

--- misc/apply-cache-diff.py
+++ misc/apply-cache-diff.py
@@ -3,16 +3,22 @@
 
 With some infrastructure, this can allow for distributing small cache diffs to users in
 many cases instead of full cache artifacts.
+
+Use diff-cache.py to generate a cache diff.
 """
 
 from __future__ import annotations
 
 import argparse
+import base64
 import os
 import sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from librt.internal import ReadBuffer
+
+from mypy.cache import CacheMeta
 from mypy.metastore import FilesystemMetadataStore, MetadataStore, SqliteMetadataStore
 from mypy.util import json_dumps, json_loads
 
@@ -35,11 +41,19 @@ def apply_diff(cache_dir: str, diff_file: str, sqlite: bool = False) -> None:
         if data is None:
             cache.remove(file)
         else:
-            data_bytes = data.encode() if isinstance(data, str) else data
+            if file.endswith(".ff"):
+                data_bytes = base64.b64decode(data)
+            else:
+                data_bytes = data.encode() if isinstance(data, str) else data
             cache.write(file, data_bytes)
-            if file.endswith(".meta.json") and "@deps" not in file:
-                meta = json_loads(data_bytes)
-                old_deps["snapshot"][meta["id"]] = meta["hash"]
+            if file.endswith(".meta.ff"):
+                buf = ReadBuffer(data_bytes[2:])
+                meta = CacheMeta.read(buf, data_file="")
+                assert meta is not None
+                old_deps["snapshot"][meta.id] = meta.hash
+            elif file.endswith(".meta.json") and "@deps" not in file:
+                meta_dict = json_loads(data_bytes)
+                old_deps["snapshot"][meta_dict["id"]] = meta_dict["hash"]
 
     cache.write("@deps.meta.json", json_dumps(old_deps))
 
