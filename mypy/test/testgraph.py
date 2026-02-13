5ab2d1ffa654ec7084173876f95579cbd99e0de0
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2026-02-13 15:21:38 +0000

    Rename topsort2 to topsort, and remove the old function
    I've verified that the topological sort is no longer a significant
    CPU bottleneck when using the new function.
    
    I used codex to validate and update the docstring.

--- mypy/test/testgraph.py
+++ mypy/test/testgraph.py
@@ -8,7 +8,7 @@ from collections.abc import Set as AbstractSet
 from mypy.build import BuildManager, BuildSourceSet, State, order_ascc, sorted_components
 from mypy.errors import Errors
 from mypy.fscache import FileSystemCache
-from mypy.graph_utils import strongly_connected_components, topsort, topsort2
+from mypy.graph_utils import strongly_connected_components, topsort
 from mypy.modulefinder import SearchPaths
 from mypy.options import Options
 from mypy.plugin import Plugin
@@ -20,75 +20,63 @@ from mypy.version import __version__
 class GraphSuite(Suite):
     def test_topsort_empty(self) -> None:
         data: dict[AbstractSet[str], set[AbstractSet[str]]] = {}
-        assert_equal(list(topsort2(data)), [])
+        assert_equal(list(topsort(data)), [])
 
     def test_topsort(self) -> None:
-        for topsort_func in [topsort, topsort2]:
-            a = frozenset({"A"})
-            b = frozenset({"B"})
-            c = frozenset({"C"})
-            d = frozenset({"D"})
-            data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b, c}, b: {d}, c: {d}}
-            res = list(topsort_func(data))
-            assert_equal(res, [{d}, {b, c}, {a}])
+        a = frozenset({"A"})
+        b = frozenset({"B"})
+        c = frozenset({"C"})
+        d = frozenset({"D"})
+        data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b, c}, b: {d}, c: {d}}
+        res = list(topsort(data))
+        assert_equal(res, [{d}, {b, c}, {a}])
 
     def test_topsort_orphan(self) -> None:
-        for topsort_func in [topsort, topsort2]:
-            a = frozenset({"A"})
-            b = frozenset({"B"})
-            data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b}}
-            res = list(topsort_func(data))
-            assert_equal(res, [{b}, {a}])
+        a = frozenset({"A"})
+        b = frozenset({"B"})
+        data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b}}
+        res = list(topsort(data))
+        assert_equal(res, [{b}, {a}])
 
     def test_topsort_independent(self) -> None:
-        for topsort_func in [topsort, topsort2]:
-            a = frozenset({"A"})
-            b = frozenset({"B"})
-            c = frozenset({"C"})
-            data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: set(), b: set(), c: set()}
-            res = list(topsort_func(data))
-            assert_equal(res, [{a, b, c}])
+        a = frozenset({"A"})
+        b = frozenset({"B"})
+        c = frozenset({"C"})
+        data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: set(), b: set(), c: set()}
+        res = list(topsort(data))
+        assert_equal(res, [{a, b, c}])
 
     def test_topsort_linear_chain(self) -> None:
-        for topsort_func in [topsort, topsort2]:
-            a = frozenset({"A"})
-            b = frozenset({"B"})
-            c = frozenset({"C"})
-            d = frozenset({"D"})
-            data: dict[AbstractSet[str], set[AbstractSet[str]]] = {
-                a: {b},
-                b: {c},
-                c: {d},
-                d: set(),
-            }
-            res = list(topsort_func(data))
-            assert_equal(res, [{d}, {c}, {b}, {a}])
+        a = frozenset({"A"})
+        b = frozenset({"B"})
+        c = frozenset({"C"})
+        d = frozenset({"D"})
+        data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b}, b: {c}, c: {d}, d: set()}
+        res = list(topsort(data))
+        assert_equal(res, [{d}, {c}, {b}, {a}])
 
     def test_topsort_self_dependency(self) -> None:
-        for topsort_func in [topsort, topsort2]:
-            a = frozenset({"A"})
-            b = frozenset({"B"})
-            data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {a, b}, b: set()}
-            res = list(topsort_func(data))
-            assert_equal(res, [{b}, {a}])
+        a = frozenset({"A"})
+        b = frozenset({"B"})
+        data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {a, b}, b: set()}
+        res = list(topsort(data))
+        assert_equal(res, [{b}, {a}])
 
     def test_topsort_orphan_diamond(self) -> None:
-        for topsort_func in [topsort, topsort2]:
-            a = frozenset({"A"})
-            b = frozenset({"B"})
-            c = frozenset({"C"})
-            # B and C are orphans -- they appear only in values, not as keys.
-            data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b, c}}
-            res = list(topsort_func(data))
-            assert_equal(res, [{b, c}, {a}])
+        a = frozenset({"A"})
+        b = frozenset({"B"})
+        c = frozenset({"C"})
+        # B and C are orphans -- they appear only in values, not as keys.
+        data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b, c}}
+        res = list(topsort(data))
+        assert_equal(res, [{b, c}, {a}])
 
     def test_topsort_cycle(self) -> None:
-        for topsort_func in [topsort, topsort2]:
-            a = frozenset({"A"})
-            b = frozenset({"B"})
-            data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b}, b: {a}}
-            with self.assertRaises(AssertionError):
-                list(topsort_func(data))
+        a = frozenset({"A"})
+        b = frozenset({"B"})
+        data: dict[AbstractSet[str], set[AbstractSet[str]]] = {a: {b}, b: {a}}
+        with self.assertRaises(AssertionError):
+            list(topsort(data))
 
     def test_scc(self) -> None:
         vertices = {"A", "B", "C", "D"}
