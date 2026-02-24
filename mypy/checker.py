python/mypy

commit 270a01d44cdf5f27b8e8328e94452b2a5332a5a2

Fix loop convergence with redefinitions (#20865)
Currently, we reset the variable type each time we visit the initial
definition (textually first assignment). This can cause problems with
`--allow-redefinition-new`:
* Not all branches will be explored (see added test). This is a
correctness problem.
* Many loops will become runaway (will never converge to a fixed point)
while they actually can converge. Currently this is stopped by manual
cut-off. This is a performance problem.

The only non-trivial part here is handling of this pattern:
```python
for key in ("key_a", "key_b"):
    some_typed_dict[key]
```
It is special-cased in regular mode, and this special-casing used to
work accidentally with redefinition allowed. Now I simply make the same
special-casing apply (since for loops can never be runaway on index).

mypy/checker.py
@@ -4401,8 +4401,23 @@ class TypeChecker(NodeVisitor[None], TypeCheckerSharedApi):
         index_lvalue = None
         inferred = None
 
-        if self.is_definition(lvalue) and (
-            not isinstance(lvalue, NameExpr) or isinstance(lvalue.node, Var)
+        # When revisiting the initial assignment (for example in a loop),
+        # treat is as regular if redefinitions are allowed.
+        skip_definition = (
+            self.options.allow_redefinition_new
+            and isinstance(lvalue, NameExpr)
+            and isinstance(lvalue.node, Var)
+            and lvalue.node.is_inferred
+            and lvalue.node.type is not None
+            # Indexes in for loops require special handling, we need to reset them to
+            # a literal value on each loop, but binder doesn't work well with literals.
+            and not lvalue.node.is_index_var
+        )
+
+        if (
+            self.is_definition(lvalue)
+            and (not isinstance(lvalue, NameExpr) or isinstance(lvalue.node, Var))
+            and not skip_definition
         ):
             if isinstance(lvalue, NameExpr):
                 assert isinstance(lvalue.node, Var)
@@ -4784,7 +4799,7 @@ class TypeChecker(NodeVisitor[None], TypeCheckerSharedApi):
                     # This additional check is to give an error instead of inferring
                     # a useless type like None | list[Never] in case of "double-partial"
                     # types that are not supported yet, see issue #20257.
-                    or not is_proper_subtype(rvalue_type, inferred.type)
+                    or not is_subtype(rvalue_type, inferred.type)
                 )
             ):
                 self.msg.need_annotation_for_var(inferred, inferred, self.options)
@@ -4807,7 +4822,9 @@ class TypeChecker(NodeVisitor[None], TypeCheckerSharedApi):
                 ):
                     lvalue_type = make_simplified_union([inferred.type, new_inferred])
                     # Widen the type to the union of original and new type.
-                    self.widened_vars.append(inferred.name)
+                    if not inferred.is_index_var:
+                        # Skip index variables as they are reset on each loop.
+                        self.widened_vars.append(inferred.name)
                     self.set_inferred_type(inferred, lvalue, lvalue_type)
                     self.binder.put(lvalue, rvalue_type)
                     # TODO: A bit hacky, maybe add a binder method that does put and

test-data/unit/check-redefine2.test
@@ -1098,6 +1098,33 @@ def func() -> None:
     reveal_type(l) # N: Revealed type is "None | builtins.list[Any]"
 [builtins fixtures/list.pyi]
 
+[case testNewRedefineLoopProgressiveWidening]
+# flags: --allow-redefinition-new --local-partial-types
+def foo() -> None:
+    while int():
+        if int():
+            x = 1
+        elif isinstance(x, list):
+            x = ""
+        elif isinstance(x, str):
+            x = None
+        else:
+            x = [1]
+    reveal_type(x)  # N: Revealed type is "builtins.int | builtins.list[builtins.int] | builtins.str | None"
+[builtins fixtures/isinstancelist.pyi]
+
+[case testNewRedefineAnyOrEmptyUnpackInLoop]
+# flags: --allow-redefinition-new --local-partial-types
+from typing import Any
+
+def foo() -> None:
+    while int():
+        data: Any
+        x, y = data or ([], [])
+        # Requiring an annotation may be better, but we want to preserve old behaviour.
+        reveal_type(x)  # N: Revealed type is "Any | builtins.list[Never]"
+[builtins fixtures/isinstancelist.pyi]
+
 [case testNewRedefineNarrowingSpecialCase]
 # flags: --allow-redefinition-new --local-partial-types --warn-unreachable
 from typing import Any, Union

commit 1e0ee99ced387cd7ef3f8e18cbb886b73f68d84f

Fix regression on redefinition in deferred loop (#20879)
Trivial fix for a regression introduced in
https://github.com/python/mypy/pull/20865, a partial type is usually the
same as no type at all.

mypy/checker.py
@@ -4409,6 +4409,7 @@ class TypeChecker(NodeVisitor[None], TypeCheckerSharedApi):
             and isinstance(lvalue.node, Var)
             and lvalue.node.is_inferred
             and lvalue.node.type is not None
+            and not isinstance(lvalue.node.type, PartialType)
             # Indexes in for loops require special handling, we need to reset them to
             # a literal value on each loop, but binder doesn't work well with literals.
             and not lvalue.node.is_index_var

test-data/unit/check-redefine2.test
@@ -1141,6 +1141,23 @@ def f() -> None:
         reveal_type(x) # N: Revealed type is "Any"
 [builtins fixtures/tuple.pyi]
 
+[case testNewRedefinitionPartialTypeInLoopDeferred]
+# flags: --allow-redefinition-new --local-partial-types
+
+def f(self) -> None:
+    x = []
+    for i in [1, 2, 3]:
+        C().x
+        x.append(1)
+    reveal_type(x)  # N: Revealed type is "builtins.list[builtins.int]"
+
+class C:
+    def __init__(self) -> None:
+        self.x = defer()
+
+def defer() -> int: ...
+[builtins fixtures/list.pyi]
+
 [case testNewRedefinePartialTypeForUnderscore]
 # flags: --allow-redefinition-new --local-partial-types
 