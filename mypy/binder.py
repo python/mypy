python/mypy

commit 94bf64bb8a425d0c6d93ad56409f66453f16303e

Do not repor trivial narrowing as a change in binder (#20862)
Fixes https://github.com/python/mypy/issues/20816

This makes `--allow-redefinition-new` ~10% faster (or maybe more if you
have too many loops in your code). The idea is quite simple: there is no
difference between no narrowing, and narrowing to type that is a
variable declared type. This doesn't really make a difference for
regular mode, IIUC because there is a "fall through", i.e.
zero-iterations case, but this doesn't work with `bind_all = True`. As a
result we used to accept _every_ loop with an assignment in it at lest
twice with `--allow-redefinition-new` (which includes assignment to
iteration variable in for-loop).

Note that `declaration_type` may be `None` if there is a partial type,
but we already track partial types in accept loop separately, so we
don't need to report them as changed here as well.

mypy/binder.py
@@ -392,7 +392,22 @@ class ConditionalTypeBinder:
                             type = simplified
             if current_value is None or not is_same_type(type, current_value.type):
                 self._put(key, type, from_assignment=True)
-                changed = True
+                if current_value is not None or extract_var_from_literal_hash(key) is None:
+                    # We definitely learned something new
+                    changed = True
+                else:
+                    # If there is no current value compare with the declaration. This prevents
+                    # reporting false changes in cases like this:
+                    #     x: int
+                    #     if foo():
+                    #         x = 1
+                    #     else:
+                    #         x = 2
+                    # We check partial types and widening in accept_loop() separately, so
+                    # this should be safe.
+                    changed = declaration_type is not None and not is_same_type(
+                        type, declaration_type
+                    )
 
         self.frames[-1].unreachable = not frames
 

commit 6d723a82604ddab246e07c47e66927d23e70ec0c

Fix changed on pop with multiple variables in binder (#20892)
Fixes an obvious regression introduced in
https://github.com/python/mypy/pull/20862 (frame pop changed if at least
one variable changed).

mypy/binder.py
@@ -395,7 +395,7 @@ class ConditionalTypeBinder:
                 if current_value is not None or extract_var_from_literal_hash(key) is None:
                     # We definitely learned something new
                     changed = True
-                else:
+                elif not changed:
                     # If there is no current value compare with the declaration. This prevents
                     # reporting false changes in cases like this:
                     #     x: int

test-data/unit/check-redefine2.test
@@ -1158,6 +1158,18 @@ class C:
 def defer() -> int: ...
 [builtins fixtures/list.pyi]
 
+[case testNewRedefineCorrectChangedOnPop]
+# flags: --allow-redefinition-new --local-partial-types
+from typing import Optional
+
+def foo() -> None:
+    name: Optional[str] = None
+    name = None
+    for i in [1, 2, 3]:
+        reveal_type(name)  # N: Revealed type is "None | builtins.str"
+        if bool():
+            name = "a"
+
 [case testNewRedefinePartialTypeForUnderscore]
 # flags: --allow-redefinition-new --local-partial-types
 