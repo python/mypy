afb99ebbc180153ee7e6acd052797f38469493b1
Ivan Levkivskyi <levkivskyi@gmail.com>
2026-02-27 12:06:33 +0000

    Fix union edge case in function argument redefinition (#20908)
    Logic is simple: inference for function arguments should match inference
    for regular annotated variables exactly, including falling back to empty
    context in _union context_ (even if there are no errors) provided this
    results in a narrower type.

--- mypy/checker.py
+++ mypy/checker.py
@@ -4725,7 +4725,7 @@ class TypeChecker(NodeVisitor[None], TypeCheckerSharedApi):
         # and use it results in a narrower type. This helps with various practical
         # examples, see e.g. testOptionalTypeNarrowedByGenericCall.
         union_fallback = (
-            inferred is None
+            preferred_context is not None
             and isinstance(get_proper_type(lvalue_type), UnionType)
             and binder_version == self.binder.version
         )
@@ -4744,7 +4744,7 @@ class TypeChecker(NodeVisitor[None], TypeCheckerSharedApi):
                 not alt_local_errors.has_new_errors()
                 and is_valid_inferred_type(alt_rvalue_type, self.options)
                 and (
-                    # For redefinition fallback we are fine getting not a subtype.
+                    # For redefinition fallbacks we are fine getting not a subtype.
                     redefinition_fallback
                     or argument_redefinition_fallback
                     # Skip Any type, since it is special cased in binder.
