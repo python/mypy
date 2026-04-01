2ccc6558b818f691fb69fd4f1e284cf457383a1b
mypybot <>
2026-04-01 00:14:53 +0000

    Sync typeshed
    Source commit:
    https://github.com/python/typeshed/commit/c5e47faeda2cf9d233f91bc1dc95814b0cc7ccba

--- mypy/typeshed/stdlib/_sqlite3.pyi
+++ mypy/typeshed/stdlib/_sqlite3.pyi
@@ -19,7 +19,7 @@ from sqlite3 import (
     _IsolationLevel,
 )
 from typing import Any, Final, Literal, TypeVar, overload
-from typing_extensions import TypeAlias
+from typing_extensions import TypeAlias, deprecated
 
 if sys.version_info >= (3, 11):
     from sqlite3 import Blob as Blob
@@ -299,7 +299,11 @@ def enable_callback_tracebacks(enable: bool, /) -> None: ...
 
 if sys.version_info < (3, 12):
     # takes a pos-or-keyword argument because there is a C wrapper
-    def enable_shared_cache(do_enable: int) -> None: ...
+    @deprecated(
+        "Deprecated since Python 3.10; removed in Python 3.12. "
+        "Open database in URI mode using `cache=shared` parameter instead."
+    )
+    def enable_shared_cache(do_enable: int) -> None: ...  # undocumented
 
 if sys.version_info >= (3, 10):
     def register_adapter(type: type[_T], adapter: _Adapter[_T], /) -> None: ...
@@ -310,4 +314,4 @@ else:
     def register_converter(name: str, converter: _Converter, /) -> None: ...
 
 if sys.version_info < (3, 10):
-    OptimizedUnicode = str
+    OptimizedUnicode = str  # undocumented
