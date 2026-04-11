e4f856e43d3afd6e3134e6982d83575e15c873a7
Ivan Levkivskyi <levkivskyi@gmail.com>
2026-04-11 16:28:39 +0100

    Fix sqlite flakiness

--- mypy/metastore.py
+++ mypy/metastore.py
@@ -157,7 +157,7 @@ CREATE INDEX IF NOT EXISTS path_idx on files2(path);
 def connect_db(db_file: str) -> sqlite3.Connection:
     import sqlite3.dbapi2
 
-    db = sqlite3.dbapi2.connect(db_file)
+    db = sqlite3.dbapi2.connect(db_file, check_same_thread=False)
     # This is a bit unfortunate (as we may get corrupt cache after e.g. Ctrl + C),
     # but without this flag, commits are *very* slow, especially when using HDDs,
     # see https://www.sqlite.org/faq.html#q19 for details.
