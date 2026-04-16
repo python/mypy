692fbb7dbdb356687a010bd4f8a37bfce4d1a451
hauntsaninja <hauntsaninja@gmail.com>
2026-04-15 20:48:39 -0700

    [mypyc] Fix refleak in str index
    Found by @devdanzin

--- mypyc/lib-rt/str_ops.c
+++ mypyc/lib-rt/str_ops.c
@@ -131,7 +131,12 @@ PyObject *CPyStr_GetItem(PyObject *str, CPyTagged index) {
         }
     } else {
         PyObject *index_obj = CPyTagged_AsObject(index);
-        return PyObject_GetItem(str, index_obj);
+        if (index_obj == NULL) {
+            return NULL;
+        }
+        PyObject *result = PyObject_GetItem(str, index_obj);
+        Py_DECREF(index_obj);
+        return result;
     }
 }
 
