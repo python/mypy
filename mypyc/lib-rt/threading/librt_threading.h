ce9b478141207e2d5528f88ef4eb036709c3886f
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2026-04-27 14:11:56 +0100

    Use the more recent approach for defining librt submodules

--- mypyc/lib-rt/threading/librt_threading.h
+++ mypyc/lib-rt/threading/librt_threading.h
@@ -1,66 +1,10 @@
 #ifndef LIBRT_THREADING_H
 #define LIBRT_THREADING_H
 
-#ifndef MYPYC_EXPERIMENTAL
-
-static int
-import_librt_threading(void)
-{
-    // All librt.threading features are experimental for now, so don't set up the API here
-    return 0;
-}
-
-#else  // MYPYC_EXPERIMENTAL
-
 #include <Python.h>
 
 #define LIBRT_THREADING_ABI_VERSION 1
 #define LIBRT_THREADING_API_VERSION 2
 #define LIBRT_THREADING_API_LEN 7
-
-static void *LibRTThreading_API[LIBRT_THREADING_API_LEN];
-
-#define LibRTThreading_ABIVersion (*(int (*)(void)) LibRTThreading_API[0])
-#define LibRTThreading_APIVersion (*(int (*)(void)) LibRTThreading_API[1])
-#define LibRTThreading_Lock_type_internal (*(PyTypeObject* (*)(void)) LibRTThreading_API[2])
-#define LibRTThreading_Lock_new_internal (*(PyObject* (*)(void)) LibRTThreading_API[3])
-#define LibRTThreading_Lock_acquire_internal (*(char (*)(PyObject *self)) LibRTThreading_API[4])
-#define LibRTThreading_Lock_release_internal (*(char (*)(PyObject *self)) LibRTThreading_API[5])
-#define LibRTThreading_Lock_locked_internal (*(char (*)(PyObject *self)) LibRTThreading_API[6])
-
-static int
-import_librt_threading(void)
-{
-    PyObject *mod = PyImport_ImportModule("librt.threading");
-    if (mod == NULL)
-        return -1;
-    Py_DECREF(mod);  // we import just for the side effect of making the below work.
-    void *capsule = PyCapsule_Import("librt.threading._C_API", 0);
-    if (capsule == NULL)
-        return -1;
-    memcpy(LibRTThreading_API, capsule, sizeof(LibRTThreading_API));
-    if (LibRTThreading_ABIVersion() != LIBRT_THREADING_ABI_VERSION) {
-        char err[128];
-        snprintf(err, sizeof(err), "ABI version conflict for librt.threading, expected %d, found %d",
-            LIBRT_THREADING_ABI_VERSION,
-            LibRTThreading_ABIVersion()
-        );
-        PyErr_SetString(PyExc_ValueError, err);
-        return -1;
-    }
-    if (LibRTThreading_APIVersion() < LIBRT_THREADING_API_VERSION) {
-        char err[128];
-        snprintf(err, sizeof(err),
-                 "API version conflict for librt.threading, expected %d or newer, found %d (hint: upgrade librt)",
-            LIBRT_THREADING_API_VERSION,
-            LibRTThreading_APIVersion()
-        );
-        PyErr_SetString(PyExc_ValueError, err);
-        return -1;
-    }
-    return 0;
-}
-
-#endif  // MYPYC_EXPERIMENTAL
 
 #endif  // LIBRT_THREADING_H
