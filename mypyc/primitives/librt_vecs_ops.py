41c1499c434a8da00a06c51e83cb710deb914b2b
Jukka Lehtosalo <jukka@dropbox.com>
2026-04-28 13:43:06 +0100

    [mypyc] Add primitive for vec[u8] to bytes conversion

--- mypyc/primitives/librt_vecs_ops.py
+++ mypyc/primitives/librt_vecs_ops.py
@@ -1,6 +1,12 @@
 from mypyc.ir.deps import LIBRT_VECS, VECS_EXTRA_OPS
-from mypyc.ir.ops import ERR_NEVER
-from mypyc.ir.rtypes import bit_rprimitive, object_rprimitive
+from mypyc.ir.ops import ERR_MAGIC, ERR_NEVER
+from mypyc.ir.rtypes import (
+    RVec,
+    bit_rprimitive,
+    bytes_rprimitive,
+    object_rprimitive,
+    uint8_rprimitive,
+)
 from mypyc.primitives.registry import function_op
 
 # isinstance(obj, vec)
@@ -10,6 +16,17 @@ isinstance_vec = function_op(
     return_type=bit_rprimitive,
     c_function_name="CPyVec_Check",
     error_kind=ERR_NEVER,
+    experimental=True,
+    dependencies=[LIBRT_VECS, VECS_EXTRA_OPS],
+)
+
+# bytes(vec[u8])
+function_op(
+    name="builtins.bytes",
+    arg_types=[RVec(uint8_rprimitive)],
+    return_type=bytes_rprimitive,
+    c_function_name="CPyVecU8_ToBytes",
+    error_kind=ERR_MAGIC,
     experimental=True,
     dependencies=[LIBRT_VECS, VECS_EXTRA_OPS],
 )
