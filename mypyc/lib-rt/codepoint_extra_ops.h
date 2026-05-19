739a65244eb085703bc52e26326bd3a034e8076c
Vaggelis Danias <daniasevangelos@gmail.com>
2026-05-19 16:52:35 +0300

    [mypyc] Add `librt.strings.isalnum` codepoint primitive (#21509)
    3rd PR for https://github.com/python/mypy/issues/21418, mirroring
    `librt.strings.isdigit`.
    
    Measured on a microbenchmark this is roughly 30-40% faster for a char

--- mypyc/lib-rt/codepoint_extra_ops.h
+++ mypyc/lib-rt/codepoint_extra_ops.h
@@ -17,4 +17,8 @@ static inline bool LibRTStrings_IsDigit(int32_t c) {
     return c >= 0 && Py_UNICODE_ISDIGIT((Py_UCS4)c);
 }
 
+static inline bool LibRTStrings_IsAlnum(int32_t c) {
+    return c >= 0 && Py_UNICODE_ISALNUM((Py_UCS4)c);
+}
+
 #endif  // MYPYC_CODEPOINT_EXTRA_OPS_H
