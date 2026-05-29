7a42fa5e293264f7075f3ae46860effb0da91420
Kevin Kannammalil <kevinrk2000@gmail.com>
2026-05-29 02:02:59 -0400

    switch to CALL_ARG

--- mypy/messages.py
+++ mypy/messages.py
@@ -970,7 +970,7 @@ class MessageBuilder:
             msg = "Too many positional arguments"
         else:
             msg = "Too many positional arguments" + for_function(callee)
-        self.fail(msg, context)
+        self.fail(msg, context, code=codes.CALL_ARG)
         self.maybe_note_about_special_args(callee, context)
         self.note_defined_here(callee, context)
 
