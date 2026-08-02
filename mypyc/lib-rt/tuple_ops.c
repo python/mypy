466060c6357f921237bf37f5802fe083d3eb7a55
dnwpark <daniel.park@vercel.com>
2026-08-01 23:34:23 -0700

    Inline tuple ops.

--- mypyc/lib-rt/tuple_ops.c
+++ mypyc/lib-rt/tuple_ops.c
@@ -5,31 +5,6 @@
 #include <Python.h>
 #include "CPy.h"
 
-PyObject *CPySequenceTuple_GetItem(PyObject *tuple, CPyTagged index) {
-    if (CPyTagged_CheckShort(index)) {
-        Py_ssize_t n = CPyTagged_ShortAsSsize_t(index);
-        Py_ssize_t size = PyTuple_GET_SIZE(tuple);
-        if (n >= 0) {
-            if (n >= size) {
-                PyErr_SetString(PyExc_IndexError, "tuple index out of range");
-                return NULL;
-            }
-        } else {
-            n += size;
-            if (n < 0) {
-                PyErr_SetString(PyExc_IndexError, "tuple index out of range");
-                return NULL;
-            }
-        }
-        PyObject *result = PyTuple_GET_ITEM(tuple, n);
-        Py_INCREF(result);
-        return result;
-    } else {
-        PyErr_SetString(PyExc_OverflowError, CPYTHON_LARGE_INT_ERRMSG);
-        return NULL;
-    }
-}
-
 PyObject *CPySequenceTuple_GetSlice(PyObject *obj, CPyTagged start, CPyTagged end) {
     if (likely(PyTuple_CheckExact(obj)
                && CPyTagged_CheckShort(start) && CPyTagged_CheckShort(end))) {
@@ -44,19 +19,4 @@ PyObject *CPySequenceTuple_GetSlice(PyObject *obj, CPyTagged start, CPyTagged en
         return PyTuple_GetSlice(obj, startn, endn);
     }
     return CPyObject_GetSlice(obj, start, end);
-}
-
-// No error checking
-PyObject *CPySequenceTuple_GetItemUnsafe(PyObject *tuple, Py_ssize_t index)
-{
-    PyObject *result = PyTuple_GET_ITEM(tuple, index);
-    Py_INCREF(result);
-    return result;
-}
-
-// PyTuple_SET_ITEM does no error checking,
-// and should only be used to fill in brand new tuples.
-void CPySequenceTuple_SetItemUnsafe(PyObject *tuple, Py_ssize_t index, PyObject *value)
-{
-    PyTuple_SET_ITEM(tuple, index, value);
 }
