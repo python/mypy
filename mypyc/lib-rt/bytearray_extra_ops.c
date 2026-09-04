09d653cad348c7e7a39560d75c6b07aba462b04e
hauntsaninja <hauntsaninja@gmail.com>
2026-09-04 01:27:58 -0700

    address

--- mypyc/lib-rt/bytearray_extra_ops.c
+++ mypyc/lib-rt/bytearray_extra_ops.c
@@ -11,6 +11,12 @@ PyObject *CPyByteArray_FromBytesSlice(PyObject *obj, CPyTagged start, CPyTagged 
         Py_ssize_t size = PyBytes_GET_SIZE(obj);
         Py_ssize_t startn = start == CPY_INT_TAG ? 0 : CPyTagged_ShortAsSsize_t(start);
         Py_ssize_t endn = end == CPY_INT_TAG ? size : CPyTagged_ShortAsSsize_t(end);
+        if (startn < 0) {
+            startn += size;
+        }
+        if (endn < 0) {
+            endn += size;
+        }
         if (0 <= startn && startn <= endn && endn <= size) {
             return PyByteArray_FromStringAndSize(PyBytes_AS_STRING(obj) + startn, endn - startn);
         }
