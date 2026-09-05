3288ae8a512eff03e5b98f17c7e8fbe9d77ef25d
Dibri Nsofor <dibrinsofor@gmail.com>
2026-09-04 21:58:47 -0500

    threading through chain api
    Signed-off-by: Dibri Nsofor <dibrinsofor@gmail.com>

--- mypy/plugin.py
+++ mypy/plugin.py
@@ -923,6 +923,9 @@ class ChainedPlugin(Plugin):
     def get_function_hook(self, fullname: str) -> Callable[[FunctionContext], Type] | None:
         return self._find_hook(lambda plugin: plugin.get_function_hook(fullname))
 
+    def get_function_def_hook(self, fullname:str) -> Callable[[FunctionDefContext], FunctionDefHookResult] | None:
+        return self._find_hook(lambda plugin: plugin.get_function_def_hook(fullname))
+
     def get_method_signature_hook(
         self, fullname: str
     ) -> Callable[[MethodSigContext], FunctionLike] | None:
