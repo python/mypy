b72b92b45a477e4781a27ad73dde52dbe6f5aa0b
Kevin Kannammalil <kevinrk2000@gmail.com>
2026-09-14 12:34:48 -0400

    add constant documentation

--- mypy/defaults.py
+++ mypy/defaults.py
@@ -47,6 +47,8 @@ MANY_ERRORS_THRESHOLD: Final = -1
 
 RECURSION_LIMIT: Final = 2**14
 
+# Cap the automatic selection of workers since each worker adds roughly 10% of memory overhead.
+# Users can specify an explicit --num-workers value which can exceed this limit.
 MAX_AUTO_WORKERS: Final = 8
 
 # It looks like Windows is slow with processes, causing test flakiness even
