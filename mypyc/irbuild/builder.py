python/mypy

commit 4fa665256c52b09ff20fb91af45dfa005f8a27e8

[mypyc] Keep generator local state in C locals when possible (#21982)
Initially generate IR for generators and coroutines where locals and
temporaries are stored in registers. Only spill registers that are live
across a yield/await to the generator object. Previously most registers
were stored in the generator object as attributes. The new spill is
implemented as a new transform. We still use the old spill transform for
op results that are not stored in registers.

This makes microbenchmarks where most work happens between awaits
significantly faster, as C locals can be used for many operations
instead of attributes. This also reduces the size of generator objects,
as fewer struct fields are needed.

Remove old spill-related helpers from the IR builder as unnecessary.

I used coding agent assist.

mypyc/irbuild/builder.py
@@ -75,7 +75,6 @@ from mypyc.common import (
     KEEP_ALIVE_WHOLE_EXPRESSION,
     MODULE_PREFIX,
     SELF_NAME,
-    TEMP_ATTR_NAME,
     shared_lib_name,
 )
 from mypyc.crash import catch_errors
@@ -152,7 +151,7 @@ from mypyc.irbuild.targets import (
     AssignmentTargetRegister,
     AssignmentTargetTuple,
 )
-from mypyc.irbuild.util import bytes_from_str, is_constant
+from mypyc.irbuild.util import bytes_from_str, get_func_def, is_constant
 from mypyc.irbuild.vec import vec_set_item
 from mypyc.namegen import exported_name
 from mypyc.options import CompilerOptions
@@ -249,11 +248,8 @@ class IRBuilder:
         self.callable_class_names: set[str] = set()
         self.options = options
 
-        # These variables keep track of the number of lambdas, implicit indices, and implicit
-        # iterators instantiated so we avoid name conflicts. The indices and iterators are
-        # instantiated from for-loops.
+        # Keep track of the number of lambdas instantiated so we avoid name conflicts.
         self.lambda_counter = 0
-        self.temp_counter = 0
 
         # These variables are populated from the first-pass PreBuildVisitor.
         self.free_variables = pbv.free_variables
@@ -263,6 +259,7 @@ class IRBuilder:
         self.fdefs_to_decorators = pbv.funcs_to_decorators
         self.module_import_groups = pbv.module_import_groups
         self.comprehension_to_fitem = pbv.comprehension_to_fitem
+        self.deleted_vars = pbv.deleted_vars
 
         self.singledispatch_impls = singledispatch_impls
 
@@ -749,11 +746,11 @@ class IRBuilder:
         if line == -1:
             line = lvalue.line
         if isinstance(lvalue, NameExpr):
-            # If we are visiting a decorator, then the SymbolNode we really want to be looking at
-            # is the function that is decorated, not the entire Decorator node itself.
+            # Use the concrete implementation as the symbol-table key for
+            # decorated and overloaded functions.
             symbol = lvalue.node
-            if isinstance(symbol, Decorator):
-                symbol = symbol.func
+            if isinstance(symbol, Decorator | OverloadedFuncDef):
+                symbol = get_func_def(symbol)
             if symbol is None:
                 # Semantic analyzer doesn't create ad-hoc Vars for special forms.
                 assert lvalue.is_special_form
@@ -766,12 +763,14 @@ class IRBuilder:
                         reg_type = self.type_to_rtype(symbol.type)
                     else:
                         reg_type = self.node_type(lvalue)
-                    # If the function is a generator function, then first define a new variable
-                    # in the current function's environment class. Next, define a target that
-                    # refers to the newly defined variable in that environment class. Add the
-                    # target to the table containing class environment variables, as well as the
-                    # current environment.
-                    if self.fn_info.is_generator or self.fn_info.is_coroutine:
+                    # A deleted error-overlap value needs the environment's
+                    # definedness bitmap. Other generator locals start in
+                    # registers and are promoted later if they cross a yield.
+                    if (
+                        self.fn_info.is_generator
+                        and reg_type.error_overlap
+                        and symbol in self.deleted_vars
+                    ):
                         return self.add_var_to_env_class(
                             symbol,
                             reg_type,
@@ -780,7 +779,6 @@ class IRBuilder:
                             prefix=GENERATOR_ATTRIBUTE_PREFIX,
                         )
 
-                    # Otherwise define a new local variable.
                     return self.add_local_reg(symbol, reg_type)
                 else:
                     # Assign to a previously defined variable.
@@ -855,11 +853,6 @@ class IRBuilder:
                 return self.py_get_attr(target.obj, target.attr, line)
 
         assert False, "Unsupported lvalue: %r" % target
-
-    def read_nullable_attr(self, obj: Value, attr: str, line: int = -1) -> Value:
-        """Read an attribute that might have an error value without raising AttributeError."""
-        assert isinstance(obj.type, RInstance) and obj.type.class_ir.is_ext_class
-        return self.add(GetAttr(obj, attr, line, allow_error_value=True))
 
     def assign(self, target: Register | AssignmentTarget, rvalue_reg: Value, line: int) -> None:
         if isinstance(target, Register):
@@ -1045,47 +1038,8 @@ class IRBuilder:
     def pop_loop_stack(self) -> None:
         self.nonlocal_control.pop()
 
-    def make_spill_target(self, type: RType) -> AssignmentTarget:
-        """Moves a given Value instance into the private generator frame."""
-        frame = self.fn_info.generator_class
-        # Generator classes for overriding methods can inherit from one another. Include the
-        # module-qualified owning class name so unrelated helper spills don't alias an inherited
-        # struct field.
-        name = f"{TEMP_ATTR_NAME}1_{exported_name(frame.ir.fullname)}_{self.temp_counter}"
-        self.temp_counter += 1
-        target = self.add_var_to_class(Var(name), type, frame.ir, frame.self_reg)
-        return target
-
-    def spill(self, value: Value) -> AssignmentTarget:
-        """Moves a given Value instance into the private generator frame."""
-        target = self.make_spill_target(value.type)
-        # Shouldn't be able to fail
-        self.assign(target, value, NO_TRACEBACK_LINE_NO)
-        return target
-
-    def maybe_spill(self, value: Value) -> Value | AssignmentTarget:
-        """
-        Moves a given Value instance into the private frame for generator functions. For
-        non-generator functions, leaves the Value instance as it is.
-
-        Returns an AssignmentTarget associated with the Value for generator functions and the
-        original Value itself for non-generator functions.
-        """
-        if self.fn_info.is_generator:
-            return self.spill(value)
-        return value
-
-    def maybe_spill_assignable(self, value: Value) -> Register | AssignmentTarget:
-        """
-        Moves a given Value instance into the private frame for generator functions. For
-        non-generator functions, allocate a temporary Register.
-
-        Returns an AssignmentTarget associated with the Value for generator functions and an
-        assignable Register for non-generator functions.
-        """
-        if self.fn_info.is_generator:
-            return self.spill(value)
-
+    def ensure_register(self, value: Value) -> Register:
+        """Return an assignable register containing a value."""
         if isinstance(value, Register):
             return value
 

mypyc/irbuild/statement.py
@@ -49,12 +49,7 @@ from mypy.nodes import (
     YieldExpr,
     YieldFromExpr,
 )
-from mypyc.common import (
-    GENERATOR_HELPER_NAME,
-    KEEP_ALIVE_SHORT_LIVED,
-    KEEP_ALIVE_WHOLE_EXPRESSION,
-    TEMP_ATTR_NAME,
-)
+from mypyc.common import GENERATOR_HELPER_NAME, KEEP_ALIVE_SHORT_LIVED, KEEP_ALIVE_WHOLE_EXPRESSION
 from mypyc.ir.ops import (
     ERR_NEVER,
     NAMESPACE_MODULE,
@@ -716,7 +711,7 @@ def transform_try_except(
     # exception is raised, based on the exception in exc_info.
     builder.builder.push_error_handler(double_except_block)
     builder.activate_block(except_entry)
-    old_exc = builder.maybe_spill(builder.call_c(error_catch_op, [], line))
+    old_exc = builder.call_c(error_catch_op, [], line)
     # Compile the except blocks with the nonlocal control flow overridden to clear exc_info
     builder.nonlocal_control.append(ExceptNonlocalControl(builder.nonlocal_control[-1], old_exc))
 
@@ -796,7 +791,7 @@ def try_finally_try(
     return_entry: BasicBlock,
     main_entry: BasicBlock,
     try_body: GenFunc,
-) -> Register | AssignmentTarget | None:
+) -> Register | None:
     # Compile the try block with an error handler
     control = TryFinallyNonlocalControl(return_entry)
     builder.builder.push_error_handler(err_handler)
@@ -817,7 +812,7 @@ def try_finally_entry_blocks(
     return_entry: BasicBlock,
     main_entry: BasicBlock,
     finally_block: BasicBlock,
-    ret_reg: Register | AssignmentTarget | None,
+    ret_reg: Register | None,
 ) -> Value:
     line = builder.fn_info.fitem.line
     old_exc = Register(exc_rtuple, line=line)
@@ -862,7 +857,7 @@ def try_finally_resolve_control(
     cleanup_block: BasicBlock,
     finally_control: FinallyNonlocalControl,
     old_exc: Value,
-    ret_reg: Register | AssignmentTarget | None,
+    ret_reg: Register | None,
 ) -> BasicBlock:
     """Resolve the control flow out of a finally block.
 
@@ -883,15 +878,10 @@ def try_finally_resolve_control(
     if ret_reg:
         builder.activate_block(rest)
         return_block, rest = BasicBlock(), BasicBlock()
-        # For spill targets in try/finally, use nullable read to avoid AttributeError
-        if isinstance(ret_reg, AssignmentTargetAttr) and ret_reg.attr.startswith(TEMP_ATTR_NAME):
-            ret_val = builder.read_nullable_attr(ret_reg.obj, ret_reg.attr, line)
-        else:
-            ret_val = builder.read(ret_reg, line)
-        builder.add(Branch(ret_val, rest, return_block, Branch.IS_ERROR, line))
+        builder.add(Branch(ret_reg, rest, return_block, Branch.IS_ERROR, line))
 
         builder.activate_block(return_block)
-        builder.nonlocal_control[-1].gen_return(builder, ret_val, line)
+        builder.nonlocal_control[-1].gen_return(builder, ret_reg, line)
 
     # TODO: handle break/continue
     builder.activate_block(rest)
@@ -1136,11 +1126,11 @@ def transform_with(
         exit_ = None
     else:
         typ = builder.primitive_op(type_op, [mgr_v], line)
-        exit_ = builder.maybe_spill(builder.py_get_attr(typ, f"__{al}exit__", line))
+        exit_ = builder.py_get_attr(typ, f"__{al}exit__", line)
         value = builder.py_call(builder.py_get_attr(typ, f"__{al}enter__", line), [mgr_v], line)
 
-    mgr = builder.maybe_spill(mgr_v)
-    exc = builder.maybe_spill_assignable(builder.true())
+    mgr = mgr_v
+    exc = builder.ensure_register(builder.true())
     if is_async:
         value = emit_await(builder, value, line)
 
@@ -1213,7 +1203,7 @@ def transform_with_lock(
     # __enter__: acquire the lock
     value = builder.primitive_op(lock_acquire_op, [mgr_v], line)
 
-    mgr = builder.maybe_spill(mgr_v)
+    mgr = mgr_v
 
     def try_body() -> None:
         if target:
@@ -1288,7 +1278,7 @@ def transform_del_item(builder: IRBuilder, target: AssignmentTarget, line: int) 
         # Delete a local by assigning an error value to it, which will
         # prompt the insertion of uninit checks.
         builder.add(
-            Assign(target.register, builder.add(LoadErrorValue(target.type, undefines=True)))
+            Assign(target.register, builder.add(LoadErrorValue(target.type, undefines=True)), line)
         )
     elif isinstance(target, AssignmentTargetTuple):
         for subtarget in target.items:
@@ -1311,7 +1301,7 @@ def emit_yield(builder: IRBuilder, val: Value, line: int) -> Value:
     next_label = len(cls.continuation_blocks)
     cls.continuation_blocks.append(next_block)
     builder.assign(cls.next_label_target, Integer(next_label), line)
-    builder.add(Return(retval, yield_target=next_block))
+    builder.add(Return(retval, line, yield_target=next_block))
     builder.activate_block(next_block)
 
     add_raise_exception_blocks_to_generator_class(builder, line)
@@ -1349,7 +1339,7 @@ def emit_yield_from_or_await(
         else:
             iter_val = builder.call_c(get_op, [val], line)
 
-    iter_reg = builder.maybe_spill_assignable(iter_val)
+    iter_reg = builder.ensure_register(iter_val)
 
     stop_block, main_block, done_block = BasicBlock(), BasicBlock(), BasicBlock()
 

mypyc/test-data/run-async.test
@@ -135,6 +135,50 @@ async def sleep(t: float) -> None: ...
 
 [typing fixtures/typing-full.pyi]
 
+[case testRunAsyncLocalsAcrossAwait]
+import asyncio
+from typing import List
+
+from testutil import assertRaises
+
+async def increment(value: int) -> int:
+    await asyncio.sleep(0)
+    return value + 1
+
+async def accumulate(count: int) -> int:
+    total = 0
+    for value in range(count):
+        total += await increment(value)
+    return total
+
+async def return_through_finally(log: List[str]) -> int:
+    try:
+        return 7
+    finally:
+        await asyncio.sleep(0)
+        log.append("finally")
+
+async def deleted_across_await(delete: bool) -> str:
+    value = "live"
+    if delete:
+        del value
+    await asyncio.sleep(0)
+    return value
+
+async def test_async_locals_across_await() -> None:
+    assert await accumulate(4) == 10
+    log: List[str] = []
+    assert await return_through_finally(log) == 7
+    assert log == ["finally"]
+    assert await deleted_across_await(False) == "live"
+    with assertRaises(AttributeError):
+        await deleted_across_await(True)
+
+[file asyncio/__init__.pyi]
+async def sleep(delay: float) -> None: ...
+
+[typing fixtures/typing-full.pyi]
+
 [case testRunAsyncAwaitInVariousPositions]
 from typing import cast, Any
 

mypyc/analysis/dataflow.py
@@ -7,6 +7,7 @@ from collections.abc import Iterable, Iterator, Set as AbstractSet
 from typing import Any, Generic, TypeVar
 
 from mypyc.ir.ops import (
+    ERR_NEVER,
     Assign,
     AssignMulti,
     BasicBlock,
@@ -516,6 +517,55 @@ def analyze_live_regs(blocks: list[BasicBlock], cfg: CFG) -> AnalysisResult[Valu
         backward=True,
         kind=MAYBE_ANALYSIS,
     )
+
+
+def analyze_live_regs_with_exception_edges(
+    blocks: list[BasicBlock],
+) -> dict[BasicBlock, set[Value]]:
+    """Calculate block-entry liveness before exception handling is inserted.
+
+    Unlike get_cfg(), this models an error edge at each operation that can
+    raise. This matters when an assignment later in the same block overwrites a
+    value that the error handler can still read. Returns inserted for a yield
+    are treated as edges to their continuation blocks.
+
+    TODO: Unlike run_analysis(), this supports edges from the middle of basic
+          blocks, so we reimplement data flow analysis here. Figure out a way
+          to unify this with run_analysis().
+    """
+    visitor = LivenessVisitor()
+    live_in: dict[BasicBlock, set[Value]] = {block: set() for block in blocks}
+
+    while True:
+        changed = False
+        for block in reversed(blocks):
+            terminator = block.terminator
+            if isinstance(terminator, Return) and terminator.yield_target is not None:
+                successors = (terminator.yield_target,)
+            else:
+                successors = terminator.targets()
+
+            live: set[Value] = set()
+            for successor in successors:
+                live.update(live_in[successor])
+
+            for op in reversed(block.ops):
+                if (
+                    block.error_handler is not None
+                    and isinstance(op, RegisterOp)
+                    and op.error_kind != ERR_NEVER
+                ):
+                    live.update(live_in[block.error_handler])
+                gen, kill = op.accept(visitor)
+                live.difference_update(kill)
+                live.update(gen)
+
+            if live != live_in[block]:
+                live_in[block] = live
+                changed = True
+
+        if not changed:
+            return live_in
 
 
 # Analysis kinds

mypyc/irbuild/expression.py
@@ -1415,7 +1415,7 @@ def transform_dictionary_comprehension(builder: IRBuilder, o: DictionaryComprehe
 
 
 def _dict_comp_body(builder: IRBuilder, o: DictionaryComprehension) -> Value:
-    d = builder.maybe_spill(builder.call_c(dict_new_op, [], o.line))
+    d = builder.call_c(dict_new_op, [], o.line)
     loop_params = list(zip(o.indices, o.sequences, o.condlists, o.is_async))
 
     def gen_inner_stmts() -> None:

mypyc/irbuild/for_helpers.py
@@ -321,7 +321,7 @@ def translate_list_comprehension(builder: IRBuilder, gen: GeneratorExpr) -> Valu
     if val is not None:
         return val
 
-    list_ops = builder.maybe_spill(builder.new_list_op([], gen.line))
+    list_ops = builder.new_list_op([], gen.line)
 
     loop_params = list(zip(gen.indices, gen.sequences, gen.condlists, gen.is_async))
 
@@ -357,7 +357,7 @@ def translate_set_comprehension(builder: IRBuilder, gen: GeneratorExpr) -> Value
     if raise_error_if_contains_unreachable_names(builder, gen):
         return builder.none()
 
-    set_ops = builder.maybe_spill(builder.new_set_op([], gen.line))
+    set_ops = builder.new_set_op([], gen.line)
     loop_params = list(zip(gen.indices, gen.sequences, gen.condlists, gen.is_async))
 
     def gen_inner_stmts() -> None:
@@ -687,7 +687,7 @@ class ForGenerator:
     def gen_cleanup(self) -> None:
         """Generate post-loop cleanup (if needed)."""
 
-    def load_len(self, expr: Value | AssignmentTarget) -> Value:
+    def load_len(self, expr: Value) -> Value:
         """A helper to get collection length, used by several subclasses."""
         return self.builder.builder.builtin_len(
             self.builder.read(expr, self.line), self.line, use_pyssize_t=True
@@ -702,13 +702,11 @@ class ForIterable(ForGenerator):
         return True
 
     def init(self, expr_reg: Value, target_type: RType) -> None:
-        # Define targets to contain the expression, along with the iterator that will be used
-        # for the for-loop. If we are inside of a generator function, spill these into the
-        # private generator frame.
+        # Define a target containing the iterator used by the for-loop. The generator spill
+        # transform will promote it to the private generator frame if needed.
         builder = self.builder
         iter_reg = builder.primitive_op(iter_op, [expr_reg], self.line)
-        builder.maybe_spill(expr_reg)
-        self.iter_target = builder.maybe_spill(iter_reg)
+        self.iter_target = iter_reg
         self.target_type = target_type
 
     def gen_condition(self) -> None:
@@ -751,10 +749,9 @@ class ForNativeGenerator(ForGenerator):
         return True
 
     def init(self, expr_reg: Value, target_type: RType) -> None:
-        # Define target to contains the generator expression. It's also the iterator.
-        # If we are inside a generator function, spill these into the private generator frame.
-        builder = self.builder
-        self.iter_target = builder.maybe_spill(expr_reg)
+        # The generator expression is also the iterator. The generator spill transform will
+        # promote it to the private generator frame if needed.
+        self.iter_target = expr_reg
         self.target_type = target_type
 
     def gen_condition(self) -> None:
@@ -807,14 +804,11 @@ class ForAsyncIterable(ForGenerator):
     """Generate IR for an async for loop."""
 
     def init(self, expr_reg: Value, target_type: RType) -> None:
-        # Define targets to contain the expression, along with the
-        # iterator that will be used for the for-loop. We are inside
-        # of a generator function, so we will spill these into
-        # the private generator frame.
+        # Define a target containing the iterator used by the for-loop. The generator spill
+        # transform will promote it to the private generator frame if needed.
         builder = self.builder
         iter_reg = builder.call_c(aiter_op, [expr_reg], self.line)
-        builder.maybe_spill(expr_reg)
-        self.iter_target = builder.maybe_spill(iter_reg)
+        self.iter_target = iter_reg
         self.target_type = target_type
         self.stop_reg = Register(bool_rprimitive)
 
@@ -894,7 +888,7 @@ class ForSequence(ForGenerator):
     Supports iterating in both forward and reverse.
     """
 
-    length_reg: Value | AssignmentTarget | None
+    length_reg: Value | None
 
     def init(
         self, expr_reg: Value, target_type: RType, reverse: bool, length: Value | None = None
@@ -907,13 +901,12 @@ class ForSequence(ForGenerator):
         # Record a Value indicating the length of the sequence, if known at compile time.
         self.length = length
         self.reverse = reverse
-        # Define target to contain the expression, along with the index that will be used
-        # for the for-loop. If we are inside of a generator function, spill these into the
-        # private generator frame.
-        self.expr_target = builder.maybe_spill(expr_reg)
+        # The generator spill transform will promote loop state to the private generator frame
+        # if needed.
+        self.expr_target = expr_reg
         if is_immutable_rprimitive(expr_reg.type):
             # If the expression is an immutable type, we can load the length just once.
-            self.length_reg = builder.maybe_spill(self.length or self.load_len(self.expr_target))
+            self.length_reg = self.length or self.load_len(self.expr_target)
         else:
             # Otherwise, even if the length is known, we must recalculate the length
             # at every iteration for compatibility with python semantics.
@@ -926,7 +919,7 @@ class ForSequence(ForGenerator):
             else:
                 len_val = self.load_len(self.expr_target)
             index_reg = builder.builder.int_sub(len_val, 1)
-        self.index_target = builder.maybe_spill_assignable(index_reg)
+        self.index_target = builder.ensure_register(index_reg)
         self.target_type = target_type
 
     def gen_condition(self) -> None:
@@ -1010,15 +1003,15 @@ class ForDictionaryCommon(ForGenerator):
         builder = self.builder
         self.target_type = target_type
 
-        # Spill some values so they can be read across yield.
-        self.expr_target = builder.maybe_spill(expr_reg)
+        # The generator spill transform will promote loop state that crosses a yield.
+        self.expr_target = expr_reg
         offset = Integer(0)
-        self.offset_target = builder.maybe_spill_assignable(offset)
-        self.size = builder.maybe_spill(self.load_len(self.expr_target))
+        self.offset_target = builder.ensure_register(offset)
+        self.size = self.load_len(self.expr_target)
 
         # For dict class (not a subclass) this is the dictionary itself.
         iter_reg = builder.call_c(self.dict_iter_op, [expr_reg], self.line)
-        self.iter_target = builder.maybe_spill(iter_reg)
+        self.iter_target = iter_reg
 
     def gen_condition(self) -> None:
         """Get next key/value pair, set new offset, and check if we should continue."""
@@ -1132,7 +1125,7 @@ class ForRange(ForGenerator):
         self.start_reg = start_reg
         self.end_reg = end_reg
         self.step = step
-        self.end_target = builder.maybe_spill(end_reg)
+        self.end_target = end_reg
         if is_short_int_rprimitive(start_reg.type) and is_short_int_rprimitive(end_reg.type):
             index_type: RType = short_int_rprimitive
         elif is_fixed_width_rtype(end_reg.type):
@@ -1141,7 +1134,7 @@ class ForRange(ForGenerator):
             index_type = int_rprimitive
         index_reg = Register(index_type, line=self.line)
         builder.assign(index_reg, start_reg, self.line)
-        self.index_reg = builder.maybe_spill_assignable(index_reg)
+        self.index_reg = index_reg
         # Initialize loop index to 0. Assert that the index target is assignable.
         self.index_target: Register | AssignmentTarget = builder.get_assignment_target(self.index)
         builder.assign(self.index_target, builder.read(self.index_reg, self.line), self.line)
@@ -1195,7 +1188,7 @@ class ForInfiniteCounter(ForGenerator):
         # Create a register to store the state of the loop index and
         # initialize this register along with the loop index to 0.
         zero = Integer(0)
-        self.index_reg = builder.maybe_spill_assignable(zero)
+        self.index_reg = builder.ensure_register(zero)
         self.index_target: Register | AssignmentTarget = builder.get_assignment_target(self.index)
 
     def gen_step(self) -> None:

mypyc/irbuild/main.py
@@ -47,6 +47,7 @@ from mypyc.irbuild.prepare import (
 from mypyc.irbuild.visitor import IRBuilderVisitor
 from mypyc.irbuild.vtable import compute_vtable
 from mypyc.options import CompilerOptions
+from mypyc.transform.generator_spills import promote_generator_registers
 
 # The stubs for callable contextmanagers are busted so cast it to the
 # right type...
@@ -122,6 +123,13 @@ def build_ir(
         )
         result[module.fullname] = module_ir
         class_irs.extend(builder.classes)
+
+    # Generator helper calls are fresh C activations after every suspension.
+    # Move values that must outlive one activation to the private frame before
+    # attribute-definedness and the remaining IR transforms run.
+    for class_ir in class_irs:
+        if class_ir.env_user_function is not None:
+            promote_generator_registers(class_ir.env_user_function, class_ir)
 
     analyze_always_defined_attrs(class_irs)
 

commit 6060e6f7cc50d940dd3717caa88f531e6a531968

[mypyc] Fix compiler crash for empty async with body
Fix a regression introduced by #21982. CFG cleanup correctly removes an unreachable exception-handler block, but generated resume cleanup paths still reference its raw CPy_CatchError CallC result. This leaves malformed IR and causes a KeyError during reference count insertion.

Keep the saved exception state in a register so it remains available across the generated control flow. Add regression coverage for compiling and running an async with statement with an empty body.

mypyc/irbuild/builder.py
@@ -1039,7 +1039,10 @@ class IRBuilder:
         self.nonlocal_control.pop()
 
     def ensure_register(self, value: Value) -> Register:
-        """Return an assignable register containing a value."""
+        """Return an assignable register containing a value.
+
+        Values that may remain live across arbitrary control flow should use a register.
+        """
         if isinstance(value, Register):
             return value
 

mypyc/irbuild/statement.py
@@ -712,6 +712,9 @@ def transform_try_except(
     builder.builder.push_error_handler(double_except_block)
     builder.activate_block(except_entry)
     old_exc = builder.call_c(error_catch_op, [], line)
+    if builder.fn_info.is_generator:
+        # Let the generator spill pass preserve the saved exception state across a suspension.
+        old_exc = builder.ensure_register(old_exc)
     # Compile the except blocks with the nonlocal control flow overridden to clear exc_info
     builder.nonlocal_control.append(ExceptNonlocalControl(builder.nonlocal_control[-1], old_exc))
 

mypyc/test-data/run-async.test
@@ -292,8 +292,12 @@ async def async_with_vectorcall() -> str:
     async with ctx:
         return await async_val("vc")
 
+async def async_with_pass() -> None:
+    async with async_ctx():
+        pass
+
 [file driver.py]
-from native import async_with, async_with_vectorcall
+from native import async_with, async_with_pass, async_with_vectorcall
 from testutil import run_generator
 
 yields, val = run_generator(async_with(), [None, 'x', None])
@@ -303,6 +307,10 @@ assert val == 'x', val
 yields, val = run_generator(async_with_vectorcall(), [None, 'x', None])
 assert yields == ('enter', 'vc', 'exit'), yields
 assert val == 'x', val
+
+yields, val = run_generator(async_with_pass(), [None, None])
+assert yields == ('enter', 'exit'), yields
+assert val is None
 
 [case testAsyncReturn]
 from testutil import async_val