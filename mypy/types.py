c53935f2ed1c5404cc1f34f75b3acd07cda9de62
Ivan Levkivskyi <levkivskyi@gmail.com>
2026-09-23 00:11:24 +0100

    Try 8

--- mypy/types.py
+++ mypy/types.py
@@ -222,7 +222,7 @@ _dummy: Final[Any] = object()
 # A placeholder for int parameters
 _dummy_int: Final = -999999
 
-MAX_PROTOCOL_DEPTH: Final = 5
+MAX_PROTOCOL_DEPTH: Final = 8
 
 
 class TypeOfAny:
