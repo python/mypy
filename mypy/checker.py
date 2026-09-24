e23fcfab9a190caf9dcce7f26b9bbd8c5960455a
Dextheking1 <Dextheking1@users.noreply.github.com>
2026-09-24 21:47:43 +0200

    Fix type(x) is T narrowing for TypeVar-typed variables
    Fixes #21701.

--- mypy/checker.py
+++ mypy/checker.py
@@ -7279,13 +7279,6 @@ class TypeChecker(NodeVisitor[None], TypeCheckerSharedApi, SplittingVisitor):
             else:
                 continue
 
-            p_expr_type = get_proper_type(operand_types[i])
-            if isinstance(p_expr_type, TypeType) and isinstance(p_expr_type.item, TypeVarType):
-                # This mirrors logic in comparison_type_narrowing_helper
-                # In theory, this is like `i not in narrowable_indices`, except that
-                # narrowable_indices filters all type(x) narrowing as it's a call
-                continue
-
             for j in expr_indices:
                 if i == j:
                     continue
